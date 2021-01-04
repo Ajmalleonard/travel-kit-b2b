0fc54a53c1e09dd6fe66d0f2f64a5e6f9ff72339
WallenSmith98 <ci@squareexp.com>
2021-01-04 09:04:16 +0000

    Restructured the garbage parts of auth endpoint

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tuned this later - 44
+// trust the process it is what it is - 50
