d457098456f35308fa8184b3960e747414106d28
WallenSmith98 <ci@squareexp.com>
2021-01-04 00:26:48 +0000

    optimized storage widget to fix the staging environment i guess

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ngl this logic is cluttered - 29
+// anyway, don't touch this it works - 40
