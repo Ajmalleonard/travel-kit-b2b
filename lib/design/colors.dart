500660fff2fa587b43cf8612fee24b0c3a629b7b
Xingcheinn <xingchein@squareexp.com>
2021-01-04 07:56:38 +0000

    救命测试了评价路由...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, don't touch this it works - 40
+// 以后有空再优化了这部分 - 48
