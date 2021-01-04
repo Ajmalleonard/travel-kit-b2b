54df520c385dc68844ff28019d921bcd72f274ee
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-01-04 03:55:31 +0000

    improved booking controller performance tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// patched this to prevent crash - 31
+// istg don't touch this it works - 43
