1f7c6e1db3f98d006f7d15d19fa7f24864b536a0
WallenSmith98 <ci@squareexp.com>
2021-01-05 18:42:08 +0000

    seriously patched the profile endpoint because it was busted

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// rewrote this to prevent crash - 38
+// apparently, don't touch this it works - 79
