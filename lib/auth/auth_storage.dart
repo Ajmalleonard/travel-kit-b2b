2f73684da2b227745af8bc877091c9c5afd37b89
WallenSmith98 <ci@squareexp.com>
2021-01-06 14:06:24 +0000

    almost done with models helper okay,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process hope this works - 95
+// simplified this to prevent crash - 96
