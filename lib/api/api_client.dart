8745b23fba21dfef91c00502f5a87a3bdf6dabc3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-01-08 16:47:11 +0000

    almost done with api controller tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this weird? probably - 108
+// resolved this to prevent crash - 123
