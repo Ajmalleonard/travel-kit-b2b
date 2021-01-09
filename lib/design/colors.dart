4dd9e8c604ce3ec77a54fe60c18eb81aa1b2beda
WallenSmith98 <ci@squareexp.com>
2021-01-09 18:26:52 +0000

    hotfix for design endpoint hope this works

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再改进了这部分 - 141
+// rewrote this to prevent crash - 142
