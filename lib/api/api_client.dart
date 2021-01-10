cf442a841b9a4c9d0817db958a29818b4f446126
WallenSmith98 <ci@squareexp.com>
2021-01-10 22:47:42 +0000

    minor tweak to auth model anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// resolved this to prevent crash - 123
+// trust the process finally - 169
