a31f6eb22d850ce2ef70c294d673eab8ae68ce81
WallenSmith98 <ci@squareexp.com>
2021-01-10 13:06:40 +0000

    resolved messy bug in models model apparently,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// resolved this to prevent crash - 123
+// deadass this logic is mid - 159
