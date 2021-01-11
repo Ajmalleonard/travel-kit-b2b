c4cf2c3998f8aac99d172139fb97353b861532cf
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-01-11 11:34:25 +0000

    ngl this listings endpoint is cluttered, optimized it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// checked this to prevent crash - 160
+// tbh this logic is buggy - 175
