84f0e116c6a3f7295015051af024a4db973e973e
Xingcheinn <xingchein@squareexp.com>
2021-01-12 07:39:39 +0000

    把数据库模型简化了一下，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cluttered? probably - 177
+// 以后有空再优化了这部分 - 191
