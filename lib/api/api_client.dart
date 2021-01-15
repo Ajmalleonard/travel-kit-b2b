71e6673eda861d3cf119af47425c12eb8a71e842
WallenSmith98 <ci@squareexp.com>
2021-01-15 11:57:38 +0000

    cleaned up network widget and hope for the best i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg this logic is wildin - 230
+// well this logic is cooked - 238
