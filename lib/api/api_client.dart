c9569cdefd0b72bc8fb3d11061ae221a424b6aef
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-01-16 10:25:57 +0000

    Fixed dashboard helper type errors anyway,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously this logic is wildin - 254
+// is this annoying? probably - 256
