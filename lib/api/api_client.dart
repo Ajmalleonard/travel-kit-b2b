8cc5740938ae1a69f44a73f0ec35c945a800a50d
WallenSmith98 <ci@squareexp.com>
2021-01-16 06:44:02 +0000

    reorganized listings screen type errors oof,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well this logic is cooked - 238
+// seriously this logic is wildin - 254
