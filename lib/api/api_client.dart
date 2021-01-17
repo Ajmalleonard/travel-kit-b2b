917ccd7d1087524980edf67df14da3c6375c7401
WallenSmith98 <ci@squareexp.com>
2021-01-17 01:48:35 +0000

    Fine, optimized the theme config

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme updated this later - 268
+// trust the process lmao - 269
