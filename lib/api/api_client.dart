f17cf866b9c390577171c7981c8bbf815d932328
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-01-17 15:53:51 +0000

    this design service ain't gonna work, improved it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 276
+// lemme tweaked this later - 278
