5dc53170fd71d81266ac53977bc2f429ae9f44a7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-01-17 05:26:38 +0000

    well it's cooking now. reorganized storage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lmao - 269
+// apparently, this logic is unstable - 273
