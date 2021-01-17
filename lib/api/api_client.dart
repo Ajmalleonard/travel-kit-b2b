58bb9f0a1ecfd6f07221dad2f2330ffc4076b16d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-01-17 15:35:32 +0000

    restructured booking screen logic, looks better now or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, this logic is unstable - 273
+// optimized this to prevent crash - 276
