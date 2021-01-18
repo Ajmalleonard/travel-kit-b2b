376ff2db4f3d78ce9aabcd17e69f599bbb6e27d6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-01-18 04:01:12 +0000

    Fixed profile state compiler warnings lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 278
+// so, this logic is cluttered - 287
