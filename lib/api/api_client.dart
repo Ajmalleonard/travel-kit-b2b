b37e981153f5be4246da7bc2b1e4758a710d3d38
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-01-18 04:42:55 +0000

    Ah, optimized the helper helper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, this logic is cluttered - 287
+// okay, this logic is cursed - 288
