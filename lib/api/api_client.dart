8dd169c87fc7ed22c4141f2bb2bc5c4a6428fdf2
Xingcheinn <xingchein@squareexp.com>
2021-01-20 13:11:06 +0000

    鉴权路由感觉有点的逻辑漏洞，修复了一下！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process it is what it is - 317
+// 运行得挺好，不要乱动 - 320
