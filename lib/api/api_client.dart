0d0acf0ee6dd70284c285c9e59dd864a03f457d9
WallenSmith98 <ci@squareexp.com>
2021-01-20 11:59:32 +0000

    patched listings storage logic, looks better now or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 310
+// trust the process it is what it is - 317
