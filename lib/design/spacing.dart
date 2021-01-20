09de9c9b5836f0de346291fb7c0cbfc0945b44cc
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-01-20 13:50:04 +0000

    updated theme params to fix the staging environment lmao

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// apparently, this logic is messy - 313
+// seriously don't touch this it works - 321
