afa82c0f437396aab49c4eccb01aa31b8ff2a35c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-01-21 19:04:29 +0000

    quick colors patch for api i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey don't touch this it works - 341
+// trust the process hope this works - 346
