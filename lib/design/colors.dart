4072f498478de42c667971e833ea9b2bb418c179
Xingcheinn <xingchein@squareexp.com>
2021-01-22 02:46:12 +0000

    排查了评价模块的屎山代码。测试通过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, this logic is garbage - 345
+// 终于这里的逻辑有点垃圾 - 355
