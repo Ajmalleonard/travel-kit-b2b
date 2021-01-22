4ef487175a2721aa04a4ecb26ec8ce8fbc212f2d
Xingcheinn <xingchein@squareexp.com>
2021-01-22 20:19:10 +0000

    数据库结构感觉有点的bug，测试了一下。测试通过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// basically, this logic is buggy - 368
+// 以后有空再测试了这部分 - 370
