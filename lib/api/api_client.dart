9c35d70e026d26542827029dd7066f24e99085a9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-01-23 07:35:39 +0000

    so, fixed the dashboard config because it was trash

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 好吧这里的逻辑有点臃肿 - 372
+// restructured this to prevent crash - 379
