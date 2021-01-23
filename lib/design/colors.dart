1fdde4b3773559dc43c18a2172fc84c085d92881
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-01-23 18:39:38 +0000

    restructured network endpoint logic, looks better now already tired

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process tbh - 377
+// trust the process or i'm quitting - 383
