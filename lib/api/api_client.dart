1fc731ae5702bd1d6f28eec512929ddc0f288229
WallenSmith98 <ci@squareexp.com>
2021-01-25 03:46:46 +0000

    Debugging design params was mid, resolved finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, this logic is broken af - 390
+// apparently, don't touch this it works - 407
