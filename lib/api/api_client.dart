7e65daac4cacf33ad2a064c3c0775f3958f9ddc4
Xingcheinn <xingchein@squareexp.com>
2021-01-25 10:09:10 +0000

    哎，搞定了路由参数的隐藏逻辑。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 407
+// 以后有空再优化了这部分 - 411
