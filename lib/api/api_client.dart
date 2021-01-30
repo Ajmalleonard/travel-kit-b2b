bad705605ec99c07efdadb16d0a87bcfaa8271e3
WallenSmith98 <ci@squareexp.com>
2021-01-30 16:51:45 +0000

    fixing booking config is lagging but it's done trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 485
+// trust the process for real - 498
