8f4433912c286bc0411c3e4feaa2e0e13d867bc8
WallenSmith98 <ci@squareexp.com>
2021-01-31 22:32:30 +0000

    This profile handler ain't gonna work, restructured it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lowkey this logic is cooked - 507
+// polishing this to prevent crash - 516
