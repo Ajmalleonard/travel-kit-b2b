2a50db940ab5d9d1ad651843fa9ac59648cdbfc9
Xingcheinn <xingchein@squareexp.com>
2021-02-01 12:51:21 +0000

    改进了鉴权模块的并发问题，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, don't touch this it works - 523
+// 为了防止垃圾写的 - 529
