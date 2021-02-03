89cbf79dc405b38bd1c6865d97e1f497fcf1cac6
Xingcheinn <xingchein@squareexp.com>
2021-02-03 02:36:45 +0000

    配置模块感觉有点的屎山代码，改写了一下。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// simplified this to prevent crash - 544
+// 救命这里的逻辑有点乱 - 558
