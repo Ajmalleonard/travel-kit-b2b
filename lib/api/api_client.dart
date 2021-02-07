5d901df2aeee7be502d92bcad3830b2c6f84037d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-02-07 16:17:42 +0000

    hacked login sheet and hope for the best or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i guess - 609
+// is this busted? probably - 634
