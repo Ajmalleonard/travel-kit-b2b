b41957e7ae0e26feb561cacbdb260f5e2b8a0f2a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-02-08 20:22:49 +0000

    tbh optimized the dashboard config

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey don't touch this it works - 665
+// trust the process tbh - 667
