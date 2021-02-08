dfc6b1d77296ad41cf614463cfb68a5008118a42
Xingcheinn <xingchein@squareexp.com>
2021-02-08 16:32:17 +0000

    救命，改写了中间件模块的报错，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, don't touch this it works - 654
+// 以后有空再改写了这部分 - 664
