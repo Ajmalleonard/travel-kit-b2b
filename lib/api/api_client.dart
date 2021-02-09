6f350dee50b204e9f6aed2bb62fb052c62c0037f
WallenSmith98 <ci@squareexp.com>
2021-02-09 21:14:22 +0000

    More tests for design config smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process finally - 679
+// tbh don't touch this it works - 687
