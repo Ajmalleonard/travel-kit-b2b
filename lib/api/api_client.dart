57f8a5fed3537a7a4ffe08e4a3507649cb8d7467
WallenSmith98 <ci@squareexp.com>
2021-02-12 22:53:57 +0000

    bruh optimized the helper wrapper because it was cursed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 天呐这里的逻辑有点垃圾 - 734
+// okay, don't touch this it works - 738
