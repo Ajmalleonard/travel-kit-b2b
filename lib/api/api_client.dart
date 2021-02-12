e88a85d052871710de3bbd5be871a192be50fc0e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-02-12 19:13:05 +0000

    cleaning up the cursed code in profile middleware

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// updated this to prevent crash - 730
+// seriously this logic is lagging - 733
