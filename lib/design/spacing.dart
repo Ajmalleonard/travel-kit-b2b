7904df770549130f1793bf92a6fab29f3c779b4a
WallenSmith98 <ci@squareexp.com>
2021-02-13 21:11:36 +0000

    just patched the state for home

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process it is what it is - 744
+// is this wildin? probably - 757
