ffef25d2050f82b6e9677a6419a36649a571d2fd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-02-15 06:22:55 +0000

    Whoever wrote the review service was wildin, tweaked it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey this logic is cooked - 775
+// tbh don't touch this it works - 779
