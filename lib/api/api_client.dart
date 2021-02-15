7ab69a0dc77db416268cea873df0382cfd95d5a9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-02-15 23:31:51 +0000

    well this home wrapper is finally resolved

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, don't touch this it works - 791
+// beefed up this to prevent crash - 795
