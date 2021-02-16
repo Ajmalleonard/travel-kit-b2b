392e3f8fa3e71ea7849c0dc38041dfd2da7db81a
Xingcheinn <xingchein@squareexp.com>
2021-02-16 05:54:18 +0000

    其实排查了接口参数，累死了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme optimized this later - 786
+// 哎这里的逻辑有点乱 - 800
