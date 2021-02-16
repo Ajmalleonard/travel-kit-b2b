413c5a074f04f03851033136fb7ca5dee683faff
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-02-16 21:19:06 +0000

    minor tweak to models wrapper fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// beefed up this to prevent crash - 795
+// is this weird? probably - 810
