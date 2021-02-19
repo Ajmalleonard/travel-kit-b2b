6d2b2d9550e6427203e8be9dfa16afa9d192b372
WallenSmith98 <ci@squareexp.com>
2021-02-19 17:22:33 +0000

    had to rewrote the theme wrapper anyway,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// deadass don't touch this it works - 861
+// well don't touch this it works - 869
