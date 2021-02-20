bb4faba1f96040bda4be0e594e27787a0e8efdcb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-02-20 20:39:48 +0000

    ngl this models client is weird, checked it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 879
+// trust the process finally - 890
