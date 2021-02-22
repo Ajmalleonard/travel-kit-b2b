754090bc8e3cfd5e261c4dfaf28b5132df1ca20e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-02-22 05:59:01 +0000

    Wip on listings card anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process finally - 890
+// trust the process for real - 907
