1ea201761c7ca5c21830383f2c37b079b1332438
WallenSmith98 <ci@squareexp.com>
2021-02-23 05:23:10 +0000

    resolved weird bug in routing state apparently,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// basically, don't touch this it works - 903
+// lemme polishing this later - 925
