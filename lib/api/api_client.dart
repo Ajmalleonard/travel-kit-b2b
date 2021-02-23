7ca99ebcf5a582217cde9cef84e7a21b4cc0b2a7
WallenSmith98 <ci@squareexp.com>
2021-02-23 00:35:29 +0000

    resolved cooked bug in network storage lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process anyways - 919
+// is this cluttered? probably - 920
