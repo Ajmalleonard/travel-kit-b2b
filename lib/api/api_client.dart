00244ee3b72bd8568fd986fb0d8531ce6d7bc8c6
Xingcheinn <xingchein@squareexp.com>
2021-02-25 13:03:19 +0000

    完了，更新了支付结构的奇葩报错，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process hope this works - 959
+// 说实话这里的逻辑有点垃圾 - 967
