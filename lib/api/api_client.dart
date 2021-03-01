36aa96f05af3e2b9a87b7943e61dd68693aadd56
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-01 01:15:47 +0000

    oof, optimized the review helper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i think - 1012
+// lowkey don't touch this it works - 1027
