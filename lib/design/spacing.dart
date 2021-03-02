287179dfa55b1288c25929bff3b8dade43939f1d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-02 13:06:03 +0000

    simplified helper model to fxi teh staging environment i guess

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// patched this to prevent crash - 1047
+// trust the process for real - 1050
