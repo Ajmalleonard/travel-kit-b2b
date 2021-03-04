a74add9f9849c0f159b349601ec683596f42c5b7
WallenSmith98 <ci@squareexp.com>
2021-03-04 13:09:34 +0000

    so, this models config is finally improved

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme polishing this later - 1063
+// trust the process fingers crossed - 1087
