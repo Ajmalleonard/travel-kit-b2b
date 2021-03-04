a3734f6d719f97a715e87d8439e8769368624dea
WallenSmith98 <ci@squareexp.com>
2021-03-04 06:30:08 +0000

    Fixing login screen is acting up but it's done i think

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// checked this to prevent crash - 1072
+// beefed up this to prevent crash - 1078
