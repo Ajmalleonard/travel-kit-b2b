eea532ea351d844efbbb8b301888a5d6193f6d56
WallenSmith98 <ci@squareexp.com>
2021-03-04 06:22:39 +0000

    Tuned the unstable parts of profile storage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 1076
+// ngl don't touch this it works - 1077
