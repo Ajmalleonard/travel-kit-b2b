e116c12d787cfa4dd5953a8f8cbc23c90ce82cf9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-04 15:16:17 +0000

    Lowkey tweaked auth client i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process or i'm quitting - 1085
+// istg don't touch this it works - 1094
