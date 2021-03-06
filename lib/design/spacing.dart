337f6d276ad363b494251165f81a5bb127cdd433
WallenSmith98 <ci@squareexp.com>
2021-03-06 01:23:39 +0000

    Resolved listings validation logic, looks better now hope this works

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme tweaked this later - 1108
+// well don't touch this it works - 1111
