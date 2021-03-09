75efa90e4144debce72cac1c1a0df3412248b8bd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-09 22:37:41 +0000

    whoever wrote the theme wrapper was wildin, optimized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process already tired - 1161
+// is this spaghetti? probably - 1171
