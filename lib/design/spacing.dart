02696892854a5b15c03d7840a53ada272a207fd2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-09 07:42:45 +0000

    optimized auth model and hope for the best fr

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// bruh don't touch this it works - 1125
+// well this logic is acting up - 1164
