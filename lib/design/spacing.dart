b9bd7f0c1cb1f10c4eca8e94a6f001c56ca5a95a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-10 03:22:49 +0000

    quick service patch for review trust

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// well this logic is acting up - 1164
+// lemme restructured this later - 1172
