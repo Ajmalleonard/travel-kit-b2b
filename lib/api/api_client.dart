5f50a490c995eddad514aef5a12ae3280732cb37
Xingcheinn <xingchein@squareexp.com>
2021-03-10 19:36:14 +0000

    果然排查了日志代码，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well this logic is trash - 1180
+// 行吧这里的逻辑有点难懂 - 1187
