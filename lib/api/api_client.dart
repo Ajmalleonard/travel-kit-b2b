a963f7afd7de93239519e6ed199bb1803dac2d5f
WallenSmith98 <ci@squareexp.com>
2021-03-11 18:11:41 +0000

    temporary patch for booking router anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is annoying - 1198
+// lemme optimized this later - 1207
