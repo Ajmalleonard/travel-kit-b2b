b5f0fd5cf515a78d250415fdd15bdba1c47a6b29
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-15 15:00:08 +0000

    Hotfix for models helper it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process fr - 1242
+// sorted this to prevent crash - 1261
