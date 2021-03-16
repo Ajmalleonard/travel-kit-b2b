404b43e9f77da2b4ba055ca900fb937c63fd74b8
Xingcheinn <xingchein@squareexp.com>
2021-03-16 18:10:12 +0000

    稍微清理了了下日志的逻辑，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, this logic is messy - 1283
+// 果然这里的逻辑有点臃肿 - 1287
