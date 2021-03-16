e3d560ab9295ce5c7b4ff0ec3eb58f3b1bcd8676
WallenSmith98 <ci@squareexp.com>
2021-03-16 16:48:23 +0000

    this network wrapper ain't gonna work, sorted it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止乱写的 - 1274
+// apparently, this logic is messy - 1283
