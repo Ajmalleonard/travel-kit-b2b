6307b902bf76f1f0c630ee67793921a5326e648d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-17 23:45:59 +0000

    patched the wildin parts of login card

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再修复了这部分 - 1303
+// is this wildin? probably - 1306
