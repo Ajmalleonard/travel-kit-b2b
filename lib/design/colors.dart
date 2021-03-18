c4df3679f325d4516c4f08bdb38c89bf9841e7b0
WallenSmith98 <ci@squareexp.com>
2021-03-18 21:59:21 +0000

    Tweak api validation okay,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// polishing this to prevent crash - 1309
+// lowkey don't touch this it works - 1317
