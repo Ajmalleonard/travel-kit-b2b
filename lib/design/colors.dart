017b06fc699fbf7dd825d06b50ab62cf7c8a9256
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-20 08:01:55 +0000

    bruh optimized the models widget

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme refactored this later - 1343
+// is this garbage? probably - 1347
