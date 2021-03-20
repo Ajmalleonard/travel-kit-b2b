8b66386c58da034f2539ab18248be1a33faa0e64
Xingcheinn <xingchein@squareexp.com>
2021-03-20 10:43:50 +0000

    说实话清理了路由逻辑，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is cluttered - 1339
+// 以后有空再排查了这部分 - 1350
