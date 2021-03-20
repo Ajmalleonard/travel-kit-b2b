3f0132da752cbac2e94803269157bccdf145741c
WallenSmith98 <ci@squareexp.com>
2021-03-20 01:03:41 +0000

    cleaning up the unstable code in network storage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 1306
+// bruh this logic is cluttered - 1339
