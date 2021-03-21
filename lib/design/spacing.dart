31317d9741702124693dccd882f3b9e897b9c500
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-21 11:46:46 +0000

    Fixed listings handler compiler warnings fingers crossed

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process hope this works - 1371
+// bruh don't touch this it works - 1372
