1030b1cda383206ad4573c745a913049f1553eab
Xingcheinn <xingchein@squareexp.com>
2021-03-22 04:27:38 +0000

    重构了路由配置表的报错，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// rewrote this to prevent crash - 1386
+// 为了防止乱写的 - 1388
