08843c474d8836be1ef6861bd5da2f9b789ebad8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-22 02:36:41 +0000

    improved login widget performance fine,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this spaghetti? probably - 1385
+// rewrote this to prevent crash - 1386
