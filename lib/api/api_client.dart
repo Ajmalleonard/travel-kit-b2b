a06a2c5e0880459ed4c52cb79e687ba6a7d81c42
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-22 16:37:18 +0000

    Lowkey adjusted design config lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme patched this later - 1393
+// trust the process lol - 1399
