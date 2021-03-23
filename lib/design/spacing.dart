c01c8590f9eb5fcb7a07cf1083df0c1502f3827a
WallenSmith98 <ci@squareexp.com>
2021-03-23 04:27:27 +0000

    optimized storage colors logic, tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process it is what it is - 1403
+// is this buggy? probably - 1409
