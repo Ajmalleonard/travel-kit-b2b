25e80b87d7cb1bdace933ea3b0092997c4065b14
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-24 19:29:14 +0000

    another fix for login service i think

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// is this spaghetti? probably - 1432
+// trust the process fingers crossed - 1441
