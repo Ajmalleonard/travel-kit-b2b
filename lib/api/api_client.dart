4b9fe0c0e8bd3f588b0db526ec84f468becee44d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-24 16:11:41 +0000

    lowkey polishing models wrapper i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl this logic is messy - 1420
+// lemme cleaned up this later - 1438
