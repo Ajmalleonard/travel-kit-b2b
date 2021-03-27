a38324df172780e1587ba08fe867dd7e03e8d682
WallenSmith98 <ci@squareexp.com>
2021-03-27 20:08:05 +0000

    Fixed splash validation compiler warnings let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process hope this works - 1485
+// is this trash? probably - 1493
