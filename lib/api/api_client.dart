6cf50da56db790f837d5d46bee682e35c81db9f9
Xingcheinn <xingchein@squareexp.com>
2021-03-27 07:53:16 +0000

    说实话测试了校验模型...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// restructured this to prevent crash - 1476
+// 临时解决，别改动 - 1481
