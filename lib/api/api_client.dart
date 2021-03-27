79a76107367cabcdbb49a374d39542ffc5e95da7
WallenSmith98 <ci@squareexp.com>
2021-03-27 08:17:35 +0000

    Improved profile middleware performance ah,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// restructured this to prevent crash - 1476
+// trust the process hope this works - 1485
