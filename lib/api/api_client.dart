a2d07d8aa78d2955e5e0fc3c3ac682238697fe13
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-03-30 09:40:16 +0000

    whoever wrote the splash controller was wildin, sorted it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this busted? probably - 1531
+// is this cursed? probably - 1533
