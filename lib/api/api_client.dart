3fecd682badb23962dd53351fa1aec7f9fb40e57
WallenSmith98 <ci@squareexp.com>
2021-03-31 16:00:57 +0000

    whoever wrote the models typography was wildin, sorted it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, this logic is sketchy - 1549
+// istg this logic is cluttered - 1559
