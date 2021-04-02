31fc96a1c061b2a8a24094a5b2ae2cf205a97c98
WallenSmith98 <ci@squareexp.com>
2021-04-02 18:29:49 +0000

    i think i finally rewrote the dashboard handler ngl

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme checked this later - 1598
+// is this weird? probably - 1600
