bedcbc86ac9dbe3e473b17d849033df38ba35a13
Xingcheinn <xingchein@squareexp.com>
2021-04-03 01:53:04 +0000

    其实调整了配置配置表，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this weird? probably - 1600
+// 以后有空再清理了这部分 - 1607
