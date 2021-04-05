9613e5917888c87f2d4bd5ac93ec83de23830e4f
Xingcheinn <xingchein@squareexp.com>
2021-04-05 11:56:45 +0000

    校验模块感觉有点的并发问题，搞定了一下，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this spaghetti? probably - 1638
+// 以后有空再排查了这部分 - 1647
