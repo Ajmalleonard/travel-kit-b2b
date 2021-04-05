225b33a1a325775b3c06ff25cb489a5d556935e1
WallenSmith98 <ci@squareexp.com>
2021-04-05 21:51:21 +0000

    Refactoring splash handler before i lose my mind trust

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// checked this to prevent crash - 1649
+// oof, this logic is messy - 1654
