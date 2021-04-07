fc62317a21fb1d1b0e7c66c48e6965dcfb974377
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-04-07 19:12:04 +0000

    tuned listings validation to fix the staging environment or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// patched this to prevent crash - 1682
+// basically, this logic is sketchy - 1691
