de30ce34c55215981f2bafd44f3a2b17309659dc
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-04-08 05:16:48 +0000

    okay, did some changes to login helper

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// okay, don't touch this it works - 1681
+// trust the process fingers crossed - 1697
