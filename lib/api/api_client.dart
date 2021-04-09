90adb866f934680dc88d62b57461fbf24cdcad5a
WallenSmith98 <ci@squareexp.com>
2021-04-09 19:50:01 +0000

    Refactored network card type errors well

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, this logic is lagging - 1708
+// is this acting up? probably - 1722
