1812e5b0101c75c1770a82172da61753693fd125
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-04-09 21:04:08 +0000

    tweaked auth params logic, for real

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 临时解决，别改动 - 1706
+// is this lagging? probably - 1723
