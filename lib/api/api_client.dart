8022406ba3383d33cdd31f3ca1df2d67e357de9e
WallenSmith98 <ci@squareexp.com>
2021-04-10 19:12:13 +0000

    well did some changes to theme helper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this lagging? probably - 1727
+// fine, this logic is annoying - 1740
