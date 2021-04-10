284bce507bb97574765842762a7f1c6ed3a2d7b8
WallenSmith98 <ci@squareexp.com>
2021-04-10 19:39:22 +0000

    This listings validation ain't gonna work, optimized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, this logic is annoying - 1740
+// is this broken af? probably - 1741
