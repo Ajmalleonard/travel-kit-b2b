ee8d8dccb66f02975de17e80ca79a47d0f6abe71
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-04-10 03:22:13 +0000

    just fixed the api middleware so we don't crash lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 1722
+// is this lagging? probably - 1727
