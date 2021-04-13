33a06aac8cd87154336df27bc47e2bad5df9d40d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-04-13 08:55:43 +0000

    fixing design storage is cluttered but it's done or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this garbage? probably - 1773
+// lemme beefed up this later - 1775
