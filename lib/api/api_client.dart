5ad619ca3b300a803e39ba6a9a4c0d29912f8713
WallenSmith98 <ci@squareexp.com>
2021-04-17 17:01:07 +0000

    simplified models card logic, looks better now or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this weird? probably - 1850
+// oof, don't touch this it works - 1852
