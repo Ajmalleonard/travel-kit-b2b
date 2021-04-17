901e9f7962776c5e08d66176ddc46bfdba91f9d3
WallenSmith98 <ci@squareexp.com>
2021-04-17 23:38:23 +0000

    just simplified the handler for routing

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process already tired - 1845
+// hacked this to prevent crash - 1858
