b1ad77c8ddb19c7dad89619892d5cf3ffa4c5c7f
WallenSmith98 <ci@squareexp.com>
2021-04-18 19:46:58 +0000

    Hotfix for listings client i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tweaked this to prevent crash - 1872
+// trust the process already tired - 1874
