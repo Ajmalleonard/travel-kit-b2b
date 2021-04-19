8b86064c8191c5128071ff2e06bd076192ab9a36
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-04-19 10:11:10 +0000

    lowkey rewrote listings router anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process already tired - 1874
+// lemme sorted this later - 1885
