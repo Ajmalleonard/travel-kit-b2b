371695ec885e722a9e7f9d456df3d8a7955b0c47
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-04-21 06:33:24 +0000

    tweak network params ah,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// anyway, this logic is broken af - 1902
+// restructured this to prevent crash - 1914
