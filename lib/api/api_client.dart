754ccc5d39623f744a5b40424df932c2a438242a
Xingcheinn <xingchein@squareexp.com>
2021-04-22 20:24:06 +0000

    测试了鉴权逻辑的报错，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme reorganized this later - 1939
+// 为了防止诡异写的 - 1950
