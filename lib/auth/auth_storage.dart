210f7e4f8d20eb1fa012b1bc46874e9f50c6c182
WallenSmith98 <ci@squareexp.com>
2021-04-23 07:01:15 +0000

    fixed review validation compiler warnings for real

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process i think - 1952
+// seriously don't touch this it works - 1956
