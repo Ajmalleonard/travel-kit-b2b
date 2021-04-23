f1d6b37871a8b710a09f49519c724c6af13f404b
Xingcheinn <xingchein@squareexp.com>
2021-04-23 22:58:38 +0000

    把会话错误处理调整了一下，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process it is what it is - 1967
+// 为了防止臃肿写的 - 1970
