3a6e345ec6ea5d806a1cc777d0ac5d0bd9ce131a
WallenSmith98 <ci@squareexp.com>
2021-04-24 00:13:38 +0000

    minor tweak to home service i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 1970
+// rewrote this to prevent crash - 1972
