31030dfbc31bad15fc09aa2a0b8d31a3407a3c4f
WallenSmith98 <ci@squareexp.com>
2021-04-24 17:26:50 +0000

    just tuned the home card so we don't crash for real

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// improved this to prevent crash - 1973
+// lowkey don't touch this it works - 1984
