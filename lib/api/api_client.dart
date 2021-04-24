e6eafe77c269f312a52a6bf861c186107bfdabad
Xingcheinn <xingchein@squareexp.com>
2021-04-24 12:30:39 +0000

    优化了交通查询的逻辑漏洞，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// rewrote this to prevent crash - 1972
+// 完了这里的逻辑有点臃肿 - 1979
