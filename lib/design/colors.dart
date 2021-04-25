cea348b55f450abe6473274f185a7da9c8cfc6cd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-04-25 01:56:49 +0000

    Fixed splash state type errors tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this wildin? probably - 1980
+// istg this logic is garbage - 1990
