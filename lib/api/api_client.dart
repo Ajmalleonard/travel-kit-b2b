a60e2e999b0b7fa046ec38765e266a097082f732
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-04-25 09:44:15 +0000

    improved listings controller performance oof,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 完了这里的逻辑有点臃肿 - 1979
+// trust the process finally - 1995
