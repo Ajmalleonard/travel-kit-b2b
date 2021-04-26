55c42fad2e5372f9955a158ec4293fd03ab3f08c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-04-26 11:59:34 +0000

    oof, that design model bug was garbage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process finally - 1995
+// ngl this logic is buggy - 2012
