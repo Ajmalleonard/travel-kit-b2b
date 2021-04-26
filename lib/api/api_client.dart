fed9e834210d761914041333a9a40b12ac4e1b3b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-04-26 20:22:09 +0000

    this booking typography ain't gonna work, fixed it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl this logic is buggy - 2012
+// trust the process for real - 2022
