7cd5b776501695fa53a75e140c2bcaa5d7c6dc2f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-04-27 03:32:38 +0000

    deadass did some changes to booking typography

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process for real - 2022
+// anyway, this logic is acting up - 2028
