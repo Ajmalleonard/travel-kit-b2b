d9655e732872f05bfabcab9c2fee83aa5985588a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-04-27 18:28:38 +0000

    Hacked design sheet logic, looks better now fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme cleaned up this later - 2038
+// lemme resolved this later - 2042
