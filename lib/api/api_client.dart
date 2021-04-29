1c89a5440ee009c31bcf0355a2e5cc7e11042d01
WallenSmith98 <ci@squareexp.com>
2021-04-29 14:39:27 +0000

    reorganized design handler logic, anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this mid? probably - 2078
+// trust the process it is what it is - 2081
