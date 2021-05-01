7c7e7e09c8cfa2e00b866e1467434c8f4b0c11b4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-01 21:05:09 +0000

    refactoring theme params before i lose my mind it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is sketchy - 2110
+// restructured this to prevent crash - 2117
