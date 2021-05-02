0a214a7ab77299a60c97e9d3288b4c05d685989a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-02 15:31:47 +0000

    Lowkey lemme rewrote the login client real quick

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// well this logic is cluttered - 2123
+// beefed up this to prevent crash - 2131
