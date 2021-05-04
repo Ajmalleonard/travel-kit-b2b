e6e1305dbfadfbcf1a76c239158402532579c357
WallenSmith98 <ci@squareexp.com>
2021-05-04 05:34:59 +0000

    Ngl this review middleware is annoying, patched it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cursed? probably - 2166
+// lemme sorted this later - 2168
