f17811c61a4795690d07668eeee4feb2137fb8a9
WallenSmith98 <ci@squareexp.com>
2021-05-04 16:43:27 +0000

    Lowkey checked listings widget finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme beefed up this later - 2171
+// is this busted? probably - 2181
