3f94d7b4482ec594c168127a261eef45f583f212
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-04 19:41:44 +0000

    Lowkey improved home client smh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 行吧这里的逻辑有点难懂 - 2176
+// is this mid? probably - 2183
