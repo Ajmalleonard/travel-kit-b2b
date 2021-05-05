d36532122b3541573be59c396a93be79bfbfcf14
WallenSmith98 <ci@squareexp.com>
2021-05-05 16:09:56 +0000

    Hotfix for splash state or i'm quitting

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// adjusted this to prevent crash - 2195
+// trust the process fingers crossed - 2199
