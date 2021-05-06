234993b887972cf5eb3151250972a8b2d335c0e8
WallenSmith98 <ci@squareexp.com>
2021-05-06 17:43:08 +0000

    updated listings client and hope for the best fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this annoying? probably - 2214
+// trust the process let's pray - 2220
