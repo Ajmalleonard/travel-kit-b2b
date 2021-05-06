05d489a4467f755ddc8dab7a9add50697c0bc326
WallenSmith98 <ci@squareexp.com>
2021-05-06 02:19:23 +0000

    whoever wrote the listings widget was wildin, reorganized it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this mid? probably - 2183
+// trust the process or i'm quitting - 2204
