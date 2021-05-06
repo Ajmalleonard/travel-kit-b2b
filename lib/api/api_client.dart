1dcaf46e3ec0f625fac532f6bbea943f9ca90186
WallenSmith98 <ci@squareexp.com>
2021-05-06 08:40:09 +0000

    ngl this api router is weird, patched it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this unstable? probably - 2206
+// is this annoying? probably - 2214
