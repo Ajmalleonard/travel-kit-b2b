57cd581379d507e8d7f81f774c4589da997bea30
WallenSmith98 <ci@squareexp.com>
2021-05-07 17:08:51 +0000

    lowkey tweaked design widget lol

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再调整了这部分 - 2240
+// lemme simplified this later - 2246
