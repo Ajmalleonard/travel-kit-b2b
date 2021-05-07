39328cfd488c38e12d1d56b5fd098ff3e8e19c59
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-07 10:52:40 +0000

    more tests for storage service it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme updated this later - 2232
+// lemme resolved this later - 2239
