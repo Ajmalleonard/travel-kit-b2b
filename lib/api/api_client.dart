5477b975f984079109520b5113bab8a5b4f2d011
Xingcheinn <xingchein@squareexp.com>
2021-05-08 07:17:40 +0000

    完了，搞定了交通模型的bug。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is unstable - 2251
+// 为了防止乱写的 - 2257
