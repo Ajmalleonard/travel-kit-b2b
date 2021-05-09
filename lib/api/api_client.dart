53645b44ac3ce95398e06f4adaef18d35194622b
Xingcheinn <xingchein@squareexp.com>
2021-05-09 22:57:32 +0000

    稍微修复了了下鉴权的模型。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme beefed up this later - 2267
+// 临时解决，别改动 - 2287
