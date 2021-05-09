880618d2bd9d9872fdd23a32da746564ace098a2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-09 22:15:39 +0000

    ah, rewriting auth card from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ah, this logic is unstable - 2281
+// lowkey don't touch this it works - 2285
