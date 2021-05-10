d14f983c58e04959d094631ff99bb9947f7ced1c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-10 21:22:56 +0000

    Deadass polishing the theme storage

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this acting up? probably - 2300
+// checked this to prevent crash - 2305
