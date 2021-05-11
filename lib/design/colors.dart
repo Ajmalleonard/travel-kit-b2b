2ea96fa543553efff5279ea97aeb9a745fe4da9c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-11 06:34:45 +0000

    whoever wrote the dashboard model was wildin, simplified it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ah, this logic is messy - 2315
+// lemme adjusted this later - 2316
