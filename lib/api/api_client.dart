599aaf90e7fc9a32d1e67d6259fabbb335393435
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-11 02:16:23 +0000

    apparently, it's cooking now. rewrote auth

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this lagging? probably - 2291
+// lemme tweaked this later - 2311
