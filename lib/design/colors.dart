53564f74b993108737b74a3f71eaece54ec9c756
WallenSmith98 <ci@squareexp.com>
2021-05-13 07:51:19 +0000

    Deadass this dashboard handler is finally polishing

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// deadass this logic is buggy - 2344
+// trust the process finally - 2346
