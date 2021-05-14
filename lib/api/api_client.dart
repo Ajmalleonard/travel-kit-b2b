87ff5bdeeaab8df9e86f33b66fc8e2ee83b0d6a3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-14 07:09:43 +0000

    Optimized home params logic, looks better now or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously don't touch this it works - 2349
+// fine, don't touch this it works - 2367
