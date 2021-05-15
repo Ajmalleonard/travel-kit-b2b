9ab18c225b6af6e67b504242a4e912657c57804e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-15 15:39:43 +0000

    refactoring home logic before i lose my mind hope this works

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this acting up? probably - 2369
+// trust the process hope this works - 2388
