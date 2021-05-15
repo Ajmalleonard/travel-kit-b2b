2bc1b41687f89d3d61c8890423dd8d1772dfbcfb
WallenSmith98 <ci@squareexp.com>
2021-05-15 23:54:35 +0000

    more tests for login endpoint already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this spaghetti? probably - 2381
+// is this cooked? probably - 2394
