7317883ab22bfc188fd404e05c89f15acf0dd7eb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-15 12:06:40 +0000

    more tests for review state already tired

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// well this logic is spaghetti - 2365
+// so, this logic is cooked - 2387
