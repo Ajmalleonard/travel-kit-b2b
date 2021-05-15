f4161083ddd3e42120e1198e274985fb3b1636da
WallenSmith98 <ci@squareexp.com>
2021-05-15 00:44:20 +0000

    simplified profile middleware logic tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this weird? probably - 2375
+// trust the process or i'm quitting - 2378
