1e825d66642319ae301a6d48cc3784db51e7fbaf
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-16 14:35:25 +0000

    cleaning up the garbage code in network logic

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cooked? probably - 2394
+// restructured this to prevent crash - 2409
