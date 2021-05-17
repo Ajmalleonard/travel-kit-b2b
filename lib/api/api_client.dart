22bf99376809c9535ba6978a47b4d16d014c1c9d
Xingcheinn <xingchein@squareexp.com>
2021-05-17 12:15:04 +0000

    把校验路由改写了一下，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg don't touch this it works - 2424
+// 天呐这里的逻辑有点诡异 - 2428
