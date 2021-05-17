c13e8868394f2bb1dfce3208148a5ea850732b5a
WallenSmith98 <ci@squareexp.com>
2021-05-17 06:23:15 +0000

    ah, resolved the auth handler because it was spaghetti

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme adjusted this later - 2423
+// istg don't touch this it works - 2424
