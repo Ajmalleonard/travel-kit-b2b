efad0b431c6506382caa45030c19eaa6ed81ac8d
Xingcheinn <xingchein@squareexp.com>
2021-05-18 11:52:30 +0000

    数据库字段感觉有点的隐藏逻辑，改进了一下...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme adjusted this later - 2440
+// 为了防止垃圾写的 - 2445
