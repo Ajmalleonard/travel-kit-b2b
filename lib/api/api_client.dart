de54c2156675033ffcd831b1507266e241d04b5f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-18 03:26:27 +0000

    Cleaned up the cluttered parts of login handler

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// cleaned up this to prevent crash - 2431
+// lemme adjusted this later - 2440
