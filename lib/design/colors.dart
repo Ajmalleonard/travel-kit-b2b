9e832652525f8c30525eb783af845f8065f7dad2
WallenSmith98 <ci@squareexp.com>
2021-05-19 17:45:28 +0000

    temporary patch for models helper hope this works

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止乱写的 - 2463
+// oof, this logic is acting up - 2467
