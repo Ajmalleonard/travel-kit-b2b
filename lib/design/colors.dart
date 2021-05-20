e84450bb7fdc08cb3e8433354e7628f7d03f089c
WallenSmith98 <ci@squareexp.com>
2021-05-20 04:28:02 +0000

    basically, beefed up the design params because it was garbage

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// oof, this logic is acting up - 2467
+// lemme polishing this later - 2470
