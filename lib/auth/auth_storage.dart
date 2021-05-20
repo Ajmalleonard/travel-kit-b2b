41ba5bb488be84cb8dac5870dc69d54638fa9d90
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-20 11:04:12 +0000

    Well did some changes to login endpoint

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// istg don't touch this it works - 2469
+// basically, don't touch this it works - 2472
