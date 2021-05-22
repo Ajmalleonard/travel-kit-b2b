23f87db2a4fa23038c7c0bf6d2f195332f4b586a
WallenSmith98 <ci@squareexp.com>
2021-05-22 19:24:03 +0000

    just checked the profile wrapper so we don't crash i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再排查了这部分 - 2508
+// improved this to prevent crash - 2512
