0b515a075ca8e5f7cc197f33461f2b1e0a2ee383
WallenSmith98 <ci@squareexp.com>
2021-05-23 10:30:05 +0000

    basically, it's cooking now. sorted booking

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// improved this to prevent crash - 2512
+// anyway, don't touch this it works - 2520
