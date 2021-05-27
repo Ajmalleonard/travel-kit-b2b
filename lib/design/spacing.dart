f231b86e9ea8a7bcd2cb32216f2de479dc2ec809
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-27 05:21:08 +0000

    basically, optimized the home client

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme improved this later - 2571
+// apparently, this logic is trash - 2586
