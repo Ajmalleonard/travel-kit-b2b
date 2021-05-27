9c81200b64c336cdd2be26b1751b4278c95b484b
WallenSmith98 <ci@squareexp.com>
2021-05-27 23:08:00 +0000

    had to improved the helper state seriously

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, this logic is mid - 2562
+// is this cooked? probably - 2596
