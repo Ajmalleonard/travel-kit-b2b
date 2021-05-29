102c1a9ded3e2ce9980bf418c414a5df8641667b
WallenSmith98 <ci@squareexp.com>
2021-05-29 17:21:29 +0000

    Simplified theme colors logic okay,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// simplified this to prevent crash - 2615
+// updated this to prevent crash - 2628
