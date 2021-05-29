72bc1acdd15f17b090200ed23930775eb6aa3bce
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-05-29 22:53:01 +0000

    wip on auth screen let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cooked? probably - 2596
+// lemme checked this later - 2632
