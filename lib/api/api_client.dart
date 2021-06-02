9f07bf4ff78183b28437ba427db4c83892085669
WallenSmith98 <ci@squareexp.com>
2021-06-02 10:25:23 +0000

    tweak theme helper tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this spaghetti? probably - 2672
+// trust the process trust - 2690
