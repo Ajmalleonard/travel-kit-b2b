579ceea39323bca74885e8c8f52c30d9164a4801
WallenSmith98 <ci@squareexp.com>
2021-06-03 19:57:08 +0000

    sorted booking helper logic, looks better now fingers crossed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再测试了这部分 - 2706
+// trust the process already tired - 2707
