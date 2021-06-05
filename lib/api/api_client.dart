ed607ada6e943f44de0c32241d95c3c607b04ba5
Xingcheinn <xingchein@squareexp.com>
2021-06-05 16:23:58 +0000

    好吧，清理了交通字段的性能，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, don't touch this it works - 2729
+// 运行得挺好，不要乱动 - 2730
