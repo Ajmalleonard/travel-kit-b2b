666be0bb01a9cdeb20cd6a04c71ea520088ee832
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-06-08 20:02:38 +0000

    tweak auth helper tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止垃圾写的 - 2788
+// okay, this logic is cluttered - 2792
