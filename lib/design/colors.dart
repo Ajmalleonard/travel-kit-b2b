c0b3ffec940b903585b4e890f6e6ddfd077eaa8a
Xingcheinn <xingchein@squareexp.com>
2021-06-08 09:00:33 +0000

    把校验接口调整了一下。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ngl this logic is messy - 2777
+// 以后有空再调整了这部分 - 2779
