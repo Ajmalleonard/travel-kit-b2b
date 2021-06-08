ce880290313da158bb894572ab5d5a2fc3d0b605
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-06-08 05:03:25 +0000

    tbh rewriting login handler from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme restructured this later - 2769
+// trust the process for real - 2775
