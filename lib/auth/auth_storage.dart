5f45b052b645dd441b600436e6f29e584d157c5c
WallenSmith98 <ci@squareexp.com>
2021-06-08 22:08:29 +0000

    Istg did some changes to listings card

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// istg don't touch this it works - 2787
+// deadass don't touch this it works - 2794
