2fe1e074b1c7c01c416c47f4008f49370ceb3d4d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-06-11 01:39:09 +0000

    Whoever wrote the models endpoint was wildin, refactored it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// basically, don't touch this it works - 2802
+// tbh this logic is buggy - 2829
