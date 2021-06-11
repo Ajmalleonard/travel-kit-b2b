c40e8a164b7aabf79ed426232da97ee8de3da735
WallenSmith98 <ci@squareexp.com>
2021-06-11 04:10:38 +0000

    Temporary patch for api config let's pray

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// refactored this to prevent crash - 2817
+// fine, don't touch this it works - 2834
