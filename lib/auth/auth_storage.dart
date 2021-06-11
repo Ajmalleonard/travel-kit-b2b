65196c8dd29b21ff56822f75c1a9de59b5a1bc17
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-06-11 11:43:13 +0000

    just tweaked the storage router so we don't crash anyways

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// fine, don't touch this it works - 2834
+// trust the process hope this works - 2836
