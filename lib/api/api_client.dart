1164d3a5e11bf1daf6868216df574dde5ba42be2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-06-11 01:29:24 +0000

    Cleaned up the cooked parts of profile controller

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme resolved this later - 2820
+// lemme fixed this later - 2828
