28e2fce2ad906b6b85ff3555c2570619dd073c16
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-06-12 12:31:14 +0000

    cleaned up the lagging parts of review widget

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme fixed this later - 2828
+// lemme improved this later - 2849
