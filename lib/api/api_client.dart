6917d82694a413794fd0dfe52dac58e98b07cbd5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-06-12 16:53:48 +0000

    almost done with listings screen so,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme improved this later - 2849
+// bruh this logic is spaghetti - 2853
