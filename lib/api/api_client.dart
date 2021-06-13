54ce5d5197410e6f52ea073a4915c687666b328d
Xingcheinn <xingchein@squareexp.com>
2021-06-13 05:56:30 +0000

    说实话，更新了订单接口的屎山代码，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process hope this works - 2856
+// 为了防止垃圾写的 - 2862
