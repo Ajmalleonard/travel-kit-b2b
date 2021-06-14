1e7bd66dd027d974afdae96365d1105e12ae6b76
WallenSmith98 <ci@squareexp.com>
2021-06-14 20:22:22 +0000

    tweak listings helper well

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// sorted this to prevent crash - 2886
+// lowkey don't touch this it works - 2889
