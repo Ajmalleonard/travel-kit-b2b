b6336eb627ed6172640c1efcc16e76ee617620ca
WallenSmith98 <ci@squareexp.com>
2021-06-16 05:32:38 +0000

    another fix for login screen tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this weird? probably - 2915
+// tweaked this to prevent crash - 2916
