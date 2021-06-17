a0feaea1c57369d44032b71da52bb7c33f2d0374
Xingcheinn <xingchein@squareexp.com>
2021-06-17 22:05:14 +0000

    天呐优化了跨域查询...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// beefed up this to prevent crash - 2952
+// 行吧这里的逻辑有点诡异 - 2955
