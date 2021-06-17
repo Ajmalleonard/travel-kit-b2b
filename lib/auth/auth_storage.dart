0a2d2f948e9f7d6f91610704cb767ce1bfcfde3f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-06-17 11:05:05 +0000

    This splash storage ain't gonna work, sorted it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// optimized this to prevent crash - 2941
+// ah, don't touch this it works - 2944
