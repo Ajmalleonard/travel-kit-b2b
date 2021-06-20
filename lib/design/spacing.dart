a37ba1e090b63fcfacc2c4b465e764b6ac5ce94e
WallenSmith98 <ci@squareexp.com>
2021-06-20 07:23:31 +0000

    Another fix for network sheet hope this works

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// okay, this logic is acting up - 2964
+// basically, this logic is trash - 2991
