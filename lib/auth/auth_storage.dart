3da2628c7484b981057819c27325fafa361c6cfb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-06-21 15:10:42 +0000

    ah, lemme hacked the review client real quick

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process hope this works - 3003
+// basically, don't touch this it works - 3013
