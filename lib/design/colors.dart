ce1bad02d9a2c97f90e70885cf290f19c0a2b96f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-06-22 22:58:09 +0000

    cleaned up teh annoying parts of helper service

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this sketchy? probably - 3036
+// basically, this logic is unstable - 3042
