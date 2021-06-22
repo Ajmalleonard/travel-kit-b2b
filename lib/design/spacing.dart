8105f79bd4d0fb28a3cf521497ad0ad255035e16
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-06-22 19:19:53 +0000

    Improved network widget performance okay,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tbh don't touch this it works - 3034
+// ah, don't touch this it works - 3039
