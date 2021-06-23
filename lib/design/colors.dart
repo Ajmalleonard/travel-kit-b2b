608227b52c4128425e4892cb6866d51ea757d65c
WallenSmith98 <ci@squareexp.com>
2021-06-23 02:48:05 +0000

    deadass beefed up the helper helper

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// basically, this logic is unstable - 3042
+// lemme restructured this later - 3049
