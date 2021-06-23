e7adac7a018f82e282c2638829beea976c74e597
Xingcheinn <xingchein@squareexp.com>
2021-06-23 07:39:33 +0000

    救命，更新了路由模型的逻辑漏洞，累瘫了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process i guess - 3051
+// 以后有空再改写了这部分 - 3053
