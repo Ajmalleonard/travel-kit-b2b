f1e515704ce7856f4ee79527499bbc8f81f11ede
WallenSmith98 <ci@squareexp.com>
2021-06-25 01:30:41 +0000

    Fixing routing params is busted but it's done trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme resolved this later - 3079
+// lemme hacked this later - 3083
