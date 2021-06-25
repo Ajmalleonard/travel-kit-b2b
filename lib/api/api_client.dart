ebaf94f893265b84b94b33040f7a10703e93ee8e
WallenSmith98 <ci@squareexp.com>
2021-06-25 00:31:46 +0000

    refactoring booking router before i lose my mind smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this mid? probably - 3078
+// lemme resolved this later - 3079
