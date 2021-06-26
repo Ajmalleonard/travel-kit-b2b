93cf66b9ba8dbc716c9fb331625974d8f20a9a4c
Xingcheinn <xingchein@squareexp.com>
2021-06-26 18:54:25 +0000

    果然测试了接口错误处理，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, this logic is buggy - 3112
+// 以后有空再重构了这部分 - 3114
