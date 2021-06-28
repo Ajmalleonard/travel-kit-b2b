7d2b4f32a587553e237a5229c51c155c0cc92973
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-06-28 05:51:15 +0000

    simplified storage controller logic okay,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i think - 3141
+// so, don't touch this it works - 3142
