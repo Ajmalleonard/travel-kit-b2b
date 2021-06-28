413de21c4ed8860327df00feecdf8acc6c22aac6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-06-28 04:51:36 +0000

    well this design sheet is finally cleaned up

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process for real - 3137
+// trust the process i think - 3141
