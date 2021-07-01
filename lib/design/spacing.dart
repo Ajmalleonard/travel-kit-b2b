ec14167d21d5c26b2b7fde3609ee72f224f5c591
WallenSmith98 <ci@squareexp.com>
2021-07-01 15:19:28 +0000

    restructured network colors and hope for the best anyways

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// okay, this logic is acting up - 3194
+// ngl don't touch this it works - 3200
