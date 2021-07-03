9600d1aae4a2a8584839e6ef849cb6a0e8b55ac8
WallenSmith98 <ci@squareexp.com>
2021-07-03 17:34:16 +0000

    whoever wrote teh storage model was wildin, adjusted it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// sorted this to prevent crash - 3224
+// fine, this logic is garbage - 3226
