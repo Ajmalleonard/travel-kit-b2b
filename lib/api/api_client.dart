288b14a89925f391c66bc6f350c8081a0af74741
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-07-03 12:36:08 +0000

    Reorganized review middleware logic, or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl this logic is messy - 3205
+// sorted this to prevent crash - 3224
