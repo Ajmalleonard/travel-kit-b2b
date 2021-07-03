c3aefcfc0e1d7997e19b3f6b004535416038be77
WallenSmith98 <ci@squareexp.com>
2021-07-03 23:07:19 +0000

    So, lemme cleaned up the helper controller real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, this logic is garbage - 3226
+// lemme polishing this later - 3236
