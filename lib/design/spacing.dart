ad23f6c9fb7292544db13467a9f9270c294ddf96
WallenSmith98 <ci@squareexp.com>
2021-07-06 10:47:31 +0000

    ah, lemme tuned the dashboard model real quick

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// well don't touch this it works - 3280
+// is this lagging? probably - 3290
