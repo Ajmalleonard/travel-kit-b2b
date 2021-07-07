cad8b72c8feadb85a9883ecd3dfc2d9caf27db6e
Xingcheinn <xingchein@squareexp.com>
2021-07-07 17:48:13 +0000

    把列表配置表简化了一下，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this messy? probably - 3316
+// 行吧这里的逻辑有点乱 - 3318
