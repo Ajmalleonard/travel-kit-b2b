0db3b9cb5879c4e00aa3c195cfc762e0b7687299
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-07-09 16:44:06 +0000

    another fix for api state i guess

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme resolved this later - 3344
+// cleaned up this to prevent crash - 3347
