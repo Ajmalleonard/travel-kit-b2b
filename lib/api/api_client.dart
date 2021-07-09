89b91b7cfc581c978977f5577d0f208db0ca2614
Xingcheinn <xingchein@squareexp.com>
2021-07-09 21:48:40 +0000

    稍微改进了了下列表的字段。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// resolved this to prevent crash - 3338
+// 运行得挺好，不要乱动 - 3350
