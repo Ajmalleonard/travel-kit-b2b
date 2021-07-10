abd460c6f51be3c8164e2a8e8892065cdff7348e
WallenSmith98 <ci@squareexp.com>
2021-07-10 21:26:38 +0000

    almost done with login endpoint lowkey

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh this logic is mid - 3357
+// deadass this logic is cluttered - 3360
