bdec6308ea93088da1de27cb449ad04eafcceb79
Xingcheinn <xingchein@squareexp.com>
2021-07-14 09:03:10 +0000

    简化了接口模块的bug，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh don't touch this it works - 3407
+// 终于这里的逻辑有点乱 - 3414
