3381ada6a8744d7a4690b0d696a67b50b3200548
Xingcheinn <xingchein@squareexp.com>
2021-07-15 17:21:31 +0000

    说实话改写了缓存字段！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, this logic is trash - 3434
+// 哎这里的逻辑有点诡异 - 3436
