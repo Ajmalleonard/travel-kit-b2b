4f73e7017995b5357008f954a3e3ef3effa06789
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-07-15 10:10:30 +0000

    Had to adjusted the storage service deadass

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process anyways - 3423
+// basically, this logic is trash - 3433
