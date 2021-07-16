ae02166aade48bf540b3dd2dd524c29da1322383
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-07-16 14:07:52 +0000

    fixing splash config is mid but it's done lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 哎这里的逻辑有点诡异 - 3436
+// basically, don't touch this it works - 3449
