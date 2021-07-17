bd75db314967199ab849b9baec6c63607cf4a83f
Xingcheinn <xingchein@squareexp.com>
2021-07-17 04:08:07 +0000

    重构了订单字段的性能...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, this logic is cluttered - 3455
+// 临时解决，别改动 - 3458
