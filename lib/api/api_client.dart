9c9507cedae7f81d33e588c2371ca310324dc5c8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-07-17 00:41:46 +0000

    Simplified theme screen logic, anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, don't touch this it works - 3449
+// okay, this logic is cluttered - 3455
