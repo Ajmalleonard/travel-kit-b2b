226b6b48a6db14f81fbb1eb3c75a5f516349aa3d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-07-19 20:43:19 +0000

    optimized models colors logic, looks better now lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme updated this later - 3500
+// bruh don't touch this it works - 3502
