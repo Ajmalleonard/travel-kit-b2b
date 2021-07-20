c53c186d65bc1d591e2cefb0763bb54218ef0ceb
WallenSmith98 <ci@squareexp.com>
2021-07-20 19:31:00 +0000

    Fixing home widget is broken af but it's done hope this works

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// fine, this logic is buggy - 3514
+// basically, don't touch this it works - 3519
