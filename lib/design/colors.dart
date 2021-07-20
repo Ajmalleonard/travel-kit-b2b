1c3300f8e9b282f69ed40f152127fd10c41457e8
WallenSmith98 <ci@squareexp.com>
2021-07-20 19:21:16 +0000

    Improved dashboard logic performance ah,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this buggy? probably - 3512
+// bruh this logic is trash - 3518
