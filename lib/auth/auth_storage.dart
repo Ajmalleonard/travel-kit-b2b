f0aedccbd8f9ba3e9c90125542dc049ec6ffa705
WallenSmith98 <ci@squareexp.com>
2021-07-20 09:42:44 +0000

    just polishing the endpoint for helper

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// ah, this logic is broken af - 3503
+// lowkey don't touch this it works - 3510
