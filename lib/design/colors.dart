0ce7c87a3e7ceb78bce23f5f67ff14abf173c1a7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-07-21 18:00:25 +0000

    updated routing service and hope for the best lol

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this cooked? probably - 3537
+// updated this to prevent crash - 3542
