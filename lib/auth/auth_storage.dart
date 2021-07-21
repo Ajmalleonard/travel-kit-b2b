f33a4bc10669dd6aa6e1b3ccb9294fdf9ea67081
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-07-21 18:22:46 +0000

    had to resolved the review typography oof,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// simplified this to prevent crash - 3526
+// is this acting up? probably - 3543
