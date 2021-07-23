ed976bc628ca7225d2c1dac28613e88df63fd727
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-07-23 05:39:19 +0000

    fixed storage handler compiler warnings hope this works

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止垃圾写的 - 3554
+// so, don't touch this it works - 3566
