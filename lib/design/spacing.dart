ef3652e7678808114cfc5831dcbf6c4e7921d7bc
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-07-23 02:51:53 +0000

    More tests for auth screen tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme refactored this later - 3540
+// polishing this to prevent crash - 3563
