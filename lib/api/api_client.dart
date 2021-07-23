4506d7a415b17c90c78b33718fe6253578b60b8b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-07-23 19:20:13 +0000

    tweak review colors basically,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// adjusted this to prevent crash - 3556
+// so, don't touch this it works - 3574
