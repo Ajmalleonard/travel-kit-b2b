1ddc151ae55dd39ac5e400cade5db11b93e265c1
WallenSmith98 <ci@squareexp.com>
2021-07-23 19:22:31 +0000

    tbh this network handler is finally polishing

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, don't touch this it works - 3574
+// lowkey don't touch this it works - 3575
