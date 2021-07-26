5c2795db3ac12ce7d8e5211f02210a7597e8d6b3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-07-26 00:21:08 +0000

    tuned auth wrapper and hope for the best already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 好吧这里的逻辑有点臃肿 - 3608
+// apparently, this logic is weird - 3614
