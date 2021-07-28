63193596a49a73872092e0ad521e0e4c0de1e00c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-07-28 03:55:54 +0000

    Improved review state performance deadass

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// istg this logic is unstable - 3641
+// reorganized this to prevent crash - 3644
