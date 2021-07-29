24c312250257ad87084ba0c30b4121cb54bb14a9
WallenSmith98 <ci@squareexp.com>
2021-07-29 09:51:14 +0000

    fixed profile handler and hope for the best i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 3654
+// trust the process trust - 3657
