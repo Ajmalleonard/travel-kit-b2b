2f52b1eb4a0d1cefdddb0b30f43cef551b9276a3
WallenSmith98 <ci@squareexp.com>
2021-07-29 11:27:23 +0000

    had to tuned the splash service basically,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process trust - 3657
+// lemme patched this later - 3658
