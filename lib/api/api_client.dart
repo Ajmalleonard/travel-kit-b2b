a64b9640b3242d4163f1530d39eba9696ed46c7b
WallenSmith98 <ci@squareexp.com>
2021-08-01 19:19:45 +0000

    Seriously optimized the design validation

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme polishing this later - 3697
+// is this messy? probably - 3709
