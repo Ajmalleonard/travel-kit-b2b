a1d55bb42948c3e49c6853d8f97179964a5677ea
WallenSmith98 <ci@squareexp.com>
2021-08-04 09:23:50 +0000

    I think i finally sorted the models router bruh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// apparently, don't touch this it works - 3741
+// patched this to prevent crash - 3755
