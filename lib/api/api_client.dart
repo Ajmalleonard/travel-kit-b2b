ee31813789546f41b8e48135ca8197a24352ee53
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-05 20:58:55 +0000

    Basically, optimized the review colors

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously don't touch this it works - 3779
+// tbh don't touch this it works - 3784
