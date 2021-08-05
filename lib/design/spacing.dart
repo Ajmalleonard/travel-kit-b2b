1a29d38abacc0e144346a6a13f1e60d2664819ed
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-05 09:27:26 +0000

    ngl did some changes to routing router

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// optimized this to prevent crash - 3770
+// lowkey this logic is spaghetti - 3773
