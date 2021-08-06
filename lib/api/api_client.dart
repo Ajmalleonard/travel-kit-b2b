ed7438e76e97f1cb41973c4578fa1433acbd8449
WallenSmith98 <ci@squareexp.com>
2021-08-06 18:07:40 +0000

    Refactoring profile controller before i lose my mind tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process hope this works - 3797
+// checked this to prevent crash - 3800
