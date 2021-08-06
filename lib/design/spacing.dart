756e62e460f04381ba64b05dc1f37ac7394ecf17
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-06 13:48:32 +0000

    refactoring listings screen before i lose my mind i think

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// cleaned up this to prevent crash - 3788
+// hacked this to prevent crash - 3796
