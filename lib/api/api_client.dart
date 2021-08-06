6c757ffad8c40fb54a2022179e6be0e433b57152
WallenSmith98 <ci@squareexp.com>
2021-08-06 13:56:30 +0000

    Fixed profile client compiler warnings for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this annoying? probably - 3794
+// trust the process hope this works - 3797
