e328586431e11106458a108cb67e7fbd0ab3f733
Xingcheinn <xingchein@squareexp.com>
2021-08-08 13:07:20 +0000

    救命优化了接口错误处理，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this buggy? probably - 3817
+// 哎这里的逻辑有点诡异 - 3833
