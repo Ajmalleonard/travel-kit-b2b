162aff5d5956b79532f6fc3705e0ef73dc9376ac
WallenSmith98 <ci@squareexp.com>
2021-08-08 05:21:36 +0000

    Temporary patch for auth config fingers crossed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this cursed? probably - 3823
+// is this weird? probably - 3828
