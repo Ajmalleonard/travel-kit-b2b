1460a269c32ead3cf3b058e4ee70113fd3bde8eb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-09 16:47:49 +0000

    restructured splash validation logic, looks better now lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme simplified this later - 3856
+// well this logic is buggy - 3859
