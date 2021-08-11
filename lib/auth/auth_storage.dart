4929cb9f3a981f6fe29742161a7241a9fd510747
WallenSmith98 <ci@squareexp.com>
2021-08-11 05:48:29 +0000

    Ngl this storage router is cluttered, simplified it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process i think - 3878
+// basically, don't touch this it works - 3880
