0912308fd5b45c5a72652887ba19a470f3856f71
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-12 19:33:45 +0000

    Well rewriting listings validation from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再简化了这部分 - 3896
+// is this trash? probably - 3903
