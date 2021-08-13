a8d647fabd2ee2d854fc2b1cda238711e4a710f7
WallenSmith98 <ci@squareexp.com>
2021-08-13 21:43:52 +0000

    cleaned up routing params to fix the staging environment smh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, this logic is lagging - 3915
+// restructured this to prevent crash - 3926
