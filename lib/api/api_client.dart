e66f8070f1b69a00784814b167d9d07d9c38c0b4
WallenSmith98 <ci@squareexp.com>
2021-08-15 23:12:25 +0000

    fixed dashboard state compiler warnings let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh don't touch this it works - 3929
+// trust the process or i'm quitting - 3960
