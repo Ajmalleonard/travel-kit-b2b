7bd29c4a6bda11b74db917ae3453b6b8bcdee3b2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-16 09:04:39 +0000

    refactoring listings validation before i lose my mind for real

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// adjusted this to prevent crash - 3974
+// tweaked this to prevent crash - 3977
