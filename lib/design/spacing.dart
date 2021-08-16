2bf4a03f2662d86e13aa65aa9f7b33a2974cf8f8
WallenSmith98 <ci@squareexp.com>
2021-08-16 03:50:46 +0000

    istg it's cooking now. patched network

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process i guess - 3951
+// apparently, don't touch this it works - 3963
