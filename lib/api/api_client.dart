1b6d8803873e7dbd4963da2779b184af24d1bbfe
Xingcheinn <xingchein@squareexp.com>
2021-08-17 09:33:27 +0000

    完了，简化了配置接口的并发问题，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme resolved this later - 3979
+// 临时解决，别改动 - 3994
