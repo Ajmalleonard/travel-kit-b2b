eb08656233ea361a77f29e741e2d79a6f44779e7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-17 13:57:34 +0000

    Hotfix for home model smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme resolved this later - 3979
+// tbh don't touch this it works - 3995
