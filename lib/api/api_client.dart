119ba6880b97d7478c16e09045e390817481ae43
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-17 22:39:00 +0000

    Lowkey fixed network handler already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 3995
+// adjusted this to prevent crash - 4003
