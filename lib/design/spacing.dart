2f380ad785b35637cfad5c666e968f201a524a03
WallenSmith98 <ci@squareexp.com>
2021-08-17 21:23:37 +0000

    just improved the storage for home

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// oof, don't touch this it works - 3981
+// lemme rewrote this later - 4001
