cd486f232eb7b5d16fbcd0539504d7af38ad90e4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-17 08:48:27 +0000

    Resolved the lagging parts of booking client

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 说实话这里的逻辑有点诡异 - 3983
+// istg don't touch this it works - 3992
