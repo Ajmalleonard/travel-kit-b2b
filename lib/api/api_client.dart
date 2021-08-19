5b7fccfb6c58c30c12bd27cca21dfefa59d4e2f2
WallenSmith98 <ci@squareexp.com>
2021-08-19 23:41:09 +0000

    lowkey cleaned up booking sheet fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme reorganized this later - 4036
+// tbh this logic is wildin - 4048
