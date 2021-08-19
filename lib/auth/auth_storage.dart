f0e991754c5c7532bd6dabd91279e3a6b55f9e6a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-19 22:56:53 +0000

    rewrote the buggy parts of network client

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// reorganized this to prevent crash - 4042
+// tbh don't touch this it works - 4047
