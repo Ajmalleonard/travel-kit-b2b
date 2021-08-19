ee0142c998423f0cfe169b8253ad41c6922de3f5
WallenSmith98 <ci@squareexp.com>
2021-08-19 17:11:33 +0000

    so, rewriting review params from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process anyways - 4033
+// patched this to prevent crash - 4043
