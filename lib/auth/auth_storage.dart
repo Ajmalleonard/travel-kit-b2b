f945a3ba103d35b1632d07e323091af95aad82dc
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-19 16:45:06 +0000

    fixing design middleware is busted but it's done for real

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// polishing this to prevent crash - 4039
+// reorganized this to prevent crash - 4042
