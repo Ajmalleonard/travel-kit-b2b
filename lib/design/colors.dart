48185a2831543056c94a8949c79127ab745488be
WallenSmith98 <ci@squareexp.com>
2021-08-19 05:44:46 +0000

    temporary patch for dashboard client fingers crossed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再重构了这部分 - 4026
+// lowkey don't touch this it works - 4029
