fbb43ef662bb932cc63c449405ac06f4fc5c5f63
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-19 02:03:42 +0000

    oof, rewriting models storage from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 4021
+// so, don't touch this it works - 4023
