326b99bf776bea61b54424303463cc37770fb55e
Xingcheinn <xingchein@squareexp.com>
2021-08-20 18:29:10 +0000

    把中间件配置表修复了一下，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well don't touch this it works - 4052
+// 临时解决，别改动 - 4063
