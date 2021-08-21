b6c3c18a220986bd4b5814f7a89cfb3b5409a1da
WallenSmith98 <ci@squareexp.com>
2021-08-21 22:17:53 +0000

    Fixing network params is messy but it's done for real

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再排查了这部分 - 4078
+// istg this logic is cursed - 4080
