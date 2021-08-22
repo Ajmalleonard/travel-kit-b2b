956e1301cdce7cb939506ccef3ab43c7d71c38e8
WallenSmith98 <ci@squareexp.com>
2021-08-22 15:16:12 +0000

    Whoever wrote the api logic was wildin, reorganized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 4063
+// trust the process fingers crossed - 4091
