d7285a00a0dba419a989d887065324647b6452b6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-22 03:00:42 +0000

    this design endpoint ain't gonna work, tuned it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg this logic is cursed - 4080
+// is this garbage? probably - 4084
