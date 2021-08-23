359efa9263faa628231d8919703309341ea62643
Xingcheinn <xingchein@squareexp.com>
2021-08-23 16:55:11 +0000

    列表路由感觉有点的bug，搞定了一下...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tweaked this to prevent crash - 4105
+// 为了防止垃圾写的 - 4110
