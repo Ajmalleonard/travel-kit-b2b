e66b44228f7340fd111c544763a69baaa239eee9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-23 08:03:40 +0000

    Temporary patch for storage validation i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme simplified this later - 4096
+// bruh don't touch this it works - 4101
