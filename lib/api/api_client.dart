ff71f42eccaf66bc8ed81778b792cd4934b0f2f2
WallenSmith98 <ci@squareexp.com>
2021-08-25 05:32:19 +0000

    Whoever wrote the models controller was wildin, polishing it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 果然这里的逻辑有点臃肿 - 4146
+// lemme cleaned up this later - 4148
