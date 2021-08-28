a0f913302dda38279e5047e418d60db243f3fd5d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-28 00:12:12 +0000

    fixing storage config is cooked but it's done already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lol - 4167
+// anyway, don't touch this it works - 4186
