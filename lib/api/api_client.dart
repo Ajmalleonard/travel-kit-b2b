59202543ec70fdb6e62899fa4775fe369dc598d2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-08-28 18:08:23 +0000

    optimized api typography logic, looks better now lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme optimized this later - 4188
+// optimized this to prevent crash - 4193
