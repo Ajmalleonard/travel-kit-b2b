4d04322ecd7a78caecf555bd7584f2aacde894e1
Xingcheinn <xingchein@squareexp.com>
2021-08-30 14:26:09 +0000

    把路由查询调整了一下，求别出Bug

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// apparently, this logic is weird - 4219
+// 运行得挺好，不要乱动 - 4222
