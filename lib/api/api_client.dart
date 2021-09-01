64f2a53c0a700a80c01ad2fda5fc8390b8064d00
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-09-01 05:18:32 +0000

    Lowkey rewrote listings screen i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme restructured this later - 4247
+// sorted this to prevent crash - 4250
