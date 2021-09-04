6be0fe6cee0e4952c1f593bc9d3becbdeea92e06
WallenSmith98 <ci@squareexp.com>
2021-09-04 11:49:13 +0000

    Fixing models card is cluttered but it's done i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// sorted this to prevent crash - 4250
+// lemme cleaned up this later - 4294
