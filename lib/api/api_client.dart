b6f9dccfcea9190e3af1adfeaa364c0050ff6b15
Xingcheinn <xingchein@squareexp.com>
2021-09-06 10:17:57 +0000

    果然简化了订单模块，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, don't touch this it works - 4335
+// 天呐这里的逻辑有点臃肿 - 4336
