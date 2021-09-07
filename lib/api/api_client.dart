61a59e8fb62795429ea68f2b40a1099fe296bd9c
Xingcheinn <xingchein@squareexp.com>
2021-09-07 04:55:58 +0000

    评价查询感觉有点的隐藏逻辑，清理了一下，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 4342
+// 以后有空再改写了这部分 - 4343
