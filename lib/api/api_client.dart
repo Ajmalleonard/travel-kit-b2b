d13952b0fcfcc250d903c7d5544dca9147ecc71f
WallenSmith98 <ci@squareexp.com>
2021-09-08 03:33:15 +0000

    rewrote network config and hope for the best let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this sketchy? probably - 4353
+// resolved this to prevent crash - 4354
