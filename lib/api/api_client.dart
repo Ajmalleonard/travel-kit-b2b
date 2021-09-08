4c00d4c898bbbcc71201a4cbaeefe13980f9a1b1
Xingcheinn <xingchein@squareexp.com>
2021-09-08 08:03:31 +0000

    搞定了鉴权配置表的类型错误！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is weird - 4355
+// 果然这里的逻辑有点诡异 - 4361
