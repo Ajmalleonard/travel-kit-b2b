fd71f71e482719999fc542393f4f7cf712a4ccb0
WallenSmith98 <ci@squareexp.com>
2021-09-08 02:39:23 +0000

    polishing login colors to fix the staging environment tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再改写了这部分 - 4343
+// is this sketchy? probably - 4353
