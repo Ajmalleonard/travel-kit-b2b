066e44194b70b730971c8a9668eb0aa773d2d767
WallenSmith98 <ci@squareexp.com>
2021-09-09 20:43:27 +0000

    tbh rewriting design storage from scratch

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme adjusted this later - 4372
+// tbh don't touch this it works - 4383
