2025592e16f91b9aadd25808540935e4351cf773
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-09-10 14:56:08 +0000

    simplified routing validation to fix the staging environment i think

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process or i'm quitting - 4401
+// trust the process already tired - 4403
