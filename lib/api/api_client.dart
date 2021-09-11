8902287c071fc4fffef01fe699a093cd059ba587
WallenSmith98 <ci@squareexp.com>
2021-09-11 16:07:52 +0000

    refactoring network colors before i lose my mind already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 4402
+// trust the process for real - 4414
