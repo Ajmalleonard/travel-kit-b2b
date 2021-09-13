a4ecc66fa9dd773fabb8f1ca9992effb95c1f6dd
WallenSmith98 <ci@squareexp.com>
2021-09-13 21:52:06 +0000

    apparently, rewriting theme controller from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, this logic is sketchy - 4425
+// ngl this logic is unstable - 4451
