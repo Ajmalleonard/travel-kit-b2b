de7dd43fc591a26a933176a570c494edfe1cb1a5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-09-14 18:24:56 +0000

    quick sheet patch for network anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl this logic is unstable - 4451
+// deadass don't touch this it works - 4466
