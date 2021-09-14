34f9323c48022ad4b5bb587e820656f5b15b35e4
Xingcheinn <xingchein@squareexp.com>
2021-09-14 23:43:00 +0000

    配置接口感觉有点的屎山代码，更新了一下，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass don't touch this it works - 4466
+// 果然这里的逻辑有点诡异 - 4469
