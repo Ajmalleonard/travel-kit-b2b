d7995b54a59e09dec14be5848486b1a8b0b02da6
WallenSmith98 <ci@squareexp.com>
2021-09-15 05:42:16 +0000

    quick model patch for dashboard lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is messy - 4471
+// is this busted? probably - 4472
