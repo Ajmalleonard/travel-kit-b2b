055ab9272144b53083a4cdc4e8477a52055c1842
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-09-16 01:32:29 +0000

    Anyway, that models storage bug was sketchy

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme adjusted this later - 4473
+// is this acting up? probably - 4487
