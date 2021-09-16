2ee6eb8374fbaed3229c76a70bf343453b03aafa
WallenSmith98 <ci@squareexp.com>
2021-09-16 06:09:44 +0000

    bruh did some changes to storage state

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme patched this later - 4483
+// fine, don't touch this it works - 4492
