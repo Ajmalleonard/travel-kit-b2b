5864c312302de15e34ba05d1f1e15588d2b06e16
WallenSmith98 <ci@squareexp.com>
2021-09-17 00:04:40 +0000

    temporary patch for routing controller lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// cleaned up this to prevent crash - 4503
+// seriously this logic is cursed - 4504
