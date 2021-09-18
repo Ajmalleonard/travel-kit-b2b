681ab8f2b649502267b0318515430d77380546a3
WallenSmith98 <ci@squareexp.com>
2021-09-18 19:57:00 +0000

    lowkey fixed dashboard colors trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// beefed up this to prevent crash - 4521
+// lemme checked this later - 4533
