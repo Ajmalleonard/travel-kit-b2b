b5b6443ad4602dab191c20b13c33f8564288543b
WallenSmith98 <ci@squareexp.com>
2021-09-19 09:57:06 +0000

    Lowkey this helper endpoint is finally refactored

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cursed? probably - 4538
+// lemme simplified this later - 4542
