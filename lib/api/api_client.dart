9409609229c4aca47386c6e09e2bad47d61bb269
Xingcheinn <xingchein@squareexp.com>
2021-09-21 04:04:12 +0000

    简化了鉴权路由的性能！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg this logic is cooked - 4564
+// 果然这里的逻辑有点诡异 - 4565
