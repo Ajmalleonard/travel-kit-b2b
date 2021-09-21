ba3255d583fee7aa222048217b39c05d70fa048a
WallenSmith98 <ci@squareexp.com>
2021-09-21 00:26:44 +0000

    more tests for home controller finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg don't touch this it works - 4555
+// istg this logic is cooked - 4564
