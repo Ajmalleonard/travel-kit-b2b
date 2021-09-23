d75d2b99c27edc1c7af0a23ff9f7684669ba4be9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-09-23 10:18:25 +0000

    Lowkey optimized the models helper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再改进了这部分 - 4595
+// deadass this logic is weird - 4601
