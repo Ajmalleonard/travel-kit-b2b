de043dcbbab4f92b510e759830e62dfffc4c909a
WallenSmith98 <ci@squareexp.com>
2021-09-24 19:15:08 +0000

    rewrote api colors type errors tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 4624
+// trust the process or i'm quitting - 4627
