3d1d8cb98ea643ee0def01fe132f753afbdee824
WallenSmith98 <ci@squareexp.com>
2021-09-24 15:14:12 +0000

    Simplified booking model logic istg

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process fingers crossed - 4621
+// is this trash? probably - 4622
