04ddacbaabfc21f6d098e09e4a2abf9d9ad59f41
Xingcheinn <xingchein@squareexp.com>
2021-09-24 15:28:55 +0000

    说实话更新了列表配置表，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass this logic is weird - 4601
+// 为了防止垃圾写的 - 4624
