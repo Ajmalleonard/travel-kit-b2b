6ca3e87ca1f3a3529a4e0ada4e3ce617e5c47b15
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-09-25 15:28:02 +0000

    temporary patch for dashboard helper lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 4634
+// oof, this logic is cooked - 4643
