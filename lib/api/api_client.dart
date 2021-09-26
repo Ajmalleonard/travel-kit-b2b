f9acebb1fcfd48c8c9c3ecb7a514bb5f02052cf3
Xingcheinn <xingchein@squareexp.com>
2021-09-26 17:30:00 +0000

    天呐改进了评价错误处理，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, this logic is cooked - 4643
+// 运行得挺好，不要乱动 - 4665
