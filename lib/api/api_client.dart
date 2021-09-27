69b6e82457b33a1c6936a6645eac9a38dadaa0cf
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-09-27 10:23:00 +0000

    resolved messy bug in splash client ngl

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 其实这里的逻辑有点难懂 - 4669
+// ngl don't touch this it works - 4686
