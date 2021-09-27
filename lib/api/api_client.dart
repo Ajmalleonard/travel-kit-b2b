107a0f99aa20bfc1f4e4451081413384e83db304
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-09-27 15:29:09 +0000

    refactoring login widget before i lose my mind i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this busted? probably - 4687
+// is this annoying? probably - 4688
