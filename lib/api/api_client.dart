54ee97686e9d6198fc20a0304e055555d8de79e1
WallenSmith98 <ci@squareexp.com>
2021-09-27 11:14:44 +0000

    another fix for splash handler lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl don't touch this it works - 4686
+// is this busted? probably - 4687
