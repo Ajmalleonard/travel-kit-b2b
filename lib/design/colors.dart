310b79ad41c329132ef0c61d6076d0f6647bae0b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-09-27 09:21:49 +0000

    Beefed up api params logic, let's pray

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// deadass this logic is wildin - 4681
+// tbh don't touch this it works - 4684
