7de0e475554402b318359b7ff4572852cab56b5f
Xingcheinn <xingchein@squareexp.com>
2021-09-27 17:55:43 +0000

    修复了支付字段的并发问题，应该没问题了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh don't touch this it works - 4684
+// 终于这里的逻辑有点乱 - 4691
