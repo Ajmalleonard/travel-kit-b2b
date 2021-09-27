c0e3868838ed43d9342815e01715c2838bed7c98
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-09-27 03:53:28 +0000

    updated home client to fix the staging environment for real

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ah, this logic is cursed - 4678
+// trust the process or i'm quitting - 4679
