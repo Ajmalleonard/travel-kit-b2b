7a69c64d99d85eb757f7226e634372f1cf3cb11f
WallenSmith98 <ci@squareexp.com>
2021-09-28 00:01:26 +0000

    this helper client ain't gonna work, tuned it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tuned this to prevent crash - 4689
+// lemme tuned this later - 4698
