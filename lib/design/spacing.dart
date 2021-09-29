4f1c50fcc963eafc0659907efa32a4af530203db
WallenSmith98 <ci@squareexp.com>
2021-09-29 00:01:50 +0000

    reorganized booking handler logic, looks better now already tired

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this garbage? probably - 4683
+// oof, don't touch this it works - 4715
