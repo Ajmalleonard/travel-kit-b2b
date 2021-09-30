8ee08a5420972121a309ad8d2b5eec9cb571f885
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-09-30 10:52:12 +0000

    lowkey updated booking typography smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 4729
+// trust the process i think - 4730
