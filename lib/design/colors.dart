8b7b9b230879867abdba5fdbc1b55afd4920bb73
WallenSmith98 <ci@squareexp.com>
2021-09-30 09:50:17 +0000

    Improved theme client performance tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// apparently, this logic is wildin - 4713
+// is this buggy? probably - 4728
