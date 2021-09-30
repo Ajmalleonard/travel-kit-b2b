a51fa1d0deba5402edf6456ffbae28a4f9ec389e
WallenSmith98 <ci@squareexp.com>
2021-09-30 10:29:59 +0000

    temporary patch for splash controller for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// hacked this to prevent crash - 4726
+// optimized this to prevent crash - 4729
