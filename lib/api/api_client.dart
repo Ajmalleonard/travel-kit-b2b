53989e97e30480ffcdc50601a3f9d4e3059f19fe
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-01 01:53:28 +0000

    simplified booking helper logic basically,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well this logic is cursed - 4737
+// seriously don't touch this it works - 4741
