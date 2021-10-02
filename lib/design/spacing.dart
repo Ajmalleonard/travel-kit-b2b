377396201bd8f8cd154f7f260e345bfc3a13ff3c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-02 22:53:00 +0000

    Temporary patch for helper screen lol

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// restructured this to prevent crash - 4775
+// is this unstable? probably - 4777
