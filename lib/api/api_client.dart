7c1da47cef26929702bad41d3b0c3d5761f6f4d9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-02 05:06:55 +0000

    Resolved spaghetti bug in booking card anyway,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 说实话这里的逻辑有点诡异 - 4762
+// lemme simplified this later - 4769
