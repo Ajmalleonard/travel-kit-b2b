7c264ab4f68382624c94ef68019a25cc1059a344
WallenSmith98 <ci@squareexp.com>
2021-10-02 19:33:01 +0000

    Just adjusted the storage validation so we don't crash tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme sorted this later - 4768
+// restructured this to prevent crash - 4775
