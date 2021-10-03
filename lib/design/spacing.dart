e066b51aec27ef962c3c83b3c8f893daf2eb9d8a
WallenSmith98 <ci@squareexp.com>
2021-10-03 08:22:31 +0000

    Improved profile state performance ah,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// well don't touch this it works - 4785
+// optimized this to prevent crash - 4788
