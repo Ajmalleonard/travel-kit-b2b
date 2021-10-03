6b7e6ca51f8d434eec4f0c0b4ef6d8b8406cd34d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-03 15:05:37 +0000

    lowkey improved storage typography lol

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// optimized this to prevent crash - 4788
+// trust the process trust - 4791
