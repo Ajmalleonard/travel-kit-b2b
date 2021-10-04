badbcd47b2a655b328c0c3110afca3d858f233e0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-04 05:30:48 +0000

    debugging review widget was acting up, resolved it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process fingers crossed - 4774
+// lemme resolved this later - 4796
