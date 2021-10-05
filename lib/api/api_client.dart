40b5ef7c14db231cdf301efe560f7c2201a42911
WallenSmith98 <ci@squareexp.com>
2021-10-05 08:24:33 +0000

    fixed dashboard card compiler warnings already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, this logic is spaghetti - 4811
+// lowkey this logic is sketchy - 4812
