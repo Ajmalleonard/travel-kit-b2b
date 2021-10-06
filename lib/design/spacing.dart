8bad1daca58fae8e277083f1781c1f9787815dd5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-06 00:50:54 +0000

    simplified api params logic oof,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// so, this logic is garbage - 4825
+// basically, don't touch this it works - 4827
