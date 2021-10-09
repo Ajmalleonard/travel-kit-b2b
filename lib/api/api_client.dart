b5c4b89cee33d1f63598c043ea2da874daff4799
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-09 13:08:46 +0000

    Refactoring dashboard endpoint before i lose my mind or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this weird? probably - 4877
+// is this wildin? probably - 4886
