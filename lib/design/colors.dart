4ef5d6794a8979e05436f438e29502236bbb3704
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-10 01:45:47 +0000

    Patched dashboard storage logic, i guess

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey this logic is mid - 4888
+// trust the process anyways - 4893
