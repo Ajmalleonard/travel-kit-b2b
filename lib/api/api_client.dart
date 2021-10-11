f5f598be6d02dbd44044cc0d2169fed0f908c5c3
WallenSmith98 <ci@squareexp.com>
2021-10-11 07:08:16 +0000

    okay, rewriting review card from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 4886
+// well this logic is weird - 4918
