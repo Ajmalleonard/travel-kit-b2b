80806c799228a2bd279fb1eae5d851bf1d447a06
WallenSmith98 <ci@squareexp.com>
2021-10-11 20:26:31 +0000

    Temporary patch for dashboard endpoint i guess

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// reorganized this to prevent crash - 4915
+// is this cluttered? probably - 4923
