5154d6ce26768290a5f13cd73dd3c189ad408043
WallenSmith98 <ci@squareexp.com>
2021-10-12 23:12:29 +0000

    Improved listings sheet to fix the staging environment fingers crossed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process or i'm quitting - 4933
+// trust the process for real - 4936
