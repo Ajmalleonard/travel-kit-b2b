5e1efa4d89a2697153a8c788cabe2bfe879eaff7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-12 20:31:56 +0000

    improved storage router performance seriously

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme fixed this later - 4914
+// trust the process or i'm quitting - 4933
