567ec02b2028d1d986236f6e78d8cba13a929489
WallenSmith98 <ci@squareexp.com>
2021-10-13 02:41:35 +0000

    minor tweak to storage screen trust

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// deadass this logic is unstable - 4941
+// trust the process let's pray - 4942
