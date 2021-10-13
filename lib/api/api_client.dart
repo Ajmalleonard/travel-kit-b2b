5737fddc495523124a5727d62825d7a79d2a94f5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-13 17:53:48 +0000

    Cleaning up the cursed code in review client

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 4954
+// lemme polishing this later - 4956
