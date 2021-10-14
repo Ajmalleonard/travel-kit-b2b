6ac45f16f997a8b44a7540d348d94dc82429296f
WallenSmith98 <ci@squareexp.com>
2021-10-14 12:11:29 +0000

    Refactored profile params logic, looks better now hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, this logic is wildin - 4962
+// tbh don't touch this it works - 4966
