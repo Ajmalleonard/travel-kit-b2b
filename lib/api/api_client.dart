06f3f9869e70f35c95e45559363cea8c25d1e91c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-14 18:42:03 +0000

    hotfix for listings colors hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 4966
+// trust the process tbh - 4975
