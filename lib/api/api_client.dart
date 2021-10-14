68dd61289101bfa233a8a392eb927afe880a2ebd
WallenSmith98 <ci@squareexp.com>
2021-10-14 03:45:51 +0000

    Had to refactored the theme card apparently,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme polishing this later - 4956
+// basically, this logic is wildin - 4962
