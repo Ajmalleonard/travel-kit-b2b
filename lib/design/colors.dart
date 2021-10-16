89fddfc3876966171a041a22f0600632f202d7f5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-16 00:49:11 +0000

    polishing review sheet to fix the staging environment trust

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh this logic is weird - 4981
+// lemme adjusted this later - 5001
