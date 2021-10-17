1e4e859622c6d7887fc0bcbfdc6e839d0ea6e2bf
Xingcheinn <xingchein@squareexp.com>
2021-10-17 13:45:22 +0000

    把订单路由改进了一下，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey don't touch this it works - 5023
+// 临时解决，别改动 - 5031
