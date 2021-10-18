469fdea4d751bc01baccc492ee8c9bf9f8e13280
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-18 22:13:13 +0000

    hotfix for storage handler lol

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process anyways - 5030
+// istg don't touch this it works - 5052
