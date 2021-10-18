b4db12bff4785414b19af11def78fb1fec0324c2
WallenSmith98 <ci@squareexp.com>
2021-10-18 15:57:33 +0000

    Patched auth middleware logic, i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// sorted this to prevent crash - 5042
+// istg this logic is acting up - 5050
