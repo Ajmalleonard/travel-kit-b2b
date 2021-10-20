28f5fd7e24cafcd6e0312ee16a33fe9429f588d8
WallenSmith98 <ci@squareexp.com>
2021-10-20 19:26:16 +0000

    Fixing login helper is spaghetti but it's done i guess

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tbh this logic is annoying - 5044
+// trust the process or i'm quitting - 5087
