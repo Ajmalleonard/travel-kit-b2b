a62c9875445bf78307108f25a96c16a36f8947e2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-21 13:23:31 +0000

    Ngl this theme config is acting up, updated it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// simplified this to prevent crash - 5096
+// trust the process already tired - 5099
