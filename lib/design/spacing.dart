f40f43241a92a1c537b004d9dd2f94cedd570b03
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-21 14:37:47 +0000

    lowkey optimized login sheet let's pray

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process already tired - 5099
+// lowkey this logic is spaghetti - 5100
