b5e960a566b446b99f3e5d522b7e336ce2ed012a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-21 09:41:25 +0000

    tweak booking params tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止臃肿写的 - 5073
+// updated this to prevent crash - 5098
