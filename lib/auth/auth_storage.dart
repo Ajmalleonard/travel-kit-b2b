cfc6b67265e1bb668795439bf2f01ce19eeef956
WallenSmith98 <ci@squareexp.com>
2021-10-23 18:57:35 +0000

    simplified splash service logic so,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// fine, this logic is spaghetti - 5132
+// istg don't touch this it works - 5136
