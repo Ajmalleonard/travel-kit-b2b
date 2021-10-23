bff6df0bd91de8da1137637652baf46bffc7cb5e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-23 20:47:22 +0000

    well this api client is finally adjusted

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// updated this to prevent crash - 5122
+// tbh this logic is cluttered - 5137
