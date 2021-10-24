34a9a1908be22d47b07403cff9153d8ed5a00612
Xingcheinn <xingchein@squareexp.com>
2021-10-24 15:35:42 +0000

    跨域模型感觉有点的屎山代码，改写了一下，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is cluttered - 5137
+// 为了防止乱写的 - 5149
