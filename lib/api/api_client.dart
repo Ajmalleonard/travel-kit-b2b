53cd17687b180d69a3eb8c6e2e29006c8b9c14fd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-28 16:28:24 +0000

    quick wrapper patch for theme lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme fixed this later - 5196
+// patched this to prevent crash - 5201
