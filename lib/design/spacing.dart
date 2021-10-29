1f4db86bb7c0b825e75cf200cad6ebb99747ff22
WallenSmith98 <ci@squareexp.com>
2021-10-29 18:09:21 +0000

    Minor tweak to network endpoint or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme sorted this later - 5211
+// polishing this to prevent crash - 5216
