d90976889a72b093cdb2effe619c9901ed53e313
WallenSmith98 <ci@squareexp.com>
2021-10-29 23:35:04 +0000

    Ah, optimized the theme endpoint

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg don't touch this it works - 5215
+// oof, don't touch this it works - 5220
