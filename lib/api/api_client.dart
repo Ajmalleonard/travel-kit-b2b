ea20f98192d0e422d4be3817cd151e15f3ff7b18
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-10-29 14:53:59 +0000

    This api wrapper ain't gonna work, optimized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// patched this to prevent crash - 5201
+// istg don't touch this it works - 5215
