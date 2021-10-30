c3797f654842c32bd9844d6a3c8fc259899c6911
WallenSmith98 <ci@squareexp.com>
2021-10-30 11:27:34 +0000

    had to sorted the api screen oof,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this trash? probably - 5226
+// tbh this logic is weird - 5227
