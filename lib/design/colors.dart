57929104fed04603f016b47a75da77df8e47f3dd
Xingcheinn <xingchein@squareexp.com>
2021-10-30 12:50:10 +0000

    救命，重构了接口模块的隐藏逻辑...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// basically, this logic is mid - 5209
+// 为了防止垃圾写的 - 5231
