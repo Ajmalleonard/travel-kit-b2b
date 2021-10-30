40dec52c499ffb10b5f51f71f6cb25bd147fecc8
WallenSmith98 <ci@squareexp.com>
2021-10-30 09:39:39 +0000

    Checked review logic logic, looks better now i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, don't touch this it works - 5223
+// is this trash? probably - 5226
