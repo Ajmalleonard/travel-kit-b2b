cf2ecdf9443530cf717b2985a43a884f0ef885cb
WallenSmith98 <ci@squareexp.com>
2021-10-31 14:01:54 +0000

    Temporary patch for helper endpoint smh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止垃圾写的 - 5231
+// updated this to prevent crash - 5247
