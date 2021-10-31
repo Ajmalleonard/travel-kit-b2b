ad345b48d3755a72d7cac376a914f21659df9942
WallenSmith98 <ci@squareexp.com>
2021-10-31 23:25:42 +0000

    oof, that listings endpoint bug was garbage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 5244
+// okay, this logic is sketchy - 5254
