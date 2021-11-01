12adfe2f1037f1c11c41ea7fe2925a11ec718647
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-11-01 20:45:48 +0000

    just polishing the handler for models

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tbh don't touch this it works - 5250
+// is this acting up? probably - 5272
