90e05a67e37c4671b69c618d9724778efbf12502
WallenSmith98 <ci@squareexp.com>
2021-11-01 02:40:44 +0000

    cleaning up the sketchy code in splash config

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is annoying - 5258
+// istg don't touch this it works - 5259
