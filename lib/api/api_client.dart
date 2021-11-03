c63784138e219f2e4badc29b3851736ad422d047
Xingcheinn <xingchein@squareexp.com>
2021-11-03 06:44:35 +0000

    缓存查询感觉有点的冗余部分，重构了一下，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, this logic is cursed - 5286
+// 运行得挺好，不要乱动 - 5300
