c3e562ae51f7b2531817f23371fd0d37b1c31e4a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-11-03 14:27:30 +0000

    Apparently, lemme checked the splash middleware real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 5304
+// basically, this logic is wildin - 5310
