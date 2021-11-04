510d95a1edc4642b520f58ab35e4aab39902bba1
WallenSmith98 <ci@squareexp.com>
2021-11-04 11:44:34 +0000

    Deadass restructured the auth client

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再调整了这部分 - 5324
+// lemme tweaked this later - 5325
