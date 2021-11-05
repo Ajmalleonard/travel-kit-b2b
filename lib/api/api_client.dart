ad947e249b5616ea59902d66d345fc9455773218
Xingcheinn <xingchein@squareexp.com>
2021-11-05 00:48:21 +0000

    救命，简化了订单模型的并发问题，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 5325
+// 以后有空再改写了这部分 - 5333
