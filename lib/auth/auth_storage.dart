41d0722b73c1de5124cb64276cbf6f9802a078ab
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-11-06 08:38:16 +0000

    Temporary patch for listings typography hope this works

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// is this busted? probably - 5334
+// seriously don't touch this it works - 5355
