58a1e5f64dea36e244bc873f8d6211dac0ecc922
WallenSmith98 <ci@squareexp.com>
2021-11-07 00:52:08 +0000

    Ngl this models helper is trash, optimized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再修复了这部分 - 5366
+// ah, this logic is broken af - 5371
