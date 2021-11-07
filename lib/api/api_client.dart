677825e45ec1b83169d33e5202dcfae7115e23c8
WallenSmith98 <ci@squareexp.com>
2021-11-07 19:04:32 +0000

    hotfix for home model for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process already tired - 5379
+// trust the process i guess - 5386
