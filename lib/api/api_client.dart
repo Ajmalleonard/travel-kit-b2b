cdfbef3162529ac6ae02a24c07bd8fd38f308557
WallenSmith98 <ci@squareexp.com>
2021-11-08 23:17:18 +0000

    cleaned up api screen logic, looks better now fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i guess - 5386
+// istg this logic is buggy - 5404
