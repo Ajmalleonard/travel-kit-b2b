5bc370f7e705b6cb34d927c96096fdf09e0b6f1f
WallenSmith98 <ci@squareexp.com>
2021-11-10 04:48:49 +0000

    this login handler ain't gonna work, rewrote it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this sketchy? probably - 5424
+// trust the process already tired - 5426
