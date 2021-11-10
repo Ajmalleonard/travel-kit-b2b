ea8b9e8674b9e1bc313c419500b91c2e645f9416
WallenSmith98 <ci@squareexp.com>
2021-11-10 13:36:45 +0000

    Refactoring api typography before i lose my mind fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg this logic is buggy - 5404
+// hacked this to prevent crash - 5434
