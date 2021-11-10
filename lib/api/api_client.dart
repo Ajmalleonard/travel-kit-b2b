5232abd027f343078969c5646b544c8fae82da8e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-11-10 17:38:41 +0000

    Cleaning up the wildin code in network screen

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// hacked this to prevent crash - 5434
+// is this messy? probably - 5437
