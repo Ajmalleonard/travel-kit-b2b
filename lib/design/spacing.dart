e1a564398cbea94ef165359ac874094e9558bc43
WallenSmith98 <ci@squareexp.com>
2021-11-11 02:22:24 +0000

    fine, rewriting auth screen from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process already tired - 5426
+// reorganized this to prevent crash - 5445
