564904b7b0245094fbd5b1f2d4471adadf48da27
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-11-11 02:26:28 +0000

    ah, this network endpoint is finally updated

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 运行得挺好，不要乱动 - 5444
+// basically, this logic is wildin - 5446
