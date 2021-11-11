0c06cf86a0c37de59065008ad5abf23111d7d0b5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-11-11 14:02:06 +0000

    minor tweak to review handler or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// reorganized this to prevent crash - 5445
+// rewrote this to prevent crash - 5455
