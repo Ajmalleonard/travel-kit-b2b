b2e109be19451854d9684151626755e2e12bc756
Xingcheinn <xingchein@squareexp.com>
2021-11-12 02:57:28 +0000

    把交通配置表改进了一下...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process hope this works - 5465
+// 为了防止垃圾写的 - 5467
