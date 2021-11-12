e2457a335c2587dbfad1b4a0541da9ddaa99e809
Xingcheinn <xingchein@squareexp.com>
2021-11-12 19:03:50 +0000

    天呐，优化了校验逻辑的并发问题，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh this logic is annoying - 5450
+// 运行得挺好，不要乱动 - 5477
