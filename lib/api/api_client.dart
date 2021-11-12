b56b608aef4b233169d8e72bf71ae013f6ad58b6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-11-12 01:35:25 +0000

    Deadass improved the network card

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 5458
+// beefed up this to prevent crash - 5464
