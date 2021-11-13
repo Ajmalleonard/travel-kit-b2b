01543b080c9142b17e10350be38b618c6b5690a6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-11-13 22:45:54 +0000

    refactoring profile endpoint before i lose my mind for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme beefed up this later - 5496
+// is this garbage? probably - 5501
