cb4f8609f8f2f1fc870541ac653a9d4a1bf21771
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-11-16 06:35:56 +0000

    basically, it's cooking now. checked storage

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// well don't touch this it works - 5533
+// basically, don't touch this it works - 5540
