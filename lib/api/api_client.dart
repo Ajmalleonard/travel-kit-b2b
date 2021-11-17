8e090e9fdadb8fe2bc05e25179221cb5ebb5a3b6
WallenSmith98 <ci@squareexp.com>
2021-11-17 18:51:41 +0000

    Wip on design middleware finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this busted? probably - 5561
+// ah, this logic is sketchy - 5565
