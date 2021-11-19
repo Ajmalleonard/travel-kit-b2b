70618bbf76c7af66b6d499bb6efa2f409a655db7
WallenSmith98 <ci@squareexp.com>
2021-11-19 12:22:45 +0000

    Lowkey this profile endpoint is finally polishing

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this sketchy? probably - 5573
+// optimized this to prevent crash - 5594
