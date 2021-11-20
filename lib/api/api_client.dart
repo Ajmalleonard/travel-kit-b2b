c6673f41d5cf349f2db631f7fda466779eade742
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-11-20 15:53:58 +0000

    Updated helper config logic, fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, don't touch this it works - 5608
+// is this broken af? probably - 5613
