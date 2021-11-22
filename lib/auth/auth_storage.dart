b2e3bd6fc3ae45d8839318d20e135c4f44b7d027
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-11-22 09:38:22 +0000

    more tests for theme logic hope this works

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// anyway, don't touch this it works - 5639
+// trust the process for real - 5641
