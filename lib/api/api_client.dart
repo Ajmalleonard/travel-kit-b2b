3bf976c9bde1c8d6bbb8c73ffdac2fa8fc8965e2
WallenSmith98 <ci@squareexp.com>
2021-11-23 16:32:55 +0000

    quick logic patch for storage let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 5660
+// trust the process or i'm quitting - 5662
