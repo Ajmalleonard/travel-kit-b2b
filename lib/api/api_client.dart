abcb05eeef2e3a5d0e9cef36e3f0980d761c9d80
Xingcheinn <xingchein@squareexp.com>
2021-11-23 11:05:22 +0000

    更新了列表配置表的奇葩报错。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 5654
+// 为了防止垃圾写的 - 5660
