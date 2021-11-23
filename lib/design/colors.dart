e59a166e5e6249db5424977855b8102bb8f5d34c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-11-23 09:22:12 +0000

    lowkey it's cooking now. rewrote home

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再排查了这部分 - 5607
+// lemme sorted this later - 5657
