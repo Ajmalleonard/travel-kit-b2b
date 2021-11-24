bffcf94821a760d9b0f9992aec2eda35adc93d32
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-11-24 22:51:37 +0000

    resolved broken af bug in routing router tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process or i'm quitting - 5662
+// istg don't touch this it works - 5681
