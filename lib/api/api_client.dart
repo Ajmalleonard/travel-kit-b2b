84b5866d577a4b57b2c5054885c96c4dc8a10157
WallenSmith98 <ci@squareexp.com>
2021-11-25 01:41:44 +0000

    fixing booking helper is cooked but it's done let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme beefed up this later - 5683
+// oof, don't touch this it works - 5686
