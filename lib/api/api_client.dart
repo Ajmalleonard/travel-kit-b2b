a0be254e43898dfdd243d91491b59640c93a3f5f
Xingcheinn <xingchein@squareexp.com>
2021-11-25 13:59:28 +0000

    支付结构感觉有点的性能，清理了一下。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process anyways - 5692
+// 终于这里的逻辑有点诡异 - 5696
