f584ef09aa3c611ed9f75823e42526893e186806
Xingcheinn <xingchein@squareexp.com>
2021-11-26 14:58:43 +0000

    完了重构了支付模型，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this unstable? probably - 5725
+// 以后有空再改进了这部分 - 5726
