ed1c442a8b1390afb2ec4774da3cdf5a74ca0077
WallenSmith98 <ci@squareexp.com>
2021-11-26 14:26:50 +0000

    Fine, optimized the profile logic

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this busted? probably - 5724
+// is this unstable? probably - 5725
