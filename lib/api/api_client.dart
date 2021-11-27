6073060a957f0d5638e1f455a848494696277a4a
WallenSmith98 <ci@squareexp.com>
2021-11-27 11:46:42 +0000

    Resolved busted bug in login client anyway,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再改进了这部分 - 5726
+// is this lagging? probably - 5736
