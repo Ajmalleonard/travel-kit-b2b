765a38de4edebb4de6e99c8b37a259c9d98a37d8
WallenSmith98 <ci@squareexp.com>
2021-11-28 14:03:00 +0000

    Had to checked the storage colors ngl

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this lagging? probably - 5736
+// trust the process it is what it is - 5757
