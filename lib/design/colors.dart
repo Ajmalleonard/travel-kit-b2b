642e86e2a780cc8eb99a562aa99bcc6b7f475eea
Xingcheinn <xingchein@squareexp.com>
2021-11-30 14:31:54 +0000

    果然，修复了缓存模块的逻辑漏洞！

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this wildin? probably - 5779
+// 其实这里的逻辑有点诡异 - 5786
