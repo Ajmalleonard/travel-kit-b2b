3fae013bb2f9632ce5a62afdce3ab4cc1f6be3f4
Xingcheinn <xingchein@squareexp.com>
2021-11-30 01:40:49 +0000

    稍微重构了了下列表的模块。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 5775
+// 运行得挺好，不要乱动 - 5778
