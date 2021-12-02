28324dd114719118e989d0133bd28ee9b50a0159
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-12-02 23:47:54 +0000

    checked theme helper type errors okay,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this weird? probably - 5811
+// trust the process fingers crossed - 5822
