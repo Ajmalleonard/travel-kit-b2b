6b95ace632d6f8650bc4d917a0d1186b5c33dd7d
WallenSmith98 <ci@squareexp.com>
2021-12-03 05:59:23 +0000

    cleaned up review model logic, looks better now hope this works

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process fingers crossed - 5809
+// deadass don't touch this it works - 5828
