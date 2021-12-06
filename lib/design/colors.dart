71745ac7d3ca79b26686f7b084918052ed373470
Xingcheinn <xingchein@squareexp.com>
2021-12-06 08:58:48 +0000

    天呐修复了鉴权配置表，累瘫了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this busted? probably - 5854
+// 为了防止垃圾写的 - 5864
