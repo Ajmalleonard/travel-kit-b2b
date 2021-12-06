7e092be81962b0928c95bb72bf95cb95ba3642ae
WallenSmith98 <ci@squareexp.com>
2021-12-06 06:20:32 +0000

    another fix for login endpoint tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, this logic is messy - 5841
+// trust the process or i'm quitting - 5863
