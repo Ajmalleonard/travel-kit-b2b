2dc01ad99398d5be57b0ce734803518e30b0fbbf
WallenSmith98 <ci@squareexp.com>
2021-12-07 14:01:32 +0000

    lowkey sorted helper widget tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// cleaned up this to prevent crash - 5878
+// well don't touch this it works - 5879
