a006aea6f835572df319fc158f32cee189ea6ee1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-12-10 16:53:11 +0000

    tbh optimized the profile validation

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl this logic is busted - 5922
+// is this annoying? probably - 5932
