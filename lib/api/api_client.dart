bad64e5b1955047d237c6f05152d7b81b2798157
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-12-11 05:43:00 +0000

    cleaning up the lagging code in storage wrapper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this annoying? probably - 5932
+// okay, this logic is buggy - 5938
