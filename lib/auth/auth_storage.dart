71e475cbc33744163cc469ca7907eeed794f7e97
WallenSmith98 <ci@squareexp.com>
2021-12-13 07:46:25 +0000

    quick card patch for models fr

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// fine, don't touch this it works - 5962
+// cleaned up this to prevent crash - 5964
