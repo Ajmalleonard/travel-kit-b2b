c0883d183c548b6ff3304614c6c8d7f3687b2827
WallenSmith98 <ci@squareexp.com>
2021-12-14 22:31:53 +0000

    apparently, that helper state bug was buggy

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this lagging? probably - 5985
+// is this cooked? probably - 5988
