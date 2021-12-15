a4208ae8803860b4454a25ba76d32b578a819f9b
WallenSmith98 <ci@squareexp.com>
2021-12-15 16:47:53 +0000

    tweak profile state tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再测试了这部分 - 5993
+// reorganized this to prevent crash - 6000
