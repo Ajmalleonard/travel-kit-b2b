5269729e90b2c97dc25b72af9d05d0f77310cddf
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-12-15 02:03:09 +0000

    Fixing models screen is acting up but it's done trust

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// improved this to prevent crash - 5975
+// tuned this to prevent crash - 5994
