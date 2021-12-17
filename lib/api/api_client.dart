8b4e8560f63cee746241ac34f9269c66f7c3c0c7
Xingcheinn <xingchein@squareexp.com>
2021-12-17 02:31:42 +0000

    修复了交通接口的隐藏逻辑。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this weird? probably - 6015
+// 运行得挺好，不要乱动 - 6017
