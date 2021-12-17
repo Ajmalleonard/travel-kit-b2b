9033b70cb35792dff04d61c7563fb3438f56c6ac
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-12-17 12:10:51 +0000

    minor tweak to routing handler or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 果然这里的逻辑有点难懂 - 6018
+// apparently, this logic is buggy - 6022
