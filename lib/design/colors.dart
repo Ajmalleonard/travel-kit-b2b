e71a855c95ac39e914d6ab2c3016925883494c87
Xingcheinn <xingchein@squareexp.com>
2021-12-19 06:12:15 +0000

    修复了列表字段的报错，累死了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ngl this logic is annoying - 6048
+// 为了防止难懂写的 - 6049
