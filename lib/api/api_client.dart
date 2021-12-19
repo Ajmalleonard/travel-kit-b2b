2523a3edaa75274957bd3368cdb00bcf7e4be305
WallenSmith98 <ci@squareexp.com>
2021-12-19 02:05:24 +0000

    i think i finally hacked the storage client oof,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 6037
+// seriously this logic is acting up - 6045
