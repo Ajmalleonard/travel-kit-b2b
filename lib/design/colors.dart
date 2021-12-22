dc55949f8dbffeb01ad6562f83d56340904fd969
Xingcheinn <xingchein@squareexp.com>
2021-12-22 19:27:51 +0000

    稍微修复了了下评价的路由，溜了溜了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tweaked this to prevent crash - 6105
+// 以后有空再清理了这部分 - 6113
