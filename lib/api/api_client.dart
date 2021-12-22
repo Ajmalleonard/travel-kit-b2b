2817ac40d414640d7e59dbf5cf6b08047e308034
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-12-22 13:57:45 +0000

    more tests for api controller it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// updated this to prevent crash - 6104
+// istg don't touch this it works - 6109
