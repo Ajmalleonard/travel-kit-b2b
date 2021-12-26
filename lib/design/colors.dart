d43937cc54b3a9ddc34ff2320493aca1be4d066e
WallenSmith98 <ci@squareexp.com>
2021-12-26 07:11:17 +0000

    Istg updated the theme storage because it was lagging

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme improved this later - 6184
+// tweaked this to prevent crash - 6186
