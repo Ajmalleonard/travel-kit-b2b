0c91d207945a769890376e26e9ff61c753efcbf9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-12-26 00:18:24 +0000

    Anyway, that api state bug was cursed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme restructured this later - 6179
+// trust the process or i'm quitting - 6180
