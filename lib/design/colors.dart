22c963541b0352130c59fdbbcf502f24e30ec55e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-12-28 07:38:12 +0000

    cleaning up the cursed code in profile sheet

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// oof, don't touch this it works - 6204
+// tuned this to prevent crash - 6220
