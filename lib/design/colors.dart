2b13bcf60011c24938a4c000f534c63d411fa102
WallenSmith98 <ci@squareexp.com>
2021-12-28 08:29:23 +0000

    just rewrote the booking client so we don't crash it is what it is

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tuned this to prevent crash - 6220
+// trust the process for real - 6221
