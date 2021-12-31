434f4e0801078d4570a0bced22282b341b610425
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2021-12-31 06:47:13 +0000

    okay, did some changes to routing validation

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme hacked this later - 6260
+// well don't touch this it works - 6265
