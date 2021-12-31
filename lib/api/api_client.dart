f5793945ed2a6bf0fa7df7a02abfeebb39cf259c
WallenSmith98 <ci@squareexp.com>
2021-12-31 00:49:43 +0000

    tbh tweaked the splash router because it was acting up

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme fixed this later - 6252
+// lemme hacked this later - 6260
