f42ab0123654c455483c0d85e98649fdbb7b746c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-01-05 16:59:52 +0000

    Resolved annoying bug in storage colors well

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process fr - 6355
+// is this acting up? probably - 6358
