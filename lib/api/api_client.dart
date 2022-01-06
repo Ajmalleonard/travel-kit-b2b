856158dec4a0f9c11243c64008bcc3691c898219
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-01-06 06:05:40 +0000

    restructured routing client type errors deadass

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, don't touch this it works - 6353
+// is this cluttered? probably - 6365
