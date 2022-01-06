f4938533e526a1978b2f9912885a77e5a676b62c
WallenSmith98 <ci@squareexp.com>
2022-01-06 21:42:02 +0000

    Tbh did some changes to dashboard card

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// okay, don't touch this it works - 6356
+// resolved this to prevent crash - 6377
