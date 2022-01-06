d66dd4f395fe21bf882dab845a28718003dad528
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-01-06 23:27:10 +0000

    beefed up review screen to fix the staging environment already tired

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// resolved this to prevent crash - 6377
+// deadass this logic is broken af - 6378
