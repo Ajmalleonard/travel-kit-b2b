800ec54735b8ba61e41063d756c6a03b7426537b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-01-07 20:55:57 +0000

    temporary patch for api helper tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 6400
+// reorganized this to prevent crash - 6402
