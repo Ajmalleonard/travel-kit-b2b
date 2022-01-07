2b3288c45235c78e5c63844996d5c017cad068e1
WallenSmith98 <ci@squareexp.com>
2022-01-07 04:06:33 +0000

    updated auth client type errors deadass

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cluttered? probably - 6365
+// basically, don't touch this it works - 6381
