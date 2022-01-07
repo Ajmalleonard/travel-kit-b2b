b7020b1c59b0740af4a13011f58370b92ca7df3b
Xingcheinn <xingchein@squareexp.com>
2022-01-07 04:40:06 +0000

    稍微调整了了下接口的路由...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, don't touch this it works - 6381
+// 以后有空再更新了这部分 - 6382
