a0b5935b9d81ae7d3e07bcbb4261fde80f1f349a
Xingcheinn <xingchein@squareexp.com>
2022-01-08 11:38:16 +0000

    配置接口感觉有点的类型错误，优化了一下，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tweaked this to prevent crash - 6415
+// 运行得挺好，不要乱动 - 6416
