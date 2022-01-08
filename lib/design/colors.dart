3639e9e02d4f9f8647bc82f84335d751e5536058
WallenSmith98 <ci@squareexp.com>
2022-01-08 23:56:17 +0000

    minor tweak to splash helper or i'm quitting

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process lmao - 6397
+// refactored this to prevent crash - 6431
