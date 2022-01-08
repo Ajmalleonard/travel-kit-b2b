7d9b5154d831779211442afb1f1d2fcbfbed6cd8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-01-08 17:39:07 +0000

    simplified network service logic ah,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process for real - 6420
+// trust the process hope this works - 6425
