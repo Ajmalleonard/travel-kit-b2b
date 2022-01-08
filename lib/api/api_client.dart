b329067fcac1dfbf71b2ffe201137c8115346b2a
WallenSmith98 <ci@squareexp.com>
2022-01-08 15:37:37 +0000

    resolved home logic logic, looks better now hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, this logic is cluttered - 6422
+// hacked this to prevent crash - 6423
