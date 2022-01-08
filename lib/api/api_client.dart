3fec0899559b3a98d221c75274ecc582714286a4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-01-08 17:53:08 +0000

    Lowkey tuned review storage it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// hacked this to prevent crash - 6423
+// lemme cleaned up this later - 6426
