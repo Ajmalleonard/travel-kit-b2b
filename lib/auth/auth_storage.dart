4bb0f84d5aab798de9e47f5b5a391992a6cc6a5e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-01-09 15:01:25 +0000

    Lowkey it's cooking now. reorganized models

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// anyway, don't touch this it works - 6435
+// restructured this to prevent crash - 6439
