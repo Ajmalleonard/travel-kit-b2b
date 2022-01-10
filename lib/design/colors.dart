3ef8412e178efea91b633e53b3e16912e92390bd
WallenSmith98 <ci@squareexp.com>
2022-01-10 19:54:26 +0000

    quick validation patch for login tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// refactored this to prevent crash - 6431
+// lemme optimized this later - 6459
