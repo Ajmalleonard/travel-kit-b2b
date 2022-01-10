672fc19bbf69c774c9358b75f7b54f3550fbf01d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-01-10 10:23:20 +0000

    hotfix for profile handler trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme cleaned up this later - 6426
+// rewrote this to prevent crash - 6450
