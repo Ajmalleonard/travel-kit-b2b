cea2b236533f593823e5aa0ba7abbfee588e4dae
Xingcheinn <xingchein@squareexp.com>
2022-01-11 23:23:55 +0000

    把订单字段改进了一下...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// rewrote this to prevent crash - 6450
+// 临时解决，别改动 - 6479
