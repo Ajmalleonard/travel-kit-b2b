f4b3e35276c0147e9d43f2d67a40902b325fe4b2
WallenSmith98 <ci@squareexp.com>
2022-01-13 14:19:44 +0000

    i think i finally fixed the auth client oof,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i guess - 6491
+// trust the process lol - 6502
