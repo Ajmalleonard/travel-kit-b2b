92cb96f2a3f17db9d271141fb75cd32bdf5d6499
WallenSmith98 <ci@squareexp.com>
2022-01-13 20:09:39 +0000

    tweak home logic anyway,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process let's pray - 6504
+// okay, don't touch this it works - 6505
