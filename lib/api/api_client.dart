3d3865e319af9f15794e3bf143b4468ae0c712da
WallenSmith98 <ci@squareexp.com>
2022-01-14 02:16:05 +0000

    tbh rewriting listings router from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lol - 6502
+// basically, don't touch this it works - 6508
