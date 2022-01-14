bc32a9b751b7682658d8b1b1e01253236846486e
WallenSmith98 <ci@squareexp.com>
2022-01-14 10:18:37 +0000

    deadass hacked the dashboard wrapper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, don't touch this it works - 6508
+// lemme optimized this later - 6513
