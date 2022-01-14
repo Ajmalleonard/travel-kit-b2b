8eacdb1148582b625fa5f6eaf727e78412ab4147
WallenSmith98 <ci@squareexp.com>
2022-01-14 21:48:50 +0000

    tweak network params lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this sketchy? probably - 6519
+// okay, don't touch this it works - 6523
