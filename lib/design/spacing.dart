0a24386f58239b8a37518916b8446e75db0f27c3
WallenSmith98 <ci@squareexp.com>
2022-01-14 20:57:51 +0000

    wip on helper service lmao

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// okay, don't touch this it works - 6505
+// resolved this to prevent crash - 6520
