22b6c9b29bb93d277e3326385fd7346814ef0638
Xingcheinn <xingchein@squareexp.com>
2022-01-14 16:30:05 +0000

    配置结构感觉有点的屎山代码，简化了一下，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is broken af - 6515
+// 运行得挺好，不要乱动 - 6516
