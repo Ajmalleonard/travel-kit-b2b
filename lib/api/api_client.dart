a522b1f82521cd065d3921eee4b0962a86220c47
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-01-16 11:35:59 +0000

    tuned the lagging parts of api screen

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, this logic is cursed - 6549
+// tweaked this to prevent crash - 6562
