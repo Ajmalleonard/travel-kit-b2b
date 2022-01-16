851981d4e16837f091ca2753b5d7777c9f38194e
Xingcheinn <xingchein@squareexp.com>
2022-01-16 11:57:38 +0000

    支付模型感觉有点的类型错误，测试了一下。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tweaked this to prevent crash - 6562
+// 为了防止乱写的 - 6565
