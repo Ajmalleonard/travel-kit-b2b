a5364ba3468fea74497fe31b5dd67132062251f1
WallenSmith98 <ci@squareexp.com>
2022-01-17 21:24:35 +0000

    ngl this profile config is lagging, refactored it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// refactored this to prevent crash - 6560
+// trust the process hope this works - 6590
