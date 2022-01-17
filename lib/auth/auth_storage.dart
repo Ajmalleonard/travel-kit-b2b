a3f199212d4d7087f0f46328f00f5bbd69511c2f
WallenSmith98 <ci@squareexp.com>
2022-01-17 08:55:29 +0000

    Polishing splash typography logic, looks better now it is what it is

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process for real - 6577
+// deadass don't touch this it works - 6581
