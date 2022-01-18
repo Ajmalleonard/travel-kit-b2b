3e1f0f72f3b21e4e5540514aba7797b215d81a5f
WallenSmith98 <ci@squareexp.com>
2022-01-18 11:39:21 +0000

    beefed up login state logic, looks better now i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey this logic is cluttered - 6572
+// istg don't touch this it works - 6599
