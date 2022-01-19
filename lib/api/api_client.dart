b1cd85fdf5decc0024787b11ad2f61f4e7bdc9b0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-01-19 23:54:48 +0000

    beefed up the broken af parts of splash controller

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass don't touch this it works - 6621
+// beefed up this to prevent crash - 6625
