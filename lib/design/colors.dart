7f6621be800a4e2c312bf506e5c880f72524766d
WallenSmith98 <ci@squareexp.com>
2022-01-19 00:40:18 +0000

    Tbh it's cooking now. polishing api

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 临时解决，别改动 - 6598
+// trust the process hope this works - 6608
