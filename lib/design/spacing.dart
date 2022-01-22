7bfd60d470f1f780d96490645c2b123430ba6e39
WallenSmith98 <ci@squareexp.com>
2022-01-22 22:55:55 +0000

    lowkey improved routing widget smh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// resolved this to prevent crash - 6671
+// trust the process finally - 6673
