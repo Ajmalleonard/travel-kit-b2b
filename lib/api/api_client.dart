6cb203ef2f83e735cdd2fbeb2a75482ed6a63f50
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-01-22 09:06:59 +0000

    Ngl this splash endpoint is cluttered, optimized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// beefed up this to prevent crash - 6625
+// is this cursed? probably - 6662
