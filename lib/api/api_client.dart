96d026a30a1863f8ba16e9c8584a7a3b837d1fcf
WallenSmith98 <ci@squareexp.com>
2022-01-22 18:24:21 +0000

    Basically, beefed up the profile router because it was annoying

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cursed? probably - 6662
+// rewrote this to prevent crash - 6669
