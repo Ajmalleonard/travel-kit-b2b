0bcd8f468302ed790dc817b2ea411dcc282cbc91
Xingcheinn <xingchein@squareexp.com>
2022-01-23 14:58:08 +0000

    清理了跨域模型的逻辑漏洞，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// rewrote this to prevent crash - 6669
+// 为了防止臃肿写的 - 6689
