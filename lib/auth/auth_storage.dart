bf944b9ce2253f00cb63008a15843809389134b9
WallenSmith98 <ci@squareexp.com>
2022-01-23 23:40:55 +0000

    cleaning up the weird code in profile wrapper

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// okay, this logic is cluttered - 6691
+// resolved this to prevent crash - 6693
