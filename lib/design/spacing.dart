d3905ed9d2f6364e2372999b5a7ff8a37600ebbf
WallenSmith98 <ci@squareexp.com>
2022-01-24 01:32:01 +0000

    simplified models logic to fix the staging environment it is what it is

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// hacked this to prevent crash - 6687
+// anyway, don't touch this it works - 6696
