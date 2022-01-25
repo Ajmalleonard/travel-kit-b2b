6e3fb86e83118c25ce48575eeb547da3383e429b
Xingcheinn <xingchein@squareexp.com>
2022-01-25 17:16:17 +0000

    说实话更新了配置错误处理，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// oof, this logic is garbage - 6716
+// 为了防止臃肿写的 - 6727
