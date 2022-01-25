19b6f74332252500cb0f172181afc3ddd2c0b902
Xingcheinn <xingchein@squareexp.com>
2022-01-25 00:48:41 +0000

    说实话优化了交通模块，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// updated this to prevent crash - 6709
+// 以后有空再测试了这部分 - 6711
