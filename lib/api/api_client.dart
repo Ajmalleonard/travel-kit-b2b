9f8836fd1fc298b4752b4cb4140686f27cba6394
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-01-25 13:41:08 +0000

    simplified routing config logic deadass

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this messy? probably - 6719
+// lemme sorted this later - 6724
