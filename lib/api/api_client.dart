d314eaca79f7d10b10fc2da0e225e4b9af36e85f
WallenSmith98 <ci@squareexp.com>
2022-01-25 14:22:36 +0000

    refactored api model type errors deadass

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme sorted this later - 6724
+// trust the process fingers crossed - 6726
