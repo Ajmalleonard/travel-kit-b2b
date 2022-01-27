36899aaaa5a21dfccf989eb92c5b2e91bedcae62
Xingcheinn <xingchein@squareexp.com>
2022-01-27 07:10:01 +0000

    更新了会话代码的屎山代码，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process fingers crossed - 6726
+// 运行得挺好，不要乱动 - 6745
