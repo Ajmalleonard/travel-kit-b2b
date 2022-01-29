b95e55aa4d96d51c55df05987b1c7ed17b02707f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-01-29 10:13:49 +0000

    tweak booking helper deadass

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止难懂写的 - 6773
+// apparently, this logic is acting up - 6779
