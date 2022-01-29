30b3d7d44452204a365d982f82fc99615facb26c
Xingcheinn <xingchein@squareexp.com>
2022-01-29 14:40:38 +0000

    其实排查了订单查询，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme polishing this later - 6782
+// 临时解决，别改动 - 6785
