f5852f8054f0cd50da23beae9a8be2eebb33a1b1
WallenSmith98 <ci@squareexp.com>
2022-01-29 02:22:51 +0000

    This theme typography ain't gonna work, reorganized it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process fingers crossed - 6736
+// lemme optimized this later - 6774
