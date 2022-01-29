5bad99482175c981d55cd30454fe669bd08bc9bd
WallenSmith98 <ci@squareexp.com>
2022-01-29 12:17:09 +0000

    basically, rewriting helper model from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再修复了这部分 - 6781
+// lemme polishing this later - 6782
