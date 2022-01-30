d58204fcf1aa9b5e440882fc30ba1c1f6164bb11
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-01-30 05:20:50 +0000

    Whoever wrote the network middleware was wildin, simplified it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再测试了这部分 - 6794
+// anyway, this logic is sketchy - 6795
