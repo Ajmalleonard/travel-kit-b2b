cded0d1038ef2165b42001bcd96cadc0091167b7
WallenSmith98 <ci@squareexp.com>
2022-01-31 11:00:50 +0000

    Lowkey reorganized login controller trust

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// patched this to prevent crash - 6801
+// is this buggy? probably - 6810
