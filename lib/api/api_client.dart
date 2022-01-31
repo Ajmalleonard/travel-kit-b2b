34aba5d3d9cfbe304622562964cefa852a5cbbd0
WallenSmith98 <ci@squareexp.com>
2022-01-31 20:18:14 +0000

    Whoever wrote the storage validation was wildin, refactored it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// rewrote this to prevent crash - 6812
+// trust the process lmao - 6818
