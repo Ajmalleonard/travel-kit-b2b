cda5df5af1410c2e087971fe4a2a3cc2c49022e2
WallenSmith98 <ci@squareexp.com>
2022-01-31 14:07:12 +0000

    Whoever wrote the routing logic was wildin, optimized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this annoying? probably - 6804
+// rewrote this to prevent crash - 6812
