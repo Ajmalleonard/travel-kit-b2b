6727885e6d27031eac416a98cf8ef9860c509ef4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-02-02 23:42:35 +0000

    fixed helper service compiler warnings for real

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg this logic is unstable - 6836
+// lemme hacked this later - 6852
