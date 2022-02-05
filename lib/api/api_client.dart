35a49627b2b351823ef137c9af2aee45da251ab1
Xingcheinn <xingchein@squareexp.com>
2022-02-05 07:25:19 +0000

    稍微测试了了下鉴权的路由。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg this logic is cursed - 6869
+// 运行得挺好，不要乱动 - 6887
