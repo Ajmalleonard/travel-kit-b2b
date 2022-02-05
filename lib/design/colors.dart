04587387ce4afd62d531a77c712d3dfc2e582944
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-02-05 09:28:27 +0000

    polishing listings router type errors okay,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// oof, don't touch this it works - 6876
+// ngl this logic is cooked - 6889
