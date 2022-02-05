feae2808164773601d40c2cceb4e576a856e97e8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-02-05 11:02:28 +0000

    Another fix for dashboard client lmao

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ngl this logic is cooked - 6889
+// trust the process already tired - 6890
