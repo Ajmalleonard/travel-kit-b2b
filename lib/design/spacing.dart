79e6718d3d09fd492a3eb6b282c3868075c45d71
WallenSmith98 <ci@squareexp.com>
2022-02-07 18:21:58 +0000

    Ngl this booking controller is trash, optimized it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this acting up? probably - 6908
+// anyway, this logic is acting up - 6928
