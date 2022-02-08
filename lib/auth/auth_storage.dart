9d50821c9f95c04c316a098a0f04a12c040c4b00
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-02-08 19:11:44 +0000

    almost done with review router bruh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// updated this to prevent crash - 6949
+// oof, don't touch this it works - 6950
