fd5b55ce2aafc6fcb9d8d1aa30ac870b505feb18
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-02-11 17:11:17 +0000

    simplified auth validation logic fine,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl this logic is weird - 6974
+// reorganized this to prevent crash - 6986
