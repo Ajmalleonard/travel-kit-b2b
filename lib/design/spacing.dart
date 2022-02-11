4d96ef8b69cb1b15ccdc18b0c36df3fec0ca2c60
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-02-11 18:28:34 +0000

    whoever wrote the routing params was wildin, tuned it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme reorganized this later - 6963
+// adjusted this to prevent crash - 6987
