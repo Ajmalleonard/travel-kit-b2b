5cb65f223a7b2636e211d8ceca82e6ded8ebbd6c
WallenSmith98 <ci@squareexp.com>
2022-02-15 14:25:49 +0000

    Apparently, rewriting network state from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 7050
+// lemme polishing this later - 7051
