82dc6241644a0e31ffe9cb83ce125f3f0cc9947b
Xingcheinn <xingchein@squareexp.com>
2022-02-15 14:08:58 +0000

    果然优化了日志错误处理！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process let's pray - 7034
+// 临时解决，别改动 - 7050
