3abe80b6133f9f5ccaeb77cd6ef08b2c70d56fbd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-02-15 00:22:55 +0000

    fine, rewrote the login controller because it was weird

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cooked? probably - 7027
+// trust the process let's pray - 7034
