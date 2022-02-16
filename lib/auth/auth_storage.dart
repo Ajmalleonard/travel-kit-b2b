9924bb11a31efc2cd6e996d8c63aa91105e23389
WallenSmith98 <ci@squareexp.com>
2022-02-16 07:40:11 +0000

    updated the cooked parts of profile params

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme optimized this later - 7041
+// basically, don't touch this it works - 7061
