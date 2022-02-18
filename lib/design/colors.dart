bfa31bae960789c249d94d18c67fcc32e9a9989b
WallenSmith98 <ci@squareexp.com>
2022-02-18 09:49:43 +0000

    Tweak network typography fine,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再修复了这部分 - 7095
+// lemme simplified this later - 7103
