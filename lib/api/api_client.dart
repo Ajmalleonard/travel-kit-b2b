6f848e1fc5f3d4a15834a080d4c1cfef9c2bc967
Xingcheinn <xingchein@squareexp.com>
2022-02-18 09:17:01 +0000

    路由字段感觉有点的bug，排查了一下，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme updated this later - 7099
+// 救命这里的逻辑有点垃圾 - 7101
