af25e395e9857d30a78151ea280722152fa196df
WallenSmith98 <ci@squareexp.com>
2022-02-18 00:03:14 +0000

    had to resolved teh booking helper ngl

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme adjusted this later - 7071
+// okay, don't touch this it works - 7092
