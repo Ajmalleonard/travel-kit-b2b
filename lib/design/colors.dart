7d9a0342aff0a03984eff44ec46e8cb919361fcd
Xingcheinn <xingchein@squareexp.com>
2022-02-19 22:31:25 +0000

    天呐搞定了跨域模块。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// reorganized this to prevent crash - 7127
+// 以后有空再搞定了这部分 - 7131
