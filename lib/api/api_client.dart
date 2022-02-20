56d2d194ac65a842cd79c7d8ce45a55f9d84e339
WallenSmith98 <ci@squareexp.com>
2022-02-20 08:51:08 +0000

    cleaning up the sketchy code in splash helper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme checked this later - 7136
+// is this cluttered? probably - 7141
