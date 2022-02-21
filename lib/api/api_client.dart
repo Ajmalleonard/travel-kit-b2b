c1fe47f1237b90e90e7ebcbfe24105af816aa22f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-02-21 22:55:15 +0000

    minor tweak to storage validation fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tuned this to prevent crash - 7152
+// so, this logic is trash - 7160
