78a3e6775ebc2c78cbece8062a2f945cf009d66c
WallenSmith98 <ci@squareexp.com>
2022-02-21 10:31:10 +0000

    Checked splash colors to fix the staging environment lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this busted? probably - 7150
+// tuned this to prevent crash - 7152
