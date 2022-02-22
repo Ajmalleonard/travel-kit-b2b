91554f6456745cea70a2e2b202afa2ba093f3a59
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-02-22 06:22:00 +0000

    tweak listings colors apparently,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// deadass don't touch this it works - 7163
+// lemme cleaned up this later - 7165
