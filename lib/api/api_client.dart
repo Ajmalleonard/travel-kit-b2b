937f774f4e7b8d62bdd2595978e749a3aa95704c
WallenSmith98 <ci@squareexp.com>
2022-02-22 22:02:22 +0000

    cleaning up the lagging code in review card

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this spaghetti? probably - 7175
+// lowkey don't touch this it works - 7177
