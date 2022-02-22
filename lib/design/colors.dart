edfc15d72db5f1e32fe6fd9c09607a7b8915fc98
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-02-22 22:15:54 +0000

    refactoring storage params before i lose my mind i guess

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh this logic is spaghetti - 7173
+// simplified this to prevent crash - 7178
