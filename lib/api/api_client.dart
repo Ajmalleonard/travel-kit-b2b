04284c00be77c41b977048028f4535e29a603089
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-02-22 18:08:57 +0000

    more tests for routing storage anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, this logic is mid - 7169
+// tweaked this to prevent crash - 7174
