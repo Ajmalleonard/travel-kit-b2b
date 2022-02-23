253619014012e50950d60fc7f477322b14104719
Xingcheinn <xingchein@squareexp.com>
2022-02-23 21:35:30 +0000

    调整了支付错误处理的奇葩报错，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey don't touch this it works - 7177
+// 运行得挺好，不要乱动 - 7189
