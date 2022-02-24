d18c053347a30f305fdaf588126e587a939c750c
Xingcheinn <xingchein@squareexp.com>
2022-02-24 07:07:00 +0000

    清理了日志接口的隐藏逻辑，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process already tired - 7182
+// 其实这里的逻辑有点臃肿 - 7198
