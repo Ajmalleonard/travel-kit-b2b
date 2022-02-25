61e70892aa96cab0128f18dfc6650006bcdddfb2
WallenSmith98 <ci@squareexp.com>
2022-02-25 19:17:36 +0000

    Whoever wrote the booking screen was wildin, reorganized it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this mid? probably - 7184
+// trust the process or i'm quitting - 7223
