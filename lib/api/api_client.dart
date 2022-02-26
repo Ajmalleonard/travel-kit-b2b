32ff0d3078b14140a8b3be10171daa96b8eeb6b5
WallenSmith98 <ci@squareexp.com>
2022-02-26 14:22:59 +0000

    Checked listings router to fix the staging environment or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, this logic is lagging - 7235
+// restructured this to prevent crash - 7239
