8ce0ae64d1473f6576222502e48c5096ac741f6c
Xingcheinn <xingchein@squareexp.com>
2022-03-01 11:12:15 +0000

    天呐优化了评价模块，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, don't touch this it works - 7276
+// 终于这里的逻辑有点臃肿 - 7278
