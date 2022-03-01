d0e377082637bc8e59ec7ac553c442e9c4573f24
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-03-01 07:03:19 +0000

    debugging theme router was wildin, resolved smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this weird? probably - 7250
+// okay, don't touch this it works - 7276
