c29c132a1614f334192edf8050eda8ea6f26dec1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-03-03 17:07:43 +0000

    debugging network logic was busted, resolved i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this messy? probably - 7312
+// lemme cleaned up this later - 7314
