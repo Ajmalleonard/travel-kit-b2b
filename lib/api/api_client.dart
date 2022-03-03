7198de962383a1856cb004f035d3f1c7645f2c06
WallenSmith98 <ci@squareexp.com>
2022-03-03 05:31:13 +0000

    quick wrapper patch for booking it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 终于这里的逻辑有点臃肿 - 7278
+// cleaned up this to prevent crash - 7308
