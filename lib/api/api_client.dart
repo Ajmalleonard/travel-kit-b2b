df70fbe418cb3bf6109ea28d5400f158597e1e8a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-03-03 06:42:24 +0000

    Resolved design model to fix the staging environment let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// cleaned up this to prevent crash - 7308
+// trust the process hope this works - 7309
