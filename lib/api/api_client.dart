3ff06415dc20a40a3a6a7b4c61003041d16ace18
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-03-03 13:15:28 +0000

    Whoever wrote the profile controller was wildin, tweaked it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// refactored this to prevent crash - 7311
+// is this messy? probably - 7312
