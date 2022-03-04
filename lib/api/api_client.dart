368c061aa1ee1275c52f5558cf75c52f94105bf1
WallenSmith98 <ci@squareexp.com>
2022-03-04 00:05:32 +0000

    Cleaning up the garbage code in network middleware

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme cleaned up this later - 7314
+// is this cluttered? probably - 7318
