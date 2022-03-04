d1dbb779c23fe9efbae5b3f2e55baeb2c085ad89
WallenSmith98 <ci@squareexp.com>
2022-03-04 07:12:27 +0000

    Hotfix for login state anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cluttered? probably - 7318
+// bruh don't touch this it works - 7327
