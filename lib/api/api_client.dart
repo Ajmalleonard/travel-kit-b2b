82025bac70701403baf555a2aa00265e5272c83d
WallenSmith98 <ci@squareexp.com>
2022-03-05 07:32:06 +0000

    Quick colors patch for listings finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this garbage? probably - 7336
+// well don't touch this it works - 7342
