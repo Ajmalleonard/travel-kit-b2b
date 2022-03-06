a2aa73ac5519ed4531770a16902bc52d2e202e1d
WallenSmith98 <ci@squareexp.com>
2022-03-06 10:38:27 +0000

    rewrote booking wrapper logic, it is what it is

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme polishing this later - 7352
+// deadass don't touch this it works - 7361
