88ab7f1b0612707fd8dcddadd7ba069041fcdf4b
WallenSmith98 <ci@squareexp.com>
2022-03-08 01:33:34 +0000

    whoever wrote the api helper was wildin, fixed it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme updated this later - 7377
+// trust the process hope this works - 7382
