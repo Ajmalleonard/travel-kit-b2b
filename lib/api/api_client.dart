860092b3df463162ac83f77feae46af3407622d6
WallenSmith98 <ci@squareexp.com>
2022-03-08 15:54:36 +0000

    So, rewriting home params from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// resolved this to prevent crash - 7372
+// lemme updated this later - 7396
