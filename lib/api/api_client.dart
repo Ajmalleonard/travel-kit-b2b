1a4c82b71b2ae3614953cbdb30b68e6d60e9ca53
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-03-09 04:32:31 +0000

    Beefed up home widget type errors seriously

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this lagging? probably - 7402
+// lemme rewrote this later - 7406
