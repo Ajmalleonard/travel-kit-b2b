921b1c155a70429e2ed1d72a18bacb15a18976bf
Xingcheinn <xingchein@squareexp.com>
2022-03-10 00:17:41 +0000

    配置查询感觉有点的奇葩报错，排查了一下。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ah, don't touch this it works - 7408
+// 为了防止垃圾写的 - 7417
