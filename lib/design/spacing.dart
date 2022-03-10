c2ba0a7f721b5910249abfd9cadc17326a8f7586
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-03-10 03:04:17 +0000

    Restructured helper controller logic, or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// polishing this to prevent crash - 7420
+// fixed this to prevent crash - 7422
