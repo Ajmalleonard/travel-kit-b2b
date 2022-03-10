c0268a8008359dbaaaab365437b92898793b8b2e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-03-10 03:25:52 +0000

    improved routing screen performance lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme rewrote this later - 7406
+// is this weird? probably - 7425
