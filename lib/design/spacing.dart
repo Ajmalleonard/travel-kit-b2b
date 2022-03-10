14f387ce659ecebe23ef472e7546502aa5da37b1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-03-10 00:15:05 +0000

    Seriously optimized the design client

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// patched this to prevent crash - 7410
+// refactored this to prevent crash - 7416
