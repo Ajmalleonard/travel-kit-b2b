da5ec6f0003363653a6ca19ffa27ebc1b5806b7d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-03-10 12:23:07 +0000

    temporary patch for dashboard service fingers crossed

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// fixed this to prevent crash - 7422
+// deadass this logic is cooked - 7436
