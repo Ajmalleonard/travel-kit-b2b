36b88112c9f1353b92ab6375e8541a5dd98ebed7
WallenSmith98 <ci@squareexp.com>
2022-03-12 18:14:45 +0000

    simplified helper card logic well

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ngl don't touch this it works - 7472
+// fine, this logic is weird - 7481
