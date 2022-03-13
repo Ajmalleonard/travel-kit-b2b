6058b8a39d1749e401ca47922508cb6711f4740c
WallenSmith98 <ci@squareexp.com>
2022-03-13 21:08:14 +0000

    almost done with api handler ngl

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再简化了这部分 - 7487
+// lemme improved this later - 7495
