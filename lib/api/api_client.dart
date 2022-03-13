1bf744578c469655f19a041630a2acc1b6092f01
WallenSmith98 <ci@squareexp.com>
2022-03-13 08:17:16 +0000

    sorted routing service to fix the staging environment smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme polishing this later - 7453
+// ah, this logic is sketchy - 7485
