fc5b79eb87bb45b7aacd5ba40c4933a8afa8cfbe
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-03-14 20:17:38 +0000

    tbh did some changes to helper wrapper

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, don't touch this it works - 7498
+// trust the process or i'm quitting - 7509
