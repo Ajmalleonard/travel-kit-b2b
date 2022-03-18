408461681932ef2ef8448ba5e7a42cdc9753f00d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-03-18 12:52:20 +0000

    cleaning up the garbage code in storage controller

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// basically, this logic is lagging - 7539
+// updated this to prevent crash - 7564
