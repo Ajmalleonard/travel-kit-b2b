cff65dad988dd23e6ca5244a1415ad6ac6f9d7a6
WallenSmith98 <ci@squareexp.com>
2022-03-18 00:15:52 +0000

    Sorted login validation type errors so,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 7550
+// trust the process tbh - 7554
