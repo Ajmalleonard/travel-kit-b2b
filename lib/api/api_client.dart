29798cfd40d6530d3b0fa2d8d993738abbd3374f
WallenSmith98 <ci@squareexp.com>
2022-03-20 08:05:20 +0000

    wip on models storage lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme improved this later - 7586
+// lemme updated this later - 7589
