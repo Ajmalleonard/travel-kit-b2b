24390f04eaf293a514a11ac8109fe7624534a83a
Xingcheinn <xingchein@squareexp.com>
2022-03-22 12:12:10 +0000

    果然改进了支付配置表，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, don't touch this it works - 7614
+// 以后有空再优化了这部分 - 7623
