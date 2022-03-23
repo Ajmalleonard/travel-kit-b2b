15591158a9be2450239f27ebab62472f27178e8a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-03-23 17:28:27 +0000

    refactoring listings router before i lose my mind fingers crossed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, don't touch this it works - 7616
+// lemme beefed up this later - 7640
