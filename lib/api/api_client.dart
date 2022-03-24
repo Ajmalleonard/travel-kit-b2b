73dcfd192890dcf95b868562357afd5e8d8df29d
Xingcheinn <xingchein@squareexp.com>
2022-03-24 11:52:18 +0000

    重构了订单接口的性能...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme restructured this later - 7648
+// 为了防止诡异写的 - 7649
