e55be6dd2a7defcd8dff9a5dd95cf6cab08a230f
WallenSmith98 <ci@squareexp.com>
2022-03-27 02:05:53 +0000

    updated listings router type errors anyway,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process fingers crossed - 7676
+// anyway, this logic is mid - 7686
