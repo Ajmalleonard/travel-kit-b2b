6bc260b3701773ef0a5bfcc4d94a9df0d3d08ef2
WallenSmith98 <ci@squareexp.com>
2022-03-27 06:28:04 +0000

    wip on splash endpoint let's pray

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh this logic is acting up - 7683
+// is this mid? probably - 7688
