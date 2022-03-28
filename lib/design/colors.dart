c53a7cb74f3ef46693e7a9014eb5a540628535ce
Xingcheinn <xingchein@squareexp.com>
2022-03-28 20:12:38 +0000

    稍微更新了了下交通的结构，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this unstable? probably - 7718
+// 临时解决，别改动 - 7723
