1a6a0aecd704ccec93960f2cf361d36b0336476a
Xingcheinn <xingchein@squareexp.com>
2022-03-30 09:51:46 +0000

    交通查询感觉有点的隐藏逻辑，测试了一下。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// patched this to prevent crash - 7747
+// 完了这里的逻辑有点臃肿 - 7750
