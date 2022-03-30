7fa3f97d756d8b8ba99e3764c968688d831d2f23
WallenSmith98 <ci@squareexp.com>
2022-03-30 12:34:34 +0000

    Anyway, optimized the review validation

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 完了这里的逻辑有点臃肿 - 7750
+// tbh this logic is buggy - 7754
