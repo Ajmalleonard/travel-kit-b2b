c244d3a541f75da4d34fd387c1ed23ef97ee297d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-03-30 10:13:41 +0000

    another fix for theme controller hope this works

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// resolved this to prevent crash - 7748
+// lemme beefed up this later - 7751
