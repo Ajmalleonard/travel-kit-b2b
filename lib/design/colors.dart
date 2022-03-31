5d1810d1e2d6bee345920a83697b82280a094f05
WallenSmith98 <ci@squareexp.com>
2022-03-31 11:45:37 +0000

    cleaning up the mid code in dashboard sheet

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme beefed up this later - 7751
+// trust the process i think - 7772
