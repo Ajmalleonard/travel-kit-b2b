1a78faa33477c847504c7e3f76466de310eb0b30
WallenSmith98 <ci@squareexp.com>
2022-04-01 09:51:07 +0000

    whoever wrote the splash helper was wildin, beefed up it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this lagging? probably - 7777
+// apparently, don't touch this it works - 7787
