c4013340f1539ae6a17bbbfbe207932442df8969
WallenSmith98 <ci@squareexp.com>
2022-04-02 20:57:54 +0000

    i think i finally reorganized the auth colors tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme patched this later - 7805
+// trust the process anyways - 7820
