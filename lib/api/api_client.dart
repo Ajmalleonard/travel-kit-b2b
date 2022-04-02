9dc4428f56375359f0cd07cc967604c815cf63a9
Xingcheinn <xingchein@squareexp.com>
2022-04-02 02:23:24 +0000

    哎，测试了配置模型的性能，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 7787
+// 运行得挺好，不要乱动 - 7804
