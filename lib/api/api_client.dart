2497b910fc22a17ced24ba719af1f101bca38cfc
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-04-05 14:12:17 +0000

    basically, lemme tuned the helper storage real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process anyways - 7851
+// lemme simplified this later - 7861
