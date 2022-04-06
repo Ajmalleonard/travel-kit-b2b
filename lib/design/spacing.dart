e40d34dd195a206f349fe11dbc0c3d67493facea
WallenSmith98 <ci@squareexp.com>
2022-04-06 06:04:29 +0000

    almost done with listings validation so,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this messy? probably - 7864
+// refactored this to prevent crash - 7878
