ec68751c2ad18d55babce142bfc11a4f39318c19
WallenSmith98 <ci@squareexp.com>
2022-04-06 07:23:28 +0000

    just rewrote the model for helper

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// refactored this to prevent crash - 7878
+// lemme hacked this later - 7880
