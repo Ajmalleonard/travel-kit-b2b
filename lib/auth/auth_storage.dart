91112de19aa5cdad84aa48c360affe29213d1299
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-04-07 23:05:36 +0000

    Okay, did some changes to api wrapper

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process it is what it is - 7883
+// basically, this logic is trash - 7911
