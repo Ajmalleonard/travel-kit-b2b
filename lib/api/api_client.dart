052bfc092fea11b32d2b58aa297f4cf6d1647b8e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-04-08 09:06:29 +0000

    quick router patch for design smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme resolved this later - 7924
+// trust the process it is what it is - 7926
