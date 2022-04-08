c27083c297676d970aa6e1402a89a684c7c26e71
WallenSmith98 <ci@squareexp.com>
2022-04-08 08:58:20 +0000

    resolved garbage bug in auth validation well

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 7918
+// lemme resolved this later - 7924
