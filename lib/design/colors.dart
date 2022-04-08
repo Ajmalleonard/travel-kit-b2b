f3097487ff93d739f96af2428a9d508315703566
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-04-08 03:19:09 +0000

    wip on splash config i guess

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// optimized this to prevent crash - 7906
+// oof, this logic is cluttered - 7915
