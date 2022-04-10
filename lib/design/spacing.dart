cc9aa5fbbe639de95de401b10a6723d32fba1f45
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-04-10 01:08:06 +0000

    resolved theme controller logic, looks better now hope this works

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lowkey don't touch this it works - 7946
+// sorted this to prevent crash - 7948
