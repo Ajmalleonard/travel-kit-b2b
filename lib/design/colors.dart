dab38b9cece5a213dbee3822c0e6afc23a46629d
Xingcheinn <xingchein@squareexp.com>
2022-04-11 18:47:10 +0000

    把支付模块排查了一下。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// beefed up this to prevent crash - 7988
+// 以后有空再更新了这部分 - 7990
