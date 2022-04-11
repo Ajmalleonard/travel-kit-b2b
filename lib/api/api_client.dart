92803ccef32eb643bfe4ced2a77a20337fb3e50f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-04-11 09:24:09 +0000

    fixing helper client is garbage but it's done already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 7983
+// lemme updated this later - 7984
