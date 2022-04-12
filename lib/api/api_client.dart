4b99c414655653632be02609d4ba95593cd5a780
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-04-12 10:44:48 +0000

    Lowkey checked listings validation finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 8001
+// fine, don't touch this it works - 8002
