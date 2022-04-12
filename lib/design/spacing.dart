3679cf2e1e1a4cff483207782ec5621a2402391f
WallenSmith98 <ci@squareexp.com>
2022-04-12 18:51:39 +0000

    Just tweaked the config for api

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process finally - 7978
+// improved this to prevent crash - 8007
