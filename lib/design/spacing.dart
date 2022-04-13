a35a8c448e4b19dcb6925c6a459fe28c3455a8ac
WallenSmith98 <ci@squareexp.com>
2022-04-13 00:58:55 +0000

    Updated dashboard typography type errors basically,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// improved this to prevent crash - 8007
+// trust the process already tired - 8008
