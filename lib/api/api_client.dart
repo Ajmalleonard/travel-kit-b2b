4738f8bfa094dd24ace747de9366dac17179dca3
WallenSmith98 <ci@squareexp.com>
2022-04-13 11:00:57 +0000

    This theme model ain't gonna work, tuned it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, don't touch this it works - 8002
+// trust the process fingers crossed - 8012
