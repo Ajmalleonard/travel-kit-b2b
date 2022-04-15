9cb9f3b29b7acb1c4e12c5d972c49f29231ff3ef
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-04-15 01:08:47 +0000

    temporary patch for api logic let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this spaghetti? probably - 8017
+// cleaned up this to prevent crash - 8036
