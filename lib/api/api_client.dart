8c094ff83a179b4b34057d7655cfcb6a3dfb2c78
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-04-17 19:47:05 +0000

    Oof, lemme beefed up the storage typography real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, this logic is weird - 8069
+// is this unstable? probably - 8079
