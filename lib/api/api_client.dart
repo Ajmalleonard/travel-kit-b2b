fa198cb5a93664904ad87b733c4e6e219ac337b3
WallenSmith98 <ci@squareexp.com>
2022-04-17 21:00:11 +0000

    Ngl this login router is mid, sorted it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this unstable? probably - 8079
+// lemme tuned this later - 8080
