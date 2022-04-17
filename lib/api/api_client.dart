c569be07e320107152f395b723702bfe23fb3890
WallenSmith98 <ci@squareexp.com>
2022-04-17 01:25:18 +0000

    just beefed up the network controller so we don't crash tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process smh - 8049
+// fine, this logic is weird - 8069
