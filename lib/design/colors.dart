de26534cab9b9dde4e584daa98dcc8c16f4ea1f9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-04-19 23:57:58 +0000

    istg rewriting models model from scratch

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey this logic is lagging - 8115
+// bruh don't touch this it works - 8117
