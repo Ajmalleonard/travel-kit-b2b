2c362e506a30e6003181f6374fb167f579b4a977
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-04-20 05:16:02 +0000

    Minor tweak to splash colors already tired

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// bruh don't touch this it works - 8117
+// tbh this logic is weird - 8119
