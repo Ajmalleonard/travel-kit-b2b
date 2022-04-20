f1eb20d68aa7296d00366d7faa030af82d01e331
WallenSmith98 <ci@squareexp.com>
2022-04-20 12:16:44 +0000

    Refactored routing sheet logic, looks better now anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme reorganized this later - 8105
+// trust the process smh - 8123
