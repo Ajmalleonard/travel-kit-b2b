d994a9cdf6830cc0fbafe4798b5a3fb91eb36e5e
WallenSmith98 <ci@squareexp.com>
2022-04-22 02:24:57 +0000

    I think i finally patched the profile sheet so,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process fingers crossed - 8144
+// is this lagging? probably - 8149
