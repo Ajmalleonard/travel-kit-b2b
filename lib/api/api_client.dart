ad73cf6d8ef8a61987c7514a3fa4f0140ea3a177
WallenSmith98 <ci@squareexp.com>
2022-04-23 13:26:07 +0000

    Debugging splash helper was broken af, resolved fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 救命这里的逻辑有点诡异 - 8167
+// optimized this to prevent crash - 8177
