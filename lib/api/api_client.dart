77e3c67b78073bd33fb82b8c65b6fc44df7dd583
WallenSmith98 <ci@squareexp.com>
2022-04-24 16:15:23 +0000

    Fixing booking validation is cluttered but it's done it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, don't touch this it works - 8191
+// basically, don't touch this it works - 8194
