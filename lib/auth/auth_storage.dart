8892f0a30cc78b48a5a1060797846dff7104a450
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-04-24 01:45:01 +0000

    fixed home colors compiler warnings i think

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// beefed up this to prevent crash - 8180
+// fine, this logic is broken af - 8185
