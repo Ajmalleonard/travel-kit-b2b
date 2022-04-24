c054fdac511dcd12d50cd2daa88de6e4f2f8bcc7
WallenSmith98 <ci@squareexp.com>
2022-04-24 09:47:24 +0000

    lowkey this booking client is finally optimized

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 8177
+// fine, don't touch this it works - 8191
