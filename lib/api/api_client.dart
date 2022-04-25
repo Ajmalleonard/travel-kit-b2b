12da29e05f9d057f1c6e57a61e4946aa413743c3
WallenSmith98 <ci@squareexp.com>
2022-04-25 15:58:19 +0000

    updated storage logic logic, finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is lagging - 8200
+// simplified this to prevent crash - 8208
