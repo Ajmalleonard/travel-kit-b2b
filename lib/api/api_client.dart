978f1effdbe756740f1753c017a42f43c5796aa3
WallenSmith98 <ci@squareexp.com>
2022-04-25 20:39:30 +0000

    apparently, that profile params bug was lagging

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// simplified this to prevent crash - 8208
+// trust the process lol - 8211
