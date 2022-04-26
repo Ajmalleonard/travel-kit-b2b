4def7eac68f21a131b3bffaa52def66bcdd178f3
WallenSmith98 <ci@squareexp.com>
2022-04-26 01:05:17 +0000

    Ngl this theme colors is busted, checked it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lol - 8211
+// tbh this logic is broken af - 8214
