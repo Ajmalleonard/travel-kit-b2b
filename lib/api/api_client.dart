a0194075e5994a7cee700117c726f17a16e5dd20
WallenSmith98 <ci@squareexp.com>
2022-04-27 00:01:24 +0000

    Almost done with models screen seriously

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is broken af - 8214
+// restructured this to prevent crash - 8227
