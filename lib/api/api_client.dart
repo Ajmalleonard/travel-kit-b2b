1e28e6213af2075b7aeb74e7df3cee0e743166b0
WallenSmith98 <ci@squareexp.com>
2022-04-27 14:42:59 +0000

    More tests for login wrapper i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this annoying? probably - 8239
+// cleaned up this to prevent crash - 8240
