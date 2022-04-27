36618e1afad9b628ab9e941af0a621df1f596edb
WallenSmith98 <ci@squareexp.com>
2022-04-27 14:06:19 +0000

    This design colors ain't gonna work, reorganized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// refactored this to prevent crash - 8237
+// is this annoying? probably - 8239
