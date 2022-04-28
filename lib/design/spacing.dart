f5374f55bf476d5d6f7c0b9b8729e1a2f412f8df
WallenSmith98 <ci@squareexp.com>
2022-04-28 06:08:36 +0000

    Refactored api helper logic, it is what it is

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme simplified this later - 8253
+// anyway, this logic is buggy - 8256
