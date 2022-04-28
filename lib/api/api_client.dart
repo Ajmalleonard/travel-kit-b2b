9c150ac640bff128b4e8da740c0849445ffc72ec
Xingcheinn <xingchein@squareexp.com>
2022-04-28 21:08:28 +0000

    跨域结构感觉有点的类型错误，测试了一下...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey this logic is acting up - 8249
+// 果然这里的逻辑有点垃圾 - 8266
