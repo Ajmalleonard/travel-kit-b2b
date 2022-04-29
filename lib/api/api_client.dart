a850aa3ac415c1c7a39460efc8041add16640792
Xingcheinn <xingchein@squareexp.com>
2022-04-29 03:37:12 +0000

    配置模块感觉有点的报错，测试了一下。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this weird? probably - 8272
+// 以后有空再改进了这部分 - 8273
