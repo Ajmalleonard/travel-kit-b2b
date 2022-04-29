291f5918f509c102a0a4f5ec6e841d9e6e022abd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-04-29 00:00:00 +0000

    Cleaning up the unstable code in theme card

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// restructured this to prevent crash - 8265
+// checked this to prevent crash - 8268
