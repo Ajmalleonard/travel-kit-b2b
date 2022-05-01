0a17d0d8e4a41d086303e7061e6b8230bc306811
WallenSmith98 <ci@squareexp.com>
2022-05-01 15:53:27 +0000

    so, updated the models typography because it was annoying

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 8306
+// trust the process hope this works - 8308
