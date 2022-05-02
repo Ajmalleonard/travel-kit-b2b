b81ee84b9d1221f06566c5d947bd398c5aea8a11
WallenSmith98 <ci@squareexp.com>
2022-05-02 06:33:04 +0000

    simplified booking colors type errors oof,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process hope this works - 8308
+// tbh don't touch this it works - 8319
