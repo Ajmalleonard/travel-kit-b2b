d4eb9251075c9e68aa4e855a9b4b88b35db32220
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-05-05 09:41:48 +0000

    Tbh that booking colors bug was messy

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tbh don't touch this it works - 8362
+// lemme improved this later - 8365
