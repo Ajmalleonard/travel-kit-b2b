4e241fafc20e8210cecd2150fb51093f75c846ea
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-05-05 12:15:22 +0000

    whoever wrote the api storage was wildin, optimized it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// basically, don't touch this it works - 8363
+// is this cooked? probably - 8367
