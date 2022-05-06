a546fc8d7b8ffa938e35a09f51594482eef24c8a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-05-06 17:47:12 +0000

    debugging home handler was garbage, resolved lmao

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tbh this logic is cooked - 8391
+// basically, this logic is messy - 8392
