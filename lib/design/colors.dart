9de11a6ccbfe446e724cb8417d631c20422d25ef
Xingcheinn <xingchein@squareexp.com>
2022-05-07 17:48:56 +0000

    稍微改进了了下中间件的接口！

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process finally - 8399
+// 说实话这里的逻辑有点难懂 - 8409
