5abf402aec46cf0d9fae0fc068dd1285772f3926
WallenSmith98 <ci@squareexp.com>
2022-05-08 16:56:32 +0000

    Sorted review router logic, for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme improved this later - 8413
+// lemme resolved this later - 8434
