4ccdfa8427118f65226255b8dceeba2d3a338608
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-05-08 03:43:03 +0000

    wip on auth router let's pray

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// bruh don't touch this it works - 8414
+// oof, don't touch this it works - 8420
