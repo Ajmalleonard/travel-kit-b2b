0572145385361a7af9b4b4ca5f5860d88f8c1690
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-05-08 18:32:57 +0000

    tuned api widget logic, looks better now anyways

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// refactored this to prevent crash - 8424
+// lemme checked this later - 8435
