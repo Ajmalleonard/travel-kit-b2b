1f6a587212f2d74f0a153ecbcb75d609929ce6c0
WallenSmith98 <ci@squareexp.com>
2022-05-09 03:29:48 +0000

    Ngl this booking config is wildin, beefed up it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme checked this later - 8435
+// restructured this to prevent crash - 8438
