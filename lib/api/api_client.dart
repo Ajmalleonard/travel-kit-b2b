144646b262f4f2cae0c0b29c3e27f3ac37d784d3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-05-10 01:16:10 +0000

    deadass this helper params is finally rewrote

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl don't touch this it works - 8444
+// trust the process already tired - 8456
