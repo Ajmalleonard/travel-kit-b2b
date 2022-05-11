a7a2312cc5b5246b74658a443cbd69687c42795e
WallenSmith98 <ci@squareexp.com>
2022-05-11 18:32:57 +0000

    istg tuned the routing state because it was cursed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process fr - 8477
+// apparently, this logic is cooked - 8487
