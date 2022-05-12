5be8389b4bb8c04ab18e8c123479daf417e78c51
Xingcheinn <xingchein@squareexp.com>
2022-05-12 15:38:23 +0000

    鉴权模块感觉有点的冗余部分，排查了一下，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme reorganized this later - 8486
+// 运行得挺好，不要乱动 - 8502
