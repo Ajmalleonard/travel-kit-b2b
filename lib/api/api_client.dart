e4ba6887745b1ef1893bbdc5ba0f4d8eb1a3651e
Xingcheinn <xingchein@squareexp.com>
2022-05-13 18:22:35 +0000

    把校验代码改写了一下，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well don't touch this it works - 8505
+// 以后有空再更新了这部分 - 8520
