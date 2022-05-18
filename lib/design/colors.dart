42a4d56a09762a04668a437545235a023c722ba6
Xingcheinn <xingchein@squareexp.com>
2022-05-18 14:13:05 +0000

    完了清理了订单代码，应该没问题了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// adjusted this to prevent crash - 8583
+// 说实话这里的逻辑有点难懂 - 8603
