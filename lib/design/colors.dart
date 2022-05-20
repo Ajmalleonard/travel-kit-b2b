d36db186b31aa282845c6079bcb50db05a2548b5
Xingcheinn <xingchein@squareexp.com>
2022-05-20 00:08:17 +0000

    清理了缓存模型的冗余部分。测试通过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process finally - 8620
+// 以后有空再改写了这部分 - 8623
