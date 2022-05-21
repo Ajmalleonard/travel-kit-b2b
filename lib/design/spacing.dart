1526f047c206c1cebd7a2534d481edb13f438a09
WallenSmith98 <ci@squareexp.com>
2022-05-21 01:21:35 +0000

    Fine, simplified the review router because it was cooked

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this buggy? probably - 8639
+// basically, don't touch this it works - 8643
