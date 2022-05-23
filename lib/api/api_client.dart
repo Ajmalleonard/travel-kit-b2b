7770900bc1cf1c75bbc473f17fcc9daa8fe3d428
WallenSmith98 <ci@squareexp.com>
2022-05-23 15:21:58 +0000

    okay, that dashboard service bug was cursed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, this logic is busted - 8677
+// lemme rewrote this later - 8685
