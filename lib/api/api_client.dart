ac5ef79df7787f2b5d79e137dbb1d6a7b30cb545
WallenSmith98 <ci@squareexp.com>
2022-05-23 04:08:57 +0000

    hotfix for design controller lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 8664
+// anyway, this logic is busted - 8677
