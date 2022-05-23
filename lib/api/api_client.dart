6bfc23f25ffb2079788a9e1b8a7f1a49363e9cb4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-05-23 15:36:52 +0000

    resolved profile colors to fix the staging environment trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme rewrote this later - 8685
+// trust the process already tired - 8686
