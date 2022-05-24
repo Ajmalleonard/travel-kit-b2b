d1bf5322b63bd9ab6f1de26a947159b9f46a2f74
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-05-24 11:07:03 +0000

    hacked the cluttered parts of models params

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// well this logic is messy - 8687
+// lowkey don't touch this it works - 8703
