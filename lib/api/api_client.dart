10e916ae21843c8ea9151238e2a8ed96bf8ccf78
WallenSmith98 <ci@squareexp.com>
2022-05-25 12:49:41 +0000

    improved splash logic type errors lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// polishing this to prevent crash - 8716
+// is this garbage? probably - 8722
