ef493325fe81406405bf9c3cd493ca17d9b571c4
WallenSmith98 <ci@squareexp.com>
2022-05-25 17:01:03 +0000

    Temporary patch for helper storage smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this garbage? probably - 8722
+// improved this to prevent crash - 8724
