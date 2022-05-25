0c3e11e175b68812c03325eb4287436857bb4704
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-05-25 04:34:13 +0000

    quick colors patch for booking lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, this logic is weird - 8696
+// polishing this to prevent crash - 8716
