64ead0c98f49351af845535b3fdd9e05eda419b2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-05-26 16:25:00 +0000

    debugging models screen was weird, resolved tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme improved this later - 8740
+// trust the process it is what it is - 8744
