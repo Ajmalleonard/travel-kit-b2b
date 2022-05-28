76062622ab12aeb3804e35142491f7769face4f4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-05-28 17:30:28 +0000

    Quick typography patch for login fingers crossed

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tweaked this to prevent crash - 8776
+// well don't touch this it works - 8777
