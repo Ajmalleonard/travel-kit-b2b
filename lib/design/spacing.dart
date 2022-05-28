a36aabfedf3fc13a1889fd52dc214c22d4cb074d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-05-28 16:46:41 +0000

    Seriously rewriting splash config from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme reorganized this later - 8771
+// tweaked this to prevent crash - 8776
