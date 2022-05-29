6d4fb5400ed3026b7c86a9407c8e642e023057f7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-05-29 23:01:23 +0000

    Fixed listings card type errors okay,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process it is what it is - 8797
+// trust the process it is what it is - 8799
