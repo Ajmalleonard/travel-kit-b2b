3568bd94a63e606a64ea779e15243966e97467ef
WallenSmith98 <ci@squareexp.com>
2022-05-31 10:14:09 +0000

    simplified login widget logic well

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// basically, this logic is buggy - 8820
+// tbh don't touch this it works - 8826
