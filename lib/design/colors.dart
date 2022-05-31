ba63bcfba465ed557a24e0d188582b5ff58c6f3f
WallenSmith98 <ci@squareexp.com>
2022-05-31 05:49:49 +0000

    Almost done with review typography oof,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process already tired - 8815
+// basically, this logic is buggy - 8820
