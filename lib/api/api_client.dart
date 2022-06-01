48dd6d55faeff441d29bd9feb7c9dde4917ad67d
Xingcheinn <xingchein@squareexp.com>
2022-06-01 09:56:33 +0000

    把配置参数改进了一下，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i guess - 8823
+// 临时解决，别改动 - 8840
