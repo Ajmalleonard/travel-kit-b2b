2c62f5b0b3159c90bb060652814665d714e47a8e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-06-02 07:49:40 +0000

    quick sheet patch for login anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this spaghetti? probably - 8850
+// cleaned up this to prevent crash - 8852
