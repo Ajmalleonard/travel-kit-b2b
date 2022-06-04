d24f85750037ba509a152c4fba96de240b2fea77
WallenSmith98 <ci@squareexp.com>
2022-06-04 14:04:38 +0000

    More tests for login logic for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is garbage - 8880
+// lemme resolved this later - 8887
