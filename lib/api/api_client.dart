bbe332e3344b6baa325a9f6bf9409a752eaa6728
Xingcheinn <xingchein@squareexp.com>
2022-06-04 02:17:48 +0000

    说实话，测试了路由查询的冗余部分，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// cleaned up this to prevent crash - 8852
+// 为了防止诡异写的 - 8878
