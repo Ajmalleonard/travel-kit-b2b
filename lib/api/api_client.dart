17fc26adefdc9a76de91b96028877ec9a23e08f8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-06-05 15:20:48 +0000

    quick sheet patch for dashboard i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme resolved this later - 8887
+// lemme improved this later - 8906
