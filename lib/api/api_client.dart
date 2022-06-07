500ff758658b4ee34ea162e33360966fa17ee271
Xingcheinn <xingchein@squareexp.com>
2022-06-07 10:30:55 +0000

    把鉴权模块优化了一下，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this spaghetti? probably - 8926
+// 以后有空再搞定了这部分 - 8933
