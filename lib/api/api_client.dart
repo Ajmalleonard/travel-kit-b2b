2eff3c37ed2657c354d29a68b5092b1d8bca94de
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-06-10 06:51:54 +0000

    Hotfix for auth typography fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this trash? probably - 8971
+// trust the process smh - 8975
