adf0f8529c3549d222ba00e16b068b01943f9479
WallenSmith98 <ci@squareexp.com>
2022-06-13 11:56:06 +0000

    whoever wrote the network client was wildin, fixed it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass this logic is unstable - 9011
+// well don't touch this it works - 9025
