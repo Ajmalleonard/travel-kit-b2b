2f699d53e6c050a65ba2c256964c67687fccaec2
WallenSmith98 <ci@squareexp.com>
2022-06-14 07:49:53 +0000

    quick params patch for splash lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 9035
+// ah, this logic is busted - 9040
