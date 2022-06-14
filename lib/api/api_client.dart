bf6cdf8398f26d1ef9953b77e7873c1c437b3b88
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-06-14 00:43:16 +0000

    hotfix for storage service anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process hope this works - 9026
+// lemme improved this later - 9032
