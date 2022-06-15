a6ef89e756dbf76d9c6a4af506a07def8e0991ac
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-06-15 03:35:45 +0000

    bruh that booking widget bug was cooked

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme optimized this later - 9045
+// apparently, don't touch this it works - 9055
