a653b53434eb05ffeac6f2b9b92e338e9e3ec6f2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-06-16 06:20:07 +0000

    Cleaning up the lagging code in routing storage

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this broken af? probably - 9076
+// fine, this logic is lagging - 9080
