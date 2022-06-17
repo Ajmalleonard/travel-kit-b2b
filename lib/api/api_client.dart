5f49df533dc237bc33d5613b0d90f85cf904e1ef
WallenSmith98 <ci@squareexp.com>
2022-06-17 02:58:43 +0000

    wip on api helper it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 9055
+// is this sketchy? probably - 9098
