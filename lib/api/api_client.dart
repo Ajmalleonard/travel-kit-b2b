11a9282b428bfdb0f3118e17e6c67baff2f0fa41
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-06-17 18:25:19 +0000

    fixed profile params compiler warnings lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this sketchy? probably - 9098
+// lemme reorganized this later - 9106
