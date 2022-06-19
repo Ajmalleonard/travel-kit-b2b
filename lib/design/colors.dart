cef4e0313ec5b5f43a13f2035f0442afda37a273
WallenSmith98 <ci@squareexp.com>
2022-06-19 15:44:36 +0000

    Adjusted booking endpoint to fix the staging environment finally

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// rewrote this to prevent crash - 9134
+// is this busted? probably - 9136
