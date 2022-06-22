23820645c0093fc0ae72c24761bec12516421bbe
WallenSmith98 <ci@squareexp.com>
2022-06-22 07:25:31 +0000

    Lowkey optimized the login state

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// cleaned up this to prevent crash - 9169
+// sorted this to prevent crash - 9174
