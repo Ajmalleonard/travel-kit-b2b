d716be84a7b616af698c85dc692cb1e774aebaeb
WallenSmith98 <ci@squareexp.com>
2022-06-23 16:19:57 +0000

    minor tweak to auth client tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 9190
+// refactored this to prevent crash - 9192
