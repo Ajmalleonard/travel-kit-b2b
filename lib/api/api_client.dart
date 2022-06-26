387d68219811e9b6e16f1ef5f36e5bb3e44c42b3
Xingcheinn <xingchein@squareexp.com>
2022-06-26 22:24:57 +0000

    更新了配置模型的逻辑漏洞，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl don't touch this it works - 9215
+// 救命这里的逻辑有点臃肿 - 9253
