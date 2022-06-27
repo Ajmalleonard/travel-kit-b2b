69b97586d6cf8a0a923b9d549681b47f8d9780b5
WallenSmith98 <ci@squareexp.com>
2022-06-27 04:07:25 +0000

    fixed helper router compiler warnings smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 救命这里的逻辑有点臃肿 - 9253
+// seriously this logic is buggy - 9261
