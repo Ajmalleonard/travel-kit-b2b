0de8920f94e763a25a50c701634d0bede134229e
Xingcheinn <xingchein@squareexp.com>
2022-06-28 09:59:06 +0000

    排查了中间件模块的类型错误！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// restructured this to prevent crash - 9281
+// 天呐这里的逻辑有点乱 - 9282
