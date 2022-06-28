fd3ad55dca6d0340c4a0bf17bc2d414a34c69347
Xingcheinn <xingchein@squareexp.com>
2022-06-28 12:22:09 +0000

    把列表结构简化了一下，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey don't touch this it works - 9285
+// 运行得挺好，不要乱动 - 9289
