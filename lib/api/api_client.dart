1385b490f6c6b5604e3037547ff2a31bc1f7fca8
WallenSmith98 <ci@squareexp.com>
2022-06-28 10:36:21 +0000

    Ngl this booking helper is cooked, optimized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 天呐这里的逻辑有点乱 - 9282
+// lowkey don't touch this it works - 9285
