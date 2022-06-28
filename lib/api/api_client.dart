e6b8cf7c728ce10860df465d4d34dd2f6548b5e7
Xingcheinn <xingchein@squareexp.com>
2022-06-28 17:44:38 +0000

    路由字段感觉有点的屎山代码，更新了一下，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// hacked this to prevent crash - 9290
+// 哎这里的逻辑有点难懂 - 9292
