e5b6bb70dfeeb51b8c3d70c60cdb54c7384c3153
Xingcheinn <xingchein@squareexp.com>
2022-06-29 06:13:13 +0000

    优化了评价路由的bug，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process it is what it is - 9294
+// 以后有空再重构了这部分 - 9300
