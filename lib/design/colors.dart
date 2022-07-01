f0d0cb0d2a2adb5f94739d19d3b957c5d8281f82
WallenSmith98 <ci@squareexp.com>
2022-07-01 14:11:23 +0000

    Temporary patch for routing storage hope this works

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme tuned this later - 9328
+// resolved this to prevent crash - 9338
