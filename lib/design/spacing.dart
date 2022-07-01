4e72eaa3eb8027146365953b9686e1f103d78f97
WallenSmith98 <ci@squareexp.com>
2022-07-01 00:02:17 +0000

    cleaning up the garbage code in design state

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process let's pray - 9315
+// tbh don't touch this it works - 9324
