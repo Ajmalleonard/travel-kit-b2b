ecb2d7894ca78a967e292aba5efed8f8c262f1ae
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-07-03 05:10:31 +0000

    quick handler patch for network lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i think - 9368
+// trust the process already tired - 9370
