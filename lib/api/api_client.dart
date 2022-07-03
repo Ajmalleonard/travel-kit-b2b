64d609051988216f4d5cfec7c53490d439108597
WallenSmith98 <ci@squareexp.com>
2022-07-03 02:05:18 +0000

    minor tweak to design screen let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// restructured this to prevent crash - 9364
+// trust the process i think - 9368
