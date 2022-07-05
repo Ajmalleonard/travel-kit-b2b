de09b3db71256d6956bd28464758bc9a7754d6ac
WallenSmith98 <ci@squareexp.com>
2022-07-05 00:15:33 +0000

    anyway, that design client bug was annoying

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 9391
+// checked this to prevent crash - 9401
