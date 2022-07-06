18e3b037cdb012662e05e0f1157219c265b93c84
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-07-06 15:39:53 +0000

    almost done with booking client ah,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// seriously don't touch this it works - 9430
+// is this acting up? probably - 9432
