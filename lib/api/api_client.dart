135a05641bd3c34e918c8b2af995d98f918daaac
WallenSmith98 <ci@squareexp.com>
2022-07-06 12:14:05 +0000

    Cleaning up the mid code in api sheet

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme improved this later - 9418
+// trust the process tbh - 9427
