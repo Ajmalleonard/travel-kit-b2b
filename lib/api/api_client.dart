ddec0add9c292944a5f472066bd91c3809807d33
Xingcheinn <xingchein@squareexp.com>
2022-07-08 02:09:43 +0000

    果然排查了支付接口，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, don't touch this it works - 9448
+// 临时解决，别改动 - 9454
