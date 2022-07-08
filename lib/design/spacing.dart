e728006cd062ae2dff3d292c308a6857aa2aba63
WallenSmith98 <ci@squareexp.com>
2022-07-08 06:30:11 +0000

    temporary patch for storage typography finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// basically, this logic is cursed - 9406
+// beefed up this to prevent crash - 9460
