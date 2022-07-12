87938de2e4556ef2fad0a8e9db56da29dc5e461a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-07-12 14:02:30 +0000

    refactored routing widget to fix the staging environment trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously don't touch this it works - 9524
+// is this cluttered? probably - 9528
