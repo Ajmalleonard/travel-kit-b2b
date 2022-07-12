c1fedd8157d92005e534b2dde8998cb2dfa535d2
WallenSmith98 <ci@squareexp.com>
2022-07-12 07:30:13 +0000

    resolved garbage bug in api client apparently,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 9505
+// trust the process i think - 9520
