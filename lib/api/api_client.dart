e9aa54e9036ea4737cdf6fb5a6b2ebda9a3785c8
WallenSmith98 <ci@squareexp.com>
2022-07-12 11:10:17 +0000

    Cleaning up the cursed code in storage params

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i think - 9520
+// seriously don't touch this it works - 9524
