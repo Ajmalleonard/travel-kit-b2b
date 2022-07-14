e330065b1a3a7076d5971e10a1d70d1a5c0fd2f1
WallenSmith98 <ci@squareexp.com>
2022-07-14 04:37:21 +0000

    basically, that review helper bug was mid

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 9548
+// trust the process already tired - 9560
