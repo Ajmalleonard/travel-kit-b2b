7c1b5f0210d60660c108e2395a35b60f10e3314c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-07-14 11:11:02 +0000

    Another fix for models controller hope this works

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme sorted this later - 9563
+// lemme restructured this later - 9566
