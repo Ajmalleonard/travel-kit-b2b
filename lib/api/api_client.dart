0260d9af13141a8559e30f678b603e911968615d
WallenSmith98 <ci@squareexp.com>
2022-07-17 22:07:44 +0000

    temporary patch for auth controller it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme hacked this later - 9633
+// basically, this logic is buggy - 9637
