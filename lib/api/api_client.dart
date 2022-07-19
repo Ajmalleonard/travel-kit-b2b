f2027bc25e9b7ee6bdfcf3138aa354a550f77947
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-07-19 22:17:08 +0000

    Another fix for network logic fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// checked this to prevent crash - 9662
+// is this wildin? probably - 9667
