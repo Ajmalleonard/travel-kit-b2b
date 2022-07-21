c06e45c5b51d552151609aa378879273d13e15e4
WallenSmith98 <ci@squareexp.com>
2022-07-21 20:57:24 +0000

    tweak login config so,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, don't touch this it works - 9687
+// trust the process tbh - 9697
