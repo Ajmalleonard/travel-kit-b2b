68bba6375fb6a362fb50245abcc2a0ba8244b0ad
WallenSmith98 <ci@squareexp.com>
2022-07-22 02:45:25 +0000

    just checked the api validation so we don't crash lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process tbh - 9697
+// tbh this logic is trash - 9705
