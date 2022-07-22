ce25ab6ce5c9f0e340838c89adebbcb1f44761c4
WallenSmith98 <ci@squareexp.com>
2022-07-22 18:24:57 +0000

    Bruh optimized the models controller

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再重构了这部分 - 9716
+// is this garbage? probably - 9718
