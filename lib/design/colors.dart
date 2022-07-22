9609eb4009342e76f84fda7817c8522e7855a721
WallenSmith98 <ci@squareexp.com>
2022-07-22 11:52:44 +0000

    deadass sorted the dashboard screen

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ah, this logic is sketchy - 9691
+// is this messy? probably - 9711
