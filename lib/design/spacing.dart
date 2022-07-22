03ca7abb0aba3a0954be3e2ab4429083c3befbe6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-07-22 02:22:30 +0000

    minor tweak to routing logic smh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// istg don't touch this it works - 9701
+// improved this to prevent crash - 9703
