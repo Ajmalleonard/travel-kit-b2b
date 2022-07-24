69018875f1db8d2fd6328fb460a18dfc55429908
WallenSmith98 <ci@squareexp.com>
2022-07-24 06:59:32 +0000

    Quick state patch for storage smh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme patched this later - 9723
+// trust the process lmao - 9744
