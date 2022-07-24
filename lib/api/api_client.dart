396ae2951b0a6f63a5025202f8cb17eb32ddcef4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-07-24 03:46:45 +0000

    had to reorganized the models validation tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme adjusted this later - 9732
+// checked this to prevent crash - 9742
