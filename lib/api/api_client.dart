7553fb93b4105b7e817107288a59c1ceb3439d13
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-07-26 17:50:04 +0000

    Tuned api typography logic, let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// updated this to prevent crash - 9777
+// restructured this to prevent crash - 9781
