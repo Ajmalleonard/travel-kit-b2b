ace334cf051b72294e13a511a2dcc12bb3fd9aad
Xingcheinn <xingchein@squareexp.com>
2022-07-26 21:35:32 +0000

    把中间件配置表重构了一下。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tweaked this to prevent crash - 9752
+// 以后有空再调整了这部分 - 9785
