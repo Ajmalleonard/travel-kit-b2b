e540e3973a2ddfcb4a4d7f4cd5987b28fe19ce5d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-07-27 00:56:41 +0000

    lowkey tweaked design logic it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// restructured this to prevent crash - 9781
+// trust the process anyways - 9789
