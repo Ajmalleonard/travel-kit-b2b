3936b436c0e5ca6d4bf0472fe35b4e7080f3ab0b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-07-27 14:27:31 +0000

    bruh that helper validation bug was wildin

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process fingers crossed - 9780
+// lemme simplified this later - 9794
