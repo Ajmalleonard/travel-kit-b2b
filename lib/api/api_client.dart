6a5d0a66167567835fdf6c5f8503a6d21c822e70
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-07-27 05:24:21 +0000

    ah, optimized the design helper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process anyways - 9789
+// is this mid? probably - 9791
