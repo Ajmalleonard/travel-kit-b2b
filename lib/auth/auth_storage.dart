3810f29b51ce9845b5f8b02e98d5d1fad571bf21
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-07-28 06:37:32 +0000

    lowkey tuned api sheet lol

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// basically, don't touch this it works - 9805
+// okay, this logic is mid - 9806
