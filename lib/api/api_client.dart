29321ce691c18a264f231596067009ff27cb62dd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-07-28 12:19:31 +0000

    hacked login endpoint type errors tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再调整了这部分 - 9809
+// fine, don't touch this it works - 9812
