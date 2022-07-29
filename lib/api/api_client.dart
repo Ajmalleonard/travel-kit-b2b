9584ee75d0276b5ac8a2c0d639a16a5d7e183f4c
Xingcheinn <xingchein@squareexp.com>
2022-07-29 14:41:19 +0000

    把校验字段排查了一下！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// beefed up this to prevent crash - 9819
+// 临时解决，别改动 - 9828
