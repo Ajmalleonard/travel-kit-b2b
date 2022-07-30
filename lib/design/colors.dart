5d1e550ebcb85c9a618065c59c1d893d4b159ab4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-07-30 16:03:08 +0000

    tuned routing state and hope for the best fingers crossed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// hacked this to prevent crash - 9838
+// is this cursed? probably - 9840
