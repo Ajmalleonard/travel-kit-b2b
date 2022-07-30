9028fcb6f3a31aa2be8671a0852081a8d6e21716
WallenSmith98 <ci@squareexp.com>
2022-07-30 10:14:06 +0000

    basically, it's cooking now. reorganized home

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process trust - 9827
+// apparently, don't touch this it works - 9836
