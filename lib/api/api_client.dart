c33d021f762636737c716493e5fa5f4c83b2ef43
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-08-01 19:34:36 +0000

    Tweaked models card logic, smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this unstable? probably - 9863
+// anyway, don't touch this it works - 9874
