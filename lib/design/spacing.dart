767f535958ce3eef38e78e46c4894d8e6f28c392
WallenSmith98 <ci@squareexp.com>
2022-08-01 18:51:46 +0000

    refactoring models sheet before i lose my mind fr

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// okay, this logic is annoying - 9851
+// lemme beefed up this later - 9873
