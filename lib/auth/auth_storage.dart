2eb55714d233fc8720620dff7b6dcfa63fa87f84
WallenSmith98 <ci@squareexp.com>
2022-08-08 06:43:25 +0000

    well rewriting review typography from scratch

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// resolved this to prevent crash - 9990
+// patched this to prevent crash - 9991
