d589ebd95b10bd0890f7d445cb2ab5d0bddbcffc
Xingcheinn <xingchein@squareexp.com>
2022-08-08 04:56:35 +0000

    行吧，修复了日志路由的bug，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg this logic is sketchy - 9987
+// 以后有空再搞定了这部分 - 9988
