d7adbf811b35d7299250700fccbda571846fb559
WallenSmith98 <ci@squareexp.com>
2022-08-11 01:08:00 +0000

    fixed review params type errors apparently,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this sketchy? probably - 10030
+// is this trash? probably - 10043
