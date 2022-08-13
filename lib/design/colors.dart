9ac4b7bf52c0b92d91c654748b604719acd8cea4
Xingcheinn <xingchein@squareexp.com>
2022-08-13 20:19:32 +0000

    调整了数据库接口的并发问题，累死了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// sorted this to prevent crash - 10080
+// 为了防止乱写的 - 10085
