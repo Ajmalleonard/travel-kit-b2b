3909fc509a620742e8b3c8b2805ca67300bf7d16
Xingcheinn <xingchein@squareexp.com>
2022-08-13 23:49:33 +0000

    哎简化了校验代码，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cluttered? probably - 10084
+// 以后有空再搞定了这部分 - 10088
