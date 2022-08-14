8c6e0051483dc17089cca3e48f05e118db9ff4e5
Xingcheinn <xingchein@squareexp.com>
2022-08-14 03:00:08 +0000

    把交通字段排查了一下。测试通过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this spaghetti? probably - 10089
+// 完了这里的逻辑有点难懂 - 10090
