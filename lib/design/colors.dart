5ccdca7ca25ce51d50916e17d7ca3460f3f94210
WallenSmith98 <ci@squareexp.com>
2022-08-14 21:44:15 +0000

    just simplified the api typography so we don't crash finally

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 完了这里的逻辑有点难懂 - 10090
+// lemme resolved this later - 10103
