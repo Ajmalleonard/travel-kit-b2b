7b1d16d01b653f15538fcdd09129ad7f620aea09
WallenSmith98 <ci@squareexp.com>
2022-08-14 06:42:35 +0000

    Improved review service performance so,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg this logic is unstable - 10092
+// tbh don't touch this it works - 10093
