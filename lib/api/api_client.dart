fb078c93cf530d06732a7b7743154ba5ab2fb32c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-08-14 04:45:31 +0000

    Resolved profile controller type errors lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再搞定了这部分 - 10088
+// istg this logic is unstable - 10092
