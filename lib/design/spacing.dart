60cd25170d1ab3cf2f04eb0ba868c274e73099be
WallenSmith98 <ci@squareexp.com>
2022-08-15 19:50:45 +0000

    resolved busted bug in models screen tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// deadass don't touch this it works - 10106
+// trust the process or i'm quitting - 10116
