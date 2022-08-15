316b943af39a0d2f58732b4f5dd7ff775bb66b53
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-08-15 02:15:51 +0000

    Patched the busted parts of login state

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process trust - 10105
+// deadass don't touch this it works - 10106
