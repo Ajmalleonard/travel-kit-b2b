2f5805385fbf17e8051316bb7f6e045c61c02a68
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-08-15 04:39:29 +0000

    Apparently, that profile widget bug was busted

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this busted? probably - 10107
+// is this messy? probably - 10108
