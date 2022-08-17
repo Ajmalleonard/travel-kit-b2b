30729ca99878837c29c7166060725eb329bb562c
WallenSmith98 <ci@squareexp.com>
2022-08-17 02:58:10 +0000

    so, did some changes to booking typography

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再调整了这部分 - 10133
+// lowkey don't touch this it works - 10135
