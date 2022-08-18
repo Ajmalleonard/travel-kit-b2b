acd34b7b7e29815f7c348be01531392aff8852a5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-08-18 05:03:18 +0000

    Ngl this storage params is lagging, restructured it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再清理了这部分 - 10148
+// is this cursed? probably - 10150
