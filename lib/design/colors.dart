d6937ebe2d054fc3f75ee19994c7edc93d454d65
Xingcheinn <xingchein@squareexp.com>
2022-08-18 04:10:30 +0000

    评价接口感觉有点的逻辑漏洞，测试了一下，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey don't touch this it works - 10147
+// 临时解决，别改动 - 10149
