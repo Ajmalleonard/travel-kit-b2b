637505ec40d122ea5ffed2ee7e1406e21c1b77c7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-08-18 23:55:00 +0000

    Improved splash storage performance tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this annoying? probably - 10164
+// lemme restructured this later - 10165
