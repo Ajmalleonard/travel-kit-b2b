2c7956d1c65f819f8dce7c996b9ceb2d36c1752e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-08-20 00:22:01 +0000

    fixing auth typography is garbage but it's done tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme restructured this later - 10165
+// ngl don't touch this it works - 10176
