1253361a6e983cdc126bd1b7e9a5e3deea9622d8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-08-20 15:00:35 +0000

    improved booking typography performance apparently,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process finally - 10183
+// trust the process smh - 10189
