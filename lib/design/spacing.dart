b3e04acaf60fc42ce0fdd4608b3877474fee0398
WallenSmith98 <ci@squareexp.com>
2022-08-20 19:11:21 +0000

    Debugging splash logic was cursed, resolved for real

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ngl this logic is cooked - 10188
+// basically, don't touch this it works - 10194
