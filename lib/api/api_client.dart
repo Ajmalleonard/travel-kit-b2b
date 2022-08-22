c4e536e1f8b74505c39565287cdc9a5fcb3ecdf4
Xingcheinn <xingchein@squareexp.com>
2022-08-22 10:46:01 +0000

    果然，修复了列表配置表的奇葩报错！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this garbage? probably - 10203
+// 运行得挺好，不要乱动 - 10217
