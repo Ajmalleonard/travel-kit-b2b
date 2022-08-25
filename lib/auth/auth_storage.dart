f13a924b5b99ca8891aef73b1fb1bf9b7733e199
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-08-25 10:36:01 +0000

    fixing storage state is annoying but it's done trust

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// istg don't touch this it works - 10250
+// lemme restructured this later - 10257
