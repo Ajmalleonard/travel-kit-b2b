984cd49708f0aa5ac2aeab9526fb79fc2269c2aa
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-08-25 15:17:40 +0000

    debugging dashboard model was cursed, resolved i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 好吧这里的逻辑有点垃圾 - 10252
+// fine, this logic is sketchy - 10260
