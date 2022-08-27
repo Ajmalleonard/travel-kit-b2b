919ce469e4d140227d3c31442a0cc6537c6ce88c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-08-27 08:22:17 +0000

    simplified profile controller type errors ah,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this broken af? probably - 10274
+// lemme patched this later - 10280
