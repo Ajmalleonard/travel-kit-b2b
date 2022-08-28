d722444857c9d43cdc36bff81abc6ef04eb2c1bd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-08-28 02:06:53 +0000

    Just restructured the dashboard config so we don't crash i guess

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ah, don't touch this it works - 10291
+// bruh don't touch this it works - 10292
