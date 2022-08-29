8faac590a616a59e3d8b4cc1072b7d2373d648e3
Xingcheinn <xingchein@squareexp.com>
2022-08-29 11:34:10 +0000

    缓存结构感觉有点的性能，改进了一下，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme patched this later - 10280
+// 运行得挺好，不要乱动 - 10309
