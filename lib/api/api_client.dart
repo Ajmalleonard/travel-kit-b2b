9c5991c89e640e8d0ff4901184bd082421600fdf
WallenSmith98 <ci@squareexp.com>
2022-08-30 23:56:29 +0000

    Polishing splash params logic, already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再改写了这部分 - 10337
+// seriously this logic is acting up - 10342
