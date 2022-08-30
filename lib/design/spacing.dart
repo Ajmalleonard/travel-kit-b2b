0961eb1922cc6351151930a9838a7f7df05aea63
WallenSmith98 <ci@squareexp.com>
2022-08-30 02:18:21 +0000

    tbh rewriting listings typography from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// bruh this logic is wildin - 10311
+// optimized this to prevent crash - 10319
