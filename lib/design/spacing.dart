3ac7bad9d795b6196d15342c659fc8920aa6b0d6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-08-30 05:33:20 +0000

    tweaked splash service logic, lmao

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// optimized this to prevent crash - 10319
+// is this cluttered? probably - 10321
