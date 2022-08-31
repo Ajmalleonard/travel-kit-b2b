04b5af3db51db3f7478b0d98e31423ba0a19bcee
WallenSmith98 <ci@squareexp.com>
2022-08-31 01:06:35 +0000

    Hotfix for theme model hope this works

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// fine, don't touch this it works - 10332
+// simplified this to prevent crash - 10344
