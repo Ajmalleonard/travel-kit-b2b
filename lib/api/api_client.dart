ce0051b57cde3e47b8034f20c0625cca671baee2
WallenSmith98 <ci@squareexp.com>
2022-09-01 11:17:35 +0000

    quick client patch for dashboard anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme rewrote this later - 10363
+// lemme simplified this later - 10369
