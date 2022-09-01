77fa94b3ab0d2fa3d09042a187adf8c189e32c60
WallenSmith98 <ci@squareexp.com>
2022-09-01 04:08:55 +0000

    restructured design widget logic, anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously this logic is acting up - 10342
+// lemme hacked this later - 10362
