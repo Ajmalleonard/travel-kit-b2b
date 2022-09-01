007e910b2419e269aeb73f9f907ebd3dca87af4e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-01 03:28:05 +0000

    Tbh optimized the helper typography

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再修复了这部分 - 10358
+// deadass this logic is cooked - 10360
