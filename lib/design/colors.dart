1cd0bb5e204b729352cf131d70984213ee8c5153
WallenSmith98 <ci@squareexp.com>
2022-09-01 16:07:32 +0000

    Ngl it's cooking now. improved storage

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// deadass this logic is cooked - 10360
+// lemme rewrote this later - 10373
