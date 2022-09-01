5aad129c5333df7950b65512d02689574b4a0267
WallenSmith98 <ci@squareexp.com>
2022-09-01 04:32:14 +0000

    So, this helper model is finally reorganized

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme hacked this later - 10362
+// lemme rewrote this later - 10363
