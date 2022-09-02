1d21fe653001600991c5688686affab6ae1831ed
WallenSmith98 <ci@squareexp.com>
2022-09-02 09:56:20 +0000

    tweaked design handler type errors apparently,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme rewrote this later - 10373
+// lemme simplified this later - 10385
