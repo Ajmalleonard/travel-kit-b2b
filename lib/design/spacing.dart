8be28cfe71365769adf2a5d909a15f7b4dec173f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-03 18:37:22 +0000

    sorted routing colors logic, anyways

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// optimized this to prevent crash - 10404
+// is this cursed? probably - 10412
