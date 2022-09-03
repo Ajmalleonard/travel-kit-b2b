ec5bef85e3729ce92708e8787a52e9c485c76003
WallenSmith98 <ci@squareexp.com>
2022-09-03 09:21:17 +0000

    improved helper validation type errors istg

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this garbage? probably - 10400
+// is this mid? probably - 10403
