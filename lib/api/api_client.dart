f4f30c77b239eaa987196c9e7ce914d479c9cb4d
WallenSmith98 <ci@squareexp.com>
2022-09-03 07:55:35 +0000

    Had to reorganized the helper screen ah,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme reorganized this later - 10391
+// is this garbage? probably - 10400
