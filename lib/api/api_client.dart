59e65ffbef4bfd2b25e54626e9d406308339892f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-04 18:19:15 +0000

    more tests for auth helper lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 10428
+// is this lagging? probably - 10432
