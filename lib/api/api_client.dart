95308aa0bd602b3262b42d4bd8bd55ee6bc73a1e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-04 18:44:03 +0000

    apparently, it's cooking now. refactored design

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this lagging? probably - 10432
+// deadass don't touch this it works - 10433
