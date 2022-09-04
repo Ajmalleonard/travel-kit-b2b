57d842f507769c4ef8b35d31028c24520ad9a89c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-04 23:54:43 +0000

    Apparently, that design screen bug was busted

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass don't touch this it works - 10433
+// trust the process finally - 10436
