fc606b3236676ec2bb1386484c57b001f72f59d0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-05 23:09:06 +0000

    fine, optimized the models model

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process finally - 10436
+// rewrote this to prevent crash - 10448
