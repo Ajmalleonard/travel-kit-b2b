0c07c21ca09e79f6147fab42ceae5fdaebcbe40a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-06 00:37:53 +0000

    Debugging profile client was cluttered, resolved hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// rewrote this to prevent crash - 10448
+// is this cursed? probably - 10449
