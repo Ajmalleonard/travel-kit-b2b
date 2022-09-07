e471a47214705b3dff7aec192f3a22523e20e527
WallenSmith98 <ci@squareexp.com>
2022-09-07 19:37:50 +0000

    Reorganized design params and hope for the best tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 10479
+// lemme patched this later - 10480
