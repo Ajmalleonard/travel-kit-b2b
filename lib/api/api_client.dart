c3ba0fa5b5fb6d782a296da6cc77ff5e8642e301
WallenSmith98 <ci@squareexp.com>
2022-09-08 10:41:12 +0000

    Deadass refactored the design logic

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme patched this later - 10480
+// bruh don't touch this it works - 10493
