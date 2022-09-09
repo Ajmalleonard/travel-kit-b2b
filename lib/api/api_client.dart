0a0784ba01d1ccc0debdf09d1429295b42f0b175
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-09 21:54:54 +0000

    I think i finally checked the network typography istg

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh don't touch this it works - 10493
+// is this busted? probably - 10516
