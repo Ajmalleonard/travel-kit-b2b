a7e9e0b1d2678a109035a8502d2298fb98c9ccfe
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-11 16:04:53 +0000

    tbh lemme adjusted the routing sheet real quick

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// well this logic is broken af - 10499
+// lemme tweaked this later - 10543
