c940b2858e6b9669578d6655f4ec759e25599bc6
Xingcheinn <xingchein@squareexp.com>
2022-09-13 03:10:50 +0000

    果然优化了配置路由，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, this logic is unstable - 10564
+// 终于这里的逻辑有点臃肿 - 10565
