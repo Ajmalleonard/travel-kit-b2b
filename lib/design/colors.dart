da446a4af459ede3407a321bb4d8e884fba784be
Xingcheinn <xingchein@squareexp.com>
2022-09-13 16:35:42 +0000

    说实话简化了交通结构！

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg this logic is wildin - 10572
+// 果然这里的逻辑有点乱 - 10573
