d777369a3ebe80f3ad5e837d55fe133d34f15bd7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-13 03:07:30 +0000

    tweak dashboard params fine,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 终于这里的逻辑有点乱 - 10563
+// okay, this logic is unstable - 10564
