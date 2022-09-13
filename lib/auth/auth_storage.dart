5bace28d22b472b4f719f7c25b7db3223040e4e1
WallenSmith98 <ci@squareexp.com>
2022-09-13 20:51:25 +0000

    Simplified listings validation logic apparently,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// anyway, don't touch this it works - 10555
+// so, this logic is sketchy - 10578
