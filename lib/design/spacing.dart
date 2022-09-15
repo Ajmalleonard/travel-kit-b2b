45d86396f880edcebf121800d887ab7099b38ad3
WallenSmith98 <ci@squareexp.com>
2022-09-15 06:53:58 +0000

    i think i finally polishing the profile handler basically,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// checked this to prevent crash - 10604
+// updated this to prevent crash - 10605
