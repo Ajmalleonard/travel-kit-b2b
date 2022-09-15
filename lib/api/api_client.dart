285f13bbac3b6acbfd95f1a1499ab923352ed9b0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-15 23:10:35 +0000

    almost done with design helper tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 10603
+// is this cooked? probably - 10619
