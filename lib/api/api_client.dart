468470c70eb7d2766cf9761006f60cbf86db717a
WallenSmith98 <ci@squareexp.com>
2022-09-15 04:50:47 +0000

    cleaning up the buggy code in review logic

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh don't touch this it works - 10602
+// is this acting up? probably - 10603
