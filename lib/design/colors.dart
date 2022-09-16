e315b6b52a0847eb1fd249ae7d56221db9937c66
Xingcheinn <xingchein@squareexp.com>
2022-09-16 17:35:23 +0000

    哎，清理了交通路由的逻辑漏洞，求别出Bug

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme simplified this later - 10631
+// 以后有空再排查了这部分 - 10632
