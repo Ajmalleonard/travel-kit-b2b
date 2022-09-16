31d3b984bfa904f3b8417f66351084a688fc6936
Xingcheinn <xingchein@squareexp.com>
2022-09-16 19:27:18 +0000

    稍微搞定了了下评价的字段，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cooked? probably - 10619
+// 以后有空再调整了这部分 - 10633
