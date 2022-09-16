38aa2fbfd5bc6bd6fb78ca1eb579abdb84d0f28c
WallenSmith98 <ci@squareexp.com>
2022-09-16 19:28:48 +0000

    resolved acting up bug in booking validation anyway,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再调整了这部分 - 10633
+// hacked this to prevent crash - 10634
