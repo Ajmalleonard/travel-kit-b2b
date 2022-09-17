b0ceb5853cf7e4573ff5f4e8053af43157fb1ad5
Xingcheinn <xingchein@squareexp.com>
2022-09-17 16:05:54 +0000

    重构了校验接口的类型错误，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// hacked this to prevent crash - 10634
+// 以后有空再搞定了这部分 - 10652
