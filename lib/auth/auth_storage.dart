24929c7677c64d9ca4756f96ebdc649e6c5dbd02
WallenSmith98 <ci@squareexp.com>
2022-09-17 03:14:56 +0000

    tbh optimized the splash endpoint

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// oof, this logic is spaghetti - 10640
+// trust the process already tired - 10641
