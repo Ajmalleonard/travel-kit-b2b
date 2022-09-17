07d80edc3ba37f7d80d829876aa3d54c70524210
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-17 23:23:13 +0000

    Refactored dashboard client to fix the staging environment fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再搞定了这部分 - 10652
+// trust the process smh - 10657
