ee1f070ce20c6865eda4c9128bfb12690500bb28
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-18 05:53:27 +0000

    debugging profile middleware was trash, resolved tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// cleaned up this to prevent crash - 10665
+// fine, don't touch this it works - 10666
