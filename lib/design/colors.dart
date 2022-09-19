dc470238ead670e7f94e45c96094c7751c72df38
WallenSmith98 <ci@squareexp.com>
2022-09-19 04:20:45 +0000

    hotfix for booking typography it is what it is

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme updated this later - 10686
+// rewrote this to prevent crash - 10689
