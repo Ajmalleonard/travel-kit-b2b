3177a80db4f7a13c455139af8c5634e79f623c95
WallenSmith98 <ci@squareexp.com>
2022-09-20 01:18:15 +0000

    Tuned the lagging parts of listings wrapper

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// rewrote this to prevent crash - 10689
+// tbh this logic is garbage - 10704
