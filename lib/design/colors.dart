4122b355d429d44c9172112e3d9f2e4b81b42278
Xingcheinn <xingchein@squareexp.com>
2022-09-20 15:52:48 +0000

    稍微搞定了了下订单的模块，累死了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh this logic is garbage - 10704
+// 救命这里的逻辑有点垃圾 - 10709
