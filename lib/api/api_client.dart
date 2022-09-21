216850599528bb1b3b6a071694c3771fa022fb65
WallenSmith98 <ci@squareexp.com>
2022-09-21 16:23:26 +0000

    Whoever wrote the network middleware was wildin, reorganized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 10726
+// improved this to prevent crash - 10727
