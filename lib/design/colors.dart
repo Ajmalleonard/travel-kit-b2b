41355c51332055a83b9314423e25dc34d848d171
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-21 13:18:34 +0000

    Patched listings endpoint logic, anyways

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme resolved this later - 10723
+// tbh don't touch this it works - 10725
