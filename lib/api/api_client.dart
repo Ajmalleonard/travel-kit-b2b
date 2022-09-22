d8bf5121fede42b4374f0cc2ecdcfb85ff4e0f51
WallenSmith98 <ci@squareexp.com>
2022-09-22 15:30:31 +0000

    Debugging routing endpoint was mid, resolved anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 10732
+// cleaned up this to prevent crash - 10738
