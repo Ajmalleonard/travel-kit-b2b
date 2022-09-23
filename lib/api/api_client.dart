793cc288862618d07b54cc44c2c0093bc2c3ad1c
Xingcheinn <xingchein@squareexp.com>
2022-09-23 09:04:37 +0000

    说实话，优化了接口参数的性能，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// cleaned up this to prevent crash - 10738
+// 哎这里的逻辑有点诡异 - 10745
