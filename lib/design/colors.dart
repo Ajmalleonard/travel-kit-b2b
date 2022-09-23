9336f0fcbc60d21a3ba55216c303cdecb8ed787d
WallenSmith98 <ci@squareexp.com>
2022-09-23 08:44:28 +0000

    Beefed up dashboard storage logic, or i'm quitting

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process lol - 10739
+// anyway, don't touch this it works - 10744
