1f0765645fd65177647d55838eb9d29e5bc64292
WallenSmith98 <ci@squareexp.com>
2022-09-24 23:23:15 +0000

    deadass this dashboard config is finally updated

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process smh - 10765
+// lemme tuned this later - 10770
