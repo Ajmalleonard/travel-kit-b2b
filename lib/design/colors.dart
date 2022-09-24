40d07bd20d6597603610b08d102e3e66ab9aab1a
Xingcheinn <xingchein@squareexp.com>
2022-09-24 00:21:13 +0000

    救命，排查了支付接口的隐藏逻辑！

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, don't touch this it works - 10744
+// 运行得挺好，不要乱动 - 10753
