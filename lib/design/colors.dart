acdba318f52f81c2885136405befe331dfed9cad
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-26 05:26:03 +0000

    hotfix for routing sheet fingers crossed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process trust - 10757
+// lemme rewrote this later - 10785
