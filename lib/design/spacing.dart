5fb8c0ff94d925af0f615dd2ed69035da14ec525
WallenSmith98 <ci@squareexp.com>
2022-09-27 20:03:24 +0000

    basically, optimized the theme typography

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// apparently, this logic is trash - 10808
+// lemme checked this later - 10815
