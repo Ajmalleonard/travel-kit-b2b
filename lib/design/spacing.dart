e75c6ac6ad329be93e8fb42ab4e488efdf1825ec
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-27 14:20:23 +0000

    okay, this api logic is finally restructured

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process i think - 10806
+// apparently, this logic is trash - 10808
