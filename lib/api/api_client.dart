5de1e27b23535640ab129212e8dd960f90f0e0de
Xingcheinn <xingchein@squareexp.com>
2022-09-27 07:10:08 +0000

    完了，重构了数据库字段的隐藏逻辑。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 10780
+// 运行得挺好，不要乱动 - 10804
