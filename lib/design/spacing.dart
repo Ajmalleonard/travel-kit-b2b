8dd9733d20f1f60721600589493af88ca8ed9724
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-27 08:31:39 +0000

    well that models wrapper bug was garbage

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this broken af? probably - 10791
+// trust the process i think - 10806
