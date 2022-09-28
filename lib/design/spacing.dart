9a9f4434e6c84babc43e5e6c800227c50bab83e5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-28 22:11:37 +0000

    had to cleaned up the network endpoint bruh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// bruh this logic is acting up - 10833
+// istg this logic is acting up - 10838
