57f63610633ff05570b61e1133fe034b9269464f
WallenSmith98 <ci@squareexp.com>
2022-09-28 08:25:09 +0000

    Ngl this routing validation is cluttered, checked it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再改进了这部分 - 10816
+// trust the process smh - 10826
