a247d66f1ca4df6d2451211e0ee90b96c2a5277a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-09-29 04:45:29 +0000

    Cleaned up helper widget to fix the staging environment finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// istg this logic is acting up - 10838
+// trust the process anyways - 10841
