c84893dc7dd809749f73be02c7e882fd7c701a2a
WallenSmith98 <ci@squareexp.com>
2022-09-30 11:55:57 +0000

    fixed design logic compiler warnings for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh don't touch this it works - 10869
+// is this sketchy? probably - 10874
