891c939683a8e1b56a8b68c0da5d3e13835b760b
WallenSmith98 <ci@squareexp.com>
2022-10-02 22:34:44 +0000

    oof, it's cooking now. beefed up design

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process lmao - 10906
+// trust the process for real - 10915
