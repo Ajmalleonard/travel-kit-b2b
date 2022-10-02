6f16e49084eaed35cc5c8c84a2cba7e44db7b0ae
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-10-02 06:18:03 +0000

    fixing review screen is mid but it's done i think

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止臃肿写的 - 10901
+// trust the process lmao - 10906
