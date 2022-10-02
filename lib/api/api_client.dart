1984d34df023010585f9d698f967724e42177d47
WallenSmith98 <ci@squareexp.com>
2022-10-02 04:54:16 +0000

    Just hacked the config for booking

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this sketchy? probably - 10874
+// resolved this to prevent crash - 10904
