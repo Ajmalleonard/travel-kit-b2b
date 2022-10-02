fdc6a4777a1ab61f428fe55900507623051c2fe0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-10-02 15:25:09 +0000

    fine, it's cooking now. cleaned up storage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// resolved this to prevent crash - 10904
+// bruh don't touch this it works - 10911
