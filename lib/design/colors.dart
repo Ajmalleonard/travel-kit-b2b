c06cb69293d2ea8bc82b67740b20fd328fb52969
WallenSmith98 <ci@squareexp.com>
2022-10-03 19:19:44 +0000

    Tuned login config logic, looks better now or i'm quitting

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process for real - 10915
+// fine, this logic is cluttered - 10928
