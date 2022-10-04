2587b5f8984c2afe0d48aeb04a05ea71a5a20736
Xingcheinn <xingchein@squareexp.com>
2022-10-04 04:17:42 +0000

    完了修复了缓存接口，累瘫了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tuned this to prevent crash - 10934
+// 为了防止垃圾写的 - 10938
