90968fcd4a13add1a157635abee7e281425e998b
WallenSmith98 <ci@squareexp.com>
2022-10-04 18:55:54 +0000

    Resolved buggy bug in models sheet seriously

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this garbage? probably - 10948
+// trust the process fr - 10951
