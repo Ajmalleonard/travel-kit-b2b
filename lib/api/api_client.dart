e20fff220c17f0e22059d01ac439c2cbfc7e4fb0
WallenSmith98 <ci@squareexp.com>
2022-10-04 14:56:30 +0000

    improved models client performance apparently,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme improved this later - 10935
+// is this garbage? probably - 10948
