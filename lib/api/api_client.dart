179211105b68bfb69df3b483f7de0c7a4f755b2c
WallenSmith98 <ci@squareexp.com>
2022-10-04 01:13:34 +0000

    basically, lemme fixed the review model real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 10933
+// lemme improved this later - 10935
