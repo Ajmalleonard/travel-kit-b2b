65a775405a6883f04b2618d09415d48c10dbf08e
Xingcheinn <xingchein@squareexp.com>
2022-10-04 21:02:11 +0000

    改写了跨域模型的类型错误！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process fr - 10951
+// 行吧这里的逻辑有点诡异 - 10955
