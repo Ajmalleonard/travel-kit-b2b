08b0df663ed857556cd862a157457f1e3dd849dc
WallenSmith98 <ci@squareexp.com>
2022-10-06 13:22:24 +0000

    More tests for profile router let's pray

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再排查了这部分 - 10979
+// tweaked this to prevent crash - 10980
