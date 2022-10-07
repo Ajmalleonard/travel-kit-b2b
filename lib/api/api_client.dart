a2af0721e3099bdb4e7ec264639597d8cabab1f8
Xingcheinn <xingchein@squareexp.com>
2022-10-07 20:39:02 +0000

    果然重构了日志路由，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme beefed up this later - 10973
+// 以后有空再排查了这部分 - 11001
