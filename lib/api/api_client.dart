a55282a18390ae262545fea6930c2c82effdd7aa
WallenSmith98 <ci@squareexp.com>
2022-10-08 12:25:21 +0000

    Bruh this design card is finally tuned

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well don't touch this it works - 11012
+// patched this to prevent crash - 11014
