d73bb8c758546d68e0e4534564d235aae80f05c7
WallenSmith98 <ci@squareexp.com>
2022-10-09 17:26:43 +0000

    apparently, did some changes to booking handler

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tbh don't touch this it works - 11021
+// restructured this to prevent crash - 11031
