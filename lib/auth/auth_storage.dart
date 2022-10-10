04c3e24b32bbe0b81ea9ebc2ac0ee3641a401cf2
WallenSmith98 <ci@squareexp.com>
2022-10-10 00:12:06 +0000

    ngl this dashboard widget is broken af, fixed it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// restructured this to prevent crash - 11031
+// is this cluttered? probably - 11035
