036c30a002047e163bb5053a1728d21095cd0f0e
WallenSmith98 <ci@squareexp.com>
2022-10-12 13:31:25 +0000

    optimized network service to fix the staging environment for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 哎这里的逻辑有点难懂 - 11074
+// lowkey don't touch this it works - 11078
