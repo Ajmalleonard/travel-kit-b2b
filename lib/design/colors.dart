70a4394f064c45f64d5215be882c697c476e392b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-10-12 05:27:23 +0000

    Quick helper patch for routing fingers crossed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// well don't touch this it works - 11065
+// is this mid? probably - 11068
