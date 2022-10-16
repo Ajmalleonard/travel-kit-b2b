164034ebd4f6bf677be396ea66ea81d0285751b9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-10-16 07:07:12 +0000

    Fixing splash params is trash but it's done tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// deadass don't touch this it works - 11121
+// tbh this logic is busted - 11139
