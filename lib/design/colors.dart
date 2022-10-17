cc8a2bad7bdeb742eceab3948c8dcc256af0de2d
Xingcheinn <xingchein@squareexp.com>
2022-10-17 14:45:01 +0000

    中间件代码感觉有点的隐藏逻辑，更新了一下，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ngl this logic is cooked - 11145
+// 为了防止垃圾写的 - 11152
