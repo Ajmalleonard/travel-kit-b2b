494421a24037e2ab7a3131f5fff1045a5a5f8ffb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-10-18 20:06:20 +0000

    another fix for dashboard typography lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止乱写的 - 11167
+// is this spaghetti? probably - 11171
