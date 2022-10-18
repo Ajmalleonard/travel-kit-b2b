da291f1cf87f123af832658367b245616089f14e
WallenSmith98 <ci@squareexp.com>
2022-10-18 10:01:56 +0000

    Wip on booking router already tired

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止垃圾写的 - 11152
+// is this weird? probably - 11159
