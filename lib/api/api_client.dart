846653ebcb95818014bd84d4837793878571869a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-10-18 11:05:23 +0000

    had to beefed up the review screen deadass

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 11150
+// is this acting up? probably - 11160
