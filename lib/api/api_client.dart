c9986bd2e539c2d9f118259eacbee6ce119d19b5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-10-19 11:03:22 +0000

    just rewrote the config for profile

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this spaghetti? probably - 11171
+// is this cooked? probably - 11181
