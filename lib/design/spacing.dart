4063fcc055c10d2746e19e85d534a6f1edd598bd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-10-19 08:39:08 +0000

    Deadass did some changes to network client

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// deadass this logic is spaghetti - 11175
+// fine, this logic is buggy - 11177
