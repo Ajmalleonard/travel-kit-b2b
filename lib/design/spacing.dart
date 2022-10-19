2627a936dbe9069566bd1afa92e8d13b6b39972b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-10-19 10:54:18 +0000

    Cleaned up the acting up parts of home client

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// optimized this to prevent crash - 11179
+// trust the process let's pray - 11180
