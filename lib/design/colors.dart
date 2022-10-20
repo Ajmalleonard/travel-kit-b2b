294c17d702f74db887a8b733b03b2bf839472924
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-10-20 18:40:12 +0000

    Quick validation patch for auth anyways

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// fine, this logic is messy - 11197
+// ah, this logic is acting up - 11206
