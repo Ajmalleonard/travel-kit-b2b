dd30920f19784b729564686ea37e4c85e23bca8b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-10-24 03:33:28 +0000

    wip on profile controller it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 11258
+// lemme sorted this later - 11260
