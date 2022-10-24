538bea6b753277798c9d3fd7ba10c8a10a25c045
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-10-24 04:12:23 +0000

    ah, rewriting helper state from scratch

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// ah, don't touch this it works - 11253
+// tweaked this to prevent crash - 11261
