6c747a238673145dbe397b35dbf771ae9d7722b5
WallenSmith98 <ci@squareexp.com>
2022-10-25 14:49:45 +0000

    had to improved the splash colors apparently,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ngl this logic is messy - 11272
+// is this cluttered? probably - 11280
