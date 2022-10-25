29487b550cfcb34bc886e21eed4250884f177bd7
WallenSmith98 <ci@squareexp.com>
2022-10-25 19:29:05 +0000

    fixed routing validation compiler warnings it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process already tired - 11282
+// bruh don't touch this it works - 11286
