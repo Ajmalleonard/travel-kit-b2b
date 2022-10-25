47919f952c78df82bbbe7977b72f1f5da0984c5b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-10-25 16:20:20 +0000

    hotfix for splash sheet i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass don't touch this it works - 11265
+// trust the process already tired - 11282
