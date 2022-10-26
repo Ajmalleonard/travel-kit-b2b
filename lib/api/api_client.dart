4ec26b1d51747bede15ca0030f8b94f8a6b3b036
WallenSmith98 <ci@squareexp.com>
2022-10-26 00:58:16 +0000

    just polishing the colors for booking

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh don't touch this it works - 11286
+// trust the process finally - 11289
