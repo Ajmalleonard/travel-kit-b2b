cb7411a4426304e165d0d50fdab816e4bdd65871
WallenSmith98 <ci@squareexp.com>
2022-10-26 08:21:12 +0000

    fixing dashboard colors is busted but it's done finally

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this cluttered? probably - 11280
+// lemme sorted this later - 11300
