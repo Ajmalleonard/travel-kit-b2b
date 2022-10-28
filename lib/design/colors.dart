2adf9d26f4ba2ca8ee5067b3dff7370fcfa90867
Xingcheinn <xingchein@squareexp.com>
2022-10-28 17:24:58 +0000

    把中间件错误处理排查了一下...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme rewrote this later - 11334
+// 以后有空再简化了这部分 - 11335
