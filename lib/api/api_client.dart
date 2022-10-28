cfc4c65abeedb2e4d7aa58de97e5a2cf1ef617a3
WallenSmith98 <ci@squareexp.com>
2022-10-28 00:45:36 +0000

    tweak theme screen fine,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this lagging? probably - 11310
+// patched this to prevent crash - 11320
