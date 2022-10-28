3ae42f67ff1c43b15fa53c8dac2260aee7b1808e
WallenSmith98 <ci@squareexp.com>
2022-10-28 15:53:30 +0000

    resolved spaghetti bug in design handler seriously

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// fixed this to prevent crash - 11332
+// lemme rewrote this later - 11334
