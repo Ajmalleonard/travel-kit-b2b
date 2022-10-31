ac49a26b058c55af10a202ddacc84391ad50a3ba
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-10-31 15:46:42 +0000

    Fixing listings logic is acting up but it's done trust

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tweaked this to prevent crash - 11375
+// is this mid? probably - 11380
