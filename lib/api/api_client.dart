80adcf70ca2cab806465f0313ba11b29aa6493fc
WallenSmith98 <ci@squareexp.com>
2022-10-31 09:34:28 +0000

    Ah, rewriting splash handler from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme refactored this later - 11345
+// checked this to prevent crash - 11377
