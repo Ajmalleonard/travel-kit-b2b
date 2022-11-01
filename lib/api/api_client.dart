238f108b75a7bac25911600be6d5fe84b6bf89aa
Xingcheinn <xingchein@squareexp.com>
2022-11-01 20:46:15 +0000

    行吧，重构了数据库路由的报错。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, don't touch this it works - 11379
+// 以后有空再搞定了这部分 - 11398
