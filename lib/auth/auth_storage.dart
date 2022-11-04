07d54f3e57ed49c6aef2f699230c04b0376ccd11
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-04 05:57:42 +0000

    Temporary patch for auth wrapper lmao

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme simplified this later - 11436
+// bruh don't touch this it works - 11438
