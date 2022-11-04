b9143f6dfef8f5aac7780cdf5fd908d0ab8a7f23
WallenSmith98 <ci@squareexp.com>
2022-11-04 19:27:09 +0000

    this splash endpoint ain't gonna work, updated it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// patched this to prevent crash - 11439
+// adjusted this to prevent crash - 11447
