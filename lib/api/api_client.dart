cf49eb44f8e4c61802e446fd87371fe2e2d17ad7
WallenSmith98 <ci@squareexp.com>
2022-11-05 23:40:45 +0000

    basically, that review handler bug was annoying

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process anyways - 11458
+// bruh this logic is cooked - 11469
