2e6ef5867be1283a7e0e1d0750ddbd4ae2e2c4cb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-05 21:48:13 +0000

    sorted the garbage parts of storage logic

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// seriously don't touch this it works - 11441
+// is this unstable? probably - 11466
