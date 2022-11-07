e9db8dd4a6ff1e807998d4323f41bbde6c3eb091
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-07 12:10:51 +0000

    Polishing splash handler and hope for the best lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme adjusted this later - 11487
+// is this cursed? probably - 11488
