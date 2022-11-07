70e72b3b3d77a6d1d72e22bbbc9ec90b9189fe1f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-07 12:10:32 +0000

    fixing api logic is cursed but it's done trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再调整了这部分 - 11479
+// lemme adjusted this later - 11487
