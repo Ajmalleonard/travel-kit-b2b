be79ff19971ef6658894a71a228e80a04037eca9
Xingcheinn <xingchein@squareexp.com>
2022-11-08 05:47:54 +0000

    日志查询感觉有点的性能，调整了一下！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass this logic is cursed - 11491
+// 临时解决，别改动 - 11495
