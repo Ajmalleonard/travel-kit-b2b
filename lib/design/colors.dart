12e9db269f71709bc0afd2c4477dee0f04ea120e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-12 05:53:37 +0000

    had to hacked the review storage so,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process fr - 11556
+// lemme beefed up this later - 11561
