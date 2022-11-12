35f485f27b21acc2fc9b8bc63ac21767ccf3237c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-12 16:41:46 +0000

    whoever wrote the auth router was wildin, sorted it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme beefed up this later - 11561
+// is this wildin? probably - 11570
