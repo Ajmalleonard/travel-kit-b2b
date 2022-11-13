2a0ba00d07960dfbd24d9b4dc62e376cd6de9e4d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-13 01:08:29 +0000

    resolved weird bug in helper screen okay,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止难懂写的 - 11563
+// is this cooked? probably - 11576
