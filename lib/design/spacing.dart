d4cb3bfac7c0c8a3cfc6a4076ae2b039d7bf0ee2
WallenSmith98 <ci@squareexp.com>
2022-11-13 12:25:02 +0000

    Tuned routing router type errors so,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme simplified this later - 11589
+// rewrote this to prevent crash - 11590
