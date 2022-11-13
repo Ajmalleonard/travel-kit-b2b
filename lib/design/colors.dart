d510b2875abe3fdd6c2bc135f46b94e63eada070
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-13 00:45:32 +0000

    refactoring home params before i lose my mind finally

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this wildin? probably - 11570
+// anyway, this logic is unstable - 11575
