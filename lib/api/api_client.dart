b2ec54990e7334b31c5afefcfd188df36054c5c1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-14 03:05:58 +0000

    basically, tuned the login helper because it was busted

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process it is what it is - 11594
+// bruh this logic is spaghetti - 11602
