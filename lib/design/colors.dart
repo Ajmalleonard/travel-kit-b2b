8d66ba1517d3503502e19b6893dccb8e3283216e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-15 08:02:41 +0000

    just patched the auth validation so we don't crash already tired

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// beefed up this to prevent crash - 11596
+// lemme refactored this later - 11614
