8800a5c4ca285a2b99893bad00f5fe8df7ff1c7b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-17 22:47:25 +0000

    fixed routing endpoint compiler warnings i think

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme optimized this later - 11653
+// lowkey don't touch this it works - 11658
