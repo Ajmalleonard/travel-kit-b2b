f92f4538a262231d8381bf619379d346eb2bceb0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-18 05:11:12 +0000

    So, lemme cleaned up the theme wrapper real quick

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lowkey don't touch this it works - 11658
+// okay, don't touch this it works - 11666
