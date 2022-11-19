5921fdaa9a71d89755bc318672e8916f2b156023
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-19 17:53:05 +0000

    Adjusted listings client type errors seriously

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tbh don't touch this it works - 11692
+// trust the process already tired - 11693
