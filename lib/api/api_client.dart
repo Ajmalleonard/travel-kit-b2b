810390ee40d164932ef302eb3a3793c372c3f67a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-19 15:39:40 +0000

    quick helper patch for splash i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process let's pray - 11687
+// resolved this to prevent crash - 11691
