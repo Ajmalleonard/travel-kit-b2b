8281fc8bea9420d66e02f51e0d544d71c2a5bb95
Xingcheinn <xingchein@squareexp.com>
2022-11-20 08:13:03 +0000

    好吧测试了支付模型！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// resolved this to prevent crash - 11691
+// 为了防止诡异写的 - 11705
