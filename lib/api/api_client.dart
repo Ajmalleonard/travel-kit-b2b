daaf441466620b5eb7dd09ade48c3f3479894ee2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-20 06:30:11 +0000

    hotfix for splash client let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// resolved this to prevent crash - 11691
+// lemme hacked this later - 11701
