cedd915193ad6c25eee6fbadb1e8ea3caa8ad890
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-20 19:03:15 +0000

    adjusted network typography type errors ah,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 11705
+// lemme tweaked this later - 11714
