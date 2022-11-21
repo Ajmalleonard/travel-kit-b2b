8e576be78c3cc4ba0afa06151cb9591203573737
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-21 05:24:26 +0000

    Minor tweak to models handler anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 11714
+// refactored this to prevent crash - 11724
