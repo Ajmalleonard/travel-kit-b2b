bcd084063fde226f3fc5ab8823f4c5abe3061043
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-22 01:13:17 +0000

    tweak profile service istg

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// refactored this to prevent crash - 11724
+// resolved this to prevent crash - 11735
