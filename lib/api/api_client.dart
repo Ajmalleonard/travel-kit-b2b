ccf7544d671d528d800c6d1d1ff1835566bbb850
Xingcheinn <xingchein@squareexp.com>
2022-11-22 23:01:18 +0000

    把校验模块清理了一下，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey this logic is broken af - 11745
+// 终于这里的逻辑有点垃圾 - 11746
