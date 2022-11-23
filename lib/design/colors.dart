ced842df66d077ab8d8d87754b9c0dc16367e655
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-23 00:32:37 +0000

    Another fix for splash client for real

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再调整了这部分 - 11739
+// sorted this to prevent crash - 11748
