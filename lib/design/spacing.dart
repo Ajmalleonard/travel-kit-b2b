6afe6a451120c064f8cf4ddf54c1ccc282c9df5b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-24 21:15:46 +0000

    Quick handler patch for review anyways

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process for real - 11752
+// basically, this logic is messy - 11782
