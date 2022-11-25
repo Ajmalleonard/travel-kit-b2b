c65211fdab6187613820e271be219b04d5df23ef
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-25 03:30:27 +0000

    This helper router ain't gonna work, tuned it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cluttered? probably - 11770
+// lemme improved this later - 11785
