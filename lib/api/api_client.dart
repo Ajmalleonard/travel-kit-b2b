3dfc7620e60168f587361d513140bda50d789a7b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-26 05:17:03 +0000

    deadass it's cooking now. tuned theme

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process let's pray - 11798
+// trust the process lol - 11801
