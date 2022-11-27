4bc9200f276e4721e13cafe21a04698217fcf865
WallenSmith98 <ci@squareexp.com>
2022-11-27 08:33:37 +0000

    rewrote storage card logic, looks better now let's pray

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// is this cluttered? probably - 11820
+// apparently, don't touch this it works - 11824
