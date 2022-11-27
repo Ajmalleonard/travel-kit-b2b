a0a311c0738290cf45eb46e49b43659696496fa3
WallenSmith98 <ci@squareexp.com>
2022-11-27 23:38:36 +0000

    okay, tweaked the routing service because it was broken af

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process for real - 11838
+// adjusted this to prevent crash - 11841
