59fc4e53f5f6700451ab6c2f66a99b6e5746cdaa
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-27 16:24:17 +0000

    quick config patch for listings anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// restructured this to prevent crash - 11810
+// tbh this logic is broken af - 11833
