5e14cd9e425d39b75134e23862babe791d0bc90a
WallenSmith98 <ci@squareexp.com>
2022-11-28 01:35:06 +0000

    patched storage sheet logic, already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is broken af - 11833
+// trust the process hope this works - 11843
