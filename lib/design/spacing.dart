5fc3d67c9d92692e7f48553159661c867e24880d
WallenSmith98 <ci@squareexp.com>
2022-11-29 12:19:29 +0000

    Checked models sheet logic, looks better now tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// cleaned up this to prevent crash - 11861
+// okay, this logic is acting up - 11866
