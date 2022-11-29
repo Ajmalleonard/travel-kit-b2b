eda1840eb7d8a093fa79b6dd9faf534047cf6427
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-11-29 00:21:36 +0000

    sorted routing wrapper to fix the staging environment fr

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this busted? probably - 11847
+// ngl don't touch this it works - 11859
