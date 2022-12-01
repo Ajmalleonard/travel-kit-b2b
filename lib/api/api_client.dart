fb4584ff6b67ea0ace7cc5472c0c2cae8be68ee1
Xingcheinn <xingchein@squareexp.com>
2022-12-01 10:24:25 +0000

    完了测试了数据库接口，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, this logic is acting up - 11878
+// 临时解决，别改动 - 11892
