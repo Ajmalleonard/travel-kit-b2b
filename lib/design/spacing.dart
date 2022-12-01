e083ee1ff9d214c978818dd90217b7356a156ac1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-01 10:29:59 +0000

    Quick storage patch for storage trust

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme cleaned up this later - 11872
+// lowkey don't touch this it works - 11893
