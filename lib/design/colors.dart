6a862ed2a49b0e2069d63fe241872fbd41b86efb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-01 23:48:56 +0000

    Resolved buggy bug in dashboard router so,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh don't touch this it works - 11901
+// so, don't touch this it works - 11904
