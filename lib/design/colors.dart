6cb6210c5539574da6ae40d9a2d0ab102f1cf1f3
WallenSmith98 <ci@squareexp.com>
2022-12-02 19:59:51 +0000

    tweak booking validation tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, don't touch this it works - 11904
+// bruh this logic is busted - 11920
