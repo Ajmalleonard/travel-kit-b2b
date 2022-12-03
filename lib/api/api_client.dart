2751718e67db92d9b59cf5e0bc709da35080f3d3
Xingcheinn <xingchein@squareexp.com>
2022-12-03 11:23:08 +0000

    稍微测试了了下跨域的字段！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process fr - 11911
+// 完了这里的逻辑有点难懂 - 11929
