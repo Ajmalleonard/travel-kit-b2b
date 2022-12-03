32f1daaddffa616b891110fe202baa785e6ac7ae
WallenSmith98 <ci@squareexp.com>
2022-12-03 20:28:48 +0000

    This review handler ain't gonna work, tuned it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process i guess - 11895
+// tbh don't touch this it works - 11936
