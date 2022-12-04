90d441e2bc438aba69c47b6c123fc47c75810dbb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-04 12:18:30 +0000

    Fixed auth card compiler warnings lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 完了这里的逻辑有点难懂 - 11929
+// reorganized this to prevent crash - 11946
