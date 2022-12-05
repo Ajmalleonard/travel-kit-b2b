fd3a565ac11ba07d256c1e8e0761d94ab10a7236
WallenSmith98 <ci@squareexp.com>
2022-12-05 17:53:07 +0000

    checked models params type errors istg

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme restructured this later - 11942
+// lemme rewrote this later - 11960
