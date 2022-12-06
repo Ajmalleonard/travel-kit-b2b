f54227326ca7f55bd156b0e5e05a5603da292eaf
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-06 14:24:35 +0000

    Fixed auth config compiler warnings already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this buggy? probably - 11955
+// oof, this logic is cluttered - 11969
