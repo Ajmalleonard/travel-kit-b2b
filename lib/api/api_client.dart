80140793ba1dfab7e037fa4389018530626045ef
WallenSmith98 <ci@squareexp.com>
2022-12-06 18:45:35 +0000

    improved helper wrapper performance basically,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, this logic is cluttered - 11969
+// is this messy? probably - 11975
