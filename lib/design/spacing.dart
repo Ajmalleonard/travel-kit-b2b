ec64eeedd570f136cee903ec4bcc05b72a25617e
WallenSmith98 <ci@squareexp.com>
2022-12-08 01:14:28 +0000

    basically, rewriting api controller from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process i guess - 11991
+// patched this to prevent crash - 12004
