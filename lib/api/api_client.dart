e40aebdcc1c73c7735476f18006fa8dfc44c42b1
WallenSmith98 <ci@squareexp.com>
2022-12-09 06:06:57 +0000

    Lowkey polishing booking helper i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme hacked this later - 12019
+// lemme fixed this later - 12021
