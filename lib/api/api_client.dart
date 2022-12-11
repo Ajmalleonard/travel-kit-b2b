75fd1dfbce0a58ed2784548bf3a9ed818f8a1cb5
WallenSmith98 <ci@squareexp.com>
2022-12-11 15:13:34 +0000

    Hotfix for review middleware lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 说实话这里的逻辑有点乱 - 12038
+// lemme beefed up this later - 12054
