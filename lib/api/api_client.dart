0499cb3d5574e340b103f7b8a50f986d32b04a01
WallenSmith98 <ci@squareexp.com>
2022-12-12 10:48:55 +0000

    sorted routing helper type errors deadass

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tuned this to prevent crash - 12065
+// tbh don't touch this it works - 12066
