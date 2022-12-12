5a4544650815279d59533726b37c4f0beeac3c54
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-12 06:08:07 +0000

    Had to restructured the routing router lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme beefed up this later - 12054
+// lemme simplified this later - 12062
