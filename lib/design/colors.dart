a09216331e09569b38cfcdf583d0cab4e1636a6c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-14 01:14:04 +0000

    temporary patch for network handler tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 天呐这里的逻辑有点臃肿 - 12087
+// tbh this logic is busted - 12089
