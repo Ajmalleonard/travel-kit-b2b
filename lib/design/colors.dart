ff0d5b704656eea37079832ad65285242abeacff
Xingcheinn <xingchein@squareexp.com>
2022-12-14 07:01:27 +0000

    稍微改进了了下鉴权的字段。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh this logic is busted - 12089
+// 救命这里的逻辑有点诡异 - 12096
