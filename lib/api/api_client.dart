9af0186288aad42dd01ee970a38f58dd25f5b4b0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-15 22:05:27 +0000

    cleaned up auth config and hope for the best hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme updated this later - 12119
+// lemme sorted this later - 12126
