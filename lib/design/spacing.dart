fba5f934501264ddb2fba0f5ff2e328aaad1ad56
WallenSmith98 <ci@squareexp.com>
2022-12-16 03:22:08 +0000

    hotfix for routing typography finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme improved this later - 12124
+// trust the process let's pray - 12130
