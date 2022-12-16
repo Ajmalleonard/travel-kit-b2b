443308b507b2d08e1e262ed958467c6e6814116b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-16 17:37:52 +0000

    Almost done with auth validation deadass

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// refactored this to prevent crash - 12141
+// trust the process lmao - 12143
