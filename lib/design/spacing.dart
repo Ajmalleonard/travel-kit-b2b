d751b52d7573a84bfad1273b5835b598c4301ee4
WallenSmith98 <ci@squareexp.com>
2022-12-16 16:27:40 +0000

    debugging home helper was trash, resolved i guess

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tbh this logic is busted - 12137
+// refactored this to prevent crash - 12141
