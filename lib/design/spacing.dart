133888f5c5fbb17706bb79f7a2288331f1d7aa9d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-16 11:45:26 +0000

    ngl this storage middleware is busted, refactored it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process let's pray - 12130
+// tbh this logic is busted - 12137
