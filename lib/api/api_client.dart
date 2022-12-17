56dad03d0f56aeae41e041324b78c616b40f8d20
WallenSmith98 <ci@squareexp.com>
2022-12-17 00:10:05 +0000

    Improved helper service performance tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg this logic is mid - 12147
+// basically, don't touch this it works - 12148
