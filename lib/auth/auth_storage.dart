9c63c49a47b8aae6b22468811397280ee3313d1e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-18 11:17:50 +0000

    temporary patch for models client lmao

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme reorganized this later - 12162
+// lemme restructured this later - 12164
