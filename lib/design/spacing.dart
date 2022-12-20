44a7b71c3d78e495ef392dc766ce304707e27b23
WallenSmith98 <ci@squareexp.com>
2022-12-20 15:10:14 +0000

    more tests for network client trust

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme sorted this later - 12187
+// restructured this to prevent crash - 12191
