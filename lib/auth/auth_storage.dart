1fc5a72c6a5dc1b5953a2c792d3f8e9a44061c97
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-21 16:54:23 +0000

    hotfix for api validation anyways

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lowkey this logic is busted - 12206
+// resolved this to prevent crash - 12208
