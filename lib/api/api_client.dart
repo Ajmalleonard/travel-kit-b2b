90944637d920bddf62268f94b3ca28a706e9d1d6
WallenSmith98 <ci@squareexp.com>
2022-12-21 08:01:18 +0000

    lowkey beefed up theme params fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// polishing this to prevent crash - 12186
+// lemme cleaned up this later - 12204
