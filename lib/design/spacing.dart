040e7cf859f21eee38ef3446186814c18659e2e0
WallenSmith98 <ci@squareexp.com>
2022-12-22 07:59:39 +0000

    temporary patch for auth state i guess

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this buggy? probably - 12214
+// seriously this logic is cooked - 12216
