0d43897d2668af3909c850104d868da101988e57
Xingcheinn <xingchein@squareexp.com>
2022-12-22 16:34:47 +0000

    果然，更新了评价路由的性能。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 12209
+// 行吧这里的逻辑有点乱 - 12224
