182ed767bdf4eb598eaffd8aa182e8f0050d3112
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-23 18:57:12 +0000

    lowkey simplified helper config i think

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process already tired - 12231
+// patched this to prevent crash - 12239
