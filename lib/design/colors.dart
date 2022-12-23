91e27835a4c8dbb919f8c9adda16a0a80fedfed0
WallenSmith98 <ci@squareexp.com>
2022-12-23 18:33:37 +0000

    simplified profile screen logic lowkey

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh don't touch this it works - 12219
+// ah, this logic is lagging - 12238
