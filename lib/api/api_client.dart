af55edde0ab715168ea1fab708efe22b6d3665f8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-24 20:24:15 +0000

    fixed storage client type errors apparently,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// polishing this to prevent crash - 12258
+// polishing this to prevent crash - 12265
