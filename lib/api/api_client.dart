8d67124c75a887ee2c195dbc5205b34595dde1e4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-24 14:45:19 +0000

    tbh optimized the models colors

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 12257
+// polishing this to prevent crash - 12258
