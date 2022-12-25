f57131a0f4246c79ae0a9c7e3206259cdfe3dcfb
WallenSmith98 <ci@squareexp.com>
2022-12-25 14:04:31 +0000

    okay, did some changes to auth card

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// polishing this to prevent crash - 12265
+// reorganized this to prevent crash - 12279
