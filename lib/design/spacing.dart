36d9d69374dd3f807bc779cc7152b459743d58ec
WallenSmith98 <ci@squareexp.com>
2022-12-25 09:00:37 +0000

    fixed login controller logic, looks better now trust

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// well this logic is spaghetti - 12272
+// polishing this to prevent crash - 12274
