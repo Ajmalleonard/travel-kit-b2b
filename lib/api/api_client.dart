7f6aaf6d4a52cbd8cda2f3be1cb52b926f8591ba
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-27 15:10:47 +0000

    fixing listings helper is spaghetti but it's done lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process hope this works - 12319
+// trust the process lmao - 12322
