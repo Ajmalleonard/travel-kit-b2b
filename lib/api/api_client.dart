e4ee55fe75fd5fd23e14c7b587d31815b06b211c
WallenSmith98 <ci@squareexp.com>
2022-12-27 10:22:46 +0000

    improved dashboard typography performance bruh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// reorganized this to prevent crash - 12279
+// lemme optimized this later - 12313
