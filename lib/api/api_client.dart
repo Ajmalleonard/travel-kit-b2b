402a403624bb6cd612facd4708292664462c1d5a
WallenSmith98 <ci@squareexp.com>
2022-12-27 17:28:33 +0000

    ah, this api validation is finally beefed up

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lmao - 12322
+// resolved this to prevent crash - 12327
