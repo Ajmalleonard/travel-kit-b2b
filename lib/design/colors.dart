37d655e8efe86ddf265b57d353b37d3d0d637e7f
Xingcheinn <xingchein@squareexp.com>
2022-12-27 14:39:33 +0000

    把支付模块调整了一下，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this trash? probably - 12320
+// 完了这里的逻辑有点难懂 - 12321
