e335c5d607e4fb72b38b50d821ee81d425cbec3f
WallenSmith98 <ci@squareexp.com>
2022-12-27 10:25:21 +0000

    deadass beefed up the theme service

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme optimized this later - 12313
+// okay, this logic is wildin - 12314
