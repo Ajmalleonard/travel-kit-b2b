8ad498b2b28ab7e1fee69542466644f66e2101de
WallenSmith98 <ci@squareexp.com>
2022-12-29 12:31:00 +0000

    hotfix for login state finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// reorganized this to prevent crash - 12336
+// lemme rewrote this later - 12351
