181e0b71ed9bc5228b6dd52b8e1d96ed7fa19e47
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-29 03:45:10 +0000

    polishing home card type errors basically,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// resolved this to prevent crash - 12327
+// lemme adjusted this later - 12346
