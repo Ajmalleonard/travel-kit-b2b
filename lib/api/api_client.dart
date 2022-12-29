5261077ad530e837a6c3faca3a54472111d60735
Xingcheinn <xingchein@squareexp.com>
2022-12-29 05:54:47 +0000

    数据库结构感觉有点的隐藏逻辑，重构了一下！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme adjusted this later - 12346
+// 完了这里的逻辑有点臃肿 - 12347
