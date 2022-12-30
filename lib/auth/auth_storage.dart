b5d251e584453386dacdf79dddeddac3fc4e0e3b
WallenSmith98 <ci@squareexp.com>
2022-12-30 10:40:00 +0000

    apparently, this booking client is finally fixed

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process hope this works - 12350
+// tuned this to prevent crash - 12369
