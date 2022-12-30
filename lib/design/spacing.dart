03f43f0fa085902551295c7d657b66575c1db719
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-30 15:04:54 +0000

    tweak login client seriously

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme optimized this later - 12359
+// okay, this logic is wildin - 12373
