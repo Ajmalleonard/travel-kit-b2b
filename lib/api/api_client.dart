ea28843ebaca6d9f1d2a14f3ce543342df90b2fe
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-31 03:38:10 +0000

    Lowkey patched auth config trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再清理了这部分 - 12365
+// checked this to prevent crash - 12386
