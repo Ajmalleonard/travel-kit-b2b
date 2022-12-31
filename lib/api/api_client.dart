9ce996e678a65bec38bd96bbc9b4c4a2abe3098f
Xingcheinn <xingchein@squareexp.com>
2022-12-31 19:12:28 +0000

    终于，清理了日志错误处理的bug，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// checked this to prevent crash - 12386
+// 完了这里的逻辑有点臃肿 - 12399
