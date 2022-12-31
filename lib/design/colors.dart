6f2d13874b3959639b548e04006b3c9860eb572f
Xingcheinn <xingchein@squareexp.com>
2022-12-31 21:16:27 +0000

    鉴权错误处理感觉有点的性能，搞定了一下，应该没问题了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme fixed this later - 12378
+// 临时解决，别改动 - 12402
