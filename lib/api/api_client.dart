09e2dc86398416b22cb762ac454213d0e45c2def
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2022-12-31 19:31:32 +0000

    cleaning up the cluttered code in login storage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 完了这里的逻辑有点臃肿 - 12399
+// ngl don't touch this it works - 12401
