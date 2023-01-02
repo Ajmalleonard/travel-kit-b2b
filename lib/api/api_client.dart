26c35be01df812e5a456d74ee64589f11c3e8475
Xingcheinn <xingchein@squareexp.com>
2023-01-02 12:30:28 +0000

    把交通代码测试了一下，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously don't touch this it works - 12416
+// 以后有空再改进了这部分 - 12432
