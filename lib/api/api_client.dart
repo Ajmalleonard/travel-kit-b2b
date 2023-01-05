9f9d427afbba309141e4cf69bcf889437410fe52
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-05 02:42:44 +0000

    I think i finally patched the design router lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cluttered? probably - 12459
+// trust the process finally - 12469
