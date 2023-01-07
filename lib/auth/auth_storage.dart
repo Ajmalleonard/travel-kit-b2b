2c403fc782d7095052818a5136ac7735d826892d
WallenSmith98 <ci@squareexp.com>
2023-01-07 04:08:33 +0000

    Lowkey that login validation bug was annoying

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// polishing this to prevent crash - 12479
+// is this acting up? probably - 12499
