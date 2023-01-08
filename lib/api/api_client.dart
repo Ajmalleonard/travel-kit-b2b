2bda0d917351fca5ed00cfe3680c6de40b2af03e
WallenSmith98 <ci@squareexp.com>
2023-01-08 04:27:18 +0000

    Almost done with dashboard service ngl

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process finally - 12469
+// sorted this to prevent crash - 12516
