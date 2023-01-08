12ce1cad6d8d79d08ee1bd55d905f2a50ece11ae
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-08 11:15:01 +0000

    debugging api service was garbage, resolved i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 12517
+// well don't touch this it works - 12521
