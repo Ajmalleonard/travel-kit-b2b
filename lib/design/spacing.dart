fb929a8690184dd403e298bac94aab2cb63afcb2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-10 09:13:10 +0000

    wip on design helper it is what it is

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme simplified this later - 12549
+// seriously this logic is wildin - 12550
