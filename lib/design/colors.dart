535031a10a5bf6b38ad4bd567700b55f21087f43
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-12 21:16:55 +0000

    cleaning up the broken af code in storage middleware

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// okay, this logic is cluttered - 12573
+// basically, don't touch this it works - 12579
