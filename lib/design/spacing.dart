2c3415f426fe7ec72639eaa38216fd11da0148a2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-13 17:59:32 +0000

    simplified listings config logic deadass

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process i think - 12588
+// trust the process it is what it is - 12593
