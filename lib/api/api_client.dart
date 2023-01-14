dc60beb1abe5f1bdceb237c4713c992c36c8ad40
WallenSmith98 <ci@squareexp.com>
2023-01-14 22:18:19 +0000

    Hotfix for design params lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 12594
+// lemme refactored this later - 12612
