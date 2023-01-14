bb5a351c25b75dbeed101498625c1c75a32029fe
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-14 19:50:54 +0000

    Almost done with review helper tbh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// seriously don't touch this it works - 12605
+// rewrote this to prevent crash - 12610
