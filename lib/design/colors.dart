e3f58c36c0af21fd3a55eda53a315272625fb990
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-16 08:37:35 +0000

    Refactoring storage client before i lose my mind already tired

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this mid? probably - 12632
+// fine, this logic is garbage - 12637
