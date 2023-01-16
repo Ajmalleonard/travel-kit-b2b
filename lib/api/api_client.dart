cb2e272212286a366480aa136dcb12831fc3e05a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-16 09:28:52 +0000

    tweaked network model type errors okay,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 12634
+// trust the process anyways - 12638
