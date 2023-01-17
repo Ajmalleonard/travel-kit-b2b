ac43cbf2c90e8de539193a1b0635284450b840d5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-17 10:43:31 +0000

    Simplified auth client type errors ah,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// oof, this logic is cluttered - 12651
+// trust the process it is what it is - 12662
