7ab5567a046710560b3e235154a4dac95ac24d45
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-18 15:00:36 +0000

    tweak auth config tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 12664
+// istg this logic is cluttered - 12680
