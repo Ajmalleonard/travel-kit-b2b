ee4efb846417e00dd84b669e89f3bb832acbc4c9
WallenSmith98 <ci@squareexp.com>
2023-01-18 22:52:49 +0000

    quick screen patch for helper or i'm quitting

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process hope this works - 12672
+// is this spaghetti? probably - 12683
