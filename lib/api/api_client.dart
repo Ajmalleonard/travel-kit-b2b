8bbbb3d26d95e852ff03b1be1ebfd7c56dca6777
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-19 12:26:07 +0000

    refactoring booking logic before i lose my mind lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg this logic is cluttered - 12680
+// patched this to prevent crash - 12693
