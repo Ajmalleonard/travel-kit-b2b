54998c8e260cc8c2155196b27f88353d688ba6c8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-19 21:49:48 +0000

    Ngl this api wrapper is annoying, polishing it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// patched this to prevent crash - 12693
+// seriously don't touch this it works - 12701
