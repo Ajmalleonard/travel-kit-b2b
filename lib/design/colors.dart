73c4ca412f08f4cd2703e9296ce088df12c166d6
WallenSmith98 <ci@squareexp.com>
2023-01-20 02:33:17 +0000

    Lowkey tuned design storage lmao

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// deadass this logic is mid - 12702
+// lemme updated this later - 12703
