9a0efa55086b39510fb40781e3b7e4739a4dc0d5
Xingcheinn <xingchein@squareexp.com>
2023-01-20 05:07:45 +0000

    果然，优化了数据库接口的隐藏逻辑，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously don't touch this it works - 12701
+// 临时解决，别改动 - 12706
