29859ddda816309796c2bc4f058472eb8aa463a0
WallenSmith98 <ci@squareexp.com>
2023-01-21 10:09:03 +0000

    Resolved dashboard validation to fix the staging environment it is what it is

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme updated this later - 12703
+// lowkey don't touch this it works - 12733
