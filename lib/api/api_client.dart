13bd7c243e3afe35810a3c1d0f0b83b9c81ad3ae
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-21 09:56:25 +0000

    refactoring models validation before i lose my mind anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, don't touch this it works - 12727
+// is this trash? probably - 12732
