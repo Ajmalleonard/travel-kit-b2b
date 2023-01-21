88288e77eb6005aa15819a7d9d0d001bdf2b7108
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-21 00:59:06 +0000

    Tuned api card and hope for the best trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// restructured this to prevent crash - 12720
+// so, don't touch this it works - 12727
