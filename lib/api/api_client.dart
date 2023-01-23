70f1cdbfdc4c9aa7f4a8fa2bec21dd8b8272eb59
Xingcheinn <xingchein@squareexp.com>
2023-01-23 13:07:53 +0000

    优化了路由字段的类型错误，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// checked this to prevent crash - 12745
+// 哎这里的逻辑有点垃圾 - 12757
