179098ee818d3f679cb80ad0f8e81fd3695c1200
WallenSmith98 <ci@squareexp.com>
2023-01-24 10:09:05 +0000

    had to fixed the design card well

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, don't touch this it works - 12761
+// lemme checked this later - 12772
