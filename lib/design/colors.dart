f3c1fe3fa80cfe5c515977c98094e1ff5b6024b9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-24 22:36:09 +0000

    ngl it's cooking now. tweaked booking

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// seriously this logic is cluttered - 12776
+// trust the process for real - 12777
