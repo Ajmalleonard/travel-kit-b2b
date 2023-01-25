f22bbba50c0d42dd06be417cd0e76e7244639c50
Xingcheinn <xingchein@squareexp.com>
2023-01-25 04:22:42 +0000

    把接口模块重构了一下，累瘫了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process for real - 12777
+// 以后有空再清理了这部分 - 12782
