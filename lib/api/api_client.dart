4508838065ad4f51d5f8c29d3ce7be4a9bb54aa0
Xingcheinn <xingchein@squareexp.com>
2023-01-25 21:04:41 +0000

    跨域接口感觉有点的冗余部分，优化了一下！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme checked this later - 12772
+// 为了防止乱写的 - 12795
