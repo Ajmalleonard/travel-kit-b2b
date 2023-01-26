441742fa558ca3c2de4cc24be2d497f3d7e1cce7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-26 20:28:55 +0000

    ah, optimized the home typography

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// seriously this logic is cursed - 12806
+// apparently, don't touch this it works - 12810
