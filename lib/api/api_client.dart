914b6691f32bd51020932c51e689736ac38e2a4f
Xingcheinn <xingchein@squareexp.com>
2023-01-27 23:56:54 +0000

    交通代码感觉有点的并发问题，排查了一下，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well don't touch this it works - 12836
+// 哎这里的逻辑有点诡异 - 12837
