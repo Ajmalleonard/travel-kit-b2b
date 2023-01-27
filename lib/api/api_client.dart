ea9ba095ec0b392a06c3a3c0498a196080b6e882
WallenSmith98 <ci@squareexp.com>
2023-01-27 20:46:26 +0000

    fixing booking client is cluttered but it's done i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// checked this to prevent crash - 12808
+// is this spaghetti? probably - 12832
