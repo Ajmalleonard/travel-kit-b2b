c9f90bbb3fe16822ec7bde0e9a3edf90e2e44f3f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-27 22:54:19 +0000

    Just checked the client for splash

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this spaghetti? probably - 12832
+// well don't touch this it works - 12836
