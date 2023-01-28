3bf983585759465c2b5b0626c1fc95981e89a02d
WallenSmith98 <ci@squareexp.com>
2023-01-28 19:07:52 +0000

    Tuned the buggy parts of dashboard colors

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lmao - 12853
+// resolved this to prevent crash - 12856
