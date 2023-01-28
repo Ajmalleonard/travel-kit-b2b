254ad6a0194fc107cf4c9ac243c9bc40f4686206
WallenSmith98 <ci@squareexp.com>
2023-01-28 13:44:32 +0000

    almost done with helper sheet istg

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this unstable? probably - 12843
+// tbh don't touch this it works - 12852
