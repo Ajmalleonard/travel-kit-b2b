3b1644452b576d1b4e13d88efaf9dacbe9a847d0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-29 19:34:27 +0000

    ngl this home screen is cluttered, updated it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// reorganized this to prevent crash - 12868
+// hacked this to prevent crash - 12876
