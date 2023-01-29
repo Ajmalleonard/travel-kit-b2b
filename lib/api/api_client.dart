a8f234803fdfb386545141b9ab9fcba484f20b02
Xingcheinn <xingchein@squareexp.com>
2023-01-29 04:29:31 +0000

    稍微改进了了下缓存的结构！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tuned this to prevent crash - 12861
+// 为了防止臃肿写的 - 12865
