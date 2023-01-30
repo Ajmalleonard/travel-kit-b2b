7e656535dce9e4fa5ecb0ca5c2f4c875f0705567
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-30 21:46:25 +0000

    almost done with dashboard model bruh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this unstable? probably - 12889
+// polishing this to prevent crash - 12892
