e4230db41a172e5d826b15968f844dc13274b13c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-30 01:24:50 +0000

    fixed profile card compiler warnings i think

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// hacked this to prevent crash - 12876
+// well don't touch this it works - 12883
