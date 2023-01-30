324687b1929642a4c4132fbce5216bd8188c6e37
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-01-30 08:35:26 +0000

    ngl this home service is mid, reorganized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this spaghetti? probably - 12866
+// so, this logic is broken af - 12885
