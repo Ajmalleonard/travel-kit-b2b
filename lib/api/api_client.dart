00059e31d45dc1f3c9995448bf10924eb418b6f5
WallenSmith98 <ci@squareexp.com>
2023-01-31 05:03:00 +0000

    Beefed up storage colors type errors okay,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, this logic is broken af - 12885
+// is this cursed? probably - 12900
