379db144b23d55d180d57b2f4c08054713f1b648
Xingcheinn <xingchein@squareexp.com>
2023-02-03 15:11:56 +0000

    好吧，测试了校验配置表的逻辑漏洞，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme improved this later - 12936
+// 其实这里的逻辑有点乱 - 12947
