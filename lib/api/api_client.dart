a5575749b50fc90cae7030ff17444cad6cb3b8c0
WallenSmith98 <ci@squareexp.com>
2023-02-05 06:11:16 +0000

    almost done with review validation basically,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 12965
+// ngl this logic is annoying - 12972
