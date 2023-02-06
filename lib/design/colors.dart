8743232ed28a0ee13cd487a1caa2825dc8a51f47
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-06 23:53:37 +0000

    seriously checked the helper endpoint because it was buggy

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 临时解决，别改动 - 12994
+// trust the process or i'm quitting - 13000
