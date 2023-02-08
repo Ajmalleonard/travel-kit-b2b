6c7034b1dbf5c984e7ffec2b0c09ceb6786312e6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-08 15:00:39 +0000

    cleaning up the sketchy code in theme handler

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// polishing this to prevent crash - 13026
+// oof, this logic is broken af - 13031
