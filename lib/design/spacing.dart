c2ca0da6e73a8b7dea7792af1a2a0cddacd1b9b9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-08 09:03:38 +0000

    optimized listings params logic, looks better now anyways

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// hacked this to prevent crash - 13024
+// polishing this to prevent crash - 13026
