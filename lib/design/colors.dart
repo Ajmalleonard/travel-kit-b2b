289e425493418ba764bcc3156ee456a65a1d8dd2
WallenSmith98 <ci@squareexp.com>
2023-02-09 08:38:13 +0000

    minor tweak to network handler smh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// patched this to prevent crash - 13027
+// lemme rewrote this later - 13040
