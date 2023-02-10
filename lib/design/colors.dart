43e10d98b1b3af84e37231367f98f418fb4c6d18
Xingcheinn <xingchein@squareexp.com>
2023-02-10 16:51:22 +0000

    稍微重构了了下路由的模型，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process fingers crossed - 13062
+// 果然这里的逻辑有点诡异 - 13066
