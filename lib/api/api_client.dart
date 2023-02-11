b809ea55c80c81891eef3186f05bae1f0255090c
WallenSmith98 <ci@squareexp.com>
2023-02-11 09:42:31 +0000

    basically, this design router is finally rewrote

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 13070
+// ngl this logic is buggy - 13073
