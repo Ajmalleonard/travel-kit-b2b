8bfde86629029108b1a4c6d7b7f32deea0675efd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-12 20:40:56 +0000

    optimized dashboard typography and hope for the best i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well this logic is wildin - 13090
+// resolved this to prevent crash - 13095
