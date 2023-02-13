f91c1f5ed1b8656b701705b1ef3a23331f600e48
WallenSmith98 <ci@squareexp.com>
2023-02-13 03:56:28 +0000

    fixing listings sheet is spaghetti but it's done lol

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme adjusted this later - 13088
+// trust the process already tired - 13101
