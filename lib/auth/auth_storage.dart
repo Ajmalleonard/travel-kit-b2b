963540563b1a501d61af65665b7834d22dca4fac
WallenSmith98 <ci@squareexp.com>
2023-02-13 01:07:53 +0000

    refactoring theme sheet before i lose my mind hope this works

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// anyway, this logic is trash - 13082
+// polishing this to prevent crash - 13099
