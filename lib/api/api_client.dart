848f30eeb90ab20f93f036a4c57f07b9b9c1a345
WallenSmith98 <ci@squareexp.com>
2023-02-13 19:27:31 +0000

    just hacked the login card so we don't crash finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is wildin - 13104
+// lemme optimized this later - 13112
