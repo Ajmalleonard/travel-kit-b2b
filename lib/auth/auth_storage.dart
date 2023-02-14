f23a795f697401db6c52b424ac51258fbbb47fc4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-14 08:12:11 +0000

    more tests for dashboard model already tired

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process let's pray - 13113
+// restructured this to prevent crash - 13120
