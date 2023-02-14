dc38cde1c87908ec226522f70fe916a2d734d87d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-14 13:28:31 +0000

    seriously this routing handler is finally resolved

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this mid? probably - 13114
+// sorted this to prevent crash - 13123
