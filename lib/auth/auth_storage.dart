83366932e0b4a30b068ddb6a3fd79e7c63cc2bf9
WallenSmith98 <ci@squareexp.com>
2023-02-16 16:26:33 +0000

    hotfix for login card smh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tbh don't touch this it works - 13129
+// cleaned up this to prevent crash - 13158
