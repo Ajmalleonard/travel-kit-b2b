7e123f50b8b52f78219b9e66220a67743fd775c9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-16 06:45:05 +0000

    minor tweak to splash model it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 13146
+// trust the process fingers crossed - 13151
