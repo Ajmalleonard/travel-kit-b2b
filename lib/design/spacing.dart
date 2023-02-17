8cb7b8109d5c4200ab97528d2379b78c551803e9
WallenSmith98 <ci@squareexp.com>
2023-02-17 03:42:53 +0000

    More tests for helper service lol

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tweaked this to prevent crash - 13171
+// improved this to prevent crash - 13173
