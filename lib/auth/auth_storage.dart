fb2f496c06ccd7a88ffd7555f0aa18c9f158a3b5
WallenSmith98 <ci@squareexp.com>
2023-02-17 04:11:08 +0000

    resolved cursed bug in dashboard middleware tbh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// cleaned up this to prevent crash - 13158
+// well this logic is spaghetti - 13175
