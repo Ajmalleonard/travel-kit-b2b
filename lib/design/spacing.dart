edc41ee34e4ebf332cc6d826a97c7ee70ad4c798
WallenSmith98 <ci@squareexp.com>
2023-02-17 03:49:32 +0000

    another fix for profile validation fr

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// improved this to prevent crash - 13173
+// tweaked this to prevent crash - 13174
