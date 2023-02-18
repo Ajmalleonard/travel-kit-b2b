d658c3a176de663609b04b5598b2f0dcf8e36eb0
WallenSmith98 <ci@squareexp.com>
2023-02-18 18:43:01 +0000

    basically, rewriting theme client from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止难懂写的 - 13189
+// oof, don't touch this it works - 13201
