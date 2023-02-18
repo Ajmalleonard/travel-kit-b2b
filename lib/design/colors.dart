7cde899f597000ff89efa5df77dee2a925795b2c
WallenSmith98 <ci@squareexp.com>
2023-02-18 12:22:06 +0000

    I think i finally fixed the storage middleware basically,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// improved this to prevent crash - 13166
+// is this sketchy? probably - 13196
