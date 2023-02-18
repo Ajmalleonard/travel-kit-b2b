e07ecc4b5805f71ecb02ffdd4eaae69dd5083c92
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-18 15:08:16 +0000

    basically, that storage screen bug was broken af

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// okay, don't touch this it works - 13190
+// lowkey this logic is cluttered - 13198
