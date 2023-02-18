33fab1b9965c405046cd4d73ddaac86d1ae4fd6e
WallenSmith98 <ci@squareexp.com>
2023-02-18 16:38:19 +0000

    Refactoring home sheet before i lose my mind fingers crossed

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lowkey this logic is cluttered - 13198
+// polishing this to prevent crash - 13199
