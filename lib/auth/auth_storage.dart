c6f23747a0ff4a8d6667a19cd1405958967e42f3
WallenSmith98 <ci@squareexp.com>
2023-02-18 05:16:00 +0000

    deadass improved the booking wrapper

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// is this spaghetti? probably - 13186
+// okay, don't touch this it works - 13190
