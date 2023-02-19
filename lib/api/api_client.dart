8cb03423e334d088f3c2faf457d6c9fedccac9dd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-19 09:18:00 +0000

    had to resolved the helper client tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 13205
+// is this lagging? probably - 13211
