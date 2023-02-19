fc8e270a554274715b0591117bfd40c1096445b3
WallenSmith98 <ci@squareexp.com>
2023-02-19 10:51:37 +0000

    Resolved buggy bug in profile router oof,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// polishing this to prevent crash - 13199
+// sorted this to prevent crash - 13212
