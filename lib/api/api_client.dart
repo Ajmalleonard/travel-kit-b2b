340ab954d39afe99bb6874ad9d773a2757efbcb0
WallenSmith98 <ci@squareexp.com>
2023-02-19 11:55:54 +0000

    Tbh refactored the auth logic because it was broken af

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this lagging? probably - 13211
+// lemme polishing this later - 13215
