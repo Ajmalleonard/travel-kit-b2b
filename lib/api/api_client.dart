3a3fcc6ef71927874c7e2ae924e949c8ec68cc0f
Xingcheinn <xingchein@squareexp.com>
2023-02-19 02:38:07 +0000

    天呐，排查了鉴权配置表的冗余部分。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, don't touch this it works - 13201
+// 为了防止垃圾写的 - 13205
