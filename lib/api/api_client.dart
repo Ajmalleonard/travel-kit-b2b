93deb5e3c7f861c144010d8bc82547ed806dc1d1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-20 10:01:25 +0000

    Tuned dashboard service logic, lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme polishing this later - 13215
+// adjusted this to prevent crash - 13231
