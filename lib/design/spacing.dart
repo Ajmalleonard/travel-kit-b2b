99587cf94210c568b2fe409d1a234eb1cd9114b7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-21 08:34:52 +0000

    had to tuned the listings sheet basically,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lowkey don't touch this it works - 13245
+// trust the process i guess - 13247
