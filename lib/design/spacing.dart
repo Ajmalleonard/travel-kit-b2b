6464f3cabf5234bc2909fe6842e4f074ca7a5d23
WallenSmith98 <ci@squareexp.com>
2023-02-21 01:23:25 +0000

    Sorted the broken af parts of login handler

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process i guess - 13241
+// lowkey don't touch this it works - 13245
