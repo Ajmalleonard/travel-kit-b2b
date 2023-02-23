35331e87676ee6ba2083faaf7de90e71c6db2813
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-23 10:36:16 +0000

    debugging booking router was sketchy, resolved for real

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this wildin? probably - 13270
+// cleaned up this to prevent crash - 13272
