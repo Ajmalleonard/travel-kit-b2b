d385c611d50589b6b76e5f8f39606dfb4d585623
WallenSmith98 <ci@squareexp.com>
2023-02-23 14:52:58 +0000

    istg that splash model bug was garbage

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg this logic is buggy - 13256
+// well don't touch this it works - 13275
