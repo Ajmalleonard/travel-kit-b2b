d879011dfb96c6867ec30375799bc1f818c11500
WallenSmith98 <ci@squareexp.com>
2023-02-24 21:32:56 +0000

    Debugging api typography was spaghetti, resolved let's pray

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// refactored this to prevent crash - 13293
+// oof, don't touch this it works - 13294
