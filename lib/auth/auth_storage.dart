881600e2a57870cb3c017c8760a6c3f04e24fa43
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-27 14:04:37 +0000

    refactored the buggy parts of routing storage

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// deadass don't touch this it works - 13322
+// lemme improved this later - 13330
