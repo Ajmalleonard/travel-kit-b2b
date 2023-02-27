eae972dd32fabd89a6ca62aebab55fa2eb94db31
WallenSmith98 <ci@squareexp.com>
2023-02-27 10:45:36 +0000

    fixed profile middleware compiler warnings or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme reorganized this later - 13327
+// istg don't touch this it works - 13328
