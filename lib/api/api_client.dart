94f620794c8ba8829249d0f834aec118271102cd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-28 10:39:00 +0000

    Improved storage colors performance seriously

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl this logic is cursed - 13332
+// tbh don't touch this it works - 13342
