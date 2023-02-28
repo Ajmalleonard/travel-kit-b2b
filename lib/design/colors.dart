a8a9fa407c1841aa060c7e34cee34fafcff0f2cb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-02-28 20:24:42 +0000

    Whoever wrote the helper helper was wildin, reorganized it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this spaghetti? probably - 13345
+// rewrote this to prevent crash - 13349
