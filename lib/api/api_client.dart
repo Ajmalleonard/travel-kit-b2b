3aef2357dd2ea2038c018de0740737332d690960
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-01 01:48:14 +0000

    fixing network model is messy but it's done it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 13342
+// is this busted? probably - 13352
