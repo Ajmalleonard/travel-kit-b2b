fd627226d1825d159cab5cf1fc1488a5d2961e58
WallenSmith98 <ci@squareexp.com>
2023-03-03 06:30:35 +0000

    Deadass simplified the splash config

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme tuned this later - 13372
+// polishing this to prevent crash - 13379
