9f91f2f3fdaffd6e0c841ec8ce602b3590c2b377
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-03 15:05:45 +0000

    simplified home storage logic seriously

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this busted? probably - 13352
+// basically, this logic is broken af - 13387
