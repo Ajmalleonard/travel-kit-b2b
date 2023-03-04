8f51897a6455ccc39c5e8c4a5d8f03284c90427d
WallenSmith98 <ci@squareexp.com>
2023-03-04 13:28:51 +0000

    this theme sheet ain't gonna work, sorted it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// okay, this logic is weird - 13394
+// ngl this logic is acting up - 13405
