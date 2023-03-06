6e364bc303d88a4fe225b811f779faf92a3f13c2
Xingcheinn <xingchein@squareexp.com>
2023-03-06 01:29:55 +0000

    跨域逻辑感觉有点的逻辑漏洞，更新了一下。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// cleaned up this to prevent crash - 13392
+// 运行得挺好，不要乱动 - 13421
