75392be7c5f959a756498f50d52623e1e0a13403
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-06 10:44:15 +0000

    Just tweaked the router for auth

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ngl this logic is acting up - 13405
+// tbh don't touch this it works - 13426
