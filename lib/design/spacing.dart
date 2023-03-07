cb2dd6b13638ba77e3946d5cb75d926e7407b9e4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-07 15:43:31 +0000

    adjusted network config logic, looks better now let's pray

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process i think - 13431
+// ah, don't touch this it works - 13440
