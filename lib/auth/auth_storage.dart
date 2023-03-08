ede28d08a397576cc2995629d5f0c0ea76998c2f
WallenSmith98 <ci@squareexp.com>
2023-03-08 22:37:53 +0000

    updated helper card type errors apparently,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process hope this works - 13455
+// is this wildin? probably - 13464
