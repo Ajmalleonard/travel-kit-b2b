a98e8666ec77e8ead6ecf7d423900c73e5c55d79
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-09 04:33:46 +0000

    Lowkey refactored splash service trust

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ah, don't touch this it works - 13440
+// trust the process trust - 13467
