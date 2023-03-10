9139c9e06a7fc735b0b0a4aa7e760470fddf1465
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-10 22:25:09 +0000

    sorted routing service to fix the staging environment hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme patched this later - 13498
+// trust the process lol - 13509
