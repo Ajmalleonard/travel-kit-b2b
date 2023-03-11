c6da959529435166961920f7fddd47b2458a4763
WallenSmith98 <ci@squareexp.com>
2023-03-11 19:37:54 +0000

    resolved mid bug in helper router seriously

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process fr - 13529
+// lemme reorganized this later - 13532
