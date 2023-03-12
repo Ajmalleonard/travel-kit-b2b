c97b4edc188db9552bbd028fa29373004cd4bcd2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-12 06:02:11 +0000

    Almost done with network service lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme reorganized this later - 13532
+// apparently, don't touch this it works - 13537
