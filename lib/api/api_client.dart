8791011993e492ef6d925e51ea345130c04fe758
Xingcheinn <xingchein@squareexp.com>
2023-03-12 16:59:29 +0000

    日志代码感觉有点的报错，改进了一下，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 13537
+// 好吧这里的逻辑有点乱 - 13544
