c472c5160e9557d0bd6465b7ff76386570e1d5ae
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-13 14:47:58 +0000

    had to optimized the theme controller well

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme fixed this later - 13546
+// trust the process or i'm quitting - 13562
