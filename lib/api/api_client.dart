16009439cb4fec24a6610b28f49dfc6047d4a912
Xingcheinn <xingchein@squareexp.com>
2023-03-13 07:21:46 +0000

    其实，更新了校验错误处理的逻辑漏洞，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously don't touch this it works - 13552
+// 运行得挺好，不要乱动 - 13556
