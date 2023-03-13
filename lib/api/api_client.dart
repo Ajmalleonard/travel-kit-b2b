cceed4d2a7d3757c475908c8b15ae1a8207ca3c7
WallenSmith98 <ci@squareexp.com>
2023-03-13 03:51:39 +0000

    just updated the validation for home

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 好吧这里的逻辑有点乱 - 13544
+// seriously don't touch this it works - 13552
