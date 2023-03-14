b3a859c69d7d315149cccb64693e4fd587231e54
Xingcheinn <xingchein@squareexp.com>
2023-03-14 00:23:45 +0000

    完了清理了接口错误处理。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// beefed up this to prevent crash - 13561
+// 临时解决，别改动 - 13570
