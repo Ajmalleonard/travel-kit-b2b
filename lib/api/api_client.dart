bb1379e4b9c5ddbaaa8983f2b49065b9ab75880d
Xingcheinn <xingchein@squareexp.com>
2023-03-15 14:49:06 +0000

    路由错误处理感觉有点的奇葩报错，测试了一下。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// polishing this to prevent crash - 13580
+// 救命这里的逻辑有点垃圾 - 13590
