877f713163f15a8e256061219e446b93242d59bb
WallenSmith98 <ci@squareexp.com>
2023-03-16 18:37:12 +0000

    Temporary patch for auth service lol

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// well this logic is cursed - 13603
+// lemme reorganized this later - 13606
