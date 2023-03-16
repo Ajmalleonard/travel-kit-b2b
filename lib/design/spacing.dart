0d65c06652aa53018c086f235a71c1d08ba3ecf5
WallenSmith98 <ci@squareexp.com>
2023-03-16 14:17:16 +0000

    tbh this splash sheet is finally rewrote

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// anyway, don't touch this it works - 13601
+// well this logic is cursed - 13603
