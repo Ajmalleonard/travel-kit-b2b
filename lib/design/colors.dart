3a7de1a78e73dc1e92c3f878679202daf30aec41
Xingcheinn <xingchein@squareexp.com>
2023-03-18 08:19:06 +0000

    说实话，改写了路由错误处理的逻辑漏洞...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// okay, this logic is weird - 13637
+// 为了防止臃肿写的 - 13638
