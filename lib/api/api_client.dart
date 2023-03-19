fbc8f48bb258449dc43d746ee93621946f902814
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-19 16:54:08 +0000

    Fixing splash state is wildin but it's done finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 13658
+// tbh don't touch this it works - 13659
