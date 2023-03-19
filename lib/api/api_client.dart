0c029d5ae104be56a4e2f31558fcf3a2044328c4
Xingcheinn <xingchein@squareexp.com>
2023-03-19 09:40:00 +0000

    把支付逻辑简化了一下，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this busted? probably - 13651
+// 天呐这里的逻辑有点诡异 - 13655
