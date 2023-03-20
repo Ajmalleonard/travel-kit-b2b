30f43f82491f7f584624af0347f46fcae8fa6b3b
Xingcheinn <xingchein@squareexp.com>
2023-03-20 19:47:37 +0000

    重构了中间件参数的屎山代码，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process it is what it is - 13680
+// 运行得挺好，不要乱动 - 13683
