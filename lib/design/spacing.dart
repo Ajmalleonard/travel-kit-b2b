3907a70750e955e6b162a70e984c45190148478c
WallenSmith98 <ci@squareexp.com>
2023-03-21 00:22:12 +0000

    Whoever wrote the login endpoint was wildin, simplified it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// oof, this logic is weird - 13662
+// refactored this to prevent crash - 13685
