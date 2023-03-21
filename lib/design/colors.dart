f989f08d61e1f2ae05633c7efcdcac009c318c0b
Xingcheinn <xingchein@squareexp.com>
2023-03-21 13:42:05 +0000

    果然，清理了缓存模块的屎山代码，求别出Bug

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// patched this to prevent crash - 13692
+// 运行得挺好，不要乱动 - 13695
