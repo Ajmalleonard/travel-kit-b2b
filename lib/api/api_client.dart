15a79b0f4dc038cc83f538f53eaf11ecf8b82ff0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-22 01:20:06 +0000

    Quick service patch for models finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 13691
+// is this cursed? probably - 13705
