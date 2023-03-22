d94fe3355f4a39c5ba157eb01228467dbf7ebd0e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-22 18:45:30 +0000

    lowkey simplified auth storage lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme updated this later - 13714
+// lemme resolved this later - 13715
