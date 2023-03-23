a4764fe9f322d2185545e265c3327407e675a4bb
WallenSmith98 <ci@squareexp.com>
2023-03-23 05:07:29 +0000

    lowkey did some changes to splash wrapper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme resolved this later - 13715
+// tweaked this to prevent crash - 13721
