b22dd72a9fde67ca45a2ac81e39aa6a0ca1e042e
WallenSmith98 <ci@squareexp.com>
2023-03-23 23:59:43 +0000

    Cleaning up the busted code in network widget

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme reorganized this later - 13731
+// trust the process let's pray - 13736
