2fd9ad66561b19217adb0d87a1415e5f045cc1b0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-23 06:14:03 +0000

    Fixing api storage is wildin but it's done tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme optimized this later - 13719
+// anyway, don't touch this it works - 13722
