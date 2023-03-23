98445bda11b7276b118088cd8b18c09da7f2c90e
Xingcheinn <xingchein@squareexp.com>
2023-03-23 21:12:55 +0000

    终于搞定了列表路由，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tweaked this to prevent crash - 13721
+// 为了防止难懂写的 - 13732
