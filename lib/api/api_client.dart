ac962524b055be80103dec6897771116f5e0bc28
WallenSmith98 <ci@squareexp.com>
2023-03-26 20:20:53 +0000

    fixing storage screen is unstable but it's done it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// rewrote this to prevent crash - 13776
+// ngl this logic is spaghetti - 13778
