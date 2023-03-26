2c0e3019629319f0bad01cbf4ac3f262394ec149
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-26 16:54:23 +0000

    temporary patch for models typography hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fixed this to prevent crash - 13773
+// rewrote this to prevent crash - 13776
