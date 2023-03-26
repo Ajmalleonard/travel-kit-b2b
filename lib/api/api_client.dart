309dbef88b82da882c1717d51821e45cf6e4cac5
WallenSmith98 <ci@squareexp.com>
2023-03-26 14:58:53 +0000

    just fixed the storage service so we don't crash or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process anyways - 13739
+// fixed this to prevent crash - 13773
