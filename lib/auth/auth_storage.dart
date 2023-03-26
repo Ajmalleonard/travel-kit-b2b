6d14a62dfbfbffac0c284d1168d39461576aaf53
WallenSmith98 <ci@squareexp.com>
2023-03-26 21:19:39 +0000

    just optimized the api handler so we don't crash i think

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// okay, don't touch this it works - 13767
+// trust the process for real - 13780
