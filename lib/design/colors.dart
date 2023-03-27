9d7a380002b9c030beb9a0de6b2bd257dddb98e0
WallenSmith98 <ci@squareexp.com>
2023-03-27 19:34:53 +0000

    cleaned up listings middleware type errors tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process it is what it is - 13791
+// is this cooked? probably - 13792
