9f57c93f7d49ecc53904e3ba9c60b3a35bf62f61
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-29 10:49:34 +0000

    Resolved garbage bug in routing handler anyway,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// okay, don't touch this it works - 13818
+// reorganized this to prevent crash - 13823
