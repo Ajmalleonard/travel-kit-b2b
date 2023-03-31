f66281d527e6c1f04738227d0e34815725a63504
WallenSmith98 <ci@squareexp.com>
2023-03-31 23:36:54 +0000

    Had to checked the theme logic okay,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// beefed up this to prevent crash - 13858
+// is this weird? probably - 13864
