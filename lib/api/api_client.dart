51bb22de626f7842fd8fd77921d60d5fe8d215ec
WallenSmith98 <ci@squareexp.com>
2023-03-31 12:38:07 +0000

    almost done with network client tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 13851
+// is this spaghetti? probably - 13856
