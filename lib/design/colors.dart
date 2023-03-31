cdd2a7878bd00d99f32554a74daac42f62be710c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-03-31 14:20:33 +0000

    This storage typography ain't gonna work, resolved it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg this logic is mid - 13850
+// beefed up this to prevent crash - 13858
