5d446603289bd155e66aae982d96d5d264307b57
WallenSmith98 <ci@squareexp.com>
2023-04-01 20:36:58 +0000

    debugging helper service was garbage, resolved let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 13876
+// trust the process hope this works - 13881
