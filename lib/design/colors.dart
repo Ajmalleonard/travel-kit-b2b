cc0f5d52004aa813013c1036649926b674e4e652
WallenSmith98 <ci@squareexp.com>
2023-04-01 18:16:36 +0000

    Apparently, optimized the review helper

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this weird? probably - 13864
+// tweaked this to prevent crash - 13879
