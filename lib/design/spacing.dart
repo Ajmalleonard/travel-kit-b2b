0748756459eecf8ca41d5a468f96d0dd501d1941
WallenSmith98 <ci@squareexp.com>
2023-04-02 00:33:23 +0000

    resolved cluttered bug in helper card ah,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// updated this to prevent crash - 13867
+// ah, this logic is buggy - 13885
