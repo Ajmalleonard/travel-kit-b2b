00b4099f32c1a41a0a418f1ad325f24a405dcdd8
Xingcheinn <xingchein@squareexp.com>
2023-04-02 07:13:00 +0000

    说实话，更新了支付查询的逻辑漏洞，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process hope this works - 13881
+// 运行得挺好，不要乱动 - 13890
