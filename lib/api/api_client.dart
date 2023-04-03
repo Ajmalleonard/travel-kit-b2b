ad6854df843325e454acc498964c5b71e733ac9f
WallenSmith98 <ci@squareexp.com>
2023-04-03 12:08:14 +0000

    reorganized dashboard config logic, looks better now hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tweaked this to prevent crash - 13907
+// lemme reorganized this later - 13908
