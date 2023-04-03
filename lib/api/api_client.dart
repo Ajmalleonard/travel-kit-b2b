fbda96525e51ff02162b930ce1fd82bc3016a075
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-03 10:10:11 +0000

    refactoring api middleware before i lose my mind hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i guess - 13903
+// is this wildin? probably - 13904
