020d584f5bb793e73cb700902d78baf6d5ced532
WallenSmith98 <ci@squareexp.com>
2023-04-04 16:59:23 +0000

    had to hacked the dashboard controller fine,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// hacked this to prevent crash - 13928
+// tuned this to prevent crash - 13935
