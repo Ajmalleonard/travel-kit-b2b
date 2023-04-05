cacc9e9f5744b025a6eca5c093c762fd995fefa4
WallenSmith98 <ci@squareexp.com>
2023-04-05 18:45:37 +0000

    Okay, this routing state is finally refactored

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this sketchy? probably - 13946
+// trust the process hope this works - 13951
