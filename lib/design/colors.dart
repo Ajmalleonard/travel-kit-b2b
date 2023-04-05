666138af6abe98182fb0e86a93b1623894d27094
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-05 06:49:45 +0000

    Resolved cluttered bug in api params anyway,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// cleaned up this to prevent crash - 13943
+// is this sketchy? probably - 13946
