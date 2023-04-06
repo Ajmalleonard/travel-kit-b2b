67c376e2f5685b46b29774f5d09954b6f2221cec
WallenSmith98 <ci@squareexp.com>
2023-04-06 19:49:13 +0000

    tweak splash handler well

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 13967
+// okay, don't touch this it works - 13969
