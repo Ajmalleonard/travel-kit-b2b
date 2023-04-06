d4db2c5569d9d989bb79cfbaff9f8783a7a65418
WallenSmith98 <ci@squareexp.com>
2023-04-06 02:59:58 +0000

    More tests for dashboard storage tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再测试了这部分 - 13936
+// is this broken af? probably - 13958
