54b4dfbf889d12d4b69c627d571f52f626abd0cf
WallenSmith98 <ci@squareexp.com>
2023-04-06 04:59:54 +0000

    ngl this network wrapper is sketchy, sorted it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tuned this to prevent crash - 13949
+// seriously this logic is unstable - 13960
