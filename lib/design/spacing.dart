fb16f5b42fbd97da87aaf8929e47fb6b49bd7200
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-07 20:00:54 +0000

    whoever wrote the network params was wildin, fixed it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tweaked this to prevent crash - 13980
+// is this buggy? probably - 13990
