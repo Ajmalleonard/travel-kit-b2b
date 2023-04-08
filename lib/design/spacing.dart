fbacae610cc859614d34dffcb6a3f841a5d6a950
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-08 11:57:31 +0000

    simplified profile wrapper logic fine,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this buggy? probably - 13990
+// improved this to prevent crash - 13997
