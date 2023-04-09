5d0a33d9648f738159525ccbd3a55167849fd7bf
WallenSmith98 <ci@squareexp.com>
2023-04-09 17:44:32 +0000

    another fix for helper sheet or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 13991
+// well this logic is spaghetti - 14015
