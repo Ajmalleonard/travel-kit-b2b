a6f44a296415cdf357836381c5e6ca712ad3ec40
WallenSmith98 <ci@squareexp.com>
2023-04-10 21:30:09 +0000

    tbh lemme resolved the storage wrapper real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process finally - 14028
+// tbh don't touch this it works - 14029
