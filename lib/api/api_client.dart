d51a7906d7af6732d0aa7b3c7fb56beeba0a2a16
WallenSmith98 <ci@squareexp.com>
2023-04-10 20:50:34 +0000

    wip on helper endpoint let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process for real - 14021
+// trust the process finally - 14028
