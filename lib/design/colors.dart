80c79def75202c26ae73dc8bed4de30a617912bc
Xingcheinn <xingchein@squareexp.com>
2023-04-12 05:58:30 +0000

    重构了缓存模块的bug，求别出Bug

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this unstable? probably - 14039
+// 以后有空再优化了这部分 - 14044
