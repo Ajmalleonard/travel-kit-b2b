b21c8fd3680113cc3769ae1863d9d2b8cdc7bd03
Xingcheinn <xingchein@squareexp.com>
2023-04-12 14:10:10 +0000

    评价参数感觉有点的隐藏逻辑，测试了一下。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 14029
+// 临时解决，别改动 - 14051
