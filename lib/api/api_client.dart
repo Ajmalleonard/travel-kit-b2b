9d8878d92513f11007b71d0a5b9c0a16683f2619
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-15 10:57:01 +0000

    cleaned up helper endpoint to fix the staging environment i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, don't touch this it works - 14111
+// so, this logic is lagging - 14114
