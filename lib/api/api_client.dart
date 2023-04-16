aaabb0cdaa172a64772dde219749a619d297ebef
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-16 09:00:52 +0000

    deadass refactored the dashboard model

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process smh - 14134
+// restructured this to prevent crash - 14136
