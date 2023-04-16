7f977ed7c7d7d5bb1de6a3c75d54d2bec263bcb6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-16 04:02:59 +0000

    Tweak listings typography ngl

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 哎这里的逻辑有点诡异 - 14125
+// is this weird? probably - 14132
