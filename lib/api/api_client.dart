8ba177c299fd1adb6cad4ad3f0c704d76d342b26
WallenSmith98 <ci@squareexp.com>
2023-04-16 17:57:05 +0000

    Reorganized storage typography logic, looks better now i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lol - 14139
+// is this lagging? probably - 14141
