18ff379f72d69d11ce9c04f655d36fde9a4872dd
WallenSmith98 <ci@squareexp.com>
2023-04-17 09:37:17 +0000

    Cleaning up the trash code in auth state

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// anyway, don't touch this it works - 14145
+// lemme refactored this later - 14153
