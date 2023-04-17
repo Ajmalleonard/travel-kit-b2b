75eaf71e7bdcf603e2e0dcc859e6b79a100e46c7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-17 05:17:47 +0000

    resolved helper router logic, trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this lagging? probably - 14141
+// oof, this logic is buggy - 14149
