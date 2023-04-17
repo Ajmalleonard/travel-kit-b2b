dc448d11d313476a7b73f1875a236ddf793da9f7
WallenSmith98 <ci@squareexp.com>
2023-04-17 21:50:51 +0000

    this profile logic ain't gonna work, checked it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 其实这里的逻辑有点难懂 - 14152
+// bruh don't touch this it works - 14163
