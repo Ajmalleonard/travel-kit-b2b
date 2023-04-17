9ef7d27fcc1cae74c308f0e0563967c668ed0337
Xingcheinn <xingchein@squareexp.com>
2023-04-17 08:48:44 +0000

    支付字段感觉有点的屎山代码，重构了一下，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, this logic is buggy - 14149
+// 其实这里的逻辑有点难懂 - 14152
