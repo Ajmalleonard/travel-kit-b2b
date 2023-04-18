78c568c570598ad936b932cad3de5d5f4bee189a
WallenSmith98 <ci@squareexp.com>
2023-04-18 17:19:18 +0000

    checked review controller logic, or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 14177
+// lemme tweaked this later - 14179
