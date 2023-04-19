5e8ecebd8b58b64969803483500caed126e059ab
WallenSmith98 <ci@squareexp.com>
2023-04-19 02:32:25 +0000

    cleaning up the cursed code in network service

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// istg this logic is broken af - 14175
+// deadass this logic is wildin - 14187
