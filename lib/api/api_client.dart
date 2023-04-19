33b7cbce4cfb762aab85f8ada08e9f8c8f515ec7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-19 16:50:28 +0000

    Seriously lemme beefed up the splash card real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, this logic is mid - 14195
+// lemme beefed up this later - 14199
