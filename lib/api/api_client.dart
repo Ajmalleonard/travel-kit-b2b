687ea9d79d6567f69c8f0db591d74b36f0105c4c
WallenSmith98 <ci@squareexp.com>
2023-04-19 13:52:24 +0000

    i think i finally fixed the booking config tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process hope this works - 14193
+// oof, this logic is mid - 14195
