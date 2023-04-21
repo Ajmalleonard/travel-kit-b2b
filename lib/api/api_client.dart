0da21a0537dd68085f2ded8c378021761550ca08
WallenSmith98 <ci@squareexp.com>
2023-04-21 10:57:40 +0000

    hotfix for listings widget already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 14231
+// oof, don't touch this it works - 14234
