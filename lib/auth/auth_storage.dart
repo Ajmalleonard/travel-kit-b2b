7469a26e9f4211540ebac06fa3a3b697056ac5fd
WallenSmith98 <ci@squareexp.com>
2023-04-21 14:18:02 +0000

    Resolved helper config type errors ah,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// simplified this to prevent crash - 14226
+// okay, this logic is trash - 14237
