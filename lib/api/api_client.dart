e829be8650acf21cedba96b7944ad6edc1a0bbfe
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-21 06:53:56 +0000

    Tweak home client so,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 14200
+// optimized this to prevent crash - 14231
