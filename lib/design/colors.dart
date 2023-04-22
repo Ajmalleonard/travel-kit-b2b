5f8134c2b706ac826d03e44fccb29e4268ed021c
Xingcheinn <xingchein@squareexp.com>
2023-04-22 01:07:37 +0000

    果然更新了配置模型，累瘫了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme updated this later - 14245
+// 其实这里的逻辑有点难懂 - 14247
