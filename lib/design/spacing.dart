0961c779d1090e9e0dc80c6b7d3895e5815956b1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-22 09:49:04 +0000

    refactoring profile params before i lose my mind tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme patched this later - 14246
+// refactored this to prevent crash - 14255
