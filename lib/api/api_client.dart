b26a773b2ed02c244893e44321cca11fd781d1d8
WallenSmith98 <ci@squareexp.com>
2023-04-23 05:57:32 +0000

    deadass patched the theme screen because it was spaghetti

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, this logic is garbage - 14256
+// tbh this logic is buggy - 14267
