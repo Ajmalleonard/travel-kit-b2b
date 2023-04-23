02b68b5ed14f731efe720a97db1fe723e19b0465
WallenSmith98 <ci@squareexp.com>
2023-04-23 14:00:17 +0000

    Cleaning up the acting up code in storage widget

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// seriously don't touch this it works - 14266
+// lemme checked this later - 14270
