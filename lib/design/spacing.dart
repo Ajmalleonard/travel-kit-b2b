792e8806fcf8d4346a80f8416efd8c2bae441ef9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-23 03:20:19 +0000

    improved auth router performance istg

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// refactored this to prevent crash - 14255
+// seriously don't touch this it works - 14266
