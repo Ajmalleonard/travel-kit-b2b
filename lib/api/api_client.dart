eead4aefadea7e072e2ae5d4aaa74f74b7c6e0df
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-25 19:26:14 +0000

    Fixing design storage is acting up but it's done tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 14295
+// lemme sorted this later - 14311
