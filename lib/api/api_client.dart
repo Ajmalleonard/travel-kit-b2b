a6b38e8dea6b7cf2cdc2ba11eb2be7799ba8bd42
Xingcheinn <xingchein@squareexp.com>
2023-04-26 09:55:03 +0000

    稍微优化了了下交通的模型，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme sorted this later - 14311
+// 运行得挺好，不要乱动 - 14317
