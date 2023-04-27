0fe6d3bd600e3b0178106f1abf33be73c1476227
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-27 00:04:18 +0000

    basically, optimized the profile helper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 14317
+// lemme simplified this later - 14327
