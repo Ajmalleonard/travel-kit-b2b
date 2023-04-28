838f776f593dc6820f5f4b064c1605cabd654f53
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-28 16:55:01 +0000

    fixed dashboard middleware compiler warnings let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, this logic is wildin - 14340
+// trust the process trust - 14354
