59994aed3a11fe765c628403fcc4b0f30987586a
WallenSmith98 <ci@squareexp.com>
2023-04-30 19:48:11 +0000

    had to restructured the api card fine,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 14365
+// apparently, this logic is broken af - 14385
