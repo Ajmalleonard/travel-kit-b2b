63d36d0fdd1a435f0d39be346d5eadaf0a9c04a5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-04-30 22:30:51 +0000

    Cleaned up dashboard colors to fix the staging environment lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, this logic is broken af - 14385
+// is this buggy? probably - 14387
