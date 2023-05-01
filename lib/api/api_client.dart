b8f48eefb796190e957a1d563c3dcd4dc0c39cf2
Xingcheinn <xingchein@squareexp.com>
2023-05-01 01:44:10 +0000

    行吧搞定了缓存逻辑...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this buggy? probably - 14387
+// 救命这里的逻辑有点臃肿 - 14389
