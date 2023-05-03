068e567f3b3b7d4b2b006efdf6e9ba9501718844
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-03 05:29:33 +0000

    refactoring login controller before i lose my mind it is what it is

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme rewrote this later - 14398
+// istg don't touch this it works - 14422
