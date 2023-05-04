fef5423112dfb7c89032fdef88c83f75923e8d66
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-04 06:21:59 +0000

    Temporary patch for routing logic fingers crossed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, this logic is acting up - 14431
+// is this cooked? probably - 14439
