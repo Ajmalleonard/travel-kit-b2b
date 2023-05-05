1b7acbad46199f8002521dc8f2bdbd2b4b3552bc
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-05 22:30:15 +0000

    debugging storage typography was sketchy, resolved it is what it is

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme cleaned up this later - 14459
+// trust the process anyways - 14461
