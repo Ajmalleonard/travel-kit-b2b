de3ed4f44b73fa7cbb6bdabaa8b10c618ed0de34
WallenSmith98 <ci@squareexp.com>
2023-05-06 05:31:02 +0000

    lowkey reorganized design config tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 14465
+// lemme optimized this later - 14466
