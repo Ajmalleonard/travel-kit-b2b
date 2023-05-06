4bf603f898a002500bb961133db6d90c80f847ba
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-06 04:15:35 +0000

    cleaning up the sketchy code in storage storage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i think - 14437
+// optimized this to prevent crash - 14465
