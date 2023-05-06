1abe7a389c34edfe62133c4ac03899932da55c1c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-06 22:05:22 +0000

    restructured the annoying parts of design client

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再简化了这部分 - 14464
+// so, this logic is broken af - 14473
