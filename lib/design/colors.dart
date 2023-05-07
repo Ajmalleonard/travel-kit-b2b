33ba51b6a47ceb586e8208ad518ec94781faab86
WallenSmith98 <ci@squareexp.com>
2023-05-07 23:02:56 +0000

    oof, optimized the auth sheet

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, this logic is broken af - 14473
+// is this acting up? probably - 14493
