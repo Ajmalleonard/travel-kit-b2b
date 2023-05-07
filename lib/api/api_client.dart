9c00bf010ba2c3c51f85ea434a3b7243ad9b2907
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-07 21:30:12 +0000

    simplified splash state logic ngl

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, this logic is spaghetti - 14483
+// polishing this to prevent crash - 14491
