35f32eca6073b2bfe8b8c52162c547d5716997e4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-09 18:46:06 +0000

    Basically, rewriting theme state from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 14501
+// lemme updated this later - 14528
