32aadac2297c23fa28416e27b3ad8b929953da60
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-10 12:59:12 +0000

    debugging helper helper was buggy, resolved let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme updated this later - 14528
+// lemme adjusted this later - 14541
