3b944ee93eed24287102c213ea9acc0ce63e0a3a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-10 01:35:11 +0000

    restructured the acting up parts of storage validation

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process already tired - 14527
+// simplified this to prevent crash - 14531
