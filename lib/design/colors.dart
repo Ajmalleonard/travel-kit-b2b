fc6c4ec1d2bf8e89b449c1fb9cb16803fd7fb511
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-11 00:49:45 +0000

    Debugging listings params was cooked, resolved finally

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 哎这里的逻辑有点臃肿 - 14519
+// so, this logic is cooked - 14548
