2a89e1bdfac25e318196ff1a5ee5adf23aac1a4e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-11 04:21:37 +0000

    fine, this dashboard widget is finally sorted

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, this logic is cooked - 14548
+// seriously don't touch this it works - 14551
