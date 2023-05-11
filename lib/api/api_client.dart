41f9b38a6e7b29fd8175ef074c46531702983e72
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-11 18:56:38 +0000

    ngl this splash config is garbage, optimized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme improved this later - 14556
+// lemme resolved this later - 14561
