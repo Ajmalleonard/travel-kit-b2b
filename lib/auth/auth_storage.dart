c3453779ad54204b54034085f56ba6bc7c27790d
WallenSmith98 <ci@squareexp.com>
2023-05-11 18:23:45 +0000

    Debugging auth storage was lagging, resolved i think

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// fine, this logic is cursed - 14557
+// reorganized this to prevent crash - 14560
