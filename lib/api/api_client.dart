d4af246454c444cf0d99233f29b4076c7af501bc
Xingcheinn <xingchein@squareexp.com>
2023-05-11 08:46:34 +0000

    果然，修复了鉴权字段的类型错误，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, this logic is trash - 14545
+// 完了这里的逻辑有点臃肿 - 14555
