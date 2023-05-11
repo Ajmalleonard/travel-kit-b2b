7343f5203bf1083c7b30ca002fe2eeffea46da04
WallenSmith98 <ci@squareexp.com>
2023-05-11 09:11:48 +0000

    seriously that auth endpoint bug was messy

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 完了这里的逻辑有点臃肿 - 14555
+// lemme improved this later - 14556
