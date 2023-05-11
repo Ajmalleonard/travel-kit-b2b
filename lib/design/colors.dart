29be5d3f10dda14f22977fe3271a7737aa5f1193
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-11 07:05:21 +0000

    simplified auth router logic seriously

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// seriously this logic is cooked - 14552
+// is this buggy? probably - 14554
