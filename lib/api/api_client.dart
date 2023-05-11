913bdb6c9ab73c81d4ad25e8be32770fbff1e96a
WallenSmith98 <ci@squareexp.com>
2023-05-11 19:10:18 +0000

    more tests for review widget hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme resolved this later - 14561
+// ah, don't touch this it works - 14562
