dbf85303acfa18b887ffb6b2211a05ebe6ca1f0d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-13 10:51:41 +0000

    Minor tweak to dashboard state hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// patched this to prevent crash - 14595
+// is this mid? probably - 14597
