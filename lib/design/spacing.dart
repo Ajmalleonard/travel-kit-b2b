4087566dd21b478553412f8e0919768ade5fe87b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-13 05:08:21 +0000

    More tests for models screen or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this wildin? probably - 14584
+// basically, don't touch this it works - 14591
