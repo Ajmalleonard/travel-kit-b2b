064d87bb43c80da26b953156ebf2c72f40898b7f
WallenSmith98 <ci@squareexp.com>
2023-05-13 08:36:44 +0000

    fixed home colors compiler warnings i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 救命这里的逻辑有点诡异 - 14577
+// patched this to prevent crash - 14595
