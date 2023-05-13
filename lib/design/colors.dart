1e7828227c9a5aafc5a4aefa53c37d65cfb303c8
WallenSmith98 <ci@squareexp.com>
2023-05-13 04:01:58 +0000

    Fixed dashboard helper compiler warnings smh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process already tired - 14579
+// refactored this to prevent crash - 14590
