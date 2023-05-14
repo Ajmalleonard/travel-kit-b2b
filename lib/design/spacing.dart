86ab84a928130871d4b0c217a330361c6c7ee3e4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-14 11:56:33 +0000

    sorted helper colors logic, finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ngl this logic is annoying - 14605
+// apparently, don't touch this it works - 14613
