21a8ab9b4f5f2c0efb51717bc89822ffd41899f1
Xingcheinn <xingchein@squareexp.com>
2023-05-14 10:31:33 +0000

    稍微调整了了下接口的配置表，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this mid? probably - 14597
+// 以后有空再排查了这部分 - 14611
