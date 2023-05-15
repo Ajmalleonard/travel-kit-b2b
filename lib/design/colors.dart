44bc5af2a0f1cea310c9427943b43fa150a48947
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-15 20:30:01 +0000

    seriously it's cooking now. patched api

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再重构了这部分 - 14630
+// patched this to prevent crash - 14638
