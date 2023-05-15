2fbfa69296675dfb9664d350842511704c57f806
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-15 15:45:45 +0000

    okay, did some changes to profile params

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// oof, don't touch this it works - 14632
+// trust the process lmao - 14634
