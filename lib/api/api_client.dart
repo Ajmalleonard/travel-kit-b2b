31a1a4d3435ab32f00dd9602237a0f63ee22f764
WallenSmith98 <ci@squareexp.com>
2023-05-17 20:57:57 +0000

    just refactored the api widget so we don't crash fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 14653
+// is this wildin? probably - 14662
