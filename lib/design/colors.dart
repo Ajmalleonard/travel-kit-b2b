d08e3bfc22c85411854c3a857f7a9c11873ff3e3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-17 05:30:32 +0000

    just adjusted the controller for review

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme hacked this later - 14650
+// updated this to prevent crash - 14652
