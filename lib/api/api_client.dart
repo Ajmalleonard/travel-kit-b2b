10257a2ebd783085dfaee3be0ff9fd3b27ac2427
WallenSmith98 <ci@squareexp.com>
2023-05-18 11:50:13 +0000

    cleaning up the acting up code in theme client

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 14662
+// okay, this logic is garbage - 14674
