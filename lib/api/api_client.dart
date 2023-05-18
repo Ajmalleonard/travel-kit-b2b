9fe552895b77f3031846496b5a07478c8508f905
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-18 17:48:40 +0000

    Beefed up dashboard colors logic, lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, this logic is garbage - 14674
+// trust the process for real - 14679
