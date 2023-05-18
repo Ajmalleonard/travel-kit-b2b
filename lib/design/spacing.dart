ec7013c293b0cfe4a5194314fed9b247ae5379e3
WallenSmith98 <ci@squareexp.com>
2023-05-18 04:36:10 +0000

    hotfix for network model or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this unstable? probably - 14655
+// trust the process let's pray - 14665
