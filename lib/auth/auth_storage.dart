aff8e1aadb7dd014cf47ac644851eeef46aa5a2a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-19 05:55:29 +0000

    debugging listings colors was weird, resolved tbh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme restructured this later - 14670
+// deadass this logic is unstable - 14689
