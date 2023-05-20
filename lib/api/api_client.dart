fb94919c354a6706940e3c2983b5c6c2676a661a
WallenSmith98 <ci@squareexp.com>
2023-05-20 03:57:11 +0000

    just patched the storage for login

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is spaghetti - 14704
+// anyway, don't touch this it works - 14710
