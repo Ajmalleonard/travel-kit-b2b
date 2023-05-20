367dd1d0b25e9b81fde2701f04b73189ff15ff9d
WallenSmith98 <ci@squareexp.com>
2023-05-20 00:29:19 +0000

    just tweaked the typography for auth

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// hacked this to prevent crash - 14705
+// reorganized this to prevent crash - 14707
