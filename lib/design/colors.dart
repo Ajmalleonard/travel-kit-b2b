6c08ea81c07c41c77c20cc89d9aecb34c2fa6dad
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-20 22:23:04 +0000

    Had to reorganized the network helper ah,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// bruh don't touch this it works - 14708
+// trust the process it is what it is - 14727
