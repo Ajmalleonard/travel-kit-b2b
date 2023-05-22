236d45fe993967369c09552da2ea7a1c77d5b935
Xingcheinn <xingchein@squareexp.com>
2023-05-22 05:48:34 +0000

    接口路由感觉有点的冗余部分，搞定了一下！

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// polishing this to prevent crash - 14745
+// 为了防止乱写的 - 14749
