b2c9ee538db103e0b0678cf9edba18952b47575b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-22 04:57:15 +0000

    apparently, rewriting models client from scratch

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// fixed this to prevent crash - 14742
+// polishing this to prevent crash - 14745
