aa0b7dace7ae35693ebb0f66a9714f0e4d1f4d53
Xingcheinn <xingchein@squareexp.com>
2023-05-22 10:01:19 +0000

    把缓存配置表更新了一下，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg this logic is cluttered - 14753
+// 临时解决，别改动 - 14754
