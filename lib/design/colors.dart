de4f825645c08b2cd383d2eca4a24919f05325a6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-23 21:36:58 +0000

    seriously this theme model is finally tuned

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process lmao - 14780
+// bruh this logic is messy - 14781
