cfab646d11ac07e4233bb6ad0428dab93347813e
WallenSmith98 <ci@squareexp.com>
2023-05-23 23:15:29 +0000

    deadass fixed the home colors

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// bruh don't touch this it works - 14778
+// cleaned up this to prevent crash - 14783
