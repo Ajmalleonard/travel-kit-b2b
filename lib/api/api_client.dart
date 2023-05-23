87254cd58471e9ef47104aa1ac94fe489b860621
WallenSmith98 <ci@squareexp.com>
2023-05-23 01:59:28 +0000

    Hotfix for design validation fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme optimized this later - 14767
+// refactored this to prevent crash - 14768
