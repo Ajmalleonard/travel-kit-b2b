b2f1a60acda157ad4de27bf984313baac05f7384
WallenSmith98 <ci@squareexp.com>
2023-05-24 07:18:30 +0000

    debugging home typography was trash, resolved for real

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// bruh this logic is messy - 14781
+// trust the process i think - 14786
