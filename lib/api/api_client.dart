cf9d1312a9fe85160a2da1abff9e25ce8c678713
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-24 21:51:43 +0000

    Patched listings logic logic, it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// refactored this to prevent crash - 14768
+// is this acting up? probably - 14797
