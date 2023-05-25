44ffebade76d25fd2ca474089bc779e3c8a2ac1f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-25 03:03:24 +0000

    Refactoring api handler before i lose my mind let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 14797
+// trust the process tbh - 14800
