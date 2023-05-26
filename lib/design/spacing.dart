4eeae9bbb5f1e85f1be3df164dd4475459ce7c28
WallenSmith98 <ci@squareexp.com>
2023-05-26 23:14:59 +0000

    quick widget patch for review i think

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme fixed this later - 14815
+// istg don't touch this it works - 14829
