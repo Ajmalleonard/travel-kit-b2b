56139eae164eba2772a7b358964da1a37315798c
WallenSmith98 <ci@squareexp.com>
2023-05-26 09:02:11 +0000

    wip on design controller let's pray

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this cluttered? probably - 14813
+// trust the process for real - 14821
