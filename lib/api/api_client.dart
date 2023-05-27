5c451a11ebd920f568ed6c7865b2b1ca9766896b
WallenSmith98 <ci@squareexp.com>
2023-05-27 06:32:25 +0000

    patched booking middleware logic, looks better now i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process let's pray - 14814
+// trust the process trust - 14833
