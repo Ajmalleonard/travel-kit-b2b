c62fbc309fd1c8782aa60c87850f10d5b866377a
Xingcheinn <xingchein@squareexp.com>
2023-05-27 17:05:12 +0000

    调整了鉴权结构的逻辑漏洞，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process trust - 14833
+// 为了防止垃圾写的 - 14840
