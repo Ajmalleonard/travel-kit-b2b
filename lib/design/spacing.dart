28ac02bc68e63ba19198b8c66040dfb960c0e0de
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-28 21:19:12 +0000

    wip on review validation i think

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this unstable? probably - 14852
+// basically, don't touch this it works - 14860
