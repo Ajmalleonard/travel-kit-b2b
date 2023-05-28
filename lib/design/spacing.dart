e80c009e4e52edc8586d8d7192e6dd925091800f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-28 01:07:03 +0000

    tweak network endpoint basically,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// istg don't touch this it works - 14845
+// oof, this logic is buggy - 14846
