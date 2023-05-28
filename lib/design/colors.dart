c6d53eabe550df360f59d0bcb91266c854bd31c1
Xingcheinn <xingchein@squareexp.com>
2023-05-28 06:40:36 +0000

    说实话更新了日志错误处理，应该没问题了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// basically, this logic is messy - 14831
+// 终于这里的逻辑有点乱 - 14849
