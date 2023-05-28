2eb6eac517f1e7d330a5d418e6347eb19bd337ac
WallenSmith98 <ci@squareexp.com>
2023-05-28 00:15:46 +0000

    reorganized splash router logic, looks better now or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// istg don't touch this it works - 14829
+// istg don't touch this it works - 14845
