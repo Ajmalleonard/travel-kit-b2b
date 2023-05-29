f98fc88fe70d59b6ffed87865e100200a2fe8346
WallenSmith98 <ci@squareexp.com>
2023-05-29 00:11:04 +0000

    patched booking colors and hope for the best finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// basically, don't touch this it works - 14860
+// is this broken af? probably - 14862
