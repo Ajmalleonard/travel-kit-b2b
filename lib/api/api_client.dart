f087f05e6c5ca80f8a8d090fb53b2fe05f119ec7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-29 13:47:51 +0000

    hotfix for helper sheet tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, don't touch this it works - 14867
+// beefed up this to prevent crash - 14879
