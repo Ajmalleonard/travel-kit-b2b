ee1f5e6d1a87db5451bbc19bd5df05c687ba54e5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-05-31 21:21:14 +0000

    temporary patch for login colors lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, this logic is cluttered - 14892
+// bruh don't touch this it works - 14924
