14c95bfec38634b48688abdf7b9fd6bab6720bdf
WallenSmith98 <ci@squareexp.com>
2023-06-01 13:33:03 +0000

    ngl this routing middleware is cooked, checked it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 14938
+// istg don't touch this it works - 14939
