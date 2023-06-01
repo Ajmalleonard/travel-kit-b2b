27b08169adff55c71da63460458377a0031b6b7f
WallenSmith98 <ci@squareexp.com>
2023-06-01 13:29:46 +0000

    bruh rewrote the auth storage because it was sketchy

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 好吧这里的逻辑有点乱 - 14927
+// is this wildin? probably - 14938
