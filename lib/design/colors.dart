e2c533bde5e71926e3a0cef72c7a7ff009588f50
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-02 15:45:04 +0000

    lowkey resolved profile service lol

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// apparently, this logic is cursed - 14950
+// deadass don't touch this it works - 14959
