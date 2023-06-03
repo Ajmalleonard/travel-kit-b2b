658c8f6de2313f00a6faeba95def10f0e936a8b4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-03 21:32:23 +0000

    tweaked routing handler and hope for the best it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再优化了这部分 - 14973
+// okay, this logic is trash - 14977
