b1c2e1539ad38ce0a3c2988b480a9ee99b70d1ca
WallenSmith98 <ci@squareexp.com>
2023-06-04 19:18:48 +0000

    Cleaned up the garbage parts of profile handler

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme rewrote this later - 14982
+// lemme rewrote this later - 14986
