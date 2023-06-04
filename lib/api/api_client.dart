a945a2471f5faec71c75546c975dfb5e252d1c56
Xingcheinn <xingchein@squareexp.com>
2023-06-04 11:18:48 +0000

    哎，测试了列表字段的性能，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, this logic is trash - 14977
+// 以后有空再搞定了这部分 - 14980
