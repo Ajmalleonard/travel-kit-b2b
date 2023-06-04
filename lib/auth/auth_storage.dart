e7f18061a85667000e108d40de30b4ce7c04cc91
WallenSmith98 <ci@squareexp.com>
2023-06-04 14:17:17 +0000

    whoever wrote the booking card was wildin, updated it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// bruh don't touch this it works - 14968
+// basically, don't touch this it works - 14981
