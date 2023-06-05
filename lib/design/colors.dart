bf6cc091353e46acca8702fce9ce9144075d7ade
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-05 06:18:00 +0000

    Whoever wrote the theme model was wildin, tweaked it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this broken af? probably - 14962
+// trust the process fingers crossed - 14998
