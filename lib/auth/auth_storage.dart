790cc83d8ba03af7f42c100ad15e7b987f71ebb2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-05 13:49:20 +0000

    Debugging api colors was cluttered, resolved or i'm quitting

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// basically, don't touch this it works - 14981
+// bruh this logic is cluttered - 15005
