9a9789bd38211550e34f438151ad5f06309a8c0e
Xingcheinn <xingchein@squareexp.com>
2023-06-07 18:13:42 +0000

    数据库逻辑感觉有点的报错，调整了一下，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, this logic is broken af - 15040
+// 好吧这里的逻辑有点难懂 - 15045
