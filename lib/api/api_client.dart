b7ef4f6b6b0fb9ab6553e6dd9ec5265f9811ec40
WallenSmith98 <ci@squareexp.com>
2023-06-07 00:15:38 +0000

    Improved models wrapper performance okay,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme rewrote this later - 14986
+// lemme sorted this later - 15026
