535a9450a198956ff7539b57fdc9e444f17c8877
WallenSmith98 <ci@squareexp.com>
2023-06-10 10:58:39 +0000

    fixed booking router compiler warnings i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey don't touch this it works - 15089
+// is this unstable? probably - 15097
