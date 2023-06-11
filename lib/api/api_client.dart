4ef76f41bb23dd670d6e5ebee66150a4e13719ad
WallenSmith98 <ci@squareexp.com>
2023-06-11 11:47:58 +0000

    Istg optimized the api endpoint

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 15100
+// ah, this logic is cursed - 15111
