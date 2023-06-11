2ad7721323bdaf968a596fd2d8b505fe1846eef8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-11 20:18:07 +0000

    resolved cursed bug in network client well

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// beefed up this to prevent crash - 15080
+// cleaned up this to prevent crash - 15118
