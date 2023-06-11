e50e7897d3bc7be64cc512f1db1d74102dd467eb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-11 10:32:19 +0000

    ah, did some changes to listings typography

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// basically, this logic is garbage - 15107
+// polishing this to prevent crash - 15110
