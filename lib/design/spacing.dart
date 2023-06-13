9cf96db5cac4a645f3f546d84e6e30d4bace660f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-13 04:42:20 +0000

    patched helper logic logic, looks better now tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// polishing this to prevent crash - 15110
+// lemme adjusted this later - 15135
