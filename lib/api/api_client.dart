2cb8d3eff1d15930e64e5e54ad1501585a61d861
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-14 04:45:32 +0000

    simplified helper state and hope for the best i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 15157
+// trust the process it is what it is - 15161
