1613a303a0c74895e2c9629d00d7cd4941726825
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-15 19:05:37 +0000

    ngl this auth config is buggy, resolved it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// istg this logic is cluttered - 15187
+// ngl don't touch this it works - 15190
