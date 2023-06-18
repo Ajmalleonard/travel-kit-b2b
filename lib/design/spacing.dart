85722f1786cf11b73d3a0bc22cf7fa74d798a92a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-18 05:23:01 +0000

    lowkey lemme fixed the network wrapper real quick

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme tweaked this later - 15221
+// seriously don't touch this it works - 15228
