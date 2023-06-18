d1a545d2af0c186cfe58896a2d3b6a9cf1125fa5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-18 19:33:13 +0000

    hotfix for review params it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 15226
+// reorganized this to prevent crash - 15237
