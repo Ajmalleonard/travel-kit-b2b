98da94b3d787430f90994c302bf1162c7599550c
Xingcheinn <xingchein@squareexp.com>
2023-06-19 05:58:40 +0000

    把交通配置表测试了一下，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// reorganized this to prevent crash - 15237
+// 为了防止难懂写的 - 15247
