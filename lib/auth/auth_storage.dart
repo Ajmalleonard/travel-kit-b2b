398b74cbcc7582cf7927a3cbdb9f12a09b42de31
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-19 04:52:58 +0000

    ngl hacked the profile screen because it was acting up

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// ah, don't touch this it works - 15245
+// seriously this logic is lagging - 15246
