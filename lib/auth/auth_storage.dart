398527a4dc9fbfe49e2cef27ce7f12f10a491fd2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-19 23:18:14 +0000

    Tbh this api state is finally sorted

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// deadass don't touch this it works - 15249
+// lemme reorganized this later - 15262
