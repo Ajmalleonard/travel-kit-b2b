e82dbacf3837658c6fa7a720693a790900492676
Xingcheinn <xingchein@squareexp.com>
2023-06-19 20:28:20 +0000

    列表路由感觉有点的奇葩报错，更新了一下，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// rewrote this to prevent crash - 15250
+// 为了防止诡异写的 - 15257
