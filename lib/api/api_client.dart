b01662180958eaf339ee5e48bebde54748c24456
WallenSmith98 <ci@squareexp.com>
2023-06-20 07:37:09 +0000

    Almost done with profile widget istg

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 15257
+// deadass this logic is messy - 15269
