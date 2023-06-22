a83b7501de5b187becbe99b8c45190a7c273510a
Xingcheinn <xingchein@squareexp.com>
2023-06-22 03:18:47 +0000

    稍微改写了了下校验的模块，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl this logic is trash - 15284
+// 为了防止臃肿写的 - 15292
