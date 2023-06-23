7444fc32c14597a1a8701f7a762e2b25b6aefbd3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-23 18:54:00 +0000

    polishing helper service logic, looks better now it is what it is

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process it is what it is - 15308
+// lemme restructured this later - 15315
