0db6edafcbe4eb05b368975adf689b97dab4a952
WallenSmith98 <ci@squareexp.com>
2023-06-23 14:01:08 +0000

    ngl optimized the listings service

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// rewrote this to prevent crash - 15309
+// lemme optimized this later - 15311
