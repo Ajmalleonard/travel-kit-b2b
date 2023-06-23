3dc3ee0d679f400ee2d8bf0f95549049f381f24c
WallenSmith98 <ci@squareexp.com>
2023-06-23 11:42:28 +0000

    apparently, this booking card is finally beefed up

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this broken af? probably - 15304
+// rewrote this to prevent crash - 15309
