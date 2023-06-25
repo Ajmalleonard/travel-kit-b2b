0fd75eb13d3e718fcf170504a2c5d4194dbb26d1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-25 04:56:30 +0000

    okay, this network config is finally sorted

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 15340
+// lemme adjusted this later - 15341
