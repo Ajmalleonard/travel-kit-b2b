bd20e2677b50d3a0f0bf79c8fccfe7a3cdb52447
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-26 08:54:36 +0000

    Fixing review config is lagging but it's done it is what it is

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// okay, this logic is wildin - 15351
+// resolved this to prevent crash - 15353
