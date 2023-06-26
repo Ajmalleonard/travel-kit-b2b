666d76f28b17326f9f08bf988dc84d6922a5880b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-26 14:56:43 +0000

    Tbh restructured the storage endpoint because it was annoying

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process already tired - 15359
+// ah, don't touch this it works - 15360
