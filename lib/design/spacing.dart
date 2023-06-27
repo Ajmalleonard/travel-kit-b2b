056a2aa14ac57b6f4c15104ef4aeb3bd5555f176
WallenSmith98 <ci@squareexp.com>
2023-06-27 03:32:08 +0000

    Improved profile card logic, looks better now lol

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// resolved this to prevent crash - 15353
+// trust the process for real - 15368
