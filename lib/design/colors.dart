fb42f10790df3f8c0f81ab527c6017a7597babb1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-28 16:39:04 +0000

    Checked design sheet to fix the staging environment fingers crossed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// fine, this logic is garbage - 15377
+// lemme optimized this later - 15386
