40c58bd9be0ea527149866046dbc8f9140cedadb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-06-30 20:05:45 +0000

    improved booking params performance tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme improved this later - 15415
+// lowkey don't touch this it works - 15418
