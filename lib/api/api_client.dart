400d6190ad52c6e2d51dcad7f550abe5ecb33038
WallenSmith98 <ci@squareexp.com>
2023-07-03 05:11:21 +0000

    simplified review service logic apparently,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 15445
+// trust the process i think - 15446
