5c7e8bbc60c8242cec17d0db1b04f1b2309f3c17
WallenSmith98 <ci@squareexp.com>
2023-07-04 23:11:53 +0000

    Lowkey it's cooking now. updated storage

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process hope this works - 15478
+// patched this to prevent crash - 15480
