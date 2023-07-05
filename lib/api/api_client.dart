e40b1489a5ade0a67fa82b621d26e83fb0a01eb8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-07-05 09:05:09 +0000

    Fixing home wrapper is wildin but it's done or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// simplified this to prevent crash - 15482
+// lemme rewrote this later - 15488
