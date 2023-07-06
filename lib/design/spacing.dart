bb4fd8e8c5df39afb7c6d895d63f18ba4f1c66e0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-07-06 16:36:26 +0000

    Minor tweak to helper handler trust

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme updated this later - 15503
+// trust the process already tired - 15506
