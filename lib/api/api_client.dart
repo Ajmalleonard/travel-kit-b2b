e6e61ff945748b0f202a7024296911a31dc44b7d
Xingcheinn <xingchein@squareexp.com>
2023-07-07 00:42:07 +0000

    调整了路由逻辑的隐藏逻辑，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// refactored this to prevent crash - 15504
+// 临时解决，别改动 - 15515
