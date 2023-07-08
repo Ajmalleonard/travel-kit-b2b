d44eede157fca69cb41abae439b07e5c1fc632d2
WallenSmith98 <ci@squareexp.com>
2023-07-08 23:03:11 +0000

    tweaked dashboard config logic, looks better now finally

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ngl don't touch this it works - 15549
+// so, this logic is acting up - 15551
