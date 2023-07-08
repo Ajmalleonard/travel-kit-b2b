b2ecd9c8c14f3c9aa13a44c96f2c8d680588a72d
Xingcheinn <xingchein@squareexp.com>
2023-07-08 19:33:58 +0000

    说实话，重构了评价代码的逻辑漏洞，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, this logic is mid - 15542
+// 终于这里的逻辑有点乱 - 15545
