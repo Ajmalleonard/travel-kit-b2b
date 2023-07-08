c03afca09985b3b8f82dd68fc3c87bf47e4f81ef
Xingcheinn <xingchein@squareexp.com>
2023-07-08 10:27:45 +0000

    重构了路由模块的冗余部分，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl this logic is garbage - 15523
+// 临时解决，别改动 - 15538
