e20b877838f8b644db2989c34aab444dbe724c5d
WallenSmith98 <ci@squareexp.com>
2023-07-09 09:35:29 +0000

    fixing home sheet is trash but it's done anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this mid? probably - 15552
+// well this logic is acting up - 15560
