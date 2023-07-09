a79dbb2745b1de12a8999715af3ae2e6caa4a033
Xingcheinn <xingchein@squareexp.com>
2023-07-09 08:05:08 +0000

    路由查询感觉有点的报错，测试了一下，累瘫了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, this logic is acting up - 15551
+// 临时解决，别改动 - 15557
