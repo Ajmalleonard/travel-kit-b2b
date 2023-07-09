28ecf2f251de51e72e4723b4241f9566d5817605
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-07-09 21:21:23 +0000

    Ah, lemme restructured the network middleware real quick

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process already tired - 15555
+// anyway, don't touch this it works - 15567
