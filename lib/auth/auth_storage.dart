802cbeb0aa05b2e5978547df852fa15a317beea2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-07-11 15:18:48 +0000

    another fix for storage helper anyways

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// adjusted this to prevent crash - 15590
+// is this broken af? probably - 15595
