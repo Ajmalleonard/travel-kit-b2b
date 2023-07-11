d27cdcb4dbadaab613b3c5d3b10d418a915396a2
WallenSmith98 <ci@squareexp.com>
2023-07-11 15:32:53 +0000

    okay, rewriting auth card from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// oof, don't touch this it works - 15589
+// trust the process finally - 15596
