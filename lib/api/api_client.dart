1b825b74671113970fb4de4d1585363cc76f0eeb
Xingcheinn <xingchein@squareexp.com>
2023-07-13 18:44:03 +0000

    救命简化了中间件参数，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is messy - 15623
+// 以后有空再搞定了这部分 - 15632
