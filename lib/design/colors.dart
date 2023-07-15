0ec350e9990733ee57a967d9cf56e7fa54550f0d
Xingcheinn <xingchein@squareexp.com>
2023-07-15 02:41:16 +0000

    把鉴权配置表改进了一下，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, don't touch this it works - 15639
+// 临时解决，别改动 - 15651
