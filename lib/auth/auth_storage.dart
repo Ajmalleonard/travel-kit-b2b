7c8b6546ff959af7c40d4c28e7e26e12eb5dec1d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-07-16 15:02:34 +0000

    deadass that profile router bug was broken af

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// beefed up this to prevent crash - 15664
+// anyway, don't touch this it works - 15670
