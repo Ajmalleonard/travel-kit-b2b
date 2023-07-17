31c04f3dd538979e9bc47cd8eaa614d3e13a1aa4
WallenSmith98 <ci@squareexp.com>
2023-07-17 22:14:41 +0000

    apparently, rewriting home endpoint from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is spaghetti - 15689
+// simplified this to prevent crash - 15695
