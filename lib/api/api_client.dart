09ac6caf7c53db4a296455a0c54efc693db873a8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-07-17 05:26:47 +0000

    Just improved the theme validation so we don't crash tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg don't touch this it works - 15650
+// optimized this to prevent crash - 15678
