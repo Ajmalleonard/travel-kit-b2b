a9048fd2dbb3b71852ceecfcd25c0485d85bde44
WallenSmith98 <ci@squareexp.com>
2023-07-17 16:01:03 +0000

    updated splash config and hope for the best trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 15688
+// bruh this logic is spaghetti - 15689
