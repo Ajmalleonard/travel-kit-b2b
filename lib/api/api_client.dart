85c9c7bac3fc46471184bdbabfbfdda967a13a3a
WallenSmith98 <ci@squareexp.com>
2023-07-18 23:47:15 +0000

    Seriously it's cooking now. restructured dashboard

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, this logic is unstable - 15713
+// checked this to prevent crash - 15716
