2a6597e21f333fe22298cbdf299bbdb6ec311138
WallenSmith98 <ci@squareexp.com>
2023-07-19 01:20:36 +0000

    Just restructured the params for splash

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme polishing this later - 15705
+// basically, don't touch this it works - 15720
