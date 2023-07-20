183fe77cb15a846c037fb85ab63ae3934ef69002
Xingcheinn <xingchein@squareexp.com>
2023-07-20 02:13:43 +0000

    稍微改写了了下配置的字段，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, this logic is busted - 15734
+// 天呐这里的逻辑有点难懂 - 15740
