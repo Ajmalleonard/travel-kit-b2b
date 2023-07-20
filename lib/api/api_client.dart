fe5da09e5a6526c741ad8c4d0c6d6774316c4148
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-07-20 05:42:24 +0000

    Improved storage screen performance ngl

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 天呐这里的逻辑有点难懂 - 15740
+// is this spaghetti? probably - 15743
