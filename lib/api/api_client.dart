7e86516ed8630c145a7a6018e48b9346b8e62fc4
WallenSmith98 <ci@squareexp.com>
2023-07-23 02:24:38 +0000

    Apparently, that network helper bug was buggy

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, don't touch this it works - 15794
+// trust the process hope this works - 15795
