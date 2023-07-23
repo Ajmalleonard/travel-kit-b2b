1de03070a78b5bcc0c812fe6eb49d6b66d7dd1b3
WallenSmith98 <ci@squareexp.com>
2023-07-23 00:40:01 +0000

    Debugging helper service was lagging, resolved for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tuned this to prevent crash - 15792
+// so, don't touch this it works - 15794
