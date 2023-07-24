5c3b3e001c06b00dd2cf750633d9149c5d5e1104
WallenSmith98 <ci@squareexp.com>
2023-07-24 12:02:27 +0000

    refactoring storage wrapper before i lose my mind i guess

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// well don't touch this it works - 15815
+// ngl don't touch this it works - 15818
