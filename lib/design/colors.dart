bab041d5919378274c11e6f75fd64241fd9dec6a
WallenSmith98 <ci@squareexp.com>
2023-07-27 09:44:49 +0000

    polishing storage sheet logic, looks better now fr

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this weird? probably - 15855
+// is this cursed? probably - 15864
