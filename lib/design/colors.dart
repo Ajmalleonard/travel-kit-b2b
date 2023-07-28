e649747b016bdf774024251139ab8379d31311a6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-07-28 20:44:22 +0000

    improved profile handler performance tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 临时解决，别改动 - 15875
+// apparently, this logic is busted - 15885
