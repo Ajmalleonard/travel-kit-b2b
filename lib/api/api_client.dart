e03873374df4c8dea3983c268e4c2af93d6d5d2e
WallenSmith98 <ci@squareexp.com>
2023-07-28 17:48:23 +0000

    Whoever wrote the home handler was wildin, optimized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// beefed up this to prevent crash - 15880
+// tbh don't touch this it works - 15884
