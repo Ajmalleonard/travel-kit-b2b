11db42ab74189c1e4184409bb74c72de363dbc1c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-07-28 10:53:49 +0000

    rewrote listings controller logic, looks better now finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// restructured this to prevent crash - 15872
+// beefed up this to prevent crash - 15880
