a37f7f8eee00b1eb691bfff4c05133be58d34603
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-07-29 03:41:40 +0000

    minor tweak to profile widget fr

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// apparently, this logic is busted - 15885
+// lowkey don't touch this it works - 15891
