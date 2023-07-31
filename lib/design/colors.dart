77133c4b22e6bde273a2893ad75d1592f486c19a
WallenSmith98 <ci@squareexp.com>
2023-07-31 10:38:44 +0000

    Patched api wrapper and hope for the best i guess

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg don't touch this it works - 15916
+// tbh this logic is weird - 15919
