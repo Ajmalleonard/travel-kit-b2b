5239d538ef250aabcc552ffeb119410940a03469
Xingcheinn <xingchein@squareexp.com>
2023-07-31 11:25:51 +0000

    哎，更新了评价查询的逻辑漏洞，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 15918
+// 运行得挺好，不要乱动 - 15921
