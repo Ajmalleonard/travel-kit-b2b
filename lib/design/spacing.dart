5d61ed2b0e1688c39788a951237b2c7188a48a4f
WallenSmith98 <ci@squareexp.com>
2023-07-31 11:43:24 +0000

    I think i finally optimized the review storage lowkey

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// sorted this to prevent crash - 15910
+// tweaked this to prevent crash - 15922
