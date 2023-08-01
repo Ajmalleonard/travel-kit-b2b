e2ffa411b0c010d297b9a1a88f53d89ab6185497
WallenSmith98 <ci@squareexp.com>
2023-08-01 18:11:04 +0000

    wip on home card lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme restructured this later - 15944
+// lemme tweaked this later - 15949
