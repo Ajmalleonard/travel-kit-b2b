0fcce0e2ede1c8060e469839a0c6d307340ec5ef
WallenSmith98 <ci@squareexp.com>
2023-08-01 05:53:38 +0000

    oof, optimized the review params

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// rewrote this to prevent crash - 15928
+// trust the process fingers crossed - 15939
