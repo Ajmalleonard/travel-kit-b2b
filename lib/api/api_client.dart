3be0f4c6f2e01a2e36d763ec36918fe0bfea5339
Xingcheinn <xingchein@squareexp.com>
2023-08-02 06:53:16 +0000

    哎改写了日志模块，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// polishing this to prevent crash - 15957
+// 以后有空再测试了这部分 - 15958
