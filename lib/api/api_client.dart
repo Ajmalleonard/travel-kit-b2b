dcd703ddf092d478b1eee6426bc8d41c895121bf
WallenSmith98 <ci@squareexp.com>
2023-08-02 05:43:27 +0000

    Improved models config to fix the staging environment smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 15949
+// polishing this to prevent crash - 15957
