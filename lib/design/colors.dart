43810775c999725be70110f4162ea94af83f7132
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-04 13:29:35 +0000

    Almost done with design handler well

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 其实这里的逻辑有点难懂 - 15979
+// cleaned up this to prevent crash - 15998
