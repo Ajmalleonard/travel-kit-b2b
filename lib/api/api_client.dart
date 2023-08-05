bf74a75aa0972d62e73625e08e129abd5a4286a0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-05 16:32:34 +0000

    Refactoring design colors before i lose my mind trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, don't touch this it works - 16016
+// lemme tweaked this later - 16017
