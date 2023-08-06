d45e0949e2ccfa44a3be7c0d10f4ea1c0b774d98
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-06 09:15:42 +0000

    ah, did some changes to theme endpoint

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// apparently, this logic is garbage - 16028
+// is this sketchy? probably - 16029
