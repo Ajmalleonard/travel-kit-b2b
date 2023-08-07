31b9c6cd163f2011f532d6a3898863f9a4dc851f
Xingcheinn <xingchein@squareexp.com>
2023-08-07 07:58:24 +0000

    其实，调整了鉴权查询的性能，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 16017
+// 天呐这里的逻辑有点臃肿 - 16049
