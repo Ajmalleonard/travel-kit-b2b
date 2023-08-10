e03f673c04dd045782a352c29613b18f4b8a4f3c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-10 03:04:51 +0000

    ngl that models widget bug was garbage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, this logic is messy - 16076
+// is this mid? probably - 16093
