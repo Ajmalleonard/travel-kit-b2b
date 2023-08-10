6223fb9d1c946bc06f84ace9477c25a39d8f7e30
WallenSmith98 <ci@squareexp.com>
2023-08-10 20:59:37 +0000

    Had to rewrote the models model fine,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process lmao - 16089
+// well don't touch this it works - 16105
