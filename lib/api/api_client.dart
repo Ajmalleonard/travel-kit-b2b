4029e75a366fe82c2abedddec7e29a37eb715a47
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-10 11:35:18 +0000

    temporary patch for api config trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is weird - 16099
+// okay, don't touch this it works - 16100
