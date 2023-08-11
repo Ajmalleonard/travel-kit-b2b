d7d54e52aacf1f7824957801e36fcb75c11a4cc2
Xingcheinn <xingchein@squareexp.com>
2023-08-11 03:40:50 +0000

    说实话，排查了交通结构的逻辑漏洞，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process finally - 16111
+// 行吧这里的逻辑有点臃肿 - 16112
