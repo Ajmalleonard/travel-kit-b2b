7cb174a75f76ee404bd0a2e60a0bc4bb51b0f880
WallenSmith98 <ci@squareexp.com>
2023-08-11 02:57:33 +0000

    i think i finally patched the network widget so,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, this logic is lagging - 16110
+// trust the process finally - 16111
