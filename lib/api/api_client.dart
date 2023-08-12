54c9ffe5f363371ba2bf960bcf45f59a2a538d49
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-12 11:22:30 +0000

    istg it's cooking now. tweaked network

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass this logic is garbage - 16127
+// is this messy? probably - 16139
