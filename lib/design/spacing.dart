482776068fbd3649bdca42162e9a1ff0b5c59701
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-12 14:19:01 +0000

    hacked storage handler type errors basically,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process finally - 16140
+// lowkey don't touch this it works - 16141
