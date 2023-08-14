be6d3d68e96eaefa6c8fea308d6e0f24d3f008c7
WallenSmith98 <ci@squareexp.com>
2023-08-14 04:39:07 +0000

    Deadass that listings endpoint bug was cooked

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this mid? probably - 16167
+// simplified this to prevent crash - 16171
