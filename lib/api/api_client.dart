686f2dae230aa6cb78f06f09359214281a3620bc
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-17 06:09:22 +0000

    tweaked storage helper logic, looks better now finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, don't touch this it works - 16198
+// is this cursed? probably - 16210
