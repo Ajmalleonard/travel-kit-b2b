4adf60eb26effe6c17632b1f4f5db0cad2184856
WallenSmith98 <ci@squareexp.com>
2023-08-17 09:10:32 +0000

    Refactoring auth logic before i lose my mind trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cursed? probably - 16210
+// well don't touch this it works - 16214
