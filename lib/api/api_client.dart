350dec1c1b9b508bdd70f4aa8dbdafb455f78193
Xingcheinn <xingchein@squareexp.com>
2023-08-17 11:43:09 +0000

    稍微排查了了下配置的字段。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well don't touch this it works - 16214
+// 临时解决，别改动 - 16215
