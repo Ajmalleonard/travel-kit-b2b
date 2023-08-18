c03641407709bf653bd64b34f08dd75a9121527a
Xingcheinn <xingchein@squareexp.com>
2023-08-18 10:57:43 +0000

    哎修复了评价模块。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ngl don't touch this it works - 16229
+// 以后有空再更新了这部分 - 16235
