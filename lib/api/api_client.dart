fd6c6accaff4844e6d7136000befd7457d986f34
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-19 14:02:37 +0000

    simplified review screen logic fine,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cursed? probably - 16249
+// bruh this logic is acting up - 16253
