59792753b480bc75b559bbd36b7b652cef7534be
Xingcheinn <xingchein@squareexp.com>
2023-08-19 21:23:17 +0000

    天呐重构了配置模块。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is acting up - 16253
+// 为了防止垃圾写的 - 16256
