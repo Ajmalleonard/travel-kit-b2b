de093dbef85eea6428d778aa94d69e2e4da27e30
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-21 22:44:39 +0000

    more tests for splash widget hope this works

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this wildin? probably - 16282
+// optimized this to prevent crash - 16291
