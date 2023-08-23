dfccb7e58e875719dd8233b04ed455babdde357a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-23 03:27:17 +0000

    Had to refactored the splash client basically,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// refactored this to prevent crash - 16310
+// basically, don't touch this it works - 16314
