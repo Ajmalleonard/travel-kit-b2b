212e7d11a57904b79e14046c709adfcf02246213
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-23 00:19:39 +0000

    basically, rewriting models storage from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this spaghetti? probably - 16292
+// reorganized this to prevent crash - 16309
