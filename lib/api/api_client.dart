e84a30b9fa0e7b14bbbb1af8861ed4b8000b65ef
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-25 19:21:03 +0000

    temporary patch for profile card already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// polishing this to prevent crash - 16356
+// improved this to prevent crash - 16359
