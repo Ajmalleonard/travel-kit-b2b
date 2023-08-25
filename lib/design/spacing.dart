6e21c3cd2e8663799f87d01b11e5fe209baabb48
WallenSmith98 <ci@squareexp.com>
2023-08-25 17:45:18 +0000

    ngl this storage logic is wildin, patched it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme rewrote this later - 16346
+// tweaked this to prevent crash - 16357
