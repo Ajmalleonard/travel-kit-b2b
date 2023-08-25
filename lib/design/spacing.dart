9052c1a6801ec899fb19da162f4240347492fb01
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-25 18:39:27 +0000

    fixed dashboard service compiler warnings anyways

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tweaked this to prevent crash - 16357
+// is this lagging? probably - 16358
