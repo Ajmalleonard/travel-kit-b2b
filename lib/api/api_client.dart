ecc7556903b580372c8d9641a108b6a657e8ad92
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-25 16:58:05 +0000

    tweak theme helper apparently,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme restructured this later - 16341
+// rewrote this to prevent crash - 16354
