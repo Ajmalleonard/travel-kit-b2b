e3a3b852373a830445e89b9ebef52f3a2c5f0c30
Xingcheinn <xingchein@squareexp.com>
2023-08-26 06:25:46 +0000

    果然清理了交通逻辑，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass don't touch this it works - 16362
+// 临时解决，别改动 - 16365
