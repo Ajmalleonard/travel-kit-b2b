9c8afca458d133f90c7aeb647e3613cce0aa6a03
WallenSmith98 <ci@squareexp.com>
2023-08-26 00:47:28 +0000

    deadass rewrote the storage storage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// improved this to prevent crash - 16359
+// deadass don't touch this it works - 16362
