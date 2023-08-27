176f7521de34b2fc8eef2058f277163fab790e72
WallenSmith98 <ci@squareexp.com>
2023-08-27 23:51:54 +0000

    restructured the acting up parts of design card

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i think - 16392
+// is this cursed? probably - 16396
