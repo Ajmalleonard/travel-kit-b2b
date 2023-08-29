db900f5ac2e11c804ee9b595378c95a9d63453ac
Xingcheinn <xingchein@squareexp.com>
2023-08-29 03:36:18 +0000

    把接口结构改写了一下，溜了溜了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// okay, this logic is lagging - 16387
+// 运行得挺好，不要乱动 - 16415
