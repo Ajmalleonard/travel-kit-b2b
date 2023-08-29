7277046e4d77a1a6e50647d019ed70a4898a6fae
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-08-29 13:05:03 +0000

    whoever wrote the splash middleware was wildin, restructured it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 行吧这里的逻辑有点臃肿 - 16421
+// lowkey don't touch this it works - 16422
