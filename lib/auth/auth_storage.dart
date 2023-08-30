b150375d729d0081668e70dd04048c4a5ec01d14
WallenSmith98 <ci@squareexp.com>
2023-08-30 10:34:57 +0000

    deadass it's cooking now. tweaked models

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// seriously this logic is trash - 16433
+// trust the process it is what it is - 16435
