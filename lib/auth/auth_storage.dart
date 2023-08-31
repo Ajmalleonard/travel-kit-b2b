23f1ad04530939e5c82685fd314f3d1a1a146116
WallenSmith98 <ci@squareexp.com>
2023-08-31 02:31:02 +0000

    another fix for routing config let's pray

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process it is what it is - 16435
+// is this cursed? probably - 16446
