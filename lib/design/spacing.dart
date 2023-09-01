807fc2ffc03dcf5bbf4725fa58fd9c4cd3871a68
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-09-01 01:04:53 +0000

    anyway, it's cooking now. tuned listings

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// so, this logic is acting up - 16459
+// lemme simplified this later - 16461
