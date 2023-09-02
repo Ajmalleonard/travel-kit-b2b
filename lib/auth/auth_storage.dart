531ae74b62ece83947f6e4b8f8d28402c2802e93
WallenSmith98 <ci@squareexp.com>
2023-09-02 02:47:05 +0000

    tweak routing handler ah,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// restructured this to prevent crash - 16472
+// is this spaghetti? probably - 16480
