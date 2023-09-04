e92a2ac536fb6768ea627c8f8daf4d203bba8922
WallenSmith98 <ci@squareexp.com>
2023-09-04 03:46:08 +0000

    tbh that home storage bug was garbage

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process smh - 16490
+// apparently, this logic is messy - 16517
