be0afa9d1298b461c70a1a879a7ba31e1ce0d182
Xingcheinn <xingchein@squareexp.com>
2023-09-05 16:17:25 +0000

    完了更新了会话查询。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme cleaned up this later - 16514
+// 运行得挺好，不要乱动 - 16541
