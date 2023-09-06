57686fd3d3ac63be66048be2db34764c35465aa2
Xingcheinn <xingchein@squareexp.com>
2023-09-06 12:01:40 +0000

    修复了配置配置表的逻辑漏洞，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme refactored this later - 16556
+// 运行得挺好，不要乱动 - 16557
