470f4d9dcd4fd2f035c42f8f61fc58d89e14c817
Xingcheinn <xingchein@squareexp.com>
2023-09-07 19:48:07 +0000

    救命改写了数据库模型，累死了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, don't touch this it works - 16546
+// 其实这里的逻辑有点臃肿 - 16574
