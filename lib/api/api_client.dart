90a56e55932267fb76fc7e6e71ec82be8e739307
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-09-09 00:23:56 +0000

    fixed design screen compiler warnings already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this buggy? probably - 16584
+// improved this to prevent crash - 16596
