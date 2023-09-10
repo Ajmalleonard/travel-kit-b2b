5bc8f592bc3a19619688d2a96da86e8f1c328b7d
WallenSmith98 <ci@squareexp.com>
2023-09-10 18:21:28 +0000

    temporary patch for api endpoint finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 16613
+// bruh this logic is cooked - 16624
