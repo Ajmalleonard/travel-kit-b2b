4b9adf1012e6b76245e76f66789dfe38d424f8a4
WallenSmith98 <ci@squareexp.com>
2023-09-10 02:17:36 +0000

    improved review service to fix the staging environment or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止乱写的 - 16601
+// apparently, don't touch this it works - 16613
