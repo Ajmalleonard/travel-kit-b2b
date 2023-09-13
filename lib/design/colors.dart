15be734235a4182739fd4093317fdd4174e0db54
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-09-13 01:32:35 +0000

    quick endpoint patch for design fr

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// well this logic is cursed - 16660
+// lemme checked this later - 16664
