09cc0fe1d58bd85b767b2e098fdd26e227339203
Xingcheinn <xingchein@squareexp.com>
2023-09-13 01:20:08 +0000

    稍微改进了了下日志的路由！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg this logic is wildin - 16652
+// 其实这里的逻辑有点垃圾 - 16663
