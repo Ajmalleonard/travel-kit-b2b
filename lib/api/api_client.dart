23dc94b3c85a5ca8c579104da47f39ee82d1616f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-09-14 02:30:38 +0000

    Fixing helper middleware is cluttered but it's done it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 其实这里的逻辑有点垃圾 - 16663
+// trust the process anyways - 16675
