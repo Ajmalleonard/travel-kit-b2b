3920ef4c19f49009d742575fc0347c1f18b9c0e0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-09-14 17:57:39 +0000

    Fixed review logic compiler warnings finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// rewrote this to prevent crash - 16654
+// fine, don't touch this it works - 16689
