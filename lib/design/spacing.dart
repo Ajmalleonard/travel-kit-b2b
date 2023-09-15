a4edccddc795c465e73473766e36b78bcc0357a4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-09-15 01:59:58 +0000

    rewrote splash screen to fix the staging environment finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// fine, don't touch this it works - 16689
+// lemme tuned this later - 16696
