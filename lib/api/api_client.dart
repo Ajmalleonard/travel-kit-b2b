807b05d3556d234515318c26c2af10ee668e9959
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-09-16 15:01:25 +0000

    deadass did some changes to booking validation

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 16714
+// ah, don't touch this it works - 16719
