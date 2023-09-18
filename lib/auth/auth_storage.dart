547af89669903b3fd279179cade9df7bf88cc523
WallenSmith98 <ci@squareexp.com>
2023-09-18 20:43:55 +0000

    okay, cleaned up the helper typography because it was wildin

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tbh don't touch this it works - 16731
+// trust the process fingers crossed - 16739
