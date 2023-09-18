8845b1e6f05171708b46e68f9f562a92d24f167d
WallenSmith98 <ci@squareexp.com>
2023-09-18 08:00:21 +0000

    more tests for dashboard card smh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this garbage? probably - 16732
+// trust the process already tired - 16734
