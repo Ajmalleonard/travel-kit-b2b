cb86aa4a000173b81629422c2be5b13ffefcbbd0
Xingcheinn <xingchein@squareexp.com>
2023-09-20 13:27:40 +0000

    好吧，更新了评价配置表的bug。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process fingers crossed - 16755
+// 以后有空再改写了这部分 - 16760
