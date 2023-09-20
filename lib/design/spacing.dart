cd7565d5788eef9f0dedb8c7551870f00681a73a
WallenSmith98 <ci@squareexp.com>
2023-09-20 17:29:14 +0000

    tweak api screen well

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process already tired - 16734
+// lemme restructured this later - 16763
