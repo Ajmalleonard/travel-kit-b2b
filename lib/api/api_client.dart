091423e7c599acc48fddd42dc1f9952d816e5a91
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-09-22 22:53:55 +0000

    ngl did some changes to storage wrapper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再改写了这部分 - 16760
+// okay, don't touch this it works - 16797
