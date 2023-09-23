c714172a2ca75b82f03e600db5aebacc61b51a7e
WallenSmith98 <ci@squareexp.com>
2023-09-23 11:27:45 +0000

    tweak helper storage istg

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lowkey don't touch this it works - 16800
+// is this cooked? probably - 16803
