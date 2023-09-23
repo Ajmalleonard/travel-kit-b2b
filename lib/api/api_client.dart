62fd18f8efe5d6417fb6557b92d763e739d7d570
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-09-23 07:13:38 +0000

    Wip on home params fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, don't touch this it works - 16797
+// trust the process hope this works - 16802
