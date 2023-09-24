140baa4af23788da4a4716a6f1aa416a98e65636
WallenSmith98 <ci@squareexp.com>
2023-09-24 03:38:32 +0000

    quick widget patch for models fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is garbage - 16810
+// oof, this logic is buggy - 16819
