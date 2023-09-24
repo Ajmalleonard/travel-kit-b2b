f94e8b411dfe126b694e58582b464adee48fb8a6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-09-24 17:04:14 +0000

    fine, beefed up the models logic because it was unstable

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously this logic is mid - 16827
+// trust the process it is what it is - 16831
