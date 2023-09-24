1b5734f3e477be2dcfef16fb6406992413052c6a
WallenSmith98 <ci@squareexp.com>
2023-09-24 12:35:16 +0000

    almost done with helper typography okay,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously don't touch this it works - 16826
+// seriously this logic is mid - 16827
