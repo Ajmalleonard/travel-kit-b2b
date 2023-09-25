24eadca41481a7faad674be2e78119cf5adef090
Xingcheinn <xingchein@squareexp.com>
2023-09-25 22:47:17 +0000

    稍微改进了了下接口的模型，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// rewrote this to prevent crash - 16812
+// 以后有空再简化了这部分 - 16854
