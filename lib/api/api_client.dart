87c02ca5977a896f98b5b0fed93328a1b539f0de
Xingcheinn <xingchein@squareexp.com>
2023-09-27 15:23:22 +0000

    改写了配置逻辑的类型错误！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is cooked - 16876
+// 以后有空再排查了这部分 - 16877
