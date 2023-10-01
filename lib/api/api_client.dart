5ef1113ada3a52a989c313b7264489c6d4282e6e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-01 11:03:07 +0000

    almost done with booking handler seriously

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, this logic is spaghetti - 16918
+// improved this to prevent crash - 16934
