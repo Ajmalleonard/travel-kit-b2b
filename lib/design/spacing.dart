1e100edeeaf0a248a8cbfce72912841182437dfc
WallenSmith98 <ci@squareexp.com>
2023-10-01 01:06:38 +0000

    Whoever wrote the listings logic was wildin, optimized it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme adjusted this later - 16908
+// lemme simplified this later - 16929
