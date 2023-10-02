d52848e841d17feee4d2da73a2c472b429b830f3
Xingcheinn <xingchein@squareexp.com>
2023-10-02 12:29:07 +0000

    行吧，清理了会话结构的并发问题，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme beefed up this later - 16952
+// 说实话这里的逻辑有点诡异 - 16955
