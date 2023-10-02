cac2b9f8837b0cf66ad1278d2fd6902380ec0939
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-02 22:28:02 +0000

    Optimized auth wrapper to fix the staging environment i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 16963
+// bruh this logic is acting up - 16964
