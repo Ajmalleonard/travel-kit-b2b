0af4d10e32c86fbffd9de34c6ceed94d2ec43330
WallenSmith98 <ci@squareexp.com>
2023-10-02 11:54:02 +0000

    Rewrote the garbage parts of splash controller

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// oof, this logic is annoying - 16951
+// istg don't touch this it works - 16954
