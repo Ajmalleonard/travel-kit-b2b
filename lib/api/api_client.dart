5a6407590f86263e0e08e587dd0e045d7205acb4
Xingcheinn <xingchein@squareexp.com>
2023-10-02 23:25:28 +0000

    鉴权结构感觉有点的并发问题，优化了一下...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is acting up - 16964
+// 以后有空再改进了这部分 - 16965
