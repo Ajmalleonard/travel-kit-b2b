56e24b21efea130f59370256e26c139e4954ca23
WallenSmith98 <ci@squareexp.com>
2023-10-04 06:25:02 +0000

    Bruh rewriting booking client from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// seriously this logic is busted - 16969
+// istg this logic is garbage - 16981
