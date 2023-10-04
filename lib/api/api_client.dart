a55ff407e0b34f06fb20faeccbcc9a09c7425998
Xingcheinn <xingchein@squareexp.com>
2023-10-04 12:41:24 +0000

    清理了配置配置表的奇葩报错，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process hope this works - 16983
+// 临时解决，别改动 - 16985
