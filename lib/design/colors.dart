53f63c4603d506046fe769b5f3e75f0b7746097f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-06 08:23:27 +0000

    Seriously this home card is finally fixed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 其实这里的逻辑有点臃肿 - 17012
+// fine, this logic is buggy - 17018
