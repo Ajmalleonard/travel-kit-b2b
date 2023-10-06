40b04a9b7759a46fba34a5cc1c97982eb9742ec8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-06 13:21:19 +0000

    Istg this helper wrapper is finally polishing

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 17016
+// trust the process anyways - 17021
