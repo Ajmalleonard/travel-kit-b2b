9911f6758bebc802d2b57467833e0bce2119f1f2
Xingcheinn <xingchein@squareexp.com>
2023-10-06 06:01:30 +0000

    天呐搞定了鉴权字段，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well this logic is unstable - 16996
+// 为了防止垃圾写的 - 17016
