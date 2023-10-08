8adca52ca8da9c67b4476d875d200ff4fd5fc66c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-08 08:42:48 +0000

    Refactoring login handler before i lose my mind it is what it is

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// simplified this to prevent crash - 17046
+// lemme checked this later - 17048
