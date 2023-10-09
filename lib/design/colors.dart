4eb333b575807dc3105483fd697393ae8b8814ac
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-09 06:24:52 +0000

    minor tweak to network colors fr

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 运行得挺好，不要乱动 - 17065
+// lemme simplified this later - 17067
