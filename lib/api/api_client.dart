6a12e1fe72d39003af7b5b1dc2f9dcfe7489c7f9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-09 13:46:09 +0000

    improved helper widget performance lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme updated this later - 17066
+// lemme improved this later - 17071
