6dbdcf54f0f0cac7ac372999e94a106b778f7fa0
WallenSmith98 <ci@squareexp.com>
2023-10-09 14:25:57 +0000

    ah, did some changes to home typography

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再修复了这部分 - 17068
+// bruh this logic is cursed - 17072
