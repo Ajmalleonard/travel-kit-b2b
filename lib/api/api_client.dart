8113174c9d965ad45c0b7b64588b2c8fcd6633a5
WallenSmith98 <ci@squareexp.com>
2023-10-10 10:41:00 +0000

    Tbh that splash logic bug was lagging

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme improved this later - 17071
+// fine, don't touch this it works - 17083
