184eef05099337c0f6d3b2fe4878024992e583af
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-10 15:47:10 +0000

    bruh optimized the auth helper

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// okay, this logic is buggy - 17078
+// lowkey don't touch this it works - 17086
