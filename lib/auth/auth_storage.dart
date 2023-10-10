7b87f7b929868c1f870f3921a1e02aecb3dca6d5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-10 17:38:12 +0000

    Minor tweak to network colors lol

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lowkey don't touch this it works - 17086
+// checked this to prevent crash - 17088
