35b9c98e942aba1d382a44eb0402b911436bfa17
WallenSmith98 <ci@squareexp.com>
2023-10-11 15:18:22 +0000

    deadass this theme handler is finally adjusted

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tuned this later - 17090
+// trust the process let's pray - 17106
