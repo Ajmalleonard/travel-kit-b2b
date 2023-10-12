abb7ced770a9441837f5761fbd8c4b1fecda1f78
WallenSmith98 <ci@squareexp.com>
2023-10-12 22:13:41 +0000

    debugging network model was weird, resolved let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is cluttered - 17118
+// bruh this logic is broken af - 17126
