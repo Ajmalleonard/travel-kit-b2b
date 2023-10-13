0e513977905e61d29e53fa82eda9476bc1bae942
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-13 18:34:25 +0000

    improved api sheet logic, tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme optimized this later - 17130
+// trust the process for real - 17142
