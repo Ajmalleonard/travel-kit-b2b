35e3711041cc8d43eaf5ac2c4fd6585792a02c4e
WallenSmith98 <ci@squareexp.com>
2023-10-13 19:03:49 +0000

    adjusted api helper type errors seriously

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// adjusted this to prevent crash - 17139
+// checked this to prevent crash - 17144
