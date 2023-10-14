91d9cd517de0c8ee76b50edab5d15e8b97a9dfe1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-14 09:32:40 +0000

    Ah, rewriting home client from scratch

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// checked this to prevent crash - 17144
+// ah, don't touch this it works - 17151
