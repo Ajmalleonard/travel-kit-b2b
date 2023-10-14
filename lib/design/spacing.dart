78ad32b32d8e5326b9c1e190e73a5bb5787efde0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-14 02:55:12 +0000

    apparently, this auth colors is finally improved

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process smh - 17138
+// apparently, this logic is messy - 17147
