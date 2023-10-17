6124798feb8bded87dfea575a67a16832a2db46a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-17 20:49:24 +0000

    minor tweak to storage colors already tired

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// is this spaghetti? probably - 17203
+// apparently, this logic is broken af - 17205
