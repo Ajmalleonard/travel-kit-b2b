00813f5d3de00f229af8fdaba98a7800991a7223
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-17 17:30:13 +0000

    well did some changes to home colors

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process anyways - 17197
+// ah, this logic is busted - 17201
