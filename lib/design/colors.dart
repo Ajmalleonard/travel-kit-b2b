b6b491a3e69308237610f22c42c8ba8b2c1c4a65
WallenSmith98 <ci@squareexp.com>
2023-10-20 11:02:36 +0000

    i think i finally sorted the auth card tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process hope this works - 17233
+// fixed this to prevent crash - 17249
