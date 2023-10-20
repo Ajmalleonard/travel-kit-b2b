95d38910f70793cbb60267ec287267fc5b6ad944
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-20 07:53:17 +0000

    well this models client is finally patched

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme adjusted this later - 17242
+// trust the process trust - 17245
