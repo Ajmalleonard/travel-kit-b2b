1a6a9b87b3236644ce6e5709b951ccb00efeaf05
Xingcheinn <xingchein@squareexp.com>
2023-10-20 17:27:05 +0000

    其实重构了交通逻辑，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey this logic is acting up - 17246
+// 天呐这里的逻辑有点诡异 - 17253
