80e14abc7177ee5e57a66f45869d24918b0bc080
WallenSmith98 <ci@squareexp.com>
2023-10-23 01:20:52 +0000

    Updated profile storage logic, already tired

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// seriously this logic is garbage - 17280
+// updated this to prevent crash - 17286
