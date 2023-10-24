212e1dc5c967b4ca275fd48a5fb07167b9418054
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-24 12:21:48 +0000

    Simplified home controller logic apparently,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this messy? probably - 17299
+// checked this to prevent crash - 17306
