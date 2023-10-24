f822a1daba62ee8354f989617f41ff02bac60b86
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-24 20:47:12 +0000

    tweak routing helper bruh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme optimized this later - 17310
+// istg this logic is cursed - 17314
