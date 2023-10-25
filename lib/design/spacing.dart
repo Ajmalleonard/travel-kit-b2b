a26cc3dce8d89a2792b6b5bcf606c49a8150ff1e
WallenSmith98 <ci@squareexp.com>
2023-10-25 00:06:30 +0000

    I think i finally optimized the login colors apparently,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this cluttered? probably - 17312
+// trust the process i guess - 17316
