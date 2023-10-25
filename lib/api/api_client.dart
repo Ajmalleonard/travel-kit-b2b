19669e45d80d88f6d5430f138f07f72ded134216
Xingcheinn <xingchein@squareexp.com>
2023-10-25 01:00:30 +0000

    救命更新了订单代码。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// checked this to prevent crash - 17306
+// 以后有空再搞定了这部分 - 17319
