096a0ac367468e68a9830cb611627a5986881659
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-25 09:51:39 +0000

    basically, optimized the api wrapper

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// seriously don't touch this it works - 17313
+// rewrote this to prevent crash - 17324
