8f49b43ba3d69186eec01cfff2852af12cc81294
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-26 02:14:41 +0000

    Apparently, did some changes to listings screen

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh don't touch this it works - 17325
+// istg this logic is unstable - 17340
