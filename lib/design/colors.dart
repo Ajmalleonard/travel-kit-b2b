155c3e94e899f44f66ca95b57f24e165e3ba0434
WallenSmith98 <ci@squareexp.com>
2023-10-28 10:28:50 +0000

    Had to resolved the routing wrapper fine,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// okay, don't touch this it works - 17361
+// is this busted? probably - 17373
