f63095291863afd5436d2cd91595c9f515590df0
WallenSmith98 <ci@squareexp.com>
2023-10-29 16:29:47 +0000

    Improved helper logic performance seriously

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// updated this to prevent crash - 17379
+// trust the process it is what it is - 17391
