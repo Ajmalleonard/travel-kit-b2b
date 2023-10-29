dbf616fe4fd34657c26ea6a9b9a4ae92cbc422bf
Xingcheinn <xingchein@squareexp.com>
2023-10-29 12:28:39 +0000

    把跨域查询优化了一下...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this busted? probably - 17373
+// 果然这里的逻辑有点难懂 - 17385
