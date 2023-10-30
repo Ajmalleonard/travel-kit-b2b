fb4363ba079127bb471e333655e7e11200c7476e
WallenSmith98 <ci@squareexp.com>
2023-10-30 05:33:55 +0000

    whoever wrote the splash handler was wildin, reorganized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, don't touch this it works - 17395
+// lemme polishing this later - 17406
