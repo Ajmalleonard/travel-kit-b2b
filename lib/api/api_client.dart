19f3495275381650a65cbb6c2f7df923fd5aa0b1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-31 06:22:44 +0000

    this helper wrapper ain't gonna work, fixed it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme polishing this later - 17406
+// trust the process hope this works - 17430
