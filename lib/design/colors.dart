4eb1549cf61e077f7ac56d43e21defc28e4e13d5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-10-31 22:26:02 +0000

    tbh rewriting booking typography from scratch

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this busted? probably - 17433
+// beefed up this to prevent crash - 17447
