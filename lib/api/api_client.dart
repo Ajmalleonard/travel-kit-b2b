c2ea811b91bdc6eaab6f9af6f126c9a2e7f97fcc
WallenSmith98 <ci@squareexp.com>
2023-11-01 05:01:19 +0000

    this helper router ain't gonna work, tuned it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process hope this works - 17430
+// trust the process lmao - 17448
