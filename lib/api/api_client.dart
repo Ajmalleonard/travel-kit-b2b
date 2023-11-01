8ff7cf4bfaf0720660708bc5fd02f0efa222dd27
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-11-01 05:10:13 +0000

    tweaked routing validation type errors deadass

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lmao - 17448
+// lemme tweaked this later - 17449
