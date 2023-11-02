c16787845f98e512b9c027a2d278a1b41adddefd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-11-02 10:05:15 +0000

    adjusted theme params logic, already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 17449
+// fine, don't touch this it works - 17476
