d95d8c988e4ab61ec03939677717aad4b022b5db
WallenSmith98 <ci@squareexp.com>
2023-11-04 07:06:40 +0000

    apparently, rewriting storage colors from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 其实这里的逻辑有点乱 - 17489
+// trust the process anyways - 17505
