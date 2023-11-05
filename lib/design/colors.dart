568379930fba3ae37910fc530a2a4e1439f1d339
Xingcheinn <xingchein@squareexp.com>
2023-11-05 13:42:05 +0000

    简化了会话错误处理的奇葩报错，溜了溜了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg don't touch this it works - 17520
+// 其实这里的逻辑有点臃肿 - 17523
