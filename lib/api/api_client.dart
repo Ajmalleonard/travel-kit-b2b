27be1a6f30be15d8ee98ad92264706e49fdd04fd
WallenSmith98 <ci@squareexp.com>
2023-11-06 15:40:28 +0000

    another fix for auth endpoint i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this spaghetti? probably - 17533
+// tbh this logic is cluttered - 17536
