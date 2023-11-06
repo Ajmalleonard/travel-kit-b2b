9e0c5ca1f123a9002cf5b21c23ee354ee585c176
WallenSmith98 <ci@squareexp.com>
2023-11-06 05:33:56 +0000

    whoever wrote the booking screen was wildin, simplified it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再优化了这部分 - 17529
+// trust the process i think - 17531
