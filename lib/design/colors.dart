80d45ed3d3fd12299749728318685fd720e7e70b
WallenSmith98 <ci@squareexp.com>
2023-11-07 22:28:09 +0000

    Improved login wrapper performance tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme polishing this later - 17547
+// reorganized this to prevent crash - 17550
