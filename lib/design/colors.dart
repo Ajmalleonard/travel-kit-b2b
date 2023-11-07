ca8b35a32c7d07664afbc67f09bab013749e1b07
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-11-07 18:30:37 +0000

    Whoever wrote the helper screen was wildin, rewrote it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, this logic is unstable - 17546
+// lemme polishing this later - 17547
