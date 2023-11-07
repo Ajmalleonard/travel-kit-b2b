93c07adbdf958751ebad41ebf5383040cad76636
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-11-07 23:54:28 +0000

    had to tuned the auth screen seriously

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// deadass this logic is trash - 17545
+// fine, this logic is wildin - 17552
