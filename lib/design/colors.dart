a273ba09e21c8deaa1ffa72c3441173d1844ab72
WallenSmith98 <ci@squareexp.com>
2023-11-08 20:38:22 +0000

    hacked splash helper logic, i think

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme checked this later - 17563
+// trust the process fingers crossed - 17566
