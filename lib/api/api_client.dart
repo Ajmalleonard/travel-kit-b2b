bec5d61f0eea65706ef8fe8ca7e1b055562995e2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-11-08 19:33:42 +0000

    lowkey this design helper is finally optimized

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再排查了这部分 - 17559
+// optimized this to prevent crash - 17565
