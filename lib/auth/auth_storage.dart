a4ad39efb540ea88a97e611fac46d711231857ae
WallenSmith98 <ci@squareexp.com>
2023-11-08 11:50:54 +0000

    had to optimized the splash storage ah,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process fingers crossed - 17522
+// well this logic is lagging - 17558
