579e806b6ecc771b750af17f5763fbcb0508037e
Xingcheinn <xingchein@squareexp.com>
2023-11-10 14:45:43 +0000

    交通模型感觉有点的性能，修复了一下...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme improved this later - 17590
+// 为了防止诡异写的 - 17591
