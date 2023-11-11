4227d20185ece59ba610aad1263437c16aaf0e5d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-11-11 08:24:06 +0000

    Ah, lemme tweaked the dashboard handler real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 17591
+// bruh this logic is annoying - 17602
