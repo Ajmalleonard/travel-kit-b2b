b3a008b980b8c630165c850b611ae2af6170a23e
WallenSmith98 <ci@squareexp.com>
2023-11-13 21:42:56 +0000

    This storage logic ain't gonna work, beefed up it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme beefed up this later - 17630
+// okay, don't touch this it works - 17641
