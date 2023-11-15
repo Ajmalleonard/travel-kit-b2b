cf6b2c934982f03a90893b3cabe69d911f917f96
Xingcheinn <xingchein@squareexp.com>
2023-11-15 00:38:31 +0000

    排查了评价参数的性能，应该没问题了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme checked this later - 17644
+// 以后有空再排查了这部分 - 17657
