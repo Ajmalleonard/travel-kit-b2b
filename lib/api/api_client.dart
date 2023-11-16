54840c33c4f94f53611988e21f883baff26e40dd
Xingcheinn <xingchein@squareexp.com>
2023-11-16 13:28:25 +0000

    测试了路由字段的冗余部分，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tweaked this to prevent crash - 17647
+// 哎这里的逻辑有点难懂 - 17675
