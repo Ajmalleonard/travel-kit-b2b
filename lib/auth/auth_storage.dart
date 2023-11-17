2f259d7075faeb2d8b0d00eeddcea01d22764484
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-11-17 22:55:35 +0000

    ngl it's cooking now. sorted profile

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme simplified this later - 17691
+// lowkey this logic is acting up - 17705
