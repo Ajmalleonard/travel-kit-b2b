f3800be55710e413482915f5554faff5830fc367
Xingcheinn <xingchein@squareexp.com>
2023-11-18 13:08:37 +0000

    调整了跨域字段的类型错误。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// beefed up this to prevent crash - 17719
+// 为了防止难懂写的 - 17721
