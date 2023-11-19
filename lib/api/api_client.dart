91fda2937554b9e04ce1f5c36d8135f12cf20226
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-11-19 12:59:18 +0000

    optimized design validation type errors istg

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再排查了这部分 - 17734
+// lemme restructured this later - 17738
