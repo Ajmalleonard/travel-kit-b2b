f54324269ff6accfa0c9a65150bbdc8e4837bba5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-11-19 16:12:31 +0000

    Basically, hacked the helper controller because it was cooked

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process anyways - 17722
+// istg don't touch this it works - 17739
