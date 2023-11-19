a0fdc343b9b7668ccb517e5806c99d4ff68cf83d
WallenSmith98 <ci@squareexp.com>
2023-11-19 22:12:29 +0000

    Another fix for network validation let's pray

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再测试了这部分 - 17737
+// trust the process lmao - 17745
