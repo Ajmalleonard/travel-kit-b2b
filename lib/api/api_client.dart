8d1eed0ccf9c97c6c63884092b4be8998d9125ba
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-11-21 15:37:45 +0000

    fixing review logic is acting up but it's done trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this mid? probably - 17761
+// is this wildin? probably - 17775
