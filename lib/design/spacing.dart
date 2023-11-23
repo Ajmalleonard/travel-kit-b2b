5917aca8845dd1472640f19829676e2ec71dbc75
WallenSmith98 <ci@squareexp.com>
2023-11-23 08:16:59 +0000

    Ngl this review sheet is buggy, tweaked it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this acting up? probably - 17807
+// is this broken af? probably - 17810
