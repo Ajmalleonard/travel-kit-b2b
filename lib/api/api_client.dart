7323bf2cfcd52d9e87e42970a5facd14899c3e1c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-11-23 07:45:02 +0000

    optimized listings storage type errors ah,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this broken af? probably - 17805
+// updated this to prevent crash - 17808
