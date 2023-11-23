bcdccb72e6b6a30b2a602fd13a7afba358572313
WallenSmith98 <ci@squareexp.com>
2023-11-23 06:22:14 +0000

    resolved lagging bug in booking state tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// adjusted this to prevent crash - 17792
+// is this acting up? probably - 17807
