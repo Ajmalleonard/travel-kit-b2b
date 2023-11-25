a88df283b305ef11e2a9076ab8a53b543c6668ad
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-11-25 09:55:36 +0000

    had to beefed up the auth helper deadass

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well don't touch this it works - 17837
+// fine, don't touch this it works - 17846
