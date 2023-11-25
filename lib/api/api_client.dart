83ba005f6545bb4efb0a1ef16bb8e8c74aa1ed34
Xingcheinn <xingchein@squareexp.com>
2023-11-25 13:58:29 +0000

    把列表模型更新了一下，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass this logic is unstable - 17847
+// 运行得挺好，不要乱动 - 17849
