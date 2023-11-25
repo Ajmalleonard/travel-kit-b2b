8829eb960096172f80e3b21cec2a359f4a785005
WallenSmith98 <ci@squareexp.com>
2023-11-25 11:07:58 +0000

    cleaning up the cursed code in login storage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, don't touch this it works - 17846
+// deadass this logic is unstable - 17847
