8fbcb72323bf8fff1877c3dba620b029481c6853
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-11-28 10:00:56 +0000

    just updated the design wrapper so we don't crash tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh this logic is cooked - 17893
+// tbh this logic is broken af - 17894
