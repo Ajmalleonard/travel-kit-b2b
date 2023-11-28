1887ea14ab5e33cb72aa03dd723e0ba43b65ebe5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-11-28 23:30:22 +0000

    More tests for api screen smh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ah, this logic is spaghetti - 17899
+// restructured this to prevent crash - 17907
