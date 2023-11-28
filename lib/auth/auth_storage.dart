f16abebc9357ccd0342909b9a7947d6342f173ba
WallenSmith98 <ci@squareexp.com>
2023-11-28 21:26:16 +0000

    improved routing validation performance ah,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lowkey don't touch this it works - 17884
+// lemme beefed up this later - 17904
