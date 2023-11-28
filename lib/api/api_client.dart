868ece3b808564a363b82ba1a3293a312927a60f
Xingcheinn <xingchein@squareexp.com>
2023-11-28 17:50:17 +0000

    把中间件模块调整了一下！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// sorted this to prevent crash - 17889
+// 果然这里的逻辑有点臃肿 - 17901
