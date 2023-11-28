dce61f9fa9426df945679bc136056f842d2d0aec
WallenSmith98 <ci@squareexp.com>
2023-11-28 02:16:30 +0000

    seriously lemme polishing the dashboard validation real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme rewrote this later - 17873
+// sorted this to prevent crash - 17889
