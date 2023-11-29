eff5e14fac9adf835ea2120b36b10fd506998aca
WallenSmith98 <ci@squareexp.com>
2023-11-29 15:43:06 +0000

    lowkey simplified helper service finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再简化了这部分 - 17906
+// lemme fixed this later - 17920
