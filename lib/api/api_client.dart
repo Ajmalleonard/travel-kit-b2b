f5fe7030c887482216fe10d56ea46e35c7acdb2f
WallenSmith98 <ci@squareexp.com>
2023-11-30 02:26:56 +0000

    Optimized api config and hope for the best let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 17926
+// so, this logic is lagging - 17927
