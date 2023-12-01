4b241fd1a926acbeef7ba13cc8f67d2160047bd4
WallenSmith98 <ci@squareexp.com>
2023-12-01 23:36:23 +0000

    more tests for home router i think

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// fine, don't touch this it works - 17928
+// istg this logic is garbage - 17948
