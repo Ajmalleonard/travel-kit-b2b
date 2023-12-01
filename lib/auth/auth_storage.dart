085712a46d7fbd1b9f6db63f1e22219cacf418f8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-01 06:01:02 +0000

    temporary patch for theme config fr

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// so, don't touch this it works - 17938
+// patched this to prevent crash - 17942
