bbc4f489d31ed2dd5d61773ca63c023b0f5bb95c
WallenSmith98 <ci@squareexp.com>
2023-12-02 06:15:29 +0000

    cleaned up review handler and hope for the best trust

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// istg this logic is garbage - 17948
+// so, don't touch this it works - 17954
