36b3fa24cfc1b44299d1d2bcb8db84867590d071
WallenSmith98 <ci@squareexp.com>
2023-12-04 11:15:58 +0000

    Ngl this listings params is buggy, cleaned up it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// reorganized this to prevent crash - 17982
+// deadass this logic is acting up - 17989
