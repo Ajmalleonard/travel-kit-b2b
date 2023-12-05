efd4e27711d197261a374b3eb784b16b531d0241
WallenSmith98 <ci@squareexp.com>
2023-12-05 20:45:32 +0000

    istg rewriting listings router from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this lagging? probably - 18008
+// trust the process for real - 18012
