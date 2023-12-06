907f20dd39bc95c835b4e16aa7b0c9c8bb6b7859
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-06 11:24:08 +0000

    minor tweak to design handler trust

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process let's pray - 18020
+// reorganized this to prevent crash - 18021
