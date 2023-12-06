52b9e7040111564aad20598070b3f072d598ce66
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-06 09:52:14 +0000

    I think i finally adjusted the api handler tbh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// cleaned up this to prevent crash - 18014
+// trust the process let's pray - 18018
