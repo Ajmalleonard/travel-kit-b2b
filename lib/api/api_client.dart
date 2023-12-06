0bfd058f910133ac467741f8e34db73f33100204
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-06 17:20:49 +0000

    fixed listings widget compiler warnings it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process for real - 18012
+// okay, don't touch this it works - 18025
