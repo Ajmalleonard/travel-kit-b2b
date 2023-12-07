3c60f540e2d2293fedee109bddc7f8f7456fa105
WallenSmith98 <ci@squareexp.com>
2023-12-07 22:14:24 +0000

    just rewrote the dashboard screen so we don't crash smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme simplified this later - 18036
+// lemme adjusted this later - 18041
