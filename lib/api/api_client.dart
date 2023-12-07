086c03d4b682a65c21b860582dd4ef1e6192f5c3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-07 15:19:59 +0000

    hacked the garbage parts of splash validation

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, don't touch this it works - 18025
+// lemme simplified this later - 18036
