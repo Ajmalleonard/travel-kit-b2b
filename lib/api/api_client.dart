4d4137647f4dea6878ab60c042410ec635f5b1d1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-07 22:59:45 +0000

    adjusted profile validation logic, looks better now trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme adjusted this later - 18041
+// trust the process fr - 18042
