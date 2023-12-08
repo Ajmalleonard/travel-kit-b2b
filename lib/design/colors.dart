7c9dab45ba9957d76b7b3250b0d7efba9cc039eb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-08 18:29:07 +0000

    Improved theme model logic, i guess

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, this logic is cluttered - 18039
+// is this trash? probably - 18057
