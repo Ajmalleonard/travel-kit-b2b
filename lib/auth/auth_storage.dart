8198689441c49bc7e5c5e045703900c3c720edf3
WallenSmith98 <ci@squareexp.com>
2023-12-10 15:22:14 +0000

    Debugging design wrapper was mid, resolved fr

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process fingers crossed - 18092
+// istg don't touch this it works - 18097
