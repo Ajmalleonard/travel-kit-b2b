d449a8554aa74cfc119ea637225bf15fdd8339ca
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-10 11:11:27 +0000

    Well adjusted the dashboard screen because it was busted

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tbh don't touch this it works - 18089
+// trust the process fingers crossed - 18092
