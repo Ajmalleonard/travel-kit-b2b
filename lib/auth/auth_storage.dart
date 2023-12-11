ebcfbcd76a3c374ed927a479fd1b2a84475803f4
WallenSmith98 <ci@squareexp.com>
2023-12-11 03:09:19 +0000

    deadass improved the listings helper

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// istg don't touch this it works - 18097
+// bruh this logic is spaghetti - 18104
