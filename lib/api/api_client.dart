61fa8573dffe2bbe57e6a5839a7544271e0b92f5
Xingcheinn <xingchein@squareexp.com>
2023-12-13 18:47:50 +0000

    更新了会话模块的屎山代码。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cursed? probably - 18143
+// 运行得挺好，不要乱动 - 18152
