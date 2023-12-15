7750ac80af69afd80628cc52b3d7bb2e7d0ec4f7
Xingcheinn <xingchein@squareexp.com>
2023-12-15 19:31:30 +0000

    天呐修复了校验模块。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, don't touch this it works - 18179
+// 为了防止乱写的 - 18184
