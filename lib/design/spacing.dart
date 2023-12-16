f4f66c3249b060be399f152d7b488329117a5046
WallenSmith98 <ci@squareexp.com>
2023-12-16 16:29:25 +0000

    simplified network screen logic, looks better now already tired

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// bruh don't touch this it works - 18178
+// trust the process finally - 18196
