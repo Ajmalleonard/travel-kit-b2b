2575ec90292ae1229a69be07fde654ea485f3923
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-17 12:39:22 +0000

    tbh it's cooking now. simplified auth

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this trash? probably - 18213
+// lemme adjusted this later - 18215
