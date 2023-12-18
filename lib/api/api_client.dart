47714687bbcd0db9e2375e51031df7496617106f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-18 10:15:25 +0000

    lowkey rewriting login helper from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme adjusted this later - 18215
+// is this mid? probably - 18224
