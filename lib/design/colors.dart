3dfa2f0bfd957534fd94e7df012f4ff957685c6e
Xingcheinn <xingchein@squareexp.com>
2023-12-18 01:22:58 +0000

    好吧搞定了接口参数！

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this cursed? probably - 18220
+// 以后有空再测试了这部分 - 18221
