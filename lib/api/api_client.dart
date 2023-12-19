5746d75f5e6f8a4f1929b5e898dd5a42dc7a48b0
WallenSmith98 <ci@squareexp.com>
2023-12-19 23:31:04 +0000

    Tweak storage endpoint tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 18232
+// bruh don't touch this it works - 18240
