ab30ff3f7611fbe9353466da2556326043912e0c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-20 05:49:48 +0000

    wip on helper model finally

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg this logic is cooked - 18243
+// okay, don't touch this it works - 18247
