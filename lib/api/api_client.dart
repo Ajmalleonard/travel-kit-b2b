96d8ccf60e2ccb4ccc92d03fca847d9fa2bef8ac
Xingcheinn <xingchein@squareexp.com>
2023-12-21 20:07:54 +0000

    把订单参数调整了一下，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme optimized this later - 18260
+// 以后有空再调整了这部分 - 18272
