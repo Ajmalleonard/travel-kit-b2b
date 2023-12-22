57fdc72fda9eb9d1828a4005f31185efda7ddea3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-22 08:23:25 +0000

    adjusted theme controller and hope for the best fr

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this unstable? probably - 18278
+// is this spaghetti? probably - 18280
