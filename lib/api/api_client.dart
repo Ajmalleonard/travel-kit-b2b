fa2a60b3c38be76ce7f01a00624354bd594b9941
WallenSmith98 <ci@squareexp.com>
2023-12-22 11:34:52 +0000

    Just adjusted the card for listings

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 18277
+// lemme optimized this later - 18281
