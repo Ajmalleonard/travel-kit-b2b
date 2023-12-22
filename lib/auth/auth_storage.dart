191103b6069b3a1619675d0b2379db85b65c3cd9
WallenSmith98 <ci@squareexp.com>
2023-12-22 01:24:35 +0000

    tweaked models validation type errors ngl

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process already tired - 18267
+// lowkey don't touch this it works - 18276
