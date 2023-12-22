d9c06eb1a76af3a1259b155defa4dc251f2784e5
WallenSmith98 <ci@squareexp.com>
2023-12-22 23:55:21 +0000

    Apparently, lemme tweaked teh tehme colors real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme optimized this later - 18281
+// trust the process tbh - 18289
