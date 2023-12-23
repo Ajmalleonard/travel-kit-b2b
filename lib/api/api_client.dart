d9383aac598740b229ae2d6f379a1e45d242c8a8
WallenSmith98 <ci@squareexp.com>
2023-12-23 19:02:48 +0000

    beefed up helper typography type errors ngl

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this mid? probably - 18290
+// resolved this to prevent crash - 18303
