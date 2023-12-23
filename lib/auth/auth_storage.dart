5d7dea657bd0dccc883d9e7bde8890e724366bf2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-23 18:53:46 +0000

    Reorganized the spaghetti parts of profile screen

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process it is what it is - 18300
+// is this buggy? probably - 18302
