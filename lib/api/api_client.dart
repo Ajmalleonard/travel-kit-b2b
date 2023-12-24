b1bac404dbb602b6ed3497204f58b76b9585601d
WallenSmith98 <ci@squareexp.com>
2023-12-24 11:16:20 +0000

    Refactoring api endpoint before i lose my mind smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, this logic is spaghetti - 18313
+// trust the process tbh - 18316
