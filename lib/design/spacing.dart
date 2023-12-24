b633b93097d8d2bdb46b0c6e49e73ba24012cd80
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-24 07:47:32 +0000

    refactoring models model before i lose my mind tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process it is what it is - 18311
+// tbh this logic is garbage - 18312
