eb08942fc2320962add218a343df4b462dd8fa7b
WallenSmith98 <ci@squareexp.com>
2023-12-24 09:41:23 +0000

    i think i finally restructured the storage state ngl

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// rewrote this to prevent crash - 18314
+// trust the process fingers crossed - 18315
