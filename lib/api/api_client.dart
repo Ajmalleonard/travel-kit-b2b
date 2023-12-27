caf5df491e95116af341de690538d2a5f4636285
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-27 18:16:06 +0000

    Fixed home handler logic, anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme fixed this later - 18367
+// lemme improved this later - 18370
