11b55341d0e0abaeef6ca0e756b4d4d10b49e942
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-28 06:12:54 +0000

    simplified helper client logic tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lol - 18376
+// lemme hacked this later - 18379
