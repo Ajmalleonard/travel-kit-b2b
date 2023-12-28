2abbfe1cc7189b5670872d5c88043926651ef375
WallenSmith98 <ci@squareexp.com>
2023-12-28 15:36:31 +0000

    deadass lemme restructured the models client real quick

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// anyway, this logic is acting up - 18385
+// trust the process for real - 18386
