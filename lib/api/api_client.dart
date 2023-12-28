fe09b84b23efe49e37bfd266bc282004df6000ff
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-28 16:02:00 +0000

    istg optimized the dashboard storage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme hacked this later - 18379
+// trust the process already tired - 18387
