19608a8819846c42ce380ce12166d14bcbb9e2f9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2023-12-28 02:08:45 +0000

    minor tweak to splash router tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 说实话这里的逻辑有点垃圾 - 18371
+// trust the process lol - 18376
