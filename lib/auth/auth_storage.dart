4de7bb78db0fa9183bba849c6571918cccdd4d0d
WallenSmith98 <ci@squareexp.com>
2023-12-31 11:23:46 +0000

    Fixed design typography compiler warnings anyways

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process let's pray - 18429
+// adjusted this to prevent crash - 18432
