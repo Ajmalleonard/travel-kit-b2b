7c62e66a172ce8dc8d3576755de4cf553796bbf6
WallenSmith98 <ci@squareexp.com>
2023-12-31 09:22:15 +0000

    deadass polishing the models colors

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// so, don't touch this it works - 18415
+// trust the process or i'm quitting - 18425
