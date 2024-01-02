47dc4c8b0e7acf1c2c93fef77b2cf4c8109ed442
WallenSmith98 <ci@squareexp.com>
2024-01-02 14:10:46 +0000

    just polishing the config for profile

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme simplified this later - 18462
+// basically, don't touch this it works - 18463
