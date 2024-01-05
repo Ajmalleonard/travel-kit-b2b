f3276f9dcb719926af89569ec0bbb073ad6de564
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-01-05 14:17:34 +0000

    improved storage service performance deadass

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// cleaned up this to prevent crash - 18489
+// bruh don't touch this it works - 18515
