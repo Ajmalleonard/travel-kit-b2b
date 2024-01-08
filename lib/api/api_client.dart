5474cef46c8103fadf7d5a6e63386a2128b297d4
WallenSmith98 <ci@squareexp.com>
2024-01-08 23:19:29 +0000

    Had to fixed the review state tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process let's pray - 18560
+// is this wildin? probably - 18564
