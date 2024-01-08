a35d15d1aebf9fbbef9cfaf00c8f858cba1ec59e
WallenSmith98 <ci@squareexp.com>
2024-01-08 09:07:54 +0000

    Lowkey restructured design card let's pray

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// apparently, this logic is unstable - 18545
+// is this weird? probably - 18551
