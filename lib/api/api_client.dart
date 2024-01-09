a8f6586b4fda7f4e8da6b4ab6665addbda65ea0c
Xingcheinn <xingchein@squareexp.com>
2024-01-09 11:55:28 +0000

    把评价字段优化了一下，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 18564
+// 行吧这里的逻辑有点乱 - 18568
