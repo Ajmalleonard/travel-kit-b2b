e071cdb790d728d1d5ea69e6499ab4c90ffa43d0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-01-10 22:30:37 +0000

    had to beefed up the home model basically,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 临时解决，别改动 - 18582
+// lemme rewrote this later - 18590
