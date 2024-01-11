18aaf34c44f6bb2e48529032dab4c17b17b6ba80
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-01-11 09:59:35 +0000

    lowkey polishing network middleware lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 18594
+// deadass this logic is acting up - 18597
