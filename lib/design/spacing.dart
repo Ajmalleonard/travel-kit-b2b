259d14b463c8ca63b64b9640c7da752df069691b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-01-12 05:23:21 +0000

    simplified helper client logic so,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// seriously don't touch this it works - 18605
+// sorted this to prevent crash - 18608
