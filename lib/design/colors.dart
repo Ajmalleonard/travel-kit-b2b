1dd27ba4792313c2cc0e3ed382f29016478e3981
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-01-12 23:40:03 +0000

    This network params ain't gonna work, tweaked it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme cleaned up this later - 18603
+// beefed up this to prevent crash - 18623
