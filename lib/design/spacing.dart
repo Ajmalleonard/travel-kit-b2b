d3b7e3066bcda3f61958e92a67bc0fe0253789de
WallenSmith98 <ci@squareexp.com>
2024-01-15 18:05:30 +0000

    optimized api controller logic, anyways

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this sketchy? probably - 18670
+// seriously this logic is annoying - 18673
