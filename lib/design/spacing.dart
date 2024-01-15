35e18c224ffb8c65c39e6f5ef6e6fb7a2457c9e6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-01-15 13:51:21 +0000

    ah, this listings widget is finally simplified

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// so, don't touch this it works - 18669
+// is this sketchy? probably - 18670
