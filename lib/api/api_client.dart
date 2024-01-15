fab360f7dc16ed4be3b63ee0e5095a40be9748be
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-01-15 18:41:45 +0000

    improved auth client type errors bruh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl this logic is buggy - 18671
+// is this wildin? probably - 18674
