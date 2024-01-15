d7c5726e7af140cccad722a96cb17fc220457baa
WallenSmith98 <ci@squareexp.com>
2024-01-15 11:23:09 +0000

    fixing routing state is cooked but it's done i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is weird - 18662
+// lowkey this logic is sketchy - 18667
