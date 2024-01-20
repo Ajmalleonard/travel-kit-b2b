71a045a3b4fcf55602c2490a1bf8ec94ddf064cd
Xingcheinn <xingchein@squareexp.com>
2024-01-20 18:02:25 +0000

    数据库路由感觉有点的并发问题，测试了一下！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme updated this later - 18757
+// 运行得挺好，不要乱动 - 18761
