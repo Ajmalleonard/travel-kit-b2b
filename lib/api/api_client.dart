3e312fedcc874246b5d95e1a45dd1a704a8fef71
WallenSmith98 <ci@squareexp.com>
2024-01-20 13:32:56 +0000

    okay, rewriting home storage from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process for real - 18734
+// lemme updated this later - 18757
