d0dd2c272bc9cf316f1ed541396a533363488f0a
WallenSmith98 <ci@squareexp.com>
2024-01-22 05:25:03 +0000

    Refactoring storage params before i lose my mind tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// improved this to prevent crash - 18777
+// tbh this logic is cooked - 18785
