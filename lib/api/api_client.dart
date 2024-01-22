163b0b6b89af206275f095b18a6c32d4c8173bf3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-01-22 19:28:48 +0000

    fixed review logic compiler warnings anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// patched this to prevent crash - 18793
+// well this logic is trash - 18799
