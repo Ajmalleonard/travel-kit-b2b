c2dd6f743dcb47504136b7dfa3a8ed48f1c90639
Xingcheinn <xingchein@squareexp.com>
2024-01-24 19:22:39 +0000

    天呐，调整了数据库配置表的冗余部分，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 18822
+// 以后有空再调整了这部分 - 18830
