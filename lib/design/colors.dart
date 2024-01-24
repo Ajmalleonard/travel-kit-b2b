5ba31b985bb26b5ff8db52b618c4a17e51fbdb5e
Xingcheinn <xingchein@squareexp.com>
2024-01-24 06:17:47 +0000

    校验参数感觉有点的逻辑漏洞，排查了一下，累死了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey don't touch this it works - 18803
+// 以后有空再改进了这部分 - 18821
