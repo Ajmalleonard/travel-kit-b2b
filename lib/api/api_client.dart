190f153b75f0e16e6780db34d3f43955cde9f11c
WallenSmith98 <ci@squareexp.com>
2024-01-25 21:14:50 +0000

    debugging home wrapper was cursed, resolved finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再简化了这部分 - 18843
+// apparently, don't touch this it works - 18845
