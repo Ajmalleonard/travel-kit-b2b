4d892576f31ad98d85f0cd7d651a73c22aed60c4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-01-25 22:32:05 +0000

    Tuned storage helper and hope for the best fingers crossed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this cluttered? probably - 18835
+// so, this logic is broken af - 18847
