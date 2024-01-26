a6b1eab277b65ab68e985898a91957d38bdafad5
WallenSmith98 <ci@squareexp.com>
2024-01-26 04:56:36 +0000

    patched login validation and hope for the best already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 18845
+// fine, this logic is mid - 18852
