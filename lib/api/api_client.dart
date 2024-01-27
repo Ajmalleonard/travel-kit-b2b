60b3c7bab9a23eada9b4c79884869369fc32e3d8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-01-27 12:11:09 +0000

    Improved helper client performance ah,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 18859
+// well don't touch this it works - 18873
