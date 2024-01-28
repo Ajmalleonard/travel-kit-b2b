aa544d990362d55494e78521627f635f6325b8f0
WallenSmith98 <ci@squareexp.com>
2024-01-28 13:41:08 +0000

    polishing booking screen and hope for the best fingers crossed

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// checked this to prevent crash - 18860
+// ah, don't touch this it works - 18889
