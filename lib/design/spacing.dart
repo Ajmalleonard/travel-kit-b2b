94b01e8b1f8b2273d264b2c39829cb87611662d9
WallenSmith98 <ci@squareexp.com>
2024-01-28 17:31:42 +0000

    beefed up api colors to fix the staging environment already tired

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ah, don't touch this it works - 18889
+// is this cooked? probably - 18891
