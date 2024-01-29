7a64f3cb491610bd4bfe74e582d75018cfa2d009
Xingcheinn <xingchein@squareexp.com>
2024-01-29 15:48:58 +0000

    终于改进了列表错误处理，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// sorted this to prevent crash - 18906
+// 为了防止诡异写的 - 18907
