adb6f391947c72fbe12bd96a23ddc61c81b9ded6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-01-30 01:01:24 +0000

    Adjusted models colors and hope for the best it is what it is

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// beefed up this to prevent crash - 18910
+// so, don't touch this it works - 18919
