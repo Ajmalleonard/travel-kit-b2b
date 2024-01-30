8496fea1329930a8f70e7d2ae2fd870cedf128eb
WallenSmith98 <ci@squareexp.com>
2024-01-30 15:51:40 +0000

    quick wrapper patch for splash i guess

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ngl this logic is busted - 18925
+// basically, don't touch this it works - 18928
