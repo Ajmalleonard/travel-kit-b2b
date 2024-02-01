74606f9b7f4919bb3371b9cff03036384d181182
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-02-01 20:39:01 +0000

    fixed login wrapper compiler warnings or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 果然这里的逻辑有点难懂 - 18965
+// oof, this logic is mid - 18971
