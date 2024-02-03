889107d015b32938bbd1ccafe7c4a37343ff9761
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-02-03 05:58:23 +0000

    so, this auth widget is finally tuned

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme beefed up this later - 18973
+// tbh this logic is weird - 18995
