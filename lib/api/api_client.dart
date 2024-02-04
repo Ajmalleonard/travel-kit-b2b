a91d79ae39905ac72cd2d9401566e7ce5121036b
Xingcheinn <xingchein@squareexp.com>
2024-02-04 17:46:28 +0000

    改进了订单错误处理的逻辑漏洞。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is weird - 18995
+// 以后有空再清理了这部分 - 19013
