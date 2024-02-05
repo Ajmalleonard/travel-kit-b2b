541392b2de01b4e73a663a8cf499cad9af8b5647
WallenSmith98 <ci@squareexp.com>
2024-02-05 21:21:53 +0000

    simplified models router logic okay,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process trust - 19012
+// polishing this to prevent crash - 19030
