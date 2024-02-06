0cf37926ff12f39ac8b6fac7a1d89ae407495f77
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-02-06 22:05:24 +0000

    resolved wildin bug in models params ngl

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process i think - 19042
+// cleaned up this to prevent crash - 19044
