0160480267bbe8f4ab374ce5d63959d235564117
WallenSmith98 <ci@squareexp.com>
2024-02-07 01:24:59 +0000

    fine, hacked the network validation because it was mid

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// fine, this logic is cluttered - 19027
+// ah, this logic is cursed - 19049
