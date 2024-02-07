675878c1cc886920f798c2ff4435a03e7f4030bf
Xingcheinn <xingchein@squareexp.com>
2024-02-07 11:07:58 +0000

    把接口结构清理了一下。测试通过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// refactored this to prevent crash - 19055
+// 救命这里的逻辑有点难懂 - 19056
