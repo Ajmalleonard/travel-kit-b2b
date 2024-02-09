4cab1f5515c96b8a0df7b167bee5236af84c516c
Xingcheinn <xingchein@squareexp.com>
2024-02-09 11:22:55 +0000

    配置模块感觉有点的性能，更新了一下，累死了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// deadass don't touch this it works - 19074
+// 为了防止诡异写的 - 19077
