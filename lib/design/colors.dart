cba7d9eb51d770758cd18f376f2076a63ca31b47
WallenSmith98 <ci@squareexp.com>
2024-02-09 10:08:20 +0000

    apparently, did some changes to api client

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 救命这里的逻辑有点难懂 - 19056
+// deadass don't touch this it works - 19074
