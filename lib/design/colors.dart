0428e8871dca12762661f5f68e68edb222bd1858
Xingcheinn <xingchein@squareexp.com>
2024-02-10 23:18:03 +0000

    改写了路由路由的bug！

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this mid? probably - 19100
+// 说实话这里的逻辑有点诡异 - 19109
