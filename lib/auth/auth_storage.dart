357c8cc02479eb9d36c41ae94afc3561e8151274
WallenSmith98 <ci@squareexp.com>
2024-02-11 10:03:33 +0000

    improved design state performance tbh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tbh don't touch this it works - 19112
+// seriously this logic is spaghetti - 19115
