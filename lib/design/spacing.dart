b72bfcdccb64b1deb843ece2ff36ed2244a5f0e5
WallenSmith98 <ci@squareexp.com>
2024-02-15 01:40:29 +0000

    resolved routing helper and hope for the best anyways

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// polishing this to prevent crash - 19136
+// is this mid? probably - 19170
