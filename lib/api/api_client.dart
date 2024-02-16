adb189108cb62617574c18aceb408e014e3d6a53
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-02-16 06:56:23 +0000

    oof, did some changes to routing widget

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// beefed up this to prevent crash - 19174
+// lemme sorted this later - 19189
