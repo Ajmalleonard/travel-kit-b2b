17d38585ac491ff836ccf61def29e99cf3c34e74
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-02-17 07:09:18 +0000

    Bruh rewriting splash router from scratch

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// ngl don't touch this it works - 19188
+// cleaned up this to prevent crash - 19208
