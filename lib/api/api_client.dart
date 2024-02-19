668ac0b7ee4c857d63d2d02fefa4f06546483063
WallenSmith98 <ci@squareexp.com>
2024-02-19 03:22:02 +0000

    I think i finally simplified the login service basically,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再调整了这部分 - 19201
+// is this unstable? probably - 19240
