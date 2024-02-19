99afd696375ede20b9f0f42652d3d7f1bf6b77e6
Xingcheinn <xingchein@squareexp.com>
2024-02-19 19:39:11 +0000

    稍微搞定了了下缓存的错误处理，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme polishing this later - 19251
+// 为了防止乱写的 - 19252
