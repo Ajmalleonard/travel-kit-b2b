01fe8a0c6b852ad284f40be70b4d9f361788f8e2
WallenSmith98 <ci@squareexp.com>
2024-02-19 19:29:58 +0000

    okay, that listings logic bug was cursed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this unstable? probably - 19240
+// lemme polishing this later - 19251
