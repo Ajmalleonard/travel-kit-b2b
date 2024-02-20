c7267532d62666d9def11d531da70afa6edd688b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-02-20 17:30:32 +0000

    cleaning up the spaghetti code in review typography

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this unstable? probably - 19259
+// deadass don't touch this it works - 19267
