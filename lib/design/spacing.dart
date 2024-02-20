7238119a9bddeab20fbc54cbcb66b18916614de3
WallenSmith98 <ci@squareexp.com>
2024-02-20 05:56:13 +0000

    Well this api middleware is finally cleaned up

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// fine, this logic is cursed - 19227
+// is this unstable? probably - 19259
