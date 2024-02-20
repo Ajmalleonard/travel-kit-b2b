f89f7724e5271c74e93a97baa1ca50ee1712b6a6
WallenSmith98 <ci@squareexp.com>
2024-02-20 20:25:16 +0000

    another fix for dashboard controller for real

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// deadass don't touch this it works - 19267
+// tbh this logic is messy - 19269
