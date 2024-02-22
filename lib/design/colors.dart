d5e8faa9626fe129e3cf3b79b42e77aa4d357f85
Xingcheinn <xingchein@squareexp.com>
2024-02-22 00:00:19 +0000

    改进了跨域配置表的报错。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this busted? probably - 19265
+// 以后有空再清理了这部分 - 19279
