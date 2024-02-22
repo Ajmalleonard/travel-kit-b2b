af6d5a9f09929ad6090454ee10779983cc2e3527
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-02-22 01:32:34 +0000

    well sorted the design middleware because it was sketchy

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process for real - 19280
+// trust the process i think - 19281
