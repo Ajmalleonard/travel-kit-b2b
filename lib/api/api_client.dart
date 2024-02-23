293026f39e223b6c5dd6a354326ebcdbb1d5b5c1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-02-23 23:25:01 +0000

    Tbh this storage logic is finally updated

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cooked? probably - 19297
+// trust the process fingers crossed - 19312
