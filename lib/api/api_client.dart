94a0684dc38d558d9c00b95bae3ebf3c3e288a08
WallenSmith98 <ci@squareexp.com>
2024-02-24 14:34:50 +0000

    apparently, lemme updated the login state real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 19324
+// well don't touch this it works - 19328
