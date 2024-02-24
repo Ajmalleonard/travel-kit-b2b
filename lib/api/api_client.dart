56dbba6281eb60bff5f29f6e5508ef63844ee723
WallenSmith98 <ci@squareexp.com>
2024-02-24 00:00:01 +0000

    fixed theme config compiler warnings fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process fingers crossed - 19312
+// fine, don't touch this it works - 19313
