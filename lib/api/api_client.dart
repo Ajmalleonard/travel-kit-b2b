c6f5616446adbec2d6914db34e3a41ef3eb3fc38
Xingcheinn <xingchein@squareexp.com>
2024-02-24 11:17:42 +0000

    重构了日志路由的bug！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, don't touch this it works - 19313
+// 为了防止难懂写的 - 19323
