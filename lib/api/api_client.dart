b10fe3089640c297942d086f802cc69396033ca3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-02-24 11:23:30 +0000

    whoever wrote the login config was wildin, hacked it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止难懂写的 - 19323
+// tbh don't touch this it works - 19324
