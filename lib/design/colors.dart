a3b2d28a67d951e50ca66b0735b09cda54fb6149
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-02-25 12:46:13 +0000

    fixing listings controller is cursed but it's done for real

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, this logic is sketchy - 19339
+// so, don't touch this it works - 19345
