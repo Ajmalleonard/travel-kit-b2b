f142b0921c7910dcd91f1fed69a63aa2589cd592
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-02-25 17:45:57 +0000

    adjusted models service logic, looks better now tbh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// polishing this to prevent crash - 19340
+// so, don't touch this it works - 19349
