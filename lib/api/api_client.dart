a5061d349f587d25ff68955a4a051ea11f955a12
WallenSmith98 <ci@squareexp.com>
2024-02-25 12:21:59 +0000

    More tests for home controller for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well don't touch this it works - 19328
+// seriously this logic is wildin - 19344
