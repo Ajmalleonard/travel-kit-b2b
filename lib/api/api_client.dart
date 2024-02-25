eea6924b2a69a9f1487fcebe4a5afa5b2716695a
Xingcheinn <xingchein@squareexp.com>
2024-02-25 17:40:33 +0000

    排查了评价结构的性能，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously this logic is wildin - 19344
+// 以后有空再调整了这部分 - 19348
