5008a1d72e1bf32e3f085257f31e15700fa5f240
Xingcheinn <xingchein@squareexp.com>
2024-02-28 16:47:30 +0000

    好吧，测试了校验逻辑的逻辑漏洞，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, don't touch this it works - 19392
+// 临时解决，别改动 - 19397
