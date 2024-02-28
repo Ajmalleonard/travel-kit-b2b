9cbbd7882fb0dea7457e81d03c24e24e9e08d1af
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-02-28 10:46:26 +0000

    tweak listings endpoint well

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 说实话这里的逻辑有点难懂 - 19371
+// fine, don't touch this it works - 19392
