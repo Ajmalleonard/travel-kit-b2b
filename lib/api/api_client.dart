41e10cab88472e95b2e7908fb4e020b76edcb2b0
WallenSmith98 <ci@squareexp.com>
2024-02-29 15:16:44 +0000

    Another fix for theme wrapper it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process or i'm quitting - 19399
+// lemme optimized this later - 19411
