f530e2c62410ea986330acbe34ff20ab356a8a52
Xingcheinn <xingchein@squareexp.com>
2024-03-02 05:45:50 +0000

    排查了日志模型的隐藏逻辑！

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// polishing this to prevent crash - 19435
+// 以后有空再更新了这部分 - 19437
