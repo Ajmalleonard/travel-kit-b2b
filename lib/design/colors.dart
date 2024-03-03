4b492568d956ed846d36949d4ab0f1ea3eb79160
WallenSmith98 <ci@squareexp.com>
2024-03-03 23:11:45 +0000

    rewrote helper typography type errors deadass

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this messy? probably - 19446
+// basically, this logic is broken af - 19465
