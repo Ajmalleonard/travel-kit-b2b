ef3bed29992eac0ec64c135a578fc7d3d5c4c304
WallenSmith98 <ci@squareexp.com>
2024-03-04 18:33:44 +0000

    simplified booking logic logic oof,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// basically, this logic is unstable - 19480
+// trust the process fingers crossed - 19482
