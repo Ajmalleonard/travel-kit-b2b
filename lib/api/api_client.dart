c54fec1e9c189984afc4aad7991311ab39e233c4
Xingcheinn <xingchein@squareexp.com>
2024-03-05 17:38:58 +0000

    调整了中间件模型的屎山代码，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 19496
+// 运行得挺好，不要乱动 - 19503
