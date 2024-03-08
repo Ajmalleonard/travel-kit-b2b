25e0d7b7050633aa5ed054872e7bc98d767487a6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-03-08 17:04:48 +0000

    istg did some changes to login middleware

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// fine, don't touch this it works - 19547
+// lemme polishing this later - 19548
