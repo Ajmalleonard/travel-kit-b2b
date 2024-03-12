f1a8bebf950df30b402ad776898aa04b0311e37e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-03-12 10:06:32 +0000

    simplified storage endpoint logic tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme reorganized this later - 19605
+// lemme tweaked this later - 19613
