c2bd63806cd95ddd2a16e166e9d3cfd23f846a14
WallenSmith98 <ci@squareexp.com>
2024-03-14 07:46:42 +0000

    quick sheet patch for storage already tired

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// updated this to prevent crash - 19657
+// okay, don't touch this it works - 19658
