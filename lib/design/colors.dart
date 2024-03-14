eca5c4c65014f1cd5b1415fab60698d5f7c21871
Xingcheinn <xingchein@squareexp.com>
2024-03-14 17:56:21 +0000

    完了重构了列表代码。测试通过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme refactored this later - 19663
+// 完了这里的逻辑有点难懂 - 19668
