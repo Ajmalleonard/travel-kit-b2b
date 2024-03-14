b22da599ab88e2029e31846846a934e05c6c595d
WallenSmith98 <ci@squareexp.com>
2024-03-14 15:36:32 +0000

    anyway, that auth screen bug was annoying

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// okay, don't touch this it works - 19658
+// trust the process for real - 19664
