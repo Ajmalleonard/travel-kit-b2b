9b7bcb35062be5f158b45acd94a9b44f573f3f63
WallenSmith98 <ci@squareexp.com>
2024-03-16 22:16:05 +0000

    restructured storage config logic, let's pray

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lowkey don't touch this it works - 19701
+// okay, this logic is annoying - 19708
