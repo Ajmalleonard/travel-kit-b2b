808b469ecd93bd71d2ad5629cf149198bd44a971
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-03-17 23:12:54 +0000

    minor tweak to storage typography for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this broken af? probably - 19709
+// trust the process trust - 19725
