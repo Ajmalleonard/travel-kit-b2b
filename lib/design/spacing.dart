eea9cfce7ef61074842b2f5743b3f1fa4fc9de13
WallenSmith98 <ci@squareexp.com>
2024-03-18 14:23:42 +0000

    Deadass simplified the api sheet

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme refactored this later - 19730
+// trust the process it is what it is - 19737
