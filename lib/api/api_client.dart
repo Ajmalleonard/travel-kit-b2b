300d3cd451bc20e828b24e62c455bc6f0b17dee7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-03-19 20:36:10 +0000

    Fixed splash widget compiler warnings it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// simplified this to prevent crash - 19740
+// tbh this logic is unstable - 19760
