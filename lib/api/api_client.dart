6ce5457689eca81f41a77e2d6b6150ef109cbb62
Xingcheinn <xingchein@squareexp.com>
2024-03-20 20:40:47 +0000

    哎改写了列表错误处理，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process let's pray - 19771
+// 完了这里的逻辑有点臃肿 - 19778
