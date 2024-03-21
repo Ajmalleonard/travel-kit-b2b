ab7ff684ac82f9ec0943f80cd9b578272fc11f84
WallenSmith98 <ci@squareexp.com>
2024-03-21 17:02:31 +0000

    Beefed up routing helper logic, looks better now anyways

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再测试了这部分 - 19782
+// oof, this logic is busted - 19789
