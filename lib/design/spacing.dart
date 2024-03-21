30cd7f316e0d15cbcb6cbeec12c636867dd60aef
WallenSmith98 <ci@squareexp.com>
2024-03-21 13:39:06 +0000

    resolved mid bug in network screen okay,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tuned this to prevent crash - 19785
+// istg don't touch this it works - 19787
