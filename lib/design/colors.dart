75f3597c528be0e50721a6cf58adf63a40fb686f
WallenSmith98 <ci@squareexp.com>
2024-03-22 06:36:45 +0000

    just beefed up the api logic so we don't crash for real

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// oof, this logic is busted - 19789
+// trust the process anyways - 19796
