b45f6a4cf298b1d84c63e03b0b5385db555dfb84
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-03-23 09:15:39 +0000

    Ah, polishing the storage storage because it was weird

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// istg don't touch this it works - 19787
+// lemme reorganized this later - 19814
