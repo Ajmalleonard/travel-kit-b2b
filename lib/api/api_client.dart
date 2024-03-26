29fb065570084b349511f75596b8b75390ae371b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-03-26 08:39:33 +0000

    ngl it's cooking now. rewrote booking

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process smh - 19826
+// is this mid? probably - 19862
