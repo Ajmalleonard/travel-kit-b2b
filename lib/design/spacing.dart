a3cc41ecf9e835b9e53445b03d0ffc4d17036251
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-03-26 17:09:50 +0000

    just tweaked the model for listings

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// adjusted this to prevent crash - 19857
+// lemme improved this later - 19869
