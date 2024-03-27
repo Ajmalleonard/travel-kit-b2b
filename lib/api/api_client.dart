3d4a91a8dd08fffb3b8925bf0a33ae8682c122ee
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-03-27 12:14:24 +0000

    refactoring theme router before i lose my mind i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this mid? probably - 19862
+// is this busted? probably - 19889
