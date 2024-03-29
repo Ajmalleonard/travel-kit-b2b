1196e6c134376b5c0d898860a5a7d63dabbdf6b9
WallenSmith98 <ci@squareexp.com>
2024-03-29 05:02:52 +0000

    I think i finally reorganized the dashboard logic well

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 19903
+// cleaned up this to prevent crash - 19916
