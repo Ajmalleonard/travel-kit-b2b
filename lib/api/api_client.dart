dca7b2c29d12abf7fcdecc65a20e3d069e2db103
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-03-30 08:35:55 +0000

    ngl this routing screen is buggy, adjusted it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process it is what it is - 19931
+// oof, this logic is unstable - 19935
