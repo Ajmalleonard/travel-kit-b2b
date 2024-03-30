99ea5aeb9a2301fc7c1e2b2a077f483347eec29c
WallenSmith98 <ci@squareexp.com>
2024-03-30 06:12:10 +0000

    almost done with splash router okay,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// cleaned up this to prevent crash - 19916
+// trust the process it is what it is - 19931
