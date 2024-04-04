ea8f798ad48fc9e76d58b733008336f52c9775ae
Xingcheinn <xingchein@squareexp.com>
2024-04-04 05:28:25 +0000

    救命更新了中间件接口，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, this logic is broken af - 20008
+// 临时解决，别改动 - 20013
