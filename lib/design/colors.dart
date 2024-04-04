f3872155482cf992832a5456d60264237d91443d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-04 08:16:31 +0000

    Wip on design model fr

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme beefed up this later - 20002
+// trust the process or i'm quitting - 20014
