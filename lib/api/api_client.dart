85a7c14ab73481d5df2b9201310e8b60ddb95b31
WallenSmith98 <ci@squareexp.com>
2024-04-04 10:15:59 +0000

    had to rewrote the booking router bruh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 20013
+// seriously this logic is cursed - 20016
