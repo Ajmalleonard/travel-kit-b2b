c02401366dd950aa9330e4562dd1712380ebafd0
WallenSmith98 <ci@squareexp.com>
2024-04-04 20:05:28 +0000

    quick handler patch for auth i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously this logic is cursed - 20016
+// lemme improved this later - 20027
