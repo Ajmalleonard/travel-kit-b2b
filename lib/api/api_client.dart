1824d874d563bae8da1f51118caab5f4a53b2c51
WallenSmith98 <ci@squareexp.com>
2024-04-05 11:29:53 +0000

    basically, did some changes to auth screen

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme improved this later - 20027
+// lemme refactored this later - 20036
