067ad829d4a6a8cb89051f95a5b951f5c168e0c7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-05 22:59:03 +0000

    tbh resolved the review widget because it was sketchy

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh this logic is spaghetti - 20037
+// tbh this logic is garbage - 20041
