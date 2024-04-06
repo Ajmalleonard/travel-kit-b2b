57039495bb5ed409def3180203904595617281c3
WallenSmith98 <ci@squareexp.com>
2024-04-06 10:40:05 +0000

    fixing theme sheet is cursed but it's done lol

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh this logic is garbage - 20041
+// lemme resolved this later - 20050
