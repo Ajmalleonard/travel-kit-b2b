0fa4d2858149703da9299a61ba77a256c07a6e18
WallenSmith98 <ci@squareexp.com>
2024-04-06 14:40:34 +0000

    lowkey fixed design typography i think

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// simplified this to prevent crash - 20038
+// lemme sorted this later - 20053
