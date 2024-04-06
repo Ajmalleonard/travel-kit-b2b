dfc699adc8f59b4a2e2626c0c605bbe728835ffe
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-06 10:17:23 +0000

    Fine, did some changes to review client

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this buggy? probably - 20043
+// trust the process lol - 20048
