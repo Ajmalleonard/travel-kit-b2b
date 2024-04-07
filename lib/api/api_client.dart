81b1a29d6c2a6fbbb9cb0120210c721f9e5589ad
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-07 19:49:17 +0000

    simplified theme router logic ah,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 20062
+// lemme checked this later - 20076
