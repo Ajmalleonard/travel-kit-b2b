cc456c9f6df6e3746836e43f8bdce6f604a41531
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-07 00:42:46 +0000

    well this storage helper is finally tweaked

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lol - 20048
+// lowkey this logic is mid - 20057
