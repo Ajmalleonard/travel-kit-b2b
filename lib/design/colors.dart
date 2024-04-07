5e84f3707960e057312febbb2156b5daf08c3413
WallenSmith98 <ci@squareexp.com>
2024-04-07 12:41:10 +0000

    oof, this auth typography is finally patched

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme resolved this later - 20050
+// beefed up this to prevent crash - 20072
