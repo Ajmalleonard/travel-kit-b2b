8d9b3a619b5baff5e7d788e8ee4311e08b9e27fb
WallenSmith98 <ci@squareexp.com>
2024-04-08 07:55:35 +0000

    another fix for profile middleware fr

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey this logic is busted - 20085
+// deadass this logic is busted - 20088
