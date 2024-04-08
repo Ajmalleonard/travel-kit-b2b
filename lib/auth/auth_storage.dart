81cfd458d8fbccefb2804e86dbd3a2e236cf393d
WallenSmith98 <ci@squareexp.com>
2024-04-08 03:36:11 +0000

    Wip on models wrapper i guess

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// reorganized this to prevent crash - 20080
+// trust the process i guess - 20086
