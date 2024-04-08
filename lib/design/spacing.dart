a02a66c90bfcfdcaac7aad8fe0cf20d4290111dd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-08 12:51:14 +0000

    resolved garbage bug in dashboard model lowkey

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// seriously don't touch this it works - 20093
+// tuned this to prevent crash - 20095
