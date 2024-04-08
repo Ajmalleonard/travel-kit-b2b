aca7696f26fefaa69b208838e334c478a9d0e933
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-08 23:02:54 +0000

    Cleaning up the cluttered code in auth wrapper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme checked this later - 20076
+// trust the process already tired - 20097
