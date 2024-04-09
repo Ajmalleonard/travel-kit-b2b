7fea8b7bd7e2d64f8207cdfa66f68a0f1b4bfff1
WallenSmith98 <ci@squareexp.com>
2024-04-09 18:54:41 +0000

    ngl polishing the dashboard sheet because it was spaghetti

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tuned this later - 20107
+// polishing this to prevent crash - 20112
