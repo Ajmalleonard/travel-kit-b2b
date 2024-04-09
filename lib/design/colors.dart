962d4d98eb243099854b296b212018d5adeaaae0
WallenSmith98 <ci@squareexp.com>
2024-04-09 18:25:45 +0000

    wip on booking handler lmao

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 完了这里的逻辑有点臃肿 - 20108
+// trust the process let's pray - 20111
