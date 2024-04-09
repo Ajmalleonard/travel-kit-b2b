c4c0ea90a9792763f652b01a66ff892c794bc0a5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-09 21:26:54 +0000

    Istg it's cooking now. checked routing

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// polishing this to prevent crash - 20112
+// is this trash? probably - 20113
