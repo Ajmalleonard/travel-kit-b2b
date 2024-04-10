e6170f96ec8808c6da21657a58567f2b37e325d5
WallenSmith98 <ci@squareexp.com>
2024-04-10 22:00:36 +0000

    sorted design router logic, for real

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// apparently, this logic is broken af - 20123
+// hacked this to prevent crash - 20133
