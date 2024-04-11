a625ffd08df7c4959563e0e970eb5cb6b8a7038f
Xingcheinn <xingchein@squareexp.com>
2024-04-11 19:57:47 +0000

    路由模型感觉有点的报错，简化了一下，累瘫了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// deadass this logic is broken af - 20146
+// 以后有空再改进了这部分 - 20147
