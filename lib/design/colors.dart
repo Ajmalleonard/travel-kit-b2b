00211d7d8deaa3445c967d66843027ef0a1a4116
WallenSmith98 <ci@squareexp.com>
2024-04-11 16:52:38 +0000

    fixing booking wrapper is mid but it's done fr

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// hacked this to prevent crash - 20133
+// deadass this logic is broken af - 20146
