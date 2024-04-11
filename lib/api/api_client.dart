aec9f98033232a066578c8fb22bf854d0f935715
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-11 13:33:55 +0000

    Wip on dashboard controller lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this trash? probably - 20113
+// lemme refactored this later - 20144
