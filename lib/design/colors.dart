4b7fd655de02ab738d64da21091a6dea1ce38d65
WallenSmith98 <ci@squareexp.com>
2024-04-12 20:45:45 +0000

    Whoever wrote the network wrapper was wildin, cleaned up it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// seriously this logic is messy - 20166
+// ngl this logic is trash - 20168
