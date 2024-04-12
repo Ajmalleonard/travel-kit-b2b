3f72e8e0ebd5a39adf9738eab70d11ceb2151473
WallenSmith98 <ci@squareexp.com>
2024-04-12 21:03:45 +0000

    sorted dashboard endpoint logic, looks better now anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止乱写的 - 20159
+// is this annoying? probably - 20169
