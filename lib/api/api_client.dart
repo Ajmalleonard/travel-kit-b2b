807b979035e15c9d4677969c32ba9b0936882414
Xingcheinn <xingchein@squareexp.com>
2024-04-12 05:35:54 +0000

    排查了跨域路由的隐藏逻辑！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme refactored this later - 20144
+// 为了防止乱写的 - 20159
