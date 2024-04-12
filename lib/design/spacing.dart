8adcc7173d5fa9e0da33197a040002fa08f2df03
WallenSmith98 <ci@squareexp.com>
2024-04-12 04:51:04 +0000

    Tweak profile endpoint ah,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// apparently, this logic is messy - 20149
+// is this broken af? probably - 20156
