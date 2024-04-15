836ccdca3bb0b316e2997ff297279f07c02207a3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-15 22:23:49 +0000

    Deadass this profile params is finally polishing

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// bruh don't touch this it works - 20204
+// anyway, don't touch this it works - 20212
