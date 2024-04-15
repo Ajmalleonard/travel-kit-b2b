27ccd885566c5ae6f224069a1601705eb83f11c8
WallenSmith98 <ci@squareexp.com>
2024-04-15 22:24:35 +0000

    wip on routing widget let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 救命这里的逻辑有点诡异 - 20198
+// trust the process anyways - 20213
