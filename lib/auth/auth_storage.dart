c86ba4d57bbb72e54f0114ed707d2b0ac2354c08
WallenSmith98 <ci@squareexp.com>
2024-04-16 19:53:17 +0000

    more tests for review router trust

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// ngl don't touch this it works - 20224
+// sorted this to prevent crash - 20228
