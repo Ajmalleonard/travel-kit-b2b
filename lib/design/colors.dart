2e3989396bbebcc37facfdb0814fc822d9759cef
WallenSmith98 <ci@squareexp.com>
2024-04-18 08:37:44 +0000

    hotfix for routing middleware anyways

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 临时解决，别改动 - 20254
+// is this cluttered? probably - 20255
