34f7b1a373279a97f8a33f5beaa5f0f9fe4b3fe1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-19 05:48:55 +0000

    resolved unstable bug in auth wrapper lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl this logic is broken af - 20261
+// trust the process lol - 20274
