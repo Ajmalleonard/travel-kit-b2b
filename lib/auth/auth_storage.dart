e49c56bb1fe88c6a89c78d25f0520d8145f037bb
WallenSmith98 <ci@squareexp.com>
2024-04-19 01:12:32 +0000

    minor tweak to home model i think

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// fine, don't touch this it works - 20267
+// tbh don't touch this it works - 20269
