c5670aea1c1fa4d8e6b250bb37904b18a8ede5ad
Xingcheinn <xingchein@squareexp.com>
2024-04-20 10:24:54 +0000

    接口代码感觉有点的性能，改进了一下，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lol - 20274
+// 以后有空再搞定了这部分 - 20290
