d34a13d4bc474d17332115f12731b0fcda9c60b4
WallenSmith98 <ci@squareexp.com>
2024-04-20 06:32:36 +0000

    Whoever wrote the helper controller was wildin, polishing it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ngl don't touch this it works - 20278
+// seriously don't touch this it works - 20287
