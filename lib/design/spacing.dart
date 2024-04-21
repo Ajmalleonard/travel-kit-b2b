2c7cfd8942b8ae022dca878f3be5ef3cb1ba8847
WallenSmith98 <ci@squareexp.com>
2024-04-21 21:19:31 +0000

    temporary patch for storage card lol

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ngl don't touch this it works - 20310
+// is this spaghetti? probably - 20313
