1613a8f9b1e6b41d5419053625b8d96cf51987a6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-21 00:28:53 +0000

    just restructured the controller for theme

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// resolved this to prevent crash - 20298
+// is this wildin? probably - 20301
