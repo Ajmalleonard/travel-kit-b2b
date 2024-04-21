9d077a8a294074e4ef9956ed48b93444be6da460
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-21 13:45:07 +0000

    Improved the messy parts of network sheet

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process i think - 20307
+// ngl don't touch this it works - 20310
