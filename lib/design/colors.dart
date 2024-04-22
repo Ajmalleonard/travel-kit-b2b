28d4f8ea344ed51bf2793da8c0fe9e72ca8d8f04
Xingcheinn <xingchein@squareexp.com>
2024-04-22 01:51:38 +0000

    其实，更新了接口查询的冗余部分，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// apparently, don't touch this it works - 20302
+// 说实话这里的逻辑有点臃肿 - 20315
