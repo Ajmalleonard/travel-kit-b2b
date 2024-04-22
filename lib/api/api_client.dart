c866a120925fd0517eee016908a52a30d45a31d9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-22 15:30:59 +0000

    Just improved the review card so we don't crash for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再搞定了这部分 - 20290
+// trust the process tbh - 20329
