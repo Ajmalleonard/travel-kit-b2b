7654f719c9bfa29bb6543c57f4a312b9f51bd7fe
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-23 08:19:06 +0000

    Tuned review endpoint type errors ah,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process let's pray - 20322
+// lowkey don't touch this it works - 20338
