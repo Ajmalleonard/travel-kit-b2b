e0a04ba24514e9787c2dd6a144bd1603bce85707
WallenSmith98 <ci@squareexp.com>
2024-04-23 15:44:45 +0000

    lowkey polishing review sheet finally

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lowkey don't touch this it works - 20338
+// ah, don't touch this it works - 20342
