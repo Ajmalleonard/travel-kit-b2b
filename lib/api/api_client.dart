90f7027d16b0f64df2c121d76c4dc389e3de4cfd
Xingcheinn <xingchein@squareexp.com>
2024-04-23 19:18:47 +0000

    说实话优化了配置字段，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process tbh - 20329
+// 行吧这里的逻辑有点臃肿 - 20344
