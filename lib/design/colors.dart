ff388bfc7e95afc82ceb8cfccdfa5b132de5ac2d
WallenSmith98 <ci@squareexp.com>
2024-04-24 03:02:46 +0000

    cleaned up design logic and hope for the best for real

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// okay, this logic is spaghetti - 20347
+// is this trash? probably - 20351
