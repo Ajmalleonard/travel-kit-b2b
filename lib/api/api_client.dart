9f442adda6a4cf3833621b489b861f3c5410d9ef
WallenSmith98 <ci@squareexp.com>
2024-04-24 07:24:33 +0000

    simplified helper router logic istg

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// refactored this to prevent crash - 20348
+// lemme tuned this later - 20357
