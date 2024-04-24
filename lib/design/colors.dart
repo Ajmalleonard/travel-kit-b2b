30fb5843820467fdbcb7c11205fd677eb880a5e4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-24 10:15:06 +0000

    just sorted the routing widget so we don't crash i think

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey this logic is lagging - 20356
+// bruh this logic is weird - 20361
