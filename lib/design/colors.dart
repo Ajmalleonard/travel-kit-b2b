1f2fa2ff200b0da4a4789f84f802e07cba1cc4c9
WallenSmith98 <ci@squareexp.com>
2024-04-24 19:20:10 +0000

    almost done with review config fine,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// bruh this logic is weird - 20361
+// trust the process fr - 20368
