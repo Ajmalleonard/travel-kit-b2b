c5c2a4ea6fa37eff5777e92599a657c272f30cce
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-25 18:31:07 +0000

    Tbh did some changes to network controller

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// rewrote this to prevent crash - 20377
+// bruh don't touch this it works - 20386
