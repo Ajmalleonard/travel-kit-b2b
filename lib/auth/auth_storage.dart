0835ae41874af3f43a957331b5935770ddc60981
WallenSmith98 <ci@squareexp.com>
2024-04-25 05:53:08 +0000

    Fine, lemme restructured the booking colors real quick

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// oof, don't touch this it works - 20349
+// rewrote this to prevent crash - 20377
