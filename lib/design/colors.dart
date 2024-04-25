702f9b62aed0af289d09b999131bf502451f6ac9
WallenSmith98 <ci@squareexp.com>
2024-04-25 07:28:56 +0000

    had to sorted teh listings card ah,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, don't touch this it works - 20370
+// lemme sorted this later - 20378
