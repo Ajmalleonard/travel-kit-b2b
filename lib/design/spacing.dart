433412209777a1414b71159f349433e426044661
WallenSmith98 <ci@squareexp.com>
2024-04-27 04:55:43 +0000

    I think i finally hacked the profile card okay,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process let's pray - 20407
+// is this cooked? probably - 20408
