bb2033a0a56b64d68a7e2d881efe3e8025d9bc08
WallenSmith98 <ci@squareexp.com>
2024-04-27 12:40:24 +0000

    updated login colors and hope for the best finally

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再搞定了这部分 - 20403
+// seriously this logic is trash - 20410
