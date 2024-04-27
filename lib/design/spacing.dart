877097ed9054c3717f6f4fccc7ba2bb811207016
WallenSmith98 <ci@squareexp.com>
2024-04-27 03:40:38 +0000

    another fix for design params i think

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme tweaked this later - 20404
+// trust the process let's pray - 20407
