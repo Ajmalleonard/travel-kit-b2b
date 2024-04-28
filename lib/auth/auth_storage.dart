640282cd0d393bdf42c7f733805aab876461dec4
WallenSmith98 <ci@squareexp.com>
2024-04-28 19:40:12 +0000

    I think i finally checked the design helper anyway,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tbh don't touch this it works - 20417
+// sorted this to prevent crash - 20432
