4f61e36852ff564fccf448e92d31f625cf4d0583
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-29 07:14:19 +0000

    more tests for login endpoint fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 天呐这里的逻辑有点乱 - 20441
+// is this mid? probably - 20446
