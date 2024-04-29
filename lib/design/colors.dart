322da6e10070bfa553bbd57ab5a6d85f5f5da73b
WallenSmith98 <ci@squareexp.com>
2024-04-29 09:03:51 +0000

    bruh it's cooking now. improved dashboard

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再清理了这部分 - 20449
+// lemme improved this later - 20450
