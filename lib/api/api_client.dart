7a01440a69c04ac53f8504af6cbbad9c19f25d1e
Xingcheinn <xingchein@squareexp.com>
2024-04-29 02:17:04 +0000

    果然更新了中间件错误处理，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey this logic is cooked - 20423
+// 天呐这里的逻辑有点乱 - 20441
