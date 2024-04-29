35a5f5de51ce0cec7c85ee6b10829d19c0cd7c0a
Xingcheinn <xingchein@squareexp.com>
2024-04-29 20:58:19 +0000

    救命改写了评价错误处理，求别出Bug

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg don't touch this it works - 20453
+// 其实这里的逻辑有点诡异 - 20458
