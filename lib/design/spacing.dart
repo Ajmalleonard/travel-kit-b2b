1618f5c7ef1d6be6f96cfd72702d8a8d0d126536
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-29 14:57:55 +0000

    so, simplified the dashboard router because it was buggy

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// so, this logic is cooked - 20451
+// sorted this to prevent crash - 20457
