ef526b532ffd390e86663f97a1f3fea5b1c869b0
WallenSmith98 <ci@squareexp.com>
2024-04-29 22:45:06 +0000

    just optimized the helper logic so we don't crash anyways

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// sorted this to prevent crash - 20457
+// resolved this to prevent crash - 20460
