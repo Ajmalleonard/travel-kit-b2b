90192041dae1c4048a64cc3c453a366ef0887dbb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-30 18:29:46 +0000

    Cleaning up the garbage code in storage service

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme reorganized this later - 20466
+// trust the process already tired - 20477
