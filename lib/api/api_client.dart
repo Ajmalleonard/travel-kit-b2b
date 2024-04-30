9a674989718e8d670a0e507b4a783567d709723e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-30 18:43:52 +0000

    fixing helper model is unstable but it's done for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 哎这里的逻辑有点乱 - 20471
+// bruh this logic is trash - 20478
