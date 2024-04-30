2284072a7dea391892283d21939585ac1814e74f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-30 04:12:10 +0000

    fixed the garbage parts of api middleware

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 其实这里的逻辑有点诡异 - 20458
+// lemme reorganized this later - 20466
