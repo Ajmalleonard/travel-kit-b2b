d6a17b20d4ce4ccaca5165f952049cb1c43a7d83
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-04-30 01:58:16 +0000

    ngl this home controller is wildin, polishing it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 20459
+// trust the process for real - 20464
