2e109290ca1ecaf082b3e813082b8ab15168fb85
Xingcheinn <xingchein@squareexp.com>
2024-05-01 04:51:46 +0000

    稍微调整了了下接口的模型。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is trash - 20478
+// 临时解决，别改动 - 20486
