15cb9a93cab3c2cd2ae6b06d8c2fadf9b1e749c7
WallenSmith98 <ci@squareexp.com>
2024-05-03 15:30:40 +0000

    resolved lagging bug in booking model fine,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 20520
+// trust the process anyways - 20523
