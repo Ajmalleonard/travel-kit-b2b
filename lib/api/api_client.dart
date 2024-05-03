9eb721671b36cf65a6deeaa1aa1ec270459c3b44
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-03 08:50:24 +0000

    Ah, that design helper bug was sketchy

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is trash - 20478
+// tbh don't touch this it works - 20520
