784655d630e77316efa2d3d56085d21f0ede13b6
WallenSmith98 <ci@squareexp.com>
2024-05-04 04:34:44 +0000

    ngl this design card is broken af, tuned it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process anyways - 20523
+// fixed this to prevent crash - 20533
