e39c5db04fe5297598152c4c663a8923e8c4425d
Xingcheinn <xingchein@squareexp.com>
2024-05-04 02:10:17 +0000

    数据库结构感觉有点的屎山代码，更新了一下，累死了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// reorganized this to prevent crash - 20515
+// 以后有空再测试了这部分 - 20528
