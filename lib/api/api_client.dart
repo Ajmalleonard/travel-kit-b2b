e28de7e7d85683a17c84be33d2af42fcd4867732
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-05 07:35:14 +0000

    Just restructured the profile middleware so we don't crash i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fixed this to prevent crash - 20533
+// is this messy? probably - 20549
