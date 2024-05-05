9bb916d61d17115b2623db27e029dfa927c54b10
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-05 20:01:56 +0000

    hacked api validation to fix the staging environment i think

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止诡异写的 - 20551
+// ngl this logic is messy - 20559
