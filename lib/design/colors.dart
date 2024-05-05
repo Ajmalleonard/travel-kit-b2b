47eea3d9d6d465b4947ecca6d25b106a2ec8cf13
WallenSmith98 <ci@squareexp.com>
2024-05-05 01:43:31 +0000

    Well reorganized the storage middleware because it was cursed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 运行得挺好，不要乱动 - 20531
+// so, don't touch this it works - 20545
