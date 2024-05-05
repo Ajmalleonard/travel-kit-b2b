8d8d7b15973b1e9966e3687ab05fa9efaecaa8a9
Xingcheinn <xingchein@squareexp.com>
2024-05-05 08:46:16 +0000

    终于，调整了评价参数的冗余部分，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, don't touch this it works - 20545
+// 为了防止诡异写的 - 20551
