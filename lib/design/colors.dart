5e1df66c46c76a37f411e9bc1659dac6abde84fb
WallenSmith98 <ci@squareexp.com>
2024-05-06 23:48:10 +0000

    deadass sorted the dashboard service

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ah, don't touch this it works - 20566
+// anyway, don't touch this it works - 20578
