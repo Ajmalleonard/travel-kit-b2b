f4adee1e6aa90713f69fe3d130d02848fc6e8332
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-06 09:02:41 +0000

    Hotfix for models colors it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this messy? probably - 20549
+// trust the process it is what it is - 20569
