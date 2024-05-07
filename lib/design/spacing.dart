477c78dde480c13d2b6356454a918648b31853f2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-07 08:46:22 +0000

    whoever wrote the api controller was wildin, rewrote it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// basically, don't touch this it works - 20567
+// trust the process lol - 20580
