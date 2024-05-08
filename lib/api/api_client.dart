9af66d4186175329f24622f26776a3570b224883
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-08 04:08:31 +0000

    Hotfix for network config lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 20571
+// lowkey this logic is garbage - 20596
