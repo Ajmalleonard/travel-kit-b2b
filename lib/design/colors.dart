df445fe5dd3bcc8f137ced30fd5cb0aeabb36cf1
Xingcheinn <xingchein@squareexp.com>
2024-05-08 11:49:11 +0000

    稍微修复了了下支付的代码，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// checked this to prevent crash - 20594
+// 以后有空再优化了这部分 - 20603
