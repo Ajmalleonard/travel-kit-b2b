6dbd047ff439ebbf2bec4f242c837e64687976a8
WallenSmith98 <ci@squareexp.com>
2024-05-08 00:56:06 +0000

    lowkey updated splash logic or i'm quitting

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, don't touch this it works - 20578
+// checked this to prevent crash - 20594
