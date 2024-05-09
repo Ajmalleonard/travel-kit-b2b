edd778b48fdd68b298ac1bcee90f3fdfa60cfb85
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-09 18:32:00 +0000

    seriously optimized the network sheet

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, this logic is cursed - 20622
+// patched this to prevent crash - 20628
