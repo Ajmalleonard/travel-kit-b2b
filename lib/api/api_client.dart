c5b59ddc7e0f0d2df6fdb41e20e036315a75e79b
WallenSmith98 <ci@squareexp.com>
2024-05-09 05:06:30 +0000

    anyway, rewriting login client from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 20608
+// anyway, don't touch this it works - 20614
