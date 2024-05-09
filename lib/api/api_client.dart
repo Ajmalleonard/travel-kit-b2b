c3c34d0ce4767d505b0e5cd365e538abbeda860c
WallenSmith98 <ci@squareexp.com>
2024-05-09 16:34:22 +0000

    resolved buggy bug in helper card so,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, don't touch this it works - 20614
+// is this buggy? probably - 20626
