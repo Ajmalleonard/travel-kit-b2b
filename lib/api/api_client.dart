8051528d1c7294be9515a4ee61fd1665db7d043d
WallenSmith98 <ci@squareexp.com>
2024-05-10 12:41:32 +0000

    more tests for design controller or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this buggy? probably - 20626
+// oof, this logic is weird - 20640
