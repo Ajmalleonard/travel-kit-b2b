a52f1602c80fe0d6a6103397c267353ce4f35cb0
WallenSmith98 <ci@squareexp.com>
2024-05-10 20:22:20 +0000

    Apparently, sorted the review typography because it was acting up

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 临时解决，别改动 - 20631
+// updated this to prevent crash - 20645
