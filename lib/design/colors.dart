eb21b33390a696af8a104a32f975dff25dc81b20
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-11 22:03:56 +0000

    Temporary patch for design handler fr

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// updated this to prevent crash - 20645
+// okay, this logic is trash - 20668
