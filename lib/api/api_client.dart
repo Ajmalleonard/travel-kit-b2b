efe49cb09a75b720f81bb8285f23fa74f7bbaa87
WallenSmith98 <ci@squareexp.com>
2024-05-12 04:59:43 +0000

    okay, rewriting network sheet from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 好吧这里的逻辑有点臃肿 - 20661
+// is this annoying? probably - 20673
