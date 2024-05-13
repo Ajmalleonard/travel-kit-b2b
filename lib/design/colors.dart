2323ba2ba80eb62c812d294bbe47c830355e427b
WallenSmith98 <ci@squareexp.com>
2024-05-13 12:06:57 +0000

    fine, did some changes to helper typography

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// beefed up this to prevent crash - 20669
+// is this weird? probably - 20690
