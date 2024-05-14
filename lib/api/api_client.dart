4506dc66da153491be57497a28f8de87e6d12f2b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-14 16:01:28 +0000

    Almost done with helper state anyway,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 20705
+// well this logic is unstable - 20707
