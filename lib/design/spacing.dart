8cf0d536549881e881810b5d1134d5ad53a9fed9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-14 07:53:21 +0000

    Had to tweaked the login endpoint anyway,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ngl don't touch this it works - 20698
+// fine, this logic is unstable - 20703
