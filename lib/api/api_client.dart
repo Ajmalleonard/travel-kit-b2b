c8db1395d584a93318e35cd4158edc7d34459678
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-15 03:55:55 +0000

    lowkey cleaned up dashboard validation or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme simplified this later - 20708
+// lemme hacked this later - 20715
