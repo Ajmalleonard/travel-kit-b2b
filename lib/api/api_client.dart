8bc51807c6632d36f2fe1de67aefe1df65f11174
WallenSmith98 <ci@squareexp.com>
2024-05-15 17:50:14 +0000

    hotfix for theme helper finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止难懂写的 - 20718
+// well this logic is trash - 20723
