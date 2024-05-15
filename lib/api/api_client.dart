a1b38a8e000e2d9687083de211d23ed18e8bf098
WallenSmith98 <ci@squareexp.com>
2024-05-15 22:58:58 +0000

    tbh that dashboard sheet bug was weird

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well this logic is trash - 20723
+// ah, don't touch this it works - 20724
