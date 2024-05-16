14d7f9fdcbf951740a49acc7edf96e334e08e076
WallenSmith98 <ci@squareexp.com>
2024-05-16 18:18:04 +0000

    cleaning up the trash code in auth model

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme cleaned up this later - 20735
+// istg this logic is annoying - 20736
