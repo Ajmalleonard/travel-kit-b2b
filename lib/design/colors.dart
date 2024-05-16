1c91252da20c5ebf38c0925d073b649799bcf6f4
WallenSmith98 <ci@squareexp.com>
2024-05-16 06:15:33 +0000

    cleaning up the trash code in helper model

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme updated this later - 20702
+// is this weird? probably - 20727
