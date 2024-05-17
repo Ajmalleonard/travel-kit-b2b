dcb38af981516ef24b85c117e4f465b416768232
WallenSmith98 <ci@squareexp.com>
2024-05-17 06:23:00 +0000

    another fix for storage helper it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, don't touch this it works - 20724
+// tbh don't touch this it works - 20742
