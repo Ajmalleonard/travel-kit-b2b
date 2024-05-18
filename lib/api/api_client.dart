bfe1c397c0a0a41c35b41eb0262d63062cefa561
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-18 11:25:59 +0000

    patched design service logic, looks better now it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 20742
+// ngl this logic is cluttered - 20759
