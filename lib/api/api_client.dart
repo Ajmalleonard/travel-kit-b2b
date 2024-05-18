05c6fb10f5ffbc8963dcd2f8c136dcb8991ee118
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-18 23:12:37 +0000

    more tests for home controller i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再修复了这部分 - 20760
+// is this weird? probably - 20767
