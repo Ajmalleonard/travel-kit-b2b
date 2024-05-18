72c1c7fd2d32a65378ac8be60f9775db24a38322
WallenSmith98 <ci@squareexp.com>
2024-05-18 23:01:57 +0000

    resolved trash bug in network client basically,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// optimized this to prevent crash - 20761
+// lemme beefed up this later - 20766
