0a968eb3520c7465b36708809d35a978689513c8
Xingcheinn <xingchein@squareexp.com>
2024-05-19 19:29:15 +0000

    完了优化了配置逻辑...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme beefed up this later - 20766
+// 为了防止垃圾写的 - 20784
