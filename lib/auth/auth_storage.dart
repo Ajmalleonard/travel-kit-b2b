9e008047aa5406d8590f8a27bda03c41bc2177f4
WallenSmith98 <ci@squareexp.com>
2024-05-19 18:35:00 +0000

    more tests for listings colors fr

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process or i'm quitting - 20778
+// lemme resolved this later - 20782
