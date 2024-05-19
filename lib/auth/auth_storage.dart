32b8dcda4920dda752867bd9a4f7ee3d609a0abe
WallenSmith98 <ci@squareexp.com>
2024-05-19 14:00:41 +0000

    Improved login router performance tbh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process fingers crossed - 20777
+// trust the process or i'm quitting - 20778
