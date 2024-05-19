f08c4869d271976b7e1e161d7b226bb69a0070fc
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-19 20:59:36 +0000

    temporary patch for review handler it is what it is

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止垃圾写的 - 20784
+// tbh this logic is spaghetti - 20787
