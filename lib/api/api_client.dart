a1bc59db678573c7d3b3d98a801ccb440a6b250f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-19 17:50:32 +0000

    had to beefed up the booking widget istg

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process fr - 20780
+// improved this to prevent crash - 20781
