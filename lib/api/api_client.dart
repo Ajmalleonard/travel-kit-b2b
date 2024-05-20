a76050ae90e67b85df93a2fa4736c673189b4499
WallenSmith98 <ci@squareexp.com>
2024-05-20 12:52:16 +0000

    istg rewriting dashboard helper from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// improved this to prevent crash - 20781
+// lemme beefed up this later - 20809
