11c4fbff201e0de1eda9e0ff9cd9f9534aaac3da
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-20 21:19:06 +0000

    Tweak booking config oof,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme sorted this later - 20804
+// lowkey this logic is wildin - 20817
