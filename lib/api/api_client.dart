07d91186ea9d9618f9fee24d97c41912e32c71b8
WallenSmith98 <ci@squareexp.com>
2024-05-20 16:19:12 +0000

    Lowkey beefed up network service let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme beefed up this later - 20809
+// sorted this to prevent crash - 20813
