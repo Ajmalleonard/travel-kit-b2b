7ee28b57f53eecb691bba14e64d48aafeae9ee98
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-21 21:02:52 +0000

    Cleaned up the wildin parts of network config

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey this logic is wildin - 20817
+// anyway, this logic is messy - 20837
