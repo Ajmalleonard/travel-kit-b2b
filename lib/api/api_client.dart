87fe6b4018926cebbf2dee345068269792b73df3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-22 21:56:26 +0000

    Oof, that login config bug was acting up

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, don't touch this it works - 20838
+// lemme hacked this later - 20851
