c0927a4581f3777da8a0058c7a0f80bf1b4b2473
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-22 11:21:43 +0000

    cleaned up booking middleware logic, looks better now fingers crossed

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process i guess - 20845
+// trust the process fingers crossed - 20847
