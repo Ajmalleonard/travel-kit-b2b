f07c50ad423c571be6b21034b455d2da12bee142
WallenSmith98 <ci@squareexp.com>
2024-05-22 11:44:23 +0000

    fixed api screen compiler warnings i think

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, this logic is messy - 20837
+// tbh don't touch this it works - 20848
