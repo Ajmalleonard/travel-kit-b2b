bca3c95a5ffe54968210967279d712f96992fc2a
Xingcheinn <xingchein@squareexp.com>
2024-05-23 16:16:11 +0000

    鉴权接口感觉有点的逻辑漏洞，测试了一下，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// restructured this to prevent crash - 20849
+// 以后有空再调整了这部分 - 20860
