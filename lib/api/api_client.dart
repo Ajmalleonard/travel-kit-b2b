85d0c511d139e189f213cdde0b3f48309c6ef93a
WallenSmith98 <ci@squareexp.com>
2024-05-25 07:42:16 +0000

    deadass it's cooking now. beefed up dashboard

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// cleaned up this to prevent crash - 20882
+// is this wildin? probably - 20883
