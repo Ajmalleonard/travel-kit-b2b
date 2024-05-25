96cf62ce24bac826e95ae1b8f87ae315a18cd9df
WallenSmith98 <ci@squareexp.com>
2024-05-25 07:07:35 +0000

    Hotfix for listings storage for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 救命这里的逻辑有点臃肿 - 20877
+// is this messy? probably - 20881
