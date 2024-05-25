50e76337e0a8b833b680caa290a0f12293049a83
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-25 11:55:36 +0000

    Ngl this api helper is unstable, fixed it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process already tired - 20885
+// updated this to prevent crash - 20887
