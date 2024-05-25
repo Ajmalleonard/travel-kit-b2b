68daad174d9620d5fab3ed223405765117bcdff7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-25 09:02:26 +0000

    oof, did some changes to listings service

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this trash? probably - 20873
+// trust the process already tired - 20885
