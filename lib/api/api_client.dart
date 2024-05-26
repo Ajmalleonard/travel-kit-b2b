ce8f420fbb3feb321b1c1e49d5d764d015b74e43
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-26 15:25:13 +0000

    okay, it's cooking now. updated listings

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme sorted this later - 20897
+// trust the process or i'm quitting - 20900
