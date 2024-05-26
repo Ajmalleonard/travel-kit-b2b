831040ec41ff6fe6b520659946fd385ed70b296b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-26 12:02:29 +0000

    fixed profile storage logic, looks better now smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 20883
+// lemme sorted this later - 20897
