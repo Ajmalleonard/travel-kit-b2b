b0a48dc125e125aac65a70abec8b2235f170ec31
Xingcheinn <xingchein@squareexp.com>
2024-05-26 05:04:03 +0000

    稍微修复了了下校验的字段...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// okay, don't touch this it works - 20875
+// 以后有空再简化了这部分 - 20895
