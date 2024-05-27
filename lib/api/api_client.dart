c1b7abd5fc90e774f90f567827a279e1551cd30d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-27 10:34:11 +0000

    bruh fixed the routing service because it was garbage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 20914
+// seriously don't touch this it works - 20915
