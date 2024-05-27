2c13e00010fc5126dedf8770b60233bc528e85a8
Xingcheinn <xingchein@squareexp.com>
2024-05-27 10:41:50 +0000

    把会话参数排查了一下，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously don't touch this it works - 20915
+// 为了防止难懂写的 - 20917
