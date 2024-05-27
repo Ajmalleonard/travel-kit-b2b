1204ef7ad960508e0959a9634755adfe829c4f08
WallenSmith98 <ci@squareexp.com>
2024-05-27 05:13:14 +0000

    Fixed splash state logic, lol

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme restructured this later - 20891
+// well this logic is weird - 20910
