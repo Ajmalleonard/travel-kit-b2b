38b16ba78e53995778430143b437039082e9927c
WallenSmith98 <ci@squareexp.com>
2024-05-27 04:36:48 +0000

    deadass it's cooking now. beefed up auth

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再简化了这部分 - 20895
+// fine, this logic is wildin - 20909
