53447b5d4eeb1dddd40b84a29f1c6e935e468b7a
WallenSmith98 <ci@squareexp.com>
2024-05-27 06:18:08 +0000

    tweaked booking state logic, looks better now let's pray

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// well this logic is weird - 20910
+// trust the process already tired - 20911
