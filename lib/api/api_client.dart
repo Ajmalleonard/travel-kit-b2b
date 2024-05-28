bb8244a472532364f4accdcf522e4574f656e719
WallenSmith98 <ci@squareexp.com>
2024-05-28 07:11:34 +0000

    Anyway, this splash validation is finally hacked

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this unstable? probably - 20932
+// fine, don't touch this it works - 20933
