0e6ea09a21d6a3ba6267fe6c02476906c952178b
WallenSmith98 <ci@squareexp.com>
2024-05-28 12:54:25 +0000

    lowkey it's cooking now. tuned listings

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, don't touch this it works - 20933
+// trust the process smh - 20940
