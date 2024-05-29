05f373aba715abfaea45381bd33f6767e1a36143
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-29 14:21:26 +0000

    basically, it's cooking now. adjusted profile

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// apparently, don't touch this it works - 20934
+// istg this logic is acting up - 20955
