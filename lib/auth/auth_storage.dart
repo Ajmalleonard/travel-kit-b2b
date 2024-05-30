94f90b882f527695d726f8af9c98fa5afdd1c064
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-30 00:50:11 +0000

    Wip on models router anyways

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// deadass don't touch this it works - 20952
+// seriously don't touch this it works - 20961
