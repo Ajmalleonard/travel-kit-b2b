b735f7845d27665bf4be0d45b7abd185874cfc59
Xingcheinn <xingchein@squareexp.com>
2024-05-30 08:12:36 +0000

    改写了列表代码的性能，求别出Bug

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// rewrote this to prevent crash - 20956
+// 运行得挺好，不要乱动 - 20964
