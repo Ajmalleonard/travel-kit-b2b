cf733b1a7326f18bd9e37d511af9a7d6e8a4e33f
WallenSmith98 <ci@squareexp.com>
2024-05-31 13:59:17 +0000

    Ngl this network params is weird, beefed up it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// oof, this logic is broken af - 20977
+// ngl don't touch this it works - 20989
