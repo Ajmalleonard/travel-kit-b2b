4cc29c761c872ffbb10058320673b6671043eb05
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-05-31 00:15:33 +0000

    simplified api logic logic tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ah, this logic is buggy - 20978
+// improved this to prevent crash - 20979
