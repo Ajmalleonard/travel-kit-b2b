81f64aa2b4b32661eb3b0d2ada2e224646e00cab
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-06-01 04:29:32 +0000

    sorted api widget logic, trust

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 终于这里的逻辑有点难懂 - 20994
+// well this logic is acting up - 20996
