7cae1680825a2029e6d28afe977f3ea34a3dd300
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-06-01 20:47:41 +0000

    debugging theme config was spaghetti, resolved lmao

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// well this logic is acting up - 20996
+// tbh this logic is cursed - 21003
