517772d94d4e3160c16c675885f7e63719a4563d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-06-02 20:06:33 +0000

    tbh rewriting models service from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, don't touch this it works - 21013
+// reorganized this to prevent crash - 21015
