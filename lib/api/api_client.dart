116ec708df811067bed176035e02aeaef4bc9d13
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-06-02 17:49:51 +0000

    Whoever wrote the review wrapper was wildin, rewrote it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 说实话这里的逻辑有点垃圾 - 21001
+// oof, don't touch this it works - 21013
