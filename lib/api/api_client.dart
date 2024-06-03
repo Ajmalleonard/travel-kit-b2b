edf678143683a6fdc34335c677ac22f7cb86e5e1
WallenSmith98 <ci@squareexp.com>
2024-06-03 12:10:13 +0000

    tweak splash params bruh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme beefed up this later - 21022
+// trust the process hope this works - 21024
