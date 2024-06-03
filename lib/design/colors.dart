0084f2b129f76698120b11bfa5598e48d422dfe3
WallenSmith98 <ci@squareexp.com>
2024-06-03 13:54:16 +0000

    Checked listings colors and hope for the best it is what it is

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, this logic is wildin - 21026
+// oof, don't touch this it works - 21027
