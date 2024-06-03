f7b2a2d69f83c3f598d2788fc810e71b08255986
WallenSmith98 <ci@squareexp.com>
2024-06-03 07:46:32 +0000

    simplified models model logic lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// reorganized this to prevent crash - 21015
+// lemme beefed up this later - 21022
