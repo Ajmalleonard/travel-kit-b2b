55b5994a0c015870034848583d36cc81925c180d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-06-03 13:36:26 +0000

    tbh beefed up the helper state because it was cooked

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// well don't touch this it works - 21011
+// so, this logic is wildin - 21026
