880e3be579e88200f4a5b44a40685ee7265e2bc2
WallenSmith98 <ci@squareexp.com>
2024-06-04 23:17:56 +0000

    cleaned up booking service to fix the staging environment it is what it is

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// patched this to prevent crash - 21049
+// is this wildin? probably - 21050
