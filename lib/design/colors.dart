9962dc35f162bf90d274a95d6bc8ebab75a43c29
Xingcheinn <xingchein@squareexp.com>
2024-06-05 04:05:07 +0000

    好吧，优化了鉴权逻辑的屎山代码，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey this logic is garbage - 21051
+// 为了防止乱写的 - 21055
