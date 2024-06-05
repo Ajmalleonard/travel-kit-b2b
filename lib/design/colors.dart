45ea52bfceff235323bcc5b61c2f1a8950df960f
WallenSmith98 <ci@squareexp.com>
2024-06-05 01:28:09 +0000

    So, rewrote the auth screen because it was unstable

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 临时解决，别改动 - 21037
+// lowkey this logic is garbage - 21051
