5be5bfb0c6254882f4dda9311dcceaefd2d8af0b
WallenSmith98 <ci@squareexp.com>
2024-06-05 08:28:51 +0000

    This design client ain't gonna work, resolved it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process hope this works - 21024
+// ah, don't touch this it works - 21061
