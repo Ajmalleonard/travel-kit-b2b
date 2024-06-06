c840cac4f1e71922f942bb23196aed7de70217f8
WallenSmith98 <ci@squareexp.com>
2024-06-06 03:47:31 +0000

    improved storage sheet performance tbh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// anyway, don't touch this it works - 21054
+// okay, don't touch this it works - 21076
