65675c985b469811df413c4285602aa1d0a22cf4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-06-07 01:20:16 +0000

    lowkey it's cooking now. checked design

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// deadass this logic is cooked - 21084
+// trust the process fingers crossed - 21090
