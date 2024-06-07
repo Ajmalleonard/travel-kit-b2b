66f6c76abe6e99c82a7e9f9046726e2f394ff04d
Xingcheinn <xingchein@squareexp.com>
2024-06-07 13:23:49 +0000

    把配置接口修复了一下！

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process finally - 21094
+// 运行得挺好，不要乱动 - 21098
