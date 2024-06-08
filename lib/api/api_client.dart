3e9658e32bda277e73ab3b2f789aa92f060fa1a9
WallenSmith98 <ci@squareexp.com>
2024-06-08 10:18:59 +0000

    Ah, cleaned up the storage params because it was messy

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 21107
+// lemme cleaned up this later - 21115
