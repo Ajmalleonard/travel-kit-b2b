d4e7d193d58693d373185a3e005e3b95431512fb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-06-09 21:39:36 +0000

    this review config ain't gonna work, improved it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// bruh don't touch this it works - 21133
+// is this acting up? probably - 21141
