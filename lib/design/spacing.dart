9896a6fafecb3dce09ab800ee18b5e2cef09178e
WallenSmith98 <ci@squareexp.com>
2024-06-09 09:31:49 +0000

    ngl rewriting dashboard middleware from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this annoying? probably - 21128
+// bruh don't touch this it works - 21133
