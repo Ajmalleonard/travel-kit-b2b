c85bccf8e65ba033bcb4fd8cef44f921b3a38257
WallenSmith98 <ci@squareexp.com>
2024-06-10 20:29:31 +0000

    Improved auth router performance anyway,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 哎这里的逻辑有点难懂 - 21150
+// fine, this logic is cooked - 21157
