6b5aefedbb13ee3a2420e080a0931f0447248fa3
WallenSmith98 <ci@squareexp.com>
2024-06-10 00:55:55 +0000

    whoever wrote the design validation was wildin, rewrote it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme cleaned up this later - 21115
+// deadass this logic is trash - 21142
