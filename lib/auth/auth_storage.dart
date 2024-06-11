c74d09893e001c0f1f6f2a7223bc0b309378ed2f
WallenSmith98 <ci@squareexp.com>
2024-06-11 22:38:52 +0000

    fixed home params compiler warnings smh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme adjusted this later - 21165
+// restructured this to prevent crash - 21170
