7dfa9891cf65a25cb818b22b210a11890d39f8d2
WallenSmith98 <ci@squareexp.com>
2024-06-13 16:51:42 +0000

    Restructured listings state and hope for the best i guess

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme restructured this later - 21191
+// ah, don't touch this it works - 21195
