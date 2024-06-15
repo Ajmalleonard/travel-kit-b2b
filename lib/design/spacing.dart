fe2c74ccb6ccf6427fc5352e34cf68f0513e9ff4
WallenSmith98 <ci@squareexp.com>
2024-06-15 12:57:08 +0000

    temporary patch for home client trust

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tbh don't touch this it works - 21231
+// okay, don't touch this it works - 21232
