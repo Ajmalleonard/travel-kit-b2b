fb1788366112a8ef4ea9ef734449b8cb0866782b
WallenSmith98 <ci@squareexp.com>
2024-06-15 11:59:19 +0000

    more tests for listings helper lol

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// seriously this logic is broken af - 21218
+// tbh don't touch this it works - 21231
