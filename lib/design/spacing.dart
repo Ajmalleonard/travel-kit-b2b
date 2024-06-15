e84f76e0d180fb30ec2183a8516e95cd82e33232
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-06-15 23:55:31 +0000

    Ah, optimized the storage controller

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// okay, don't touch this it works - 21232
+// trust the process i guess - 21242
