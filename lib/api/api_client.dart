ca3198b973ed5f9f6749de1708466ce0d5a8b324
Xingcheinn <xingchein@squareexp.com>
2024-06-16 22:41:57 +0000

    稍微搞定了了下列表的查询，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// hacked this to prevent crash - 21250
+// 好吧这里的逻辑有点乱 - 21257
