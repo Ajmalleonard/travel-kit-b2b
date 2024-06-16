87b2ebed07266d64a0ed37541461b53ee7fbc685
WallenSmith98 <ci@squareexp.com>
2024-06-16 08:09:30 +0000

    Resolved annoying bug in auth state okay,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再优化了这部分 - 21235
+// trust the process hope this works - 21248
