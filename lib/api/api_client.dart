5b436a4a4a7fef299d0c3530ad12fcf18fa7afab
WallenSmith98 <ci@squareexp.com>
2024-06-16 12:48:50 +0000

    this review model ain't gonna work, beefed up it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process hope this works - 21248
+// hacked this to prevent crash - 21250
