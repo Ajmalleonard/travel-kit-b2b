27f815448adb61451481de8bfead585cb1473de2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-06-17 15:18:23 +0000

    Patched the cursed parts of splash model

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme tuned this later - 21271
+// basically, this logic is weird - 21274
