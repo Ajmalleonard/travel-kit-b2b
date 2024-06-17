fd765f76684adcca8eb553e810b997953344bb71
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-06-17 13:11:57 +0000

    Bruh it's cooking now. patched theme

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme improved this later - 21262
+// oof, don't touch this it works - 21272
