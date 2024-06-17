cc0eea7912b6cc8166dbab1760e1eb0db05a513b
Xingcheinn <xingchein@squareexp.com>
2024-06-17 05:08:41 +0000

    说实话修复了路由逻辑...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// fine, don't touch this it works - 21261
+// 以后有空再搞定了这部分 - 21263
