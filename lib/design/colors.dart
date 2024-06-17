fa235a07bf176b4970903f1bbf699b9725e7695b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-06-17 20:02:45 +0000

    Wip on api state smh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process finally - 21277
+// lemme optimized this later - 21278
