3acd301418cf8f2062cfb7e676efc807dd546de1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-06-17 16:28:44 +0000

    Hotfix for helper logic hope this works

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再搞定了这部分 - 21263
+// trust the process lmao - 21276
