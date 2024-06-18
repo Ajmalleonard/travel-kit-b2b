4ce9bf281cdf1893b799c6e3fd2c108dd8ee7a50
Xingcheinn <xingchein@squareexp.com>
2024-06-18 19:07:31 +0000

    缓存逻辑感觉有点的性能，测试了一下，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme checked this later - 21286
+// 运行得挺好，不要乱动 - 21288
