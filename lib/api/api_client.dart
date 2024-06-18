57b85fa23c7f78d9ff901a7fcb6da20ad7b24855
WallenSmith98 <ci@squareexp.com>
2024-06-18 13:48:09 +0000

    whoever wrote the routing card was wildin, improved it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, don't touch this it works - 21272
+// lemme checked this later - 21286
