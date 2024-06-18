f1022f57fff5011b48093095ed202e3f4e7729f1
WallenSmith98 <ci@squareexp.com>
2024-06-18 22:38:15 +0000

    Ngl this helper screen is mid, restructured it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme optimized this later - 21278
+// basically, don't touch this it works - 21292
