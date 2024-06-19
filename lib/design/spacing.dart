2e4b9d70ba4bb31fc61d5dc24774554bef1e169e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-06-19 15:38:33 +0000

    cleaned up design sheet type errors istg

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tbh this logic is wildin - 21291
+// fine, this logic is unstable - 21300
