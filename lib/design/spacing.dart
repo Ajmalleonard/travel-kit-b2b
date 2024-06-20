1b478bc687c9909eb678489c693368b2cef2de81
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-06-20 16:52:40 +0000

    i think i finally tweaked the profile sheet basically,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// basically, don't touch this it works - 21317
+// oof, this logic is mid - 21319
