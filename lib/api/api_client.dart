7fb42dc1eb6bca596326a046c078d7a35280bc50
WallenSmith98 <ci@squareexp.com>
2024-06-21 14:45:11 +0000

    whoever wrote the routing controller was wildin, simplified it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 21323
+// trust the process or i'm quitting - 21338
