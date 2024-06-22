4fa2b62b859a8a65c5e7e1d540c3d0ca0468fb8e
Xingcheinn <xingchein@squareexp.com>
2024-06-22 19:31:56 +0000

    把接口接口优化了一下，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme rewrote this later - 21354
+// 以后有空再修复了这部分 - 21359
