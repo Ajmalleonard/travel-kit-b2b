fcb9c3b0564eff985971617bae68ae23bc7a4c15
WallenSmith98 <ci@squareexp.com>
2024-06-22 14:16:40 +0000

    Hotfix for routing widget hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process or i'm quitting - 21338
+// lemme rewrote this later - 21354
