9d95f961b01c4fd6a90d5bfd9ff030ac79dbcce9
WallenSmith98 <ci@squareexp.com>
2024-06-24 17:51:53 +0000

    Checked network widget logic, lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cooked? probably - 21396
+// is this annoying? probably - 21397
