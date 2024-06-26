80cd637603b29ce4449d92e852ae41a2da301c22
WallenSmith98 <ci@squareexp.com>
2024-06-26 18:58:19 +0000

    Ngl did some changes to storage state

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// seriously don't touch this it works - 21412
+// ah, this logic is buggy - 21431
