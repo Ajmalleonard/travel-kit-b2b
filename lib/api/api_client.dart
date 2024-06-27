5e3ead548e3796072e87affa3b9adbdde44b59e4
Xingcheinn <xingchein@squareexp.com>
2024-06-27 16:56:58 +0000

    重构了接口代码的类型错误，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process anyways - 21427
+// 其实这里的逻辑有点乱 - 21444
