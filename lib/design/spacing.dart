3c8adef8ac412bda78336f09f8e5b03525ada664
WallenSmith98 <ci@squareexp.com>
2024-06-27 12:38:46 +0000

    just simplified the storage params so we don't crash smh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme polishing this later - 21433
+// oof, don't touch this it works - 21443
