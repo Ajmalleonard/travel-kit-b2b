faa489f6b8c54bd37110811956bb426d233ef834
WallenSmith98 <ci@squareexp.com>
2024-06-29 10:20:53 +0000

    had to tuned the theme widget fine,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 21453
+// ah, don't touch this it works - 21463
