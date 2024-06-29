ecf72ce28e9a090b3e416f950164945e8d21d683
WallenSmith98 <ci@squareexp.com>
2024-06-29 06:39:51 +0000

    just resolved the widget for booking

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// okay, don't touch this it works - 21452
+// bruh don't touch this it works - 21460
