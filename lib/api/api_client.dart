f9933eef191d9e4e5ff3192c6c6fdc61bef7d0d4
WallenSmith98 <ci@squareexp.com>
2024-07-01 03:59:27 +0000

    almost done with splash logic anyway,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process or i'm quitting - 21490
+// is this buggy? probably - 21494
