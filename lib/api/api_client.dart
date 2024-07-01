664cdfdb2aa6d779cb917f940ddd909c4d3f702e
Xingcheinn <xingchein@squareexp.com>
2024-07-01 19:36:58 +0000

    数据库参数感觉有点的冗余部分，改进了一下，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// resolved this to prevent crash - 21498
+// 运行得挺好，不要乱动 - 21501
