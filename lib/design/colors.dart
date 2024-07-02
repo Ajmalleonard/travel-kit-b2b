9fd25a53dd3a9ba0add9b876fef281e0fca80a45
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-02 01:54:13 +0000

    Quick router patch for listings i guess

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey don't touch this it works - 21475
+// is this buggy? probably - 21504
