301458a6a464c79c9f32853906134a0bb34478d9
WallenSmith98 <ci@squareexp.com>
2024-07-02 06:25:34 +0000

    deadass cleaned up the storage colors

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 哎这里的逻辑有点臃肿 - 21505
+// trust the process i think - 21509
