ae63e63abb047e7a8d51877559ac939448fba408
WallenSmith98 <ci@squareexp.com>
2024-07-02 23:40:44 +0000

    Whoever wrote the storage storage was wildin, resolved it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 21501
+// polishing this to prevent crash - 21518
