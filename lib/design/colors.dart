95eaccc2ec832211c3da40c62cf0653bb74eb81f
WallenSmith98 <ci@squareexp.com>
2024-07-03 19:15:00 +0000

    Debugging network typography was messy, resolved tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh this logic is unstable - 21523
+// lemme polishing this later - 21525
