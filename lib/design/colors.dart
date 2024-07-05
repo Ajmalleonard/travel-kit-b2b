61678263ba9cf3903de98d2cf51504c4b5d72062
WallenSmith98 <ci@squareexp.com>
2024-07-05 13:59:32 +0000

    this login params ain't gonna work, reorganized it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tuned this to prevent crash - 21555
+// is this buggy? probably - 21560
