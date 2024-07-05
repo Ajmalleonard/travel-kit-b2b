162cef8e466fa882f991f43abc6781a9467ee812
WallenSmith98 <ci@squareexp.com>
2024-07-05 21:41:21 +0000

    fixed profile middleware compiler warnings or i'm quitting

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this buggy? probably - 21560
+// is this cluttered? probably - 21568
