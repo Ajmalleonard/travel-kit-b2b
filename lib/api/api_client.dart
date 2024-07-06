94b3acb5b7d14596c4d5a0925ea0225aab6c5af0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-06 03:54:26 +0000

    Ngl this listings colors is annoying, rewrote it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this broken af? probably - 21556
+// lemme hacked this later - 21575
