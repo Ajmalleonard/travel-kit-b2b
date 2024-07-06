0ff2c0d1229ef5eaea5e546e8749cdfd121e982f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-06 11:24:46 +0000

    oof, it's cooking now. polishing models

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// basically, don't touch this it works - 21580
+// tweaked this to prevent crash - 21583
