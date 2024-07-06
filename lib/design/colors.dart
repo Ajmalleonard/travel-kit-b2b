01065441379e148ee0977d0e68efab3c07af33d8
Xingcheinn <xingchein@squareexp.com>
2024-07-06 22:52:11 +0000

    稍微改写了了下路由的查询，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tweaked this to prevent crash - 21583
+// 以后有空再调整了这部分 - 21592
