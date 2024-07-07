fded4e16188d0cc889c4360695faf862ad8a77de
WallenSmith98 <ci@squareexp.com>
2024-07-07 15:50:52 +0000

    just fixed the home sheet so we don't crash lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 21605
+// is this cursed? probably - 21609
