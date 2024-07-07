e76c5cbb365289fef6ed92c750c55274482742ef
WallenSmith98 <ci@squareexp.com>
2024-07-07 19:52:32 +0000

    Fixed login service compiler warnings anyways

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// rewrote this to prevent crash - 21603
+// lemme rewrote this later - 21611
