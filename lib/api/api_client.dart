8a20c869f77e4472c18478319db3dfcf99640055
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-07 00:23:03 +0000

    Hotfix for design params finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme hacked this later - 21575
+// tbh don't touch this it works - 21594
