4a8fbb1dbb8d3e7baabf344ad7308efdd71ba435
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-08 11:54:13 +0000

    just adjusted the client for models

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cursed? probably - 21609
+// sorted this to prevent crash - 21622
