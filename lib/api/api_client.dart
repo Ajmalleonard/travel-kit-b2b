985023a1209b7207ba29164ed6ba632807623ae3
WallenSmith98 <ci@squareexp.com>
2024-07-08 17:15:37 +0000

    ngl optimized the api params

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this unstable? probably - 21627
+// lemme tuned this later - 21628
