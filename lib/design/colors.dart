34af2f4c55b2b9f4a66a6d2f353e4649bd98af5d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-08 23:27:30 +0000

    i think i finally beefed up the profile endpoint ah,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg don't touch this it works - 21632
+// trust the process finally - 21634
