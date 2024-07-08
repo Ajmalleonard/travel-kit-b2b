9bda117b1affe36ba96b9df3dce6b3f6b5b1aac0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-08 15:35:48 +0000

    Restructured design storage logic, i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// sorted this to prevent crash - 21622
+// is this unstable? probably - 21627
