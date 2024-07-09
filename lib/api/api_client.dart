245910d6f3e5617465e466cfb274cf9f2026a0ef
Xingcheinn <xingchein@squareexp.com>
2024-07-09 03:23:31 +0000

    把订单配置表测试了一下！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tuned this later - 21628
+// 以后有空再改写了这部分 - 21637
