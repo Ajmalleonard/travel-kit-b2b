93ac15c58ad84970a093767cab584220dcd2500c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-09 09:50:34 +0000

    minor tweak to models colors or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再改写了这部分 - 21637
+// apparently, this logic is annoying - 21643
