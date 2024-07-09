92bcf7dc1fde6e044be3fae7e562f1972d0eb10a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-09 14:16:16 +0000

    Well it's cooking now. cleaned up design

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// resolved this to prevent crash - 21641
+// resolved this to prevent crash - 21645
