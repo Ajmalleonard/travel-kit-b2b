a0d686de86423f3257be5808b286f7b3caa68bcb
Xingcheinn <xingchein@squareexp.com>
2024-07-10 01:18:45 +0000

    更新了数据库模型的隐藏逻辑，应该没问题了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process or i'm quitting - 21652
+// 好吧这里的逻辑有点臃肿 - 21654
