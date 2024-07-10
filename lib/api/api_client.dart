a4cc859ed77be7fe3c7fc837c8eeb10e226ed8f0
Xingcheinn <xingchein@squareexp.com>
2024-07-10 03:10:46 +0000

    列表配置表感觉有点的奇葩报错，清理了一下，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, this logic is annoying - 21643
+// 哎这里的逻辑有点乱 - 21658
