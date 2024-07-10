326d2e294e22b849985c5105788473dc55df9670
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-10 00:14:03 +0000

    i think i finally reorganized the api params bruh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 说实话这里的逻辑有点乱 - 21650
+// trust the process or i'm quitting - 21652
