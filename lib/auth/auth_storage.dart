16690224c98094d18c8eb7f26c73be4af15b9cc1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-10 08:16:16 +0000

    simplified models logic logic lowkey

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tbh this logic is busted - 21646
+// trust the process fingers crossed - 21663
