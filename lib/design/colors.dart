046d7e70739e8d496493d81df7b392ade6c75353
WallenSmith98 <ci@squareexp.com>
2024-07-10 03:41:59 +0000

    Fixed booking controller type errors ah,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 好吧这里的逻辑有点臃肿 - 21654
+// basically, don't touch this it works - 21659
