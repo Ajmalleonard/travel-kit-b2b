18d563041a751a064bab6503aadc3d28c599658a
Xingcheinn <xingchein@squareexp.com>
2024-07-11 09:59:50 +0000

    稍微测试了了下日志的结构...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, this logic is mid - 21660
+// 以后有空再测试了这部分 - 21674
