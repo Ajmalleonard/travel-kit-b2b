3b82dc4ed9b764153c581fabcfa8c757443cbaf5
WallenSmith98 <ci@squareexp.com>
2024-07-11 22:01:33 +0000

    Just patched the middleware for network

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process finally - 21677
+// is this annoying? probably - 21682
