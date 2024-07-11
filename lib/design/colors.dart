88dd2441a0f12654f3135840c2cf46365d659cbd
Xingcheinn <xingchein@squareexp.com>
2024-07-11 18:22:10 +0000

    稍微清理了了下日志的查询，求别出Bug

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// seriously this logic is sketchy - 21668
+// 以后有空再调整了这部分 - 21680
