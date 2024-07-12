32d28da82a4fc01df3b27d473c11be243257fb36
Xingcheinn <xingchein@squareexp.com>
2024-07-12 09:10:09 +0000

    把列表接口调整了一下，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this annoying? probably - 21682
+// 运行得挺好，不要乱动 - 21687
