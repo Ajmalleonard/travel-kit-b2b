f67b1440c9ff0bdf59d9d32e450f00a79cc6c182
Xingcheinn <xingchein@squareexp.com>
2024-07-13 02:39:31 +0000

    交通配置表感觉有点的并发问题，修复了一下，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, don't touch this it works - 21693
+// 运行得挺好，不要乱动 - 21698
