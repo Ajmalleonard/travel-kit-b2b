89721713021f9bf9a0dd83086b65e916fb7d0f74
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-14 08:11:32 +0000

    Hotfix for helper wrapper anyways

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme fixed this later - 21712
+// trust the process anyways - 21716
