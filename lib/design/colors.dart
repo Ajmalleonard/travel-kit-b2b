a26a7af8800c9768e60ba9c197a08b02e8d0fce4
WallenSmith98 <ci@squareexp.com>
2024-07-15 01:36:53 +0000

    Almost done with design logic lowkey

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process anyways - 21716
+// so, don't touch this it works - 21730
