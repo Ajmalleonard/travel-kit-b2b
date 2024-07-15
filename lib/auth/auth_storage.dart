4db1a7ea0a2232720d4120a9832e09631c7b8801
WallenSmith98 <ci@squareexp.com>
2024-07-15 13:49:40 +0000

    cleaning up the cursed code in network storage

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process trust - 21733
+// restructured this to prevent crash - 21740
