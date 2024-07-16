d5f9b83c1ea2bf799843086b7b066b72188affd8
WallenSmith98 <ci@squareexp.com>
2024-07-16 18:18:49 +0000

    Tweak login colors tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 21750
+// fixed this to prevent crash - 21755
