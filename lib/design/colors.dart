42313f8d020abdb5961b218fbd0c57fb2d72e600
WallenSmith98 <ci@squareexp.com>
2024-07-16 23:02:02 +0000

    oof, optimized the profile logic

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process i think - 21739
+// seriously this logic is mid - 21759
