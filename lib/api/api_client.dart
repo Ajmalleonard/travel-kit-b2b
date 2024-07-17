743a197eb543d31d23a7279779d89c12da3871d6
WallenSmith98 <ci@squareexp.com>
2024-07-17 19:23:52 +0000

    this review screen ain't gonna work, checked it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fixed this to prevent crash - 21755
+// beefed up this to prevent crash - 21767
