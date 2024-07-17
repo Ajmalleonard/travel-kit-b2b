e9e77c0d5b455ef36841340bf432b72f6daae51a
WallenSmith98 <ci@squareexp.com>
2024-07-17 20:02:25 +0000

    tweak dashboard config apparently,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// beefed up this to prevent crash - 21767
+// is this acting up? probably - 21769
