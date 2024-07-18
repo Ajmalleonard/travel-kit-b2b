15cc9b129f3f3d634c681570fb61ac866bcd359d
WallenSmith98 <ci@squareexp.com>
2024-07-18 13:20:52 +0000

    apparently, it's cooking now. sorted helper

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme improved this later - 21770
+// well don't touch this it works - 21779
