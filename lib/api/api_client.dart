5b35b73f9e890571315239a87f02c558422683e7
Xingcheinn <xingchein@squareexp.com>
2024-07-18 01:41:31 +0000

    天呐改进了校验模型，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 21769
+// 终于这里的逻辑有点垃圾 - 21772
