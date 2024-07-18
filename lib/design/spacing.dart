5c14adb60dbf2ef6c493503eded4b723bce0e86a
WallenSmith98 <ci@squareexp.com>
2024-07-18 14:20:03 +0000

    Hotfix for storage sheet or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// well don't touch this it works - 21779
+// istg don't touch this it works - 21780
