5922cfdb5a62cae162d2d9e3edd63892e741354c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-18 12:42:57 +0000

    almost done with design params tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止垃圾写的 - 21777
+// basically, don't touch this it works - 21778
