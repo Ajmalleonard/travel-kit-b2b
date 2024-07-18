5ae66d0e1fcd5e702b96e28e61c9ab1b7e80cbe8
Xingcheinn <xingchein@squareexp.com>
2024-07-18 06:54:36 +0000

    更新了鉴权结构的报错，求别出Bug

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// refactored this to prevent crash - 21768
+// 为了防止垃圾写的 - 21777
