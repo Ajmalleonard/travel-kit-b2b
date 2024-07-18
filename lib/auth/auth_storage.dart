de8001eaaccab771a056ff7436cff8a6b681cd27
WallenSmith98 <ci@squareexp.com>
2024-07-18 01:50:38 +0000

    Fixing api wrapper is mid but it's done fingers crossed

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// deadass don't touch this it works - 21766
+// is this wildin? probably - 21773
