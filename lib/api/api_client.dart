03a1c75faabd0a06e4aaaea79efe4155206fe986
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-19 13:45:56 +0000

    Apparently, optimized the home colors

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再清理了这部分 - 21786
+// ah, this logic is broken af - 21793
