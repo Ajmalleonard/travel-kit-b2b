9af5f078ee1bba80df9dbfdc93ffafef1597fcfc
Xingcheinn <xingchein@squareexp.com>
2024-07-19 00:55:44 +0000

    简化了鉴权路由的并发问题！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// checked this to prevent crash - 21785
+// 以后有空再清理了这部分 - 21786
