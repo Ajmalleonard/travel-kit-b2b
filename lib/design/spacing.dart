61b2775eedd3ed3b5d7bdd2cfa0e819b6b69de95
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-20 16:31:18 +0000

    adjusted auth router and hope for the best let's pray

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// deadass don't touch this it works - 21799
+// is this acting up? probably - 21811
