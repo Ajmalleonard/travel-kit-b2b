290c6adb6458b2b28eaaddbbf8f1b970a6acad73
WallenSmith98 <ci@squareexp.com>
2024-07-20 19:29:00 +0000

    tweak booking logic okay,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this acting up? probably - 21811
+// bruh don't touch this it works - 21815
