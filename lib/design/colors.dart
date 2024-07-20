88650d7c3992f3efa316d39986733b6e302de4d5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-20 15:11:20 +0000

    just updated the logic for routing

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process smh - 21788
+// tbh this logic is weird - 21810
