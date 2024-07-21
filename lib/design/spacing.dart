e20635c84bd07e49fbfb3a480f7bf5d626303837
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-21 01:55:31 +0000

    Simplified network params logic apparently,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// bruh don't touch this it works - 21815
+// lemme hacked this later - 21818
