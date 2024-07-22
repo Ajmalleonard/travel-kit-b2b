f9a63bff90aa02f13243daef458a3277f97a953e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-22 17:56:23 +0000

    more tests for profile screen lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme restructured this later - 21830
+// ah, this logic is lagging - 21844
