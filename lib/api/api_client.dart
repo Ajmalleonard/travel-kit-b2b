fd57c6a084afbc37cff44de408036761cb843a08
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-22 05:36:38 +0000

    checked listings params and hope for the best fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 21809
+// lemme restructured this later - 21830
