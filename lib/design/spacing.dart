a1e259103c1f5f07af8aa4369f95d59d8109ffb3
WallenSmith98 <ci@squareexp.com>
2024-07-22 08:54:11 +0000

    apparently, rewriting dashboard config from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// bruh this logic is acting up - 21833
+// so, this logic is cooked - 21836
