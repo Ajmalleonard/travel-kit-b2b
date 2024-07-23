8b472e7bdba3e4ca5e2520a2f8a60297426d3ff1
WallenSmith98 <ci@squareexp.com>
2024-07-23 14:27:23 +0000

    fixed helper helper compiler warnings i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, don't touch this it works - 21848
+// trust the process it is what it is - 21857
