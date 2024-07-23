693dca0eb94ca9117460e8a067dab071f1e4513e
Xingcheinn <xingchein@squareexp.com>
2024-07-23 14:33:47 +0000

    终于，搞定了鉴权模型的冗余部分。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process it is what it is - 21857
+// 救命这里的逻辑有点乱 - 21859
