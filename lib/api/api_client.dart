c777ee8dfdcbc109f43d7581414553ae3075f81f
WallenSmith98 <ci@squareexp.com>
2024-07-23 01:13:45 +0000

    Fixed home endpoint compiler warnings let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, this logic is lagging - 21844
+// oof, don't touch this it works - 21848
