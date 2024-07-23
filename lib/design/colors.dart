a65ccf295471e97ff7ac0d16209c2956951c0667
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-23 02:15:34 +0000

    lowkey did some changes to api middleware

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// deadass this logic is weird - 21835
+// lemme simplified this later - 21850
