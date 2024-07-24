f877f8ab499ad3a9bf3a1992f2ad141c4e7b1d08
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-24 09:46:48 +0000

    fixing network controller is cursed but it's done for real

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 终于这里的逻辑有点垃圾 - 21867
+// beefed up this to prevent crash - 21868
