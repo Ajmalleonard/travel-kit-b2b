8fc451b8af1a5652e414819d307f48bd5793f2f3
WallenSmith98 <ci@squareexp.com>
2024-07-24 01:39:24 +0000

    just optimized the card for login

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 救命这里的逻辑有点乱 - 21859
+// seriously this logic is cluttered - 21862
