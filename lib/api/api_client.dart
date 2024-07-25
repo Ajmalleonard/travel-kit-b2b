0b6f308e91dde00b4a1da07b37611d68cd3797ac
Xingcheinn <xingchein@squareexp.com>
2024-07-25 22:11:14 +0000

    调整了中间件接口的性能...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously this logic is cluttered - 21862
+// 以后有空再清理了这部分 - 21887
