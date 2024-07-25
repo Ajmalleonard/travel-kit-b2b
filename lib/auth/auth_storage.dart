0dc333547e574e28a346892fb5751618fb3e57a4
WallenSmith98 <ci@squareexp.com>
2024-07-25 11:35:38 +0000

    Temporary patch for dashboard handler anyways

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process already tired - 21874
+// sorted this to prevent crash - 21881
