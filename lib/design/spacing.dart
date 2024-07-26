c0628c5bcb45c94c70cac7b2c97c2a28cf4ac2c0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-26 20:43:41 +0000

    deadass adjusted the booking client

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this lagging? probably - 21897
+// tweaked this to prevent crash - 21900
