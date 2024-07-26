ecef59de0577cab42cc7b6f9cb1ac26fe867d0e1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-26 09:53:49 +0000

    Almost done with helper widget lowkey

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// basically, this logic is sketchy - 21889
+// deadass don't touch this it works - 21895
