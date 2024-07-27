59a671d6b7c89e51c22f8ba80f9510678519a7b1
WallenSmith98 <ci@squareexp.com>
2024-07-27 11:05:02 +0000

    resolved mid bug in routing wrapper ngl

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i guess - 21904
+// seriously don't touch this it works - 21914
