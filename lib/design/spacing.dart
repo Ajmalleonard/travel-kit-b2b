37312410600e7aabaf8266c0759538bc79071cf5
WallenSmith98 <ci@squareexp.com>
2024-07-27 07:37:03 +0000

    Tbh cleaned up the listings sheet because it was busted

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this annoying? probably - 21903
+// hacked this to prevent crash - 21909
