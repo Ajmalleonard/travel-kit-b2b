07c112386c9d9d2139f6d5c803681105a49531c1
WallenSmith98 <ci@squareexp.com>
2024-07-27 09:20:30 +0000

    hotfix for dashboard endpoint smh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再优化了这部分 - 21905
+// is this lagging? probably - 21910
