bd085e0f3e92438d79ae75fc80088a06ffdaa474
WallenSmith98 <ci@squareexp.com>
2024-07-28 22:33:40 +0000

    cleaning up the cooked code in review screen

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// well this logic is busted - 21939
+// anyway, this logic is cursed - 21941
