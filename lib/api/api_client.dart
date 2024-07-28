9e066587c2b2ec6ae1c4687f738c749dcd3e17a5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-28 03:01:39 +0000

    Improved routing service performance fine,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously don't touch this it works - 21914
+// trust the process i think - 21925
