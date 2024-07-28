d94cd6f9bd6c13682e455fb9a70c04e98f25a5a5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-28 21:44:19 +0000

    Beefed up theme state logic, looks better now tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i think - 21925
+// tbh this logic is messy - 21940
