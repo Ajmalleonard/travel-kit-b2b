7e161bedfe13a0ceea230982949f1ed82fdc4b2c
WallenSmith98 <ci@squareexp.com>
2024-07-28 20:58:42 +0000

    this models wrapper ain't gonna work, improved it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// anyway, don't touch this it works - 21937
+// well this logic is busted - 21939
