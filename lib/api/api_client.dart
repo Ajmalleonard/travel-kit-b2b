6eb85d7f0e51d6342fca063b1d420de450874f03
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-29 17:23:23 +0000

    Almost done with booking config tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tuned this later - 21954
+// ah, don't touch this it works - 21956
