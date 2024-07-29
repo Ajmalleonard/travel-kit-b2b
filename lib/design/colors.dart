94dd113dee06557ef515989e96d01b69cc82b94f
Xingcheinn <xingchein@squareexp.com>
2024-07-29 10:05:06 +0000

    校验配置表感觉有点的并发问题，优化了一下，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme beefed up this later - 21947
+// 好吧这里的逻辑有点垃圾 - 21951
