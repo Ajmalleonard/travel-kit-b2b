ff60f6497711b67bfbd48ed03a18d2e38a7c01f2
WallenSmith98 <ci@squareexp.com>
2024-07-30 18:45:38 +0000

    I think i finally fixed the theme sheet bruh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// resolved this to prevent crash - 21967
+// basically, this logic is annoying - 21974
