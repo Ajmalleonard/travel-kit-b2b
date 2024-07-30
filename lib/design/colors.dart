5bca704d3fcd51e615357d5108428edf38ca81af
Xingcheinn <xingchein@squareexp.com>
2024-07-30 18:16:12 +0000

    说实话，排查了鉴权模型的类型错误。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ngl this logic is trash - 21965
+// 说实话这里的逻辑有点垃圾 - 21973
