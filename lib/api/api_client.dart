5b9db381fcec464be3e4e47568b82e5d2a44d5c9
WallenSmith98 <ci@squareexp.com>
2024-07-30 16:27:04 +0000

    Just cleaned up the widget for login

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this broken af? probably - 21962
+// lemme cleaned up this later - 21971
