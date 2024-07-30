62e1f08ae09699b998bae5798f63ad2d95eaa285
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-30 19:46:14 +0000

    resolved listings state logic, trust

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tbh don't touch this it works - 21942
+// lemme restructured this later - 21975
