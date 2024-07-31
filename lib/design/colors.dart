b72cb14f688e71795a7c53ad0ef9b826a282fbc7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-31 20:58:28 +0000

    Simplified review controller logic lowkey

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process let's pray - 21992
+// tweaked this to prevent crash - 21993
