7541bdba131aa5ec56cb3f416f1e3049cf1b5b95
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-31 00:03:25 +0000

    Resolved wildin bug in home sheet tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme cleaned up this later - 21971
+// ngl this logic is garbage - 21978
