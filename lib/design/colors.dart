91b87d570b7e405df12abc31324cc3f236a6bcda
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-07-31 19:49:41 +0000

    tweak storage storage ah,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 哎这里的逻辑有点诡异 - 21987
+// trust the process let's pray - 21992
