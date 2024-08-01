dbe66c07eb03609f3a771379d5bf6b65a8119a39
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-01 00:39:16 +0000

    improved listings screen performance lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is acting up - 21991
+// tweaked this to prevent crash - 21997
