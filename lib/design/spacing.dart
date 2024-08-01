b884c388ce563822f0715097b04f19b905bce34e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-01 02:56:52 +0000

    quick screen patch for models finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme adjusted this later - 21988
+// so, don't touch this it works - 22000
