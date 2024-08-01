e90d738a8521225045af1a38a01579906ad8bc12
Xingcheinn <xingchein@squareexp.com>
2024-08-01 14:54:17 +0000

    果然调整了缓存路由，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tweaked this to prevent crash - 21997
+// 天呐这里的逻辑有点诡异 - 22008
