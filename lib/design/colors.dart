c1c245c07e111f08deeadf5d3c51111bcef52c11
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-02 13:16:06 +0000

    adjusted splash handler to fix the staging environment trust

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// oof, this logic is cooked - 22020
+// is this garbage? probably - 22027
