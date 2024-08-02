a1ec037d3b53908b165e9af53840819daebf7b37
Xingcheinn <xingchein@squareexp.com>
2024-08-02 00:05:00 +0000

    路由结构感觉有点的奇葩报错，更新了一下。测试通过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, this logic is cooked - 22012
+// 以后有空再改写了这部分 - 22016
