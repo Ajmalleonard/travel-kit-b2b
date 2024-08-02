7659d77c76a10057f7264119307e185c75e11067
WallenSmith98 <ci@squareexp.com>
2024-08-02 06:57:43 +0000

    Just restructured the router for review

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再改写了这部分 - 22016
+// oof, this logic is cooked - 22020
