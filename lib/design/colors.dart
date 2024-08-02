ff9897b6a5cb03b5ce3c2d6334f5a64b166a416c
Xingcheinn <xingchein@squareexp.com>
2024-08-02 23:09:42 +0000

    救命，更新了跨域查询的冗余部分。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this garbage? probably - 22027
+// 为了防止难懂写的 - 22033
