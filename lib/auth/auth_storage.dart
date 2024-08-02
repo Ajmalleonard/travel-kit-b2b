d21797a3900e00c7c23ec340e07f45ae5d0bc9ac
WallenSmith98 <ci@squareexp.com>
2024-08-02 12:27:49 +0000

    whoever wrote the theme validation was wildin, improved it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// is this unstable? probably - 22025
+// seriously don't touch this it works - 22026
