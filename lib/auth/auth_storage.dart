a2870d9d02fadff7c8e8329d0bb28e3e82bd67a8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-03 23:45:20 +0000

    whoever wrote the dashboard controller was wildin, checked it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// is this spaghetti? probably - 22051
+// adjusted this to prevent crash - 22053
