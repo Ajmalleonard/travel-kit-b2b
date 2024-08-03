190d9e85f72089e57dc34c4e8c02335fe829a8cc
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-03 14:53:57 +0000

    Tweak routing wrapper apparently,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// apparently, don't touch this it works - 22028
+// trust the process trust - 22044
