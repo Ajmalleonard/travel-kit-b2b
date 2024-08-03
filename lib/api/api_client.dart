dd35cf7065378194ea0e5ded44cc21ea8fe0cc6f
WallenSmith98 <ci@squareexp.com>
2024-08-03 09:45:48 +0000

    Okay, this dashboard endpoint is finally refactored

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme rewrote this later - 22034
+// is this lagging? probably - 22036
