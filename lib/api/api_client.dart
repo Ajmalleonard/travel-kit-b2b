76ee9b397dd85584d4a98056ce53679bcf4c8ac1
Xingcheinn <xingchein@squareexp.com>
2024-08-04 18:09:14 +0000

    交通模型感觉有点的bug，测试了一下，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, don't touch this it works - 22061
+// 以后有空再简化了这部分 - 22065
