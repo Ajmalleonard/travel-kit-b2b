ebb4f94a3ed0556a801982902d094b0cf4e9342c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-05 08:00:14 +0000

    Minor tweak to dashboard endpoint already tired

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this broken af? probably - 22068
+// tbh this logic is lagging - 22076
