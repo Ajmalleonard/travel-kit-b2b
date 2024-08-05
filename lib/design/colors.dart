a20f03da763c68d7c7448cfe7792f0f51feeaaf2
Xingcheinn <xingchein@squareexp.com>
2024-08-05 13:40:26 +0000

    更新了评价配置表的类型错误！

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey this logic is cursed - 22078
+// 救命这里的逻辑有点臃肿 - 22079
