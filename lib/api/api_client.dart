8582661747dd747dd01b21626d36dd0ef7138b41
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-05 00:23:31 +0000

    anyway, it's cooking now. refactored login

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再简化了这部分 - 22065
+// okay, this logic is broken af - 22070
