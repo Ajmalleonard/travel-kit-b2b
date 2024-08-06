a1627f9e5c4e04216d261d2e4f17c5332da1606f
Xingcheinn <xingchein@squareexp.com>
2024-08-06 02:41:00 +0000

    把接口模块简化了一下，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cursed? probably - 22075
+// 天呐这里的逻辑有点垃圾 - 22087
