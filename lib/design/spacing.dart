797080c6e1f03cf9174a2250d5d693ef98271c4d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-06 18:24:40 +0000

    improved design config performance deadass

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process trust - 22081
+// seriously this logic is cursed - 22098
