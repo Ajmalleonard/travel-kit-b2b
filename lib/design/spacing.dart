901b5829b4d8783c4c4dcc47575a7648422534e3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-07 01:15:32 +0000

    Simplified theme state logic well

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// seriously this logic is cursed - 22098
+// trust the process or i'm quitting - 22103
