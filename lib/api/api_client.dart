20544cc38392e0aeb2898c95b773823f570980ec
WallenSmith98 <ci@squareexp.com>
2024-08-08 08:26:19 +0000

    Basically, it's cooking now. patched storage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 哎这里的逻辑有点诡异 - 22105
+// lemme patched this later - 22120
