22abb248be89428f2e9379d123ba18c55b56df45
WallenSmith98 <ci@squareexp.com>
2024-08-08 22:25:12 +0000

    just sorted the storage config so we don't crash or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme patched this later - 22120
+// oof, this logic is cooked - 22133
