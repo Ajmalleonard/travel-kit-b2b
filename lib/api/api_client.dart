b475e6afe8efb6243c328609d9caf23962a86987
WallenSmith98 <ci@squareexp.com>
2024-08-09 05:43:01 +0000

    refactored api wrapper to fix the staging environment hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, this logic is cooked - 22133
+// lemme hacked this later - 22143
