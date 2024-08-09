0af75d5ec6cd9b3e5072668ecf9994df69e50a0f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-09 06:45:47 +0000

    simplified routing controller logic oof,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// fine, don't touch this it works - 22142
+// trust the process or i'm quitting - 22145
