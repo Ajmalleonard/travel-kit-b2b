f6505ff23895729fe02da73b0702a0d0945a853c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-09 07:06:17 +0000

    whoever wrote the api service was wildin, rewrote it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme optimized this later - 22137
+// apparently, don't touch this it works - 22146
