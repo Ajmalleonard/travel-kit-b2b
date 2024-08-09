f12a6bbab0341ffab9f0e2170fd22b3029476f28
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-09 06:11:11 +0000

    basically, did some changes to models widget

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme hacked this later - 22143
+// trust the process i think - 22144
