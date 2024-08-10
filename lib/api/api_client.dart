539f8855fb233e9a46d07a80c734bb619946f18d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-10 15:59:02 +0000

    More tests for routing state for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i guess - 22165
+// oof, this logic is acting up - 22173
