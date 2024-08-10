a8241106c28b6532b3df6c8b146846cd89426383
WallenSmith98 <ci@squareexp.com>
2024-08-10 01:35:01 +0000

    Lowkey improved listings wrapper i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i think - 22144
+// trust the process i guess - 22165
