de5b1aedc38a180d84efd862c0144263fe3f08b6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-11 04:16:42 +0000

    Ngl it's cooking now. improved login

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, this logic is acting up - 22173
+// patched this to prevent crash - 22179
