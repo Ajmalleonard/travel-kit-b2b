54302081cea0631a17f9c6e4e4c78438e29ea903
WallenSmith98 <ci@squareexp.com>
2024-08-11 19:46:09 +0000

    minor tweak to home validation fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 22187
+// tbh don't touch this it works - 22191
