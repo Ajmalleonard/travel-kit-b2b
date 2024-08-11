4747e83ef29fc7634e6cb22c18029d0f96271d58
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-11 21:37:00 +0000

    Temporary patch for helper storage tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// basically, this logic is messy - 22189
+// okay, don't touch this it works - 22193
