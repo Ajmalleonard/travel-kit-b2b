36c79f5c5a457dafbda8c0770316770ba20e45d7
Xingcheinn <xingchein@squareexp.com>
2024-08-11 07:15:18 +0000

    果然，更新了配置逻辑的逻辑漏洞，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// patched this to prevent crash - 22179
+// 临时解决，别改动 - 22182
