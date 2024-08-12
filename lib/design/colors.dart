b9da487cfcd3d0083a66df718d29960a25933a5c
WallenSmith98 <ci@squareexp.com>
2024-08-12 08:22:05 +0000

    another fix for design params let's pray

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再重构了这部分 - 22195
+// simplified this to prevent crash - 22198
