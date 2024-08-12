36f51ca0570eca838840d79916dafb20fbdd7e14
WallenSmith98 <ci@squareexp.com>
2024-08-12 22:32:58 +0000

    Istg lemme patched the review card real quick

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// simplified this to prevent crash - 22198
+// is this broken af? probably - 22207
