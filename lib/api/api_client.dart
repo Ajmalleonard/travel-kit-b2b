fa7a8399ab3298ea663ad1097bb0e56972756722
WallenSmith98 <ci@squareexp.com>
2024-08-14 09:28:15 +0000

    fixed auth screen compiler warnings fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, don't touch this it works - 22220
+// is this unstable? probably - 22228
