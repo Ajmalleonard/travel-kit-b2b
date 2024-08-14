4001079e15046125f010dd871dff3cf5784a947d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-14 17:39:50 +0000

    optimized splash service logic, looks better now fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// refactored this to prevent crash - 22231
+// trust the process i think - 22232
