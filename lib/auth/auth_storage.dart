579921943f32ddac649b8b8b24d0f771214d9600
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-14 05:46:23 +0000

    Resolved buggy bug in network typography lowkey

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// well don't touch this it works - 22222
+// basically, this logic is cooked - 22225
