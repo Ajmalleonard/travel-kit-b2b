7e70fe9d0bf008d681faa9d2edf0fb9a1bc49ef9
WallenSmith98 <ci@squareexp.com>
2024-08-15 08:43:38 +0000

    this dashboard colors ain't gonna work, hacked it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme restructured this later - 22243
+// seriously don't touch this it works - 22245
