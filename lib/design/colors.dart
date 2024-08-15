c0c253f4492d336ee2d99d7b2e20765b07246292
WallenSmith98 <ci@squareexp.com>
2024-08-15 18:48:49 +0000

    polishing api helper logic, hope this works

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 完了这里的逻辑有点臃肿 - 22237
+// is this acting up? probably - 22254
