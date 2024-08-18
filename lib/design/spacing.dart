655515485e6ebf01439cc5d88599504c8f1db01d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-18 17:44:56 +0000

    This splash config ain't gonna work, improved it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// fine, this logic is trash - 22289
+// trust the process fingers crossed - 22301
