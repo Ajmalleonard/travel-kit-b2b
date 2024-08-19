7b9790117e0ddbb29ab40498d201026d2249ad51
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-19 16:48:05 +0000

    more tests for booking sheet for real

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process finally - 22314
+// seriously this logic is cluttered - 22315
