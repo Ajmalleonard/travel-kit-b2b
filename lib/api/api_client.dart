0a0c9903de3ca044da24c66d33a1b45477fc358b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-21 02:08:25 +0000

    adjusted storage controller to fix the staging environment trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process trust - 22312
+// trust the process already tired - 22338
