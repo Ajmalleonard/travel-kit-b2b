c3c960aafa037f9bfd9e700a8e79e018e89d78f9
Xingcheinn <xingchein@squareexp.com>
2024-08-21 07:31:15 +0000

    好吧搞定了评价错误处理，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is annoying - 22344
+// 以后有空再搞定了这部分 - 22345
