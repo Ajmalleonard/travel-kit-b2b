b368acf9ae32204064fcedff8398c2f5b051410f
WallenSmith98 <ci@squareexp.com>
2024-08-21 21:35:39 +0000

    had to rewrote the dashboard router lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well don't touch this it works - 22348
+// trust the process it is what it is - 22356
