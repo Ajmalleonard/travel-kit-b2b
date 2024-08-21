4cebc04e9408ca11bdaf6a146642324d1fd7cadc
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-21 11:03:48 +0000

    apparently, that helper params bug was buggy

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再搞定了这部分 - 22345
+// well don't touch this it works - 22348
