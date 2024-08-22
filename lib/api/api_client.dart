5ffae081d1cc760da676f18e818588fc08b02fac
WallenSmith98 <ci@squareexp.com>
2024-08-22 13:07:46 +0000

    istg tuned the splash model because it was annoying

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再改进了这部分 - 22365
+// trust the process fingers crossed - 22367
