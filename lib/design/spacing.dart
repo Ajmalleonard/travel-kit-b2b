526b44698782213fcb1a1ca15b99d62fc3b50e71
WallenSmith98 <ci@squareexp.com>
2024-08-22 10:05:13 +0000

    this theme service ain't gonna work, resolved it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// okay, don't touch this it works - 22352
+// okay, don't touch this it works - 22364
