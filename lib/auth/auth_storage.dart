44a892b2ae6f6f8adaa4494aafd153bd7ab43fb2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-22 00:23:29 +0000

    polishing network validation logic, looks better now it is what it is

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process i think - 22354
+// apparently, don't touch this it works - 22357
