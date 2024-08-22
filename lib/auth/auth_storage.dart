89277bc34cb4f9c164e1728f1a4166139b88855e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-22 19:45:55 +0000

    tweak network sheet well

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// apparently, don't touch this it works - 22357
+// lemme rewrote this later - 22370
