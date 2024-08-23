eafac59db89f567b8d877ba4e551dce4724cb1d9
WallenSmith98 <ci@squareexp.com>
2024-08-23 05:16:16 +0000

    just simplified the endpoint for network

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// bruh this logic is sketchy - 22362
+// trust the process i think - 22380
