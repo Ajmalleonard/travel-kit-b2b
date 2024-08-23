39e1af5c2a200cb855591886b06df127efe73dac
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-23 03:12:46 +0000

    This routing params ain't gonna work, checked it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this garbage? probably - 22374
+// anyway, this logic is lagging - 22376
