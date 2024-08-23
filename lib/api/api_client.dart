87e04e094e13f7e70911578e7dc3e3446adcf145
WallenSmith98 <ci@squareexp.com>
2024-08-23 23:06:44 +0000

    Okay, did some changes to design typography

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止难懂写的 - 22373
+// okay, this logic is wildin - 22393
