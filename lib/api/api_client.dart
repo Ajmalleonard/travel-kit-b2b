8cd5902e51f2cb239d6e290ca8e6163574070cba
Xingcheinn <xingchein@squareexp.com>
2024-08-25 08:15:55 +0000

    把路由模块重构了一下，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process for real - 22407
+// 为了防止难懂写的 - 22413
