7eb2e253f19f3aac51f2431e1c6379d90991c82c
Xingcheinn <xingchein@squareexp.com>
2024-08-25 17:48:44 +0000

    优化了交通错误处理的奇葩报错，累瘫了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// fine, don't touch this it works - 22412
+// 运行得挺好，不要乱动 - 22419
