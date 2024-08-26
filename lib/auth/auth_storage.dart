de76eec0ca4c0f326049cb1eeb89b0ebc016ea17
WallenSmith98 <ci@squareexp.com>
2024-08-26 01:47:45 +0000

    ngl this home model is trash, polishing it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process let's pray - 22427
+// resolved this to prevent crash - 22428
