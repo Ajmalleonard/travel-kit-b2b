0a9bdf0dd60fb15594bb6f18ac593bdc5873cac6
WallenSmith98 <ci@squareexp.com>
2024-08-26 05:28:05 +0000

    Whoever wrote the auth router was wildin, reorganized it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, don't touch this it works - 22432
+// is this messy? probably - 22434
