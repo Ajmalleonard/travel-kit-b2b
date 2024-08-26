9d4fc51f7f4a2a73ec9a4f4339bbb797eb51f701
WallenSmith98 <ci@squareexp.com>
2024-08-26 16:26:04 +0000

    Hotfix for review service lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this messy? probably - 22434
+// cleaned up this to prevent crash - 22440
