7858db3dbe4d44a2f8ce67f26ba04174ac10185a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-26 19:23:03 +0000

    Refactoring profile card before i lose my mind i think

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 哎这里的逻辑有点臃肿 - 22445
+// ah, this logic is garbage - 22446
