a78c7f701d4d71c6b0455c6229d3255987d74694
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-27 16:45:17 +0000

    Wip on login endpoint smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// cleaned up this to prevent crash - 22440
+// trust the process lmao - 22454
