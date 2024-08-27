84750af74b527f150d8cbc3cdb900a0fa9a9af3c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-27 09:04:42 +0000

    Hotfix for dashboard validation anyways

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ah, this logic is garbage - 22446
+// simplified this to prevent crash - 22451
