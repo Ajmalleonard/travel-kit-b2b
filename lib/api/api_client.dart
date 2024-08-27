d201bb25925336fba49559409fec85070b0dccc9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-27 20:49:05 +0000

    seriously did some changes to routing colors

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lmao - 22454
+// is this buggy? probably - 22456
