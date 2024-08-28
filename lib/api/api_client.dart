39bdcc65828fd463b7b55b40f0fbd29cf55ede38
Xingcheinn <xingchein@squareexp.com>
2024-08-28 10:29:08 +0000

    好吧，更新了评价错误处理的bug...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, don't touch this it works - 22459
+// 以后有空再修复了这部分 - 22468
