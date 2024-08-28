b8add61f7e27cea133467edc18856190181f158d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-28 00:41:49 +0000

    oof, it's cooking now. adjusted login

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再优化了这部分 - 22453
+// tbh don't touch this it works - 22457
