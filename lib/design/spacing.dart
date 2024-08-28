116f3ffedcc1ed9327c66ea43ab13b17653657b8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-28 18:27:31 +0000

    Simplified splash card logic lowkey

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process for real - 22466
+// lemme restructured this later - 22471
