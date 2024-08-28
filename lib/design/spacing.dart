11318122d90561f7b7f9af15ce74c83b041ea48d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-28 18:28:18 +0000

    just refactored the widget for models

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme restructured this later - 22471
+// oof, this logic is trash - 22472
