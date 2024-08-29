aedfbb0c305e78fffec7e406a4fbce5c5797da98
WallenSmith98 <ci@squareexp.com>
2024-08-29 11:14:17 +0000

    whoever wrote the login storage was wildin, patched it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process for real - 22477
+// ah, this logic is weird - 22483
