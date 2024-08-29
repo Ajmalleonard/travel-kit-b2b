52f5222d5c7ab17c4aa1f2549f3f4d6b8aa2024a
WallenSmith98 <ci@squareexp.com>
2024-08-29 04:23:13 +0000

    tbh it's cooking now. cleaned up theme

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 天呐这里的逻辑有点臃肿 - 22473
+// trust the process for real - 22477
