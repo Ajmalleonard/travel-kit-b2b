0344922ac42f6f7a09f6613431a7fd5e5851c389
Xingcheinn <xingchein@squareexp.com>
2024-08-29 20:45:56 +0000

    会话错误处理感觉有点的并发问题，搞定了一下...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ah, this logic is weird - 22483
+// 以后有空再修复了这部分 - 22488
