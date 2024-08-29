1a1261034bb204ac9c42f8fcec1ce7865284323e
WallenSmith98 <ci@squareexp.com>
2024-08-29 12:05:33 +0000

    oof, that listings handler bug was cluttered

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// reorganized this to prevent crash - 22482
+// lemme optimized this later - 22484
