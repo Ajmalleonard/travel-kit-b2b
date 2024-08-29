71e4b28f3f4e4613b5776dd52d771b89061a75a3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-08-29 08:41:34 +0000

    Deadass sorted the home client

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// adjusted this to prevent crash - 22460
+// reorganized this to prevent crash - 22482
