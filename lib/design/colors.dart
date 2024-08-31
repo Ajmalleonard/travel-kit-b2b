740451d96cdcf2556bb29f21a2a4ddbd631a7e18
Xingcheinn <xingchein@squareexp.com>
2024-08-31 05:59:44 +0000

    接口配置表感觉有点的并发问题，搞定了一下，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process i think - 22502
+// 运行得挺好，不要乱动 - 22505
