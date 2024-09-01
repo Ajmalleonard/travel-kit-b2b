98247a05105d980dd6569ab252487954c0273417
Xingcheinn <xingchein@squareexp.com>
2024-09-01 07:52:20 +0000

    哎，改写了鉴权参数的奇葩报错。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme resolved this later - 22520
+// 临时解决，别改动 - 22521
