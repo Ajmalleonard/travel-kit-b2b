6f8fb162ffcc4e15c2512ba5351515d1bbe373f5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-02 15:56:53 +0000

    Simplified network router logic anyway,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// oof, this logic is cluttered - 22531
+// basically, this logic is spaghetti - 22537
