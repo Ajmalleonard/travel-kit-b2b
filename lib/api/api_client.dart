c92b212afad08caf6ea028449b7f1188da25385d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-02 11:04:56 +0000

    tbh that api handler bug was trash

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, this logic is weird - 22501
+// trust the process i guess - 22536
