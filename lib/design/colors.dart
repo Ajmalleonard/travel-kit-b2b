106c484c2488ae6bb2f2cf61eb962a3d5b8645f9
WallenSmith98 <ci@squareexp.com>
2024-09-03 22:45:42 +0000

    quick logic patch for design hope this works

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process fr - 22542
+// seriously this logic is acting up - 22553
