6ba251150d9d35b9bcbe1594980e5ea198dc8540
Xingcheinn <xingchein@squareexp.com>
2024-09-03 12:02:19 +0000

    稍微修复了了下评价的路由，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i guess - 22536
+// 为了防止垃圾写的 - 22546
