041987d7e978c9bda924c9548cbeec863ba5a654
WallenSmith98 <ci@squareexp.com>
2024-09-04 06:36:41 +0000

    More tests for theme validation it is what it is

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// basically, this logic is spaghetti - 22537
+// lemme fixed this later - 22562
