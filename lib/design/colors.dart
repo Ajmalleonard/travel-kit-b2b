80ba2c0c742a5635e42f9738e68deab7807025c2
Xingcheinn <xingchein@squareexp.com>
2024-09-04 20:43:04 +0000

    更新了配置模块的类型错误，累死了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh don't touch this it works - 22576
+// 以后有空再测试了这部分 - 22578
