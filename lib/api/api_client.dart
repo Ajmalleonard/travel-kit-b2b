a13bd1d1fe0fdb6eb27b7479e2ec81163cfef6af
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-04 03:35:37 +0000

    Resolved the cursed parts of design endpoint

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, this logic is sketchy - 22555
+// is this mid? probably - 22557
