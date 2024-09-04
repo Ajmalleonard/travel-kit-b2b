f8e60118f6d51dbf58df40f8750e920e7d31d007
WallenSmith98 <ci@squareexp.com>
2024-09-04 13:54:58 +0000

    tbh lemme checked the profile router real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 22565
+// is this weird? probably - 22571
