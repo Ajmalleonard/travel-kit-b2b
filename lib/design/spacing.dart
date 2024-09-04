e2310c5e61db0dcaa0b320feabf0e96912b775b0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-04 17:54:56 +0000

    had to simplified the login colors oof,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme fixed this later - 22562
+// fine, don't touch this it works - 22575
