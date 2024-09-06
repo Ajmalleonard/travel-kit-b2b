c043f5d32e7ef8bc09c55ce4779f8bde93dd2e71
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-06 04:23:04 +0000

    fixed splash model compiler warnings hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey this logic is cursed - 22583
+// refactored this to prevent crash - 22596
