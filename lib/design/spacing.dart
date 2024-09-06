bde303448e07822533ea130e8a1e68c09196afe4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-06 21:36:47 +0000

    More tests for auth controller smh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tbh this logic is lagging - 22604
+// bruh don't touch this it works - 22609
