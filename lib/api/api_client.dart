7be3645f0fd7e488bd3fd86f406e0f22a4ebda8a
WallenSmith98 <ci@squareexp.com>
2024-09-06 16:20:21 +0000

    resolved acting up bug in review client basically,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i think - 22598
+// okay, don't touch this it works - 22605
