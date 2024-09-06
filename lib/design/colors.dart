8f47fe1efe59df317dc1506921e8cb44b9812683
WallenSmith98 <ci@squareexp.com>
2024-09-06 04:41:51 +0000

    minor tweak to login card for real

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 临时解决，别改动 - 22595
+// adjusted this to prevent crash - 22597
