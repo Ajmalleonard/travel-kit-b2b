9a7f17ace255baf85ec28173db748e236cf7497b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-06 08:10:48 +0000

    seriously that network controller bug was annoying

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// refactored this to prevent crash - 22596
+// trust the process i think - 22598
