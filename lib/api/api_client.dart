5d8ffe011ab05a4118363e6d6e988a730adb8816
Xingcheinn <xingchein@squareexp.com>
2024-09-07 13:46:45 +0000

    果然，改进了订单结构的并发问题，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this buggy? probably - 22614
+// 运行得挺好，不要乱动 - 22623
