0b0bcdc973a119309e3d6892d837698e3faaebb9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-07 03:05:40 +0000

    Reorganized network endpoint logic, let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, don't touch this it works - 22611
+// is this buggy? probably - 22614
