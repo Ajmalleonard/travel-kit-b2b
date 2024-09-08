73522551a93b0616df909902a8c35aff7a9de26f
Xingcheinn <xingchein@squareexp.com>
2024-09-08 00:48:35 +0000

    缓存字段感觉有点的屎山代码，改写了一下，累死了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// seriously don't touch this it works - 22628
+// 临时解决，别改动 - 22630
