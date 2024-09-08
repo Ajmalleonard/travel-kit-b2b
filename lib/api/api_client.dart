43f32e7c770cad2b9ca2d81e6fc7e4bd59ce1243
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-08 01:37:57 +0000

    hotfix for design model anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 22623
+// trust the process anyways - 22631
