0f0484d45abc6641f3fb6cea3ba3c8066c8033c0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-08 00:09:58 +0000

    improved models controller performance fine,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// deadass this logic is weird - 22617
+// seriously don't touch this it works - 22628
