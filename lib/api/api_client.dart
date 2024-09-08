f821adc5909995f09573b71a831cc749995cb644
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-08 14:19:39 +0000

    Whoever wrote the splash widget was wildin, improved it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tuned this to prevent crash - 22632
+// hacked this to prevent crash - 22636
