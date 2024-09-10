030da24a6305ce4a4fd173815041b2201bdf186f
Xingcheinn <xingchein@squareexp.com>
2024-09-10 12:44:25 +0000

    果然修复了订单模型...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this cursed? probably - 22651
+// 救命这里的逻辑有点诡异 - 22664
