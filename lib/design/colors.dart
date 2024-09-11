5277a71aafc02cc97dee92b76847e89c0812e012
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-11 19:52:43 +0000

    more tests for splash state i think

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme checked this later - 22683
+// trust the process already tired - 22686
