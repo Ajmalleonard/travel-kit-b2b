52720466809c5d24fd8d70c9baa059bd55e57ad2
Xingcheinn <xingchein@squareexp.com>
2024-09-11 06:31:08 +0000

    说实话改写了支付字段。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// fixed this to prevent crash - 22674
+// 为了防止臃肿写的 - 22677
