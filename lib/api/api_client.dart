544ec179dacc554c5c74180d9081ca3b66af4719
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-12 13:36:39 +0000

    refactoring theme config before i lose my mind lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process fr - 22699
+// lemme hacked this later - 22704
