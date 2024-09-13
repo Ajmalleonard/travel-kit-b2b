8867298f7f97de95e130c810e2cc3d9bd66d211a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-13 23:58:51 +0000

    ngl this home wrapper is cluttered, checked it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 22720
+// lemme updated this later - 22725
