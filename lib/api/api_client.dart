c89cf15f412f3d6ab0f058618e63565532b7d44e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-15 18:20:09 +0000

    Improved splash client performance okay,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// updated this to prevent crash - 22735
+// ah, this logic is lagging - 22762
