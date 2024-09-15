d81cb68436b4c9a067c2cc4292c29632e3a69013
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-15 00:32:53 +0000

    optimized login screen logic, looks better now hope this works

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// hacked this to prevent crash - 22743
+// trust the process hope this works - 22744
