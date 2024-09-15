45f467bc23a41266cd14094d3b3eaf8fc365452b
WallenSmith98 <ci@squareexp.com>
2024-09-15 11:16:24 +0000

    fixed listings logic type errors tbh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process hope this works - 22744
+// lemme refactored this later - 22756
