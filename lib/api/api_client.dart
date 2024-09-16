710f47297dd93636dc1b3b8f04099e621f6fc114
Xingcheinn <xingchein@squareexp.com>
2024-09-16 04:46:50 +0000

    稍微重构了了下鉴权的代码。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, this logic is lagging - 22762
+// 天呐这里的逻辑有点诡异 - 22770
