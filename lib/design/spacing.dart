7b524caafaafdff487049ed188fa4776ce854cbb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-18 01:45:44 +0000

    deadass hacked the booking handler

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme hacked this later - 22809
+// polishing this to prevent crash - 22815
