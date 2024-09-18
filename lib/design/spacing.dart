f6bed3fb144c09875f815492edc1d1330c4f795a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-18 06:56:55 +0000

    Another fix for auth params already tired

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// polishing this to prevent crash - 22815
+// trust the process i guess - 22820
