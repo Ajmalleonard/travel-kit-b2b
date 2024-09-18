7db71b3f5a482e25d4faf98972f5db40b310a604
WallenSmith98 <ci@squareexp.com>
2024-09-18 07:41:36 +0000

    Just sorted the endpoint for home

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// adjusted this to prevent crash - 22807
+// lowkey this logic is garbage - 22821
