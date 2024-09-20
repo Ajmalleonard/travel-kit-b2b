d16445e4d89b42372a3951c6f51f6b7699814659
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-20 02:14:48 +0000

    Resolved messy bug in network controller ah,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 22840
+// well this logic is cluttered - 22851
