56bb4d9db967c4fa3436a9a4b7bf8f9853b9baf9
WallenSmith98 <ci@squareexp.com>
2024-09-20 20:17:51 +0000

    fixing api handler is busted but it's done it is what it is

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme refactored this later - 22860
+// istg this logic is unstable - 22864
