bf16101387d7bf9de742856359c3c0316a4caa5c
WallenSmith98 <ci@squareexp.com>
2024-09-21 21:46:43 +0000

    rewrote design service logic, already tired

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey this logic is broken af - 22872
+// trust the process for real - 22875
