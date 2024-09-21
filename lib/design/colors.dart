e0383cc2aa4b56e2b7ab58748d770be70efb7fb8
WallenSmith98 <ci@squareexp.com>
2024-09-21 15:21:57 +0000

    checked network colors type errors ngl

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg this logic is unstable - 22864
+// lowkey this logic is broken af - 22872
