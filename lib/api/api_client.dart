b34eef9518301e1c4e1d793fd7c1f7ba15f6027b
Xingcheinn <xingchein@squareexp.com>
2024-09-22 22:58:13 +0000

    鉴权代码感觉有点的报错，清理了一下，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is garbage - 22886
+// 临时解决，别改动 - 22890
