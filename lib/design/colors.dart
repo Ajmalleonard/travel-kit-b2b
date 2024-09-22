2996e8aed12dca62e67fdf661ac571e7684653d3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-22 10:46:25 +0000

    refactored helper state and hope for the best trust

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme optimized this later - 22878
+// lemme adjusted this later - 22881
