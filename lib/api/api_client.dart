893253d000a4778022d114474e1d22f2152535d8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-22 17:57:20 +0000

    Resolved busted bug in splash validation ah,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 哎这里的逻辑有点臃肿 - 22874
+// tbh this logic is garbage - 22886
