dfd06581afef9ab2b9129eec43dfd87b7c8e6773
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-23 21:40:33 +0000

    tweaked design params logic, looks better now smh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止难懂写的 - 22893
+// oof, this logic is unstable - 22900
