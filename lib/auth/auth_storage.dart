fec6a8be3d01920ef5e1e9a4df733a37fb2ccedc
WallenSmith98 <ci@squareexp.com>
2024-09-24 01:54:57 +0000

    tweaked dashboard sheet to fix the staging environment i guess

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tbh don't touch this it works - 22901
+// lowkey this logic is unstable - 22902
