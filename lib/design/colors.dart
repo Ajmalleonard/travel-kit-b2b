a4f327db3ab0314e54c0119ebd50a75b6294b337
Xingcheinn <xingchein@squareexp.com>
2024-09-24 06:54:25 +0000

    把数据库结构测试了一下，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// oof, this logic is unstable - 22900
+// 以后有空再清理了这部分 - 22905
