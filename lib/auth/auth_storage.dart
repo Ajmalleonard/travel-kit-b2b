092a8f73a9d40b05194e325e0fddb0205aab579e
WallenSmith98 <ci@squareexp.com>
2024-09-25 14:22:35 +0000

    anyway, it's cooking now. refactored listings

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// seriously this logic is weird - 22916
+// bruh don't touch this it works - 22920
