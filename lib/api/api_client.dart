5aa5717fa7b9eeb0d472abf4887370e2c57e9885
WallenSmith98 <ci@squareexp.com>
2024-09-26 14:51:58 +0000

    Fixing auth params is cursed but it's done i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this busted? probably - 22924
+// anyway, don't touch this it works - 22935
