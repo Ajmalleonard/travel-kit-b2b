331d72e8676d34564871d3ecdf7ab56d562ec692
Xingcheinn <xingchein@squareexp.com>
2024-09-26 01:57:12 +0000

    重构了日志错误处理的奇葩报错，溜了溜了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// bruh this logic is broken af - 22909
+// 以后有空再搞定了这部分 - 22926
