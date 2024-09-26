32bdb54f25a9c3bba1f5b34f0294cc0248b8e830
WallenSmith98 <ci@squareexp.com>
2024-09-26 17:03:32 +0000

    tweak review logic apparently,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// ah, don't touch this it works - 22933
+// trust the process fingers crossed - 22938
