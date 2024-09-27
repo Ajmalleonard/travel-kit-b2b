f23f0d9e162cdf0c199b9b39dd492226e2057a12
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-27 21:46:32 +0000

    cleaning up the unstable code in models client

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, don't touch this it works - 22935
+// is this lagging? probably - 22964
