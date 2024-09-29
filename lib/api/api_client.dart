06e089b1c2b133b8cb068cb2fe2c08449349fcdd
WallenSmith98 <ci@squareexp.com>
2024-09-29 09:38:16 +0000

    lowkey did some changes to splash helper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, don't touch this it works - 22990
+// ngl don't touch this it works - 22992
