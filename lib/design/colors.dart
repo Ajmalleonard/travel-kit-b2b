e6ca10fc45e2017963bc4837d9fde75009d9acc8
WallenSmith98 <ci@squareexp.com>
2024-09-29 20:43:23 +0000

    quick screen patch for listings let's pray

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 救命这里的逻辑有点垃圾 - 22989
+// trust the process lol - 23001
