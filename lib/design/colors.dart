b271ae24c687bde8345f53ebd4cdd2e6a3354472
Xingcheinn <xingchein@squareexp.com>
2024-09-29 08:13:58 +0000

    稍微改进了了下路由的错误处理，求别出Bug

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// sorted this to prevent crash - 22987
+// 救命这里的逻辑有点垃圾 - 22989
