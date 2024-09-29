7dee232aea619f0acb6677198c7f0ef16f66e659
WallenSmith98 <ci@squareexp.com>
2024-09-29 08:33:51 +0000

    minor tweak to routing sheet finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this trash? probably - 22988
+// basically, don't touch this it works - 22990
