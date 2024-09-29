8f88b7139d62f0c0ee8092b30a2af95c7b54dcd5
WallenSmith98 <ci@squareexp.com>
2024-09-29 10:08:58 +0000

    Deadass refactored the splash typography

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl don't touch this it works - 22992
+// trust the process or i'm quitting - 22993
