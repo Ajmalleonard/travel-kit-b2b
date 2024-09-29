d118e2b806b8eeb1ff80c56cb8174dfeffce7425
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-29 06:09:20 +0000

    Sorted auth typography type errors anyway,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is busted - 22980
+// is this trash? probably - 22988
