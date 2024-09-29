cf26ecd9b7754d4a0f121c9966669d4e9dcf4d7a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-29 20:42:40 +0000

    Almost done with auth wrapper basically,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// adjusted this to prevent crash - 22976
+// improved this to prevent crash - 23000
