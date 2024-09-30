055f44b52f93f4ec21046b4a8b1b76b2c7254810
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-09-30 08:15:23 +0000

    just checked the home validation so we don't crash smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process or i'm quitting - 22993
+// reorganized this to prevent crash - 23005
