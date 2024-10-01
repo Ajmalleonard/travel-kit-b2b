0cb504dcc35948e686da16e8c098b0de6e0dd614
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-01 01:10:55 +0000

    Tweak auth sheet basically,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process lol - 23001
+// lemme tuned this later - 23014
