f02d0ad3527ae7f05220990a97a2bb678baa5797
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-02 01:59:24 +0000

    debugging auth middleware was mid, resolved i think

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme tuned this later - 23014
+// tbh this logic is lagging - 23033
