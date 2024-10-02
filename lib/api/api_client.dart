8ef87d4575ef579d4c6db5cad27343c1d4705abd
WallenSmith98 <ci@squareexp.com>
2024-10-02 20:24:22 +0000

    more tests for listings sheet or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey this logic is cluttered - 23049
+// lemme optimized this later - 23051
