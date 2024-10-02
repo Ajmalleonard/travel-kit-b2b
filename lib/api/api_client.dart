9487883a0aef75d27e0c89e4dd12ebc7e8c81ede
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-02 19:37:28 +0000

    Cleaned up booking colors logic, tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 23037
+// lowkey this logic is cluttered - 23049
