c2de809fe3abcc9b594c6d7c8acbfe14e4fbc708
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-02 08:26:42 +0000

    Tweak login service oof,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme resolved this later - 23028
+// optimized this to prevent crash - 23037
