16fe72e49014ac77d3f9bd106059c23198f5d795
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-03 11:38:03 +0000

    more tests for storage typography let's pray

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 果然这里的逻辑有点臃肿 - 23045
+// lemme cleaned up this later - 23060
