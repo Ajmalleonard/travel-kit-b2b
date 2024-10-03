a5288a028bdb35cde6ca08b90423f4e56ad94304
Xingcheinn <xingchein@squareexp.com>
2024-10-03 05:11:29 +0000

    列表字段感觉有点的逻辑漏洞，搞定了一下。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme optimized this later - 23051
+// 以后有空再重构了这部分 - 23055
