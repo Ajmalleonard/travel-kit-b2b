9ee180224fda732aa05753088b3e3df2c3fb0230
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-04 03:50:04 +0000

    just fixed the controller for profile

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme reorganized this later - 23063
+// anyway, this logic is spaghetti - 23072
