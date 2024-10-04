a039669b03279b57da788a26399924eb4bebd459
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-04 03:39:10 +0000

    fixed models endpoint compiler warnings or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this messy? probably - 23061
+// trust the process hope this works - 23071
