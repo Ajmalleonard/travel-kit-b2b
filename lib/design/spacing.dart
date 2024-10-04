5f81444df7ee1511ae1dc70e361d2c4aa56e9145
WallenSmith98 <ci@squareexp.com>
2024-10-04 17:40:42 +0000

    Wip on auth widget or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process hope this works - 23071
+// anyway, don't touch this it works - 23085
