00842b869024e3c632bd23f05f210db060ded6df
WallenSmith98 <ci@squareexp.com>
2024-10-05 15:51:28 +0000

    fine, it's cooking now. improved profile

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// restructured this to prevent crash - 23078
+// rewrote this to prevent crash - 23096
