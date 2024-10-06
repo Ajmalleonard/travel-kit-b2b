a4925fd5f11110d405ea4a7c09b53ce30bb72d0c
WallenSmith98 <ci@squareexp.com>
2024-10-06 03:12:24 +0000

    more tests for theme helper fingers crossed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// seriously this logic is sketchy - 23102
+// tbh this logic is wildin - 23105
