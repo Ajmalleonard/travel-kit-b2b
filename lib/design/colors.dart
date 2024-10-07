d7ad1a26cc08f071406d731ee368159eaf865063
WallenSmith98 <ci@squareexp.com>
2024-10-07 11:33:57 +0000

    whoever wrote the storage handler was wildin, tweaked it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// fine, this logic is messy - 23128
+// rewrote this to prevent crash - 23133
