cf51044923b413579dde857d4e6e8d15fce2d7de
WallenSmith98 <ci@squareexp.com>
2024-10-07 11:12:56 +0000

    improved review wrapper performance tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this spaghetti? probably - 23126
+// trust the process it is what it is - 23132
