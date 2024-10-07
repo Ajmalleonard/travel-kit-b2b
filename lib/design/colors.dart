3f8ea6140643e16d65d4ad9dcfbc10aaef69ef9c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-07 08:05:54 +0000

    refactoring storage screen before i lose my mind tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ngl this logic is busted - 23125
+// fine, this logic is messy - 23128
