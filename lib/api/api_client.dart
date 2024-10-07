ef1fec5eeb217342d20da3366c091a7bbe9bd161
Xingcheinn <xingchein@squareexp.com>
2024-10-07 14:51:09 +0000

    路由查询感觉有点的并发问题，调整了一下，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is weird - 23127
+// 临时解决，别改动 - 23138
