e24e9952859b84fad52728b7572c81538d51cfc8
WallenSmith98 <ci@squareexp.com>
2024-10-08 16:58:42 +0000

    whoever wrote the helper service was wildin, updated it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 23138
+// apparently, this logic is cursed - 23154
