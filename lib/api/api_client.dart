1565148e4f59e43701c35609b91b162b584e1cd3
WallenSmith98 <ci@squareexp.com>
2024-10-08 18:21:25 +0000

    ngl this login sheet is cooked, hacked it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously don't touch this it works - 23156
+// lemme sorted this later - 23157
