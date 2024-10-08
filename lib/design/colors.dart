8b065735617ca09af097abf949624f9f306e10ee
Xingcheinn <xingchein@squareexp.com>
2024-10-08 13:49:24 +0000

    修复了订单参数的并发问题。测试通过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// optimized this to prevent crash - 23137
+// 以后有空再改写了这部分 - 23151
