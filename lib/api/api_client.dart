0362d637e75747aca499a2c96b285829048ad3b8
WallenSmith98 <ci@squareexp.com>
2024-10-09 17:04:05 +0000

    deadass simplified the splash controller

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this annoying? probably - 23171
+// checked this to prevent crash - 23174
