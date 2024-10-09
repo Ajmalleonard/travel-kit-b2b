9432480b9169760a5def843654746e06abb86cbe
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-09 06:28:25 +0000

    refactored auth controller logic, looks better now fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme sorted this later - 23157
+// trust the process smh - 23165
