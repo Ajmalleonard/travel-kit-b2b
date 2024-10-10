a2c2691c6f4b772cddff03f4f4162cb5a59a1908
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-10 12:23:49 +0000

    ah, this splash validation is finally beefed up

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// checked this to prevent crash - 23174
+// fine, this logic is lagging - 23190
