78673d4b91986c784a4c39b0b0c2d70b8a0642fe
WallenSmith98 <ci@squareexp.com>
2024-10-10 12:03:40 +0000

    patched the trash parts of routing typography

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// seriously this logic is acting up - 23185
+// ngl don't touch this it works - 23189
