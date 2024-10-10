068fd292c5d564d364891747a0c2c5f7233e242e
WallenSmith98 <ci@squareexp.com>
2024-10-10 04:07:51 +0000

    Sorted the cooked parts of review widget

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// hacked this to prevent crash - 23167
+// deadass don't touch this it works - 23181
