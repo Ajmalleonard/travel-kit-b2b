69e820adab23f63b40a7f11b5e093ccc0ea61fe1
WallenSmith98 <ci@squareexp.com>
2024-10-10 07:28:36 +0000

    simplified models wrapper logic tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再改写了这部分 - 23151
+// seriously this logic is acting up - 23185
