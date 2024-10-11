68572e1fb403bc2c1ea5556490d8ef76b0d97ec6
Xingcheinn <xingchein@squareexp.com>
2024-10-11 11:49:13 +0000

    把路由代码改进了一下。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, this logic is lagging - 23190
+// 运行得挺好，不要乱动 - 23210
