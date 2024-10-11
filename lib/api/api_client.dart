d8d32dc5fb88103049c660f0f01c146dcbaec287
WallenSmith98 <ci@squareexp.com>
2024-10-11 13:19:28 +0000

    basically, lemme checked the models client real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, this logic is lagging - 23190
+// is this wildin? probably - 23208
