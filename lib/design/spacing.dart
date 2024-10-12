8575084ff436fa0cf3d1f1112b7e1544918c1501
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-12 00:35:59 +0000

    Quick storage patch for review let's pray

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lowkey this logic is mid - 23214
+// ah, don't touch this it works - 23222
