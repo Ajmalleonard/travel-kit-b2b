6f6d5afa4c99df0f08982fd94c274bd78a06c6db
Xingcheinn <xingchein@squareexp.com>
2024-10-13 08:52:36 +0000

    救命，调整了配置配置表的bug，累死了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh this logic is unstable - 23240
+// 说实话这里的逻辑有点难懂 - 23249
