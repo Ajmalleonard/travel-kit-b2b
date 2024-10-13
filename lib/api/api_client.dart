6a79365972fbff177a2fd968d08b2b49be5fa26b
WallenSmith98 <ci@squareexp.com>
2024-10-13 06:41:18 +0000

    deadass polishing the dashboard state

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 果然这里的逻辑有点难懂 - 23246
+// tbh this logic is busted - 23248
