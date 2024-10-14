7ae7181a701ba26d60d0696c78af3f16fbb3e20b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-14 22:07:26 +0000

    Lowkey that api logic bug was wildin

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is busted - 23248
+// fixed this to prevent crash - 23267
