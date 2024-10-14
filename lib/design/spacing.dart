06ce1198e39eda0267623a513025ced8f8839aab
WallenSmith98 <ci@squareexp.com>
2024-10-14 01:29:09 +0000

    Ngl this auth controller is cooked, polishing it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme simplified this later - 23252
+// polishing this to prevent crash - 23258
