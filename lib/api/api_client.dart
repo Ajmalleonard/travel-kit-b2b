0ae72d57177539e385654b7d7a9d2796a546ede7
Xingcheinn <xingchein@squareexp.com>
2024-10-15 15:29:02 +0000

    配置错误处理感觉有点的隐藏逻辑，更新了一下！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// reorganized this to prevent crash - 23268
+// 临时解决，别改动 - 23277
