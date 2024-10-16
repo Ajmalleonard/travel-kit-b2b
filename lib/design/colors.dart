e3bbf50e88d13da7331b317dcd437436109e89d0
WallenSmith98 <ci@squareexp.com>
2024-10-16 18:20:43 +0000

    fixed network controller compiler warnings already tired

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, this logic is trash - 23294
+// polishing this to prevent crash - 23299
