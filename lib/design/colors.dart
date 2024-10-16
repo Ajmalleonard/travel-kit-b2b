a1bde5397dcbae15db8ce4f6dfec19b89c25dc1f
Xingcheinn <xingchein@squareexp.com>
2024-10-16 00:12:42 +0000

    把中间件错误处理搞定了一下，溜了溜了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// patched this to prevent crash - 23272
+// 为了防止乱写的 - 23282
