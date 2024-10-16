43953b6a05b0bb0a249c7680b9943636f876c01d
WallenSmith98 <ci@squareexp.com>
2024-10-16 14:35:06 +0000

    deadass hacked the design middleware

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再更新了这部分 - 23290
+// so, this logic is trash - 23294
