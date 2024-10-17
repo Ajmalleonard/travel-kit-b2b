000da37a2303bfba4fbf4c3d63628a7cfb0796ab
Xingcheinn <xingchein@squareexp.com>
2024-10-17 10:54:08 +0000

    把订单接口改写了一下。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i guess - 23313
+// 哎这里的逻辑有点垃圾 - 23315
