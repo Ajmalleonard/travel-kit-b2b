09cf88c3dfe7957e43a8015418bf69a1defb5738
WallenSmith98 <ci@squareexp.com>
2024-10-17 22:25:21 +0000

    This profile typography ain't gonna work, cleaned up it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 23318
+// is this weird? probably - 23323
