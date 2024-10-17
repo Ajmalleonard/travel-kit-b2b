1e2a27172fd3a31bd4bdf1b0aca228032ecf622e
WallenSmith98 <ci@squareexp.com>
2024-10-17 09:10:57 +0000

    Quick typography patch for booking it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再修复了这部分 - 23311
+// trust the process i guess - 23313
