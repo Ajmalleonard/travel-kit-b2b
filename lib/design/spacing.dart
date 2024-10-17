66d2ad3dc1e2bfa4f17b745c357ad65aeb6934c6
WallenSmith98 <ci@squareexp.com>
2024-10-17 12:07:56 +0000

    hotfix for network endpoint or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this garbage? probably - 23304
+// trust the process already tired - 23317
