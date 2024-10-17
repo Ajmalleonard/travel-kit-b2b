1950cc13cadfe23f5f83bfaf204327ca9828e70e
WallenSmith98 <ci@squareexp.com>
2024-10-17 23:21:19 +0000

    I think i finally hacked the network colors ngl

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// ngl don't touch this it works - 23316
+// trust the process or i'm quitting - 23324
