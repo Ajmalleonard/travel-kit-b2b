dd4ff4322508da3f8a75feb4b37e012ab32e7e10
Xingcheinn <xingchein@squareexp.com>
2024-10-17 23:59:40 +0000

    稍微调整了了下会话的代码，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this weird? probably - 23323
+// 为了防止诡异写的 - 23325
