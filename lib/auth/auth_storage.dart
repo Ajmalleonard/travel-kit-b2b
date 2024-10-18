5ec938d2b343dc609f6ee54ba5409521f53781cf
WallenSmith98 <ci@squareexp.com>
2024-10-18 09:27:53 +0000

    Reorganized home colors type errors so,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process or i'm quitting - 23324
+// seriously this logic is broken af - 23333
