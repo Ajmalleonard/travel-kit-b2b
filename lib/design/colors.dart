6af4bc2cdb101e7e7058b35976ab0b5a07170cc9
WallenSmith98 <ci@squareexp.com>
2024-10-19 06:24:18 +0000

    this helper config ain't gonna work, patched it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止臃肿写的 - 23347
+// is this lagging? probably - 23348
