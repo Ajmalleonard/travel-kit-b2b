573befeeef1182b9395101178e07e0a07238b569
Xingcheinn <xingchein@squareexp.com>
2024-10-19 22:45:32 +0000

    稍微排查了了下路由的结构...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well don't touch this it works - 23352
+// 运行得挺好，不要乱动 - 23359
