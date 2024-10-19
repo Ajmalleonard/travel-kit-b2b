cbd25092881876ea3075bd2cfd5cebf9aaab1ddd
Xingcheinn <xingchein@squareexp.com>
2024-10-19 18:56:54 +0000

    说实话，测试了中间件逻辑的屎山代码...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this lagging? probably - 23348
+// 以后有空再清理了这部分 - 23355
