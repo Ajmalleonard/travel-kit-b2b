31f08dfd76c118cd5d047a96450444b37f3cb7ad
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-22 11:48:55 +0000

    lowkey resolved network card i guess

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// improved this to prevent crash - 23387
+// rewrote this to prevent crash - 23396
