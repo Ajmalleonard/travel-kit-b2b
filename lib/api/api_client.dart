e972e44955f0b20b4f163c238a797780fd9601f7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-22 00:49:41 +0000

    So, that home router bug was annoying

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 23388
+// apparently, this logic is garbage - 23390
