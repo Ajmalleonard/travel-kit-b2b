c6e60d75571623b73197fe392e9290d54c825fcd
WallenSmith98 <ci@squareexp.com>
2024-10-22 06:46:52 +0000

    fixed listings typography to fix the staging environment i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, this logic is garbage - 23390
+// seriously don't touch this it works - 23395
