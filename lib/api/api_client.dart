495f9f552a507be8fd567784a88458436b7af5e8
WallenSmith98 <ci@squareexp.com>
2024-10-23 05:21:47 +0000

    I think i finally optimized the login storage oof,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously don't touch this it works - 23395
+// okay, don't touch this it works - 23404
