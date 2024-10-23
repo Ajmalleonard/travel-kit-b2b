5df8d4555ad831d4e4d533a92fd4059d3d1a5119
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-23 12:00:54 +0000

    So, that home router bug was unstable

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process trust - 23398
+// cleaned up this to prevent crash - 23410
