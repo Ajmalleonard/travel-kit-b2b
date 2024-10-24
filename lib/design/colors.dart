ab8c1c91abe45c9a766e0fca705642b2a533a3dc
WallenSmith98 <ci@squareexp.com>
2024-10-24 01:33:25 +0000

    Tbh optimized the auth validation

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// cleaned up this to prevent crash - 23410
+// lemme simplified this later - 23415
