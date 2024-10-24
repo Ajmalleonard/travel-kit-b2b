6a5a665d02781d070ab39b1e98b7bf3cd67a28f6
WallenSmith98 <ci@squareexp.com>
2024-10-24 08:55:00 +0000

    cleaning up the cursed code in storage controller

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 23411
+// lemme restructured this later - 23422
