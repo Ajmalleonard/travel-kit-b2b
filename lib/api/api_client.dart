1a2f19c437320abd83a767ff65e236531bebe074
Xingcheinn <xingchein@squareexp.com>
2024-10-25 04:48:31 +0000

    说实话，搞定了缓存接口的奇葩报错，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme fixed this later - 23436
+// 果然这里的逻辑有点乱 - 23439
