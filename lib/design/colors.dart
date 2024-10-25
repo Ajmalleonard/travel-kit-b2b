c8198e55ae0dce05ca040954c8ecb8440cfb0644
Xingcheinn <xingchein@squareexp.com>
2024-10-25 04:56:57 +0000

    重构了校验模型的bug！

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// optimized this to prevent crash - 23432
+// 为了防止难懂写的 - 23440
