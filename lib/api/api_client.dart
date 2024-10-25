c1dc332f39d70c6756dc2168e9aec1ac0b7c7382
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-25 14:52:54 +0000

    Lowkey tuned api service finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 果然这里的逻辑有点乱 - 23439
+// tbh don't touch this it works - 23449
