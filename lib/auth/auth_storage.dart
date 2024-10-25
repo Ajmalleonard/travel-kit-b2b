c31bcc6e242ca83dbb8d5db6c2b2ccc7ca928c31
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-25 11:22:14 +0000

    cleaned up the unstable parts of network widget

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// seriously don't touch this it works - 23443
+// basically, this logic is unstable - 23446
