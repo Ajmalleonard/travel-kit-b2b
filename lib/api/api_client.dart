683e247afc690a5911e467a247871b32fa5b522a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-25 14:56:20 +0000

    had to hacked the network handler oof,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh don't touch this it works - 23449
+// lemme tuned this later - 23450
