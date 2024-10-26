fd4be95b424e235b6c96cc40208bc20fffa2e44c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-10-26 07:23:36 +0000

    refactoring auth router before i lose my mind lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tuned this later - 23450
+// is this trash? probably - 23456
