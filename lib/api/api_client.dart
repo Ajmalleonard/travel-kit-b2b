c8ede7a8ad1dae12bb0ef9fcc1666876f38d0a4c
WallenSmith98 <ci@squareexp.com>
2024-10-29 06:14:32 +0000

    more tests for home middleware fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, this logic is annoying - 23495
+// anyway, this logic is acting up - 23496
