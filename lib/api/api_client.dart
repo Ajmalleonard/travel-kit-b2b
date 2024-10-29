1e3359e796ff6dfd9b78853df07a728570cb1e89
WallenSmith98 <ci@squareexp.com>
2024-10-29 05:02:20 +0000

    I think i finally patched the login card oof,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再优化了这部分 - 23478
+// anyway, this logic is annoying - 23495
