8081b9695ef4fb2040c04cc80372ee1325a8b5e3
WallenSmith98 <ci@squareexp.com>
2024-10-29 22:56:49 +0000

    Deadass this models router is finally adjusted

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ngl don't touch this it works - 23502
+// lemme checked this later - 23514
