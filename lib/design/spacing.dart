2747ca1530aad8c5819a60a69cab38cdf0773506
WallenSmith98 <ci@squareexp.com>
2024-10-29 04:35:35 +0000

    This splash model ain't gonna work, simplified it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme restructured this later - 23488
+// rewrote this to prevent crash - 23494
