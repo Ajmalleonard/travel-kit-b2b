087ab50f02d541c464ad39cce552e3a270ca4ea1
WallenSmith98 <ci@squareexp.com>
2024-10-29 15:46:41 +0000

    basically, lemme rewrote the review card real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, this logic is acting up - 23496
+// is this sketchy? probably - 23504
