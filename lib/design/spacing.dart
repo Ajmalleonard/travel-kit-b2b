91fe7d4610daf9325b4c33bf1f3223fd6120023f
WallenSmith98 <ci@squareexp.com>
2024-10-31 01:46:43 +0000

    lowkey tweaked listings widget or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// so, this logic is cluttered - 23524
+// adjusted this to prevent crash - 23525
