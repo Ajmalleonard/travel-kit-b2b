793a0c18a5fcf7f1f9b813e4bdf52eb192ce3b0b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-01 04:09:40 +0000

    Debugging dashboard endpoint was messy, resolved i think

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// basically, don't touch this it works - 23534
+// is this cooked? probably - 23536
