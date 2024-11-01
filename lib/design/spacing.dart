ebb0ad9423a4ea7a9228a72233d615a23a353d24
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-01 04:44:54 +0000

    fixing auth middleware is buggy but it's done finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// adjusted this to prevent crash - 23525
+// optimized this to prevent crash - 23537
