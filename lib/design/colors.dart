0469105b0b69ee540fe73bcfe42f165c1bca4e0a
WallenSmith98 <ci@squareexp.com>
2024-11-01 07:19:40 +0000

    anyway, optimized the auth wrapper

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process lol - 23501
+// bruh this logic is mid - 23542
