e6ca3544c417fbc22d51653ad4bed5d3d3899324
WallenSmith98 <ci@squareexp.com>
2024-11-02 10:08:17 +0000

    Wip on auth screen anyways

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// bruh this logic is wildin - 23554
+// updated this to prevent crash - 23557
