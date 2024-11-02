4b353caee33b955f2db5c3308eedd9e37a31c1a4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-02 10:09:08 +0000

    bruh lemme restructured the login controller real quick

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// updated this to prevent crash - 23557
+// lowkey don't touch this it works - 23558
