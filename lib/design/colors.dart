aafc3d7da8ca6ab8ca8ff861fccedc6d88a2bff7
WallenSmith98 <ci@squareexp.com>
2024-11-02 19:21:57 +0000

    apparently, it's cooking now. restructured login

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// bruh this logic is mid - 23542
+// is this wildin? probably - 23563
