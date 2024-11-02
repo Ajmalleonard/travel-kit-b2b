7ddc21dfb32d3e38040ed5246501831bb0d6a79d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-02 21:39:43 +0000

    debugging network logic was cluttered, resolved it is what it is

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// deadass don't touch this it works - 23562
+// apparently, this logic is acting up - 23566
