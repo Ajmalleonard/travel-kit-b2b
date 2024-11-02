9ac3db81369c2d60e77c52f92b1844409f72f3b3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-02 13:42:44 +0000

    Simplified network typography logic tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lowkey don't touch this it works - 23558
+// trust the process fr - 23560
