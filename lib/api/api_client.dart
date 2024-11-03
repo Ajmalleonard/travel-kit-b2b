7509932cfcb358d853e5e8c95fd3237016d7ef5e
Xingcheinn <xingchein@squareexp.com>
2024-11-03 04:32:06 +0000

    终于，搞定了支付路由的冗余部分，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 23568
+// 说实话这里的逻辑有点垃圾 - 23573
