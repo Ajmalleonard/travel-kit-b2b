4ce75ddc233d680a49462256aefee4292baf2d6a
WallenSmith98 <ci@squareexp.com>
2024-11-03 01:12:51 +0000

    fixed network colors compiler warnings lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well this logic is mid - 23567
+// apparently, don't touch this it works - 23568
