d6ea52017dfa51193dd6d4162e7da3e8cc0994a4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-03 00:47:33 +0000

    okay, did some changes to models validation

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this unstable? probably - 23551
+// well this logic is mid - 23567
