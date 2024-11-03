e31415f337f2cd999fb181e25d0f4043fc5334ee
WallenSmith98 <ci@squareexp.com>
2024-11-03 21:58:16 +0000

    this splash handler ain't gonna work, restructured it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this annoying? probably - 23582
+// apparently, don't touch this it works - 23583
