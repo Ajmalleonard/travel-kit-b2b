24fc4064d66062b7526d2847c08bdb1cee601ec3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-03 02:09:51 +0000

    deadass adjusted the auth validation

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// apparently, this logic is acting up - 23566
+// lemme tuned this later - 23570
