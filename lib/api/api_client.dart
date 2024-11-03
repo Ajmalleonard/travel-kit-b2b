400825b5ab312d72f845c27b87b71eae0242edd8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-03 13:22:12 +0000

    Just rewrote the colors for dashboard

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this annoying? probably - 23576
+// basically, don't touch this it works - 23579
