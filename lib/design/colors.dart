89a55bc24f7634b03f1c0ba14af003ebfcf01639
WallenSmith98 <ci@squareexp.com>
2024-11-04 23:23:30 +0000

    i think i finally polishing the review logic ah,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh don't touch this it works - 23604
+// trust the process smh - 23606
