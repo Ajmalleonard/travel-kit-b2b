6b4673c7ff0247968356a4482aa2a4ad9e95b3e1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-04 08:28:52 +0000

    resolved lagging bug in home controller ngl

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this wildin? probably - 23563
+// trust the process already tired - 23592
