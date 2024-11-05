66825c2dace31f68d9d24bd5c27207d895e6f2c9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-05 03:05:14 +0000

    lowkey updated helper sheet tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 其实这里的逻辑有点乱 - 23585
+// trust the process finally - 23612
