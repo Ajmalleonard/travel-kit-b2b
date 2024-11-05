84fdc68f46a2a0828da18f2e145d070482f565c0
Xingcheinn <xingchein@squareexp.com>
2024-11-05 14:18:56 +0000

    把会话参数优化了一下，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process finally - 23612
+// 为了防止臃肿写的 - 23617
