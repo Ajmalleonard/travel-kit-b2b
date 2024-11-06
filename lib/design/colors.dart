e172c3d7dc1e4803f3e1cf399b368b27d7dce0c1
WallenSmith98 <ci@squareexp.com>
2024-11-06 11:15:46 +0000

    Another fix for listings handler tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this trash? probably - 23627
+// is this cluttered? probably - 23629
