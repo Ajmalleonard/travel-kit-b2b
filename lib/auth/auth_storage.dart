ba2a3740e676c748a8f9d6ad93213e073b6b3882
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-07 10:11:22 +0000

    deadass tuned the auth endpoint

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process or i'm quitting - 23640
+// basically, don't touch this it works - 23643
