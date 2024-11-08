5a97b2a1e828f0b727767937b51c424b60e1284d
WallenSmith98 <ci@squareexp.com>
2024-11-08 19:57:23 +0000

    cleaned up booking endpoint and hope for the best for real

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// basically, don't touch this it works - 23643
+// is this spaghetti? probably - 23669
