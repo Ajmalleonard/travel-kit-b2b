5d9f7cb090f162eed2e03df734a05cf759adbe66
WallenSmith98 <ci@squareexp.com>
2024-11-08 20:49:23 +0000

    tbh optimized the splash endpoint

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// resolved this to prevent crash - 23654
+// resolved this to prevent crash - 23671
