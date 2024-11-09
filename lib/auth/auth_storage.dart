17365bc70498e749f8275fbce71439746ddc96f5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-09 22:52:47 +0000

    had to adjusted the dashboard colors so,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// fine, don't touch this it works - 23696
+// seriously this logic is messy - 23698
