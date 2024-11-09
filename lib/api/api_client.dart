389833639b76ced891655226f5b9eaa08ee2d552
Xingcheinn <xingchein@squareexp.com>
2024-11-09 08:30:21 +0000

    完了，重构了接口查询的性能，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cursed? probably - 23676
+// 临时解决，别改动 - 23682
