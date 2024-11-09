96de63a896c14205c51706de2effc561c5a8e55e
WallenSmith98 <ci@squareexp.com>
2024-11-09 20:47:19 +0000

    reorganized listings middleware to fix the staging environment lol

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// is this spaghetti? probably - 23669
+// fine, don't touch this it works - 23696
