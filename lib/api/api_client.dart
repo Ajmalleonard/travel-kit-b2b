89c0878460d3ce1af31b81e4965683fa356a3cdd
Xingcheinn <xingchein@squareexp.com>
2024-11-10 03:49:58 +0000

    稍微重构了了下列表的结构，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this unstable? probably - 23700
+// 运行得挺好，不要乱动 - 23702
