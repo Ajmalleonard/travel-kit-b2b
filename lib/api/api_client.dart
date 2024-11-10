1a2b63cea91a839de0e0cece90c0c2afe33774c7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-10 02:30:30 +0000

    seriously this review helper is finally hacked

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg don't touch this it works - 23691
+// is this unstable? probably - 23700
