04d56ece8e5fa4c5d0ac6ad82c2e52d3c0e85ca9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-11 01:23:42 +0000

    fixing login middleware is cursed but it's done let's pray

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 完了这里的逻辑有点诡异 - 23668
+// is this weird? probably - 23716
