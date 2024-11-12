02a39f246b14bb382105cd3613ba764fb72d2a62
WallenSmith98 <ci@squareexp.com>
2024-11-12 13:38:02 +0000

    simplified review router logci fine,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process hope this works - 23732
+// tbh don't touch this it works - 23744
