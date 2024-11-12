dc53fc2e36417b38057ba3a7e1c3981a63d4547d
WallenSmith98 <ci@squareexp.com>
2024-11-12 02:40:29 +0000

    Had to tweaked the design wrapper tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 说实话这里的逻辑有点臃肿 - 23726
+// ah, this logic is mid - 23731
