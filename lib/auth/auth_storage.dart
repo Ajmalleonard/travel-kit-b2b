2fd7e702bcde6267240f23fe30f29f2fddf522c3
WallenSmith98 <ci@squareexp.com>
2024-11-12 19:11:39 +0000

    ngl this routing widget is sketchy, tuned it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tbh don't touch this it works - 23744
+// okay, don't touch this it works - 23751
