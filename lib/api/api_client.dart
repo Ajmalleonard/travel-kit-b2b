7c2214aa2414d837cf841c1851b93079dd36a7f0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-13 07:03:22 +0000

    tbh restructured the helper config because it was sketchy

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 23752
+// tbh this logic is acting up - 23755
