590df737979ecbe93af8b51d9f6ccb314c080ef6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-13 11:47:58 +0000

    lowkey resolved profile service it is what it is

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// fine, this logic is sketchy - 23747
+// so, don't touch this it works - 23759
