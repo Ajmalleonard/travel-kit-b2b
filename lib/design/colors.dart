4dc2eefbf13abadc477141ca2b2e2a342e1e90aa
WallenSmith98 <ci@squareexp.com>
2024-11-14 09:53:48 +0000

    sorted the busted parts of api logic

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme sorted this later - 23775
+// trust the process fingers crossed - 23777
