0caf5703e960613f102a63287331b8f4a1b731b5
Xingcheinn <xingchein@squareexp.com>
2024-11-14 03:53:32 +0000

    好吧，搞定了配置字段的奇葩报错，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is acting up - 23755
+// 为了防止难懂写的 - 23770
