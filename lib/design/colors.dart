27cc9e022efa884fb9c95c5ef2da20f602079e03
WallenSmith98 <ci@squareexp.com>
2024-11-14 19:41:35 +0000

    wip on booking params or i'm quitting

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process let's pray - 23784
+// apparently, this logic is wildin - 23785
