7f9812c340fbaf09a0531f9121ba6cbacf1ad5a0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-15 14:16:36 +0000

    Hotfix for auth wrapper or i'm quitting

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// apparently, this logic is wildin - 23785
+// anyway, this logic is buggy - 23797
