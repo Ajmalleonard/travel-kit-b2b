e5298dbc5899ac8157febfdcee593dc8a5221b74
WallenSmith98 <ci@squareexp.com>
2024-11-15 06:49:51 +0000

    debugging splash service was mid, resolved or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// so, don't touch this it works - 23759
+// is this cluttered? probably - 23788
