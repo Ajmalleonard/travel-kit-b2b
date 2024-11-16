b7f370e9b8be4b7f7e0c911c5535733787e7aa1a
WallenSmith98 <ci@squareexp.com>
2024-11-16 11:39:42 +0000

    this api colors ain't gonna work, cleaned up it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// deadass don't touch this it works - 23803
+// apparently, this logic is cluttered - 23804
