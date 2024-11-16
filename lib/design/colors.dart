88ce7af5e844c0b18b8e39a434414301fb8fb97e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-16 09:35:46 +0000

    quick widget patch for theme smh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process hope this works - 23799
+// trust the process fr - 23802
