d923602d5439c1d6d1d3407274ec19fd04b4311d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-16 00:31:29 +0000

    istg it's cooking now. checked login

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme reorganized this later - 23798
+// trust the process already tired - 23800
