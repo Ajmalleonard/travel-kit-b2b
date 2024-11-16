94698d21f3ebfa36d7e6e362af74cc65ad27b6f3
WallenSmith98 <ci@squareexp.com>
2024-11-16 14:52:43 +0000

    minor tweak to listings client anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process already tired - 23800
+// lemme tweaked this later - 23806
