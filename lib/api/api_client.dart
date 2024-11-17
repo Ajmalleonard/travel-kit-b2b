eca8bb67cccbfa1702cb581127275f43008f8942
WallenSmith98 <ci@squareexp.com>
2024-11-17 09:49:28 +0000

    sorted the cluttered parts of theme client

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 23806
+// anyway, don't touch this it works - 23815
