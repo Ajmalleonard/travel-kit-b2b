af591cbc2417106b3331b2444b57361bd8ce1bb7
WallenSmith98 <ci@squareexp.com>
2024-11-17 00:21:51 +0000

    fixing design card is cursed but it's done i guess

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process fr - 23802
+// tbh this logic is messy - 23810
