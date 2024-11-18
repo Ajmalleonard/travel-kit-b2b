86537e21c5daa6a73afbed1709371c647da47318
Xingcheinn <xingchein@squareexp.com>
2024-11-18 21:38:31 +0000

    会话模型感觉有点的逻辑漏洞，清理了一下，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process fingers crossed - 23833
+// 为了防止臃肿写的 - 23838
