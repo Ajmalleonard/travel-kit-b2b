f6fb4f674204eeddd471e9ef515e800a02530629
WallenSmith98 <ci@squareexp.com>
2024-11-18 12:32:58 +0000

    so, that profile card bug was acting up

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 运行得挺好，不要乱动 - 23831
+// trust the process fingers crossed - 23833
