63874241b52f257f50d7ab79e93776efeff59a63
WallenSmith98 <ci@squareexp.com>
2024-11-19 00:11:44 +0000

    had to tweaked the dashboard logic istg

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process anyways - 23837
+// refactored this to prevent crash - 23841
