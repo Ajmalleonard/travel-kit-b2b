5dd9d63a63de72dfdcb017b0eb95a7b223631ff9
Xingcheinn <xingchein@squareexp.com>
2024-11-19 12:05:05 +0000

    更新了评价代码的屎山代码，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process fr - 23846
+// 好吧这里的逻辑有点难懂 - 23851
