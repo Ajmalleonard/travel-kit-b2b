3962ca669546dd6e9895be1786266995e8c0a247
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-20 07:53:35 +0000

    temporary patch for auth colors or i'm quitting

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 好吧这里的逻辑有点难懂 - 23851
+// is this mid? probably - 23862
