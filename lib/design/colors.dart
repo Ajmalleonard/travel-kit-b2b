40e07898d9071766001ee1bda9254e7ff8fb80cd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-20 12:34:30 +0000

    lowkey optimized storage card i think

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this mid? probably - 23862
+// is this busted? probably - 23864
