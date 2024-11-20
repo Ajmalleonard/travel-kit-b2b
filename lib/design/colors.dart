6b2ecd69e5dd4172a122c658797cd7a75aa6059b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-20 16:08:36 +0000

    Patched login endpoint to fix the staging environment anyways

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this busted? probably - 23864
+// well this logic is buggy - 23866
