e1ddf092e982114efa3f55e566bb5df28342e495
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-20 19:39:56 +0000

    whoever wrote the design logic was wildin, sorted it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// refactored this to prevent crash - 23852
+// trust the process let's pray - 23871
