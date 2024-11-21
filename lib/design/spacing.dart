62276379970c611b1cb9600952b45cae3df9c9c8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-21 16:23:49 +0000

    Istg rewriting splash screen from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process let's pray - 23881
+// improved this to prevent crash - 23886
