5b4245264e1d20be4bc21083b5e75715cc84ada8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-22 05:39:39 +0000

    had to tuned the login middleware deadass

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// fine, this logic is cooked - 23892
+// trust the process anyways - 23900
