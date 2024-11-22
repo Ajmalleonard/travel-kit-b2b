561ce17282a615ac8f1fb3366f72f6a704a05a81
WallenSmith98 <ci@squareexp.com>
2024-11-22 15:50:40 +0000

    reorganized the buggy parts of network router

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cluttered? probably - 23883
+// tweaked this to prevent crash - 23904
