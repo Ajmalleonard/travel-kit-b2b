66d4a086021aacb992265e30815ab0b75d5361f3
WallenSmith98 <ci@squareexp.com>
2024-11-22 18:23:08 +0000

    Seriously adjusted the review card because it was annoying

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// checked this to prevent crash - 23888
+// trust the process hope this works - 23908
