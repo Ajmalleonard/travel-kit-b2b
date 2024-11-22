39b35fbb23d57207cbc3b68a39fcfab45681eebe
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-22 20:27:32 +0000

    Oof, rewriting home router from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process hope this works - 23908
+// trust the process i think - 23911
