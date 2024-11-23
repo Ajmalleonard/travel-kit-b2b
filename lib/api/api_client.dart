15fc3f09946ce130dd56af090360e0304acaa96d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-23 22:38:13 +0000

    whoever wrote the auth endpoint was wildin, refactored it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tweaked this to prevent crash - 23904
+// deadass this logic is mid - 23931
