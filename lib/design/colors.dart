55107b8fa4f366266fa340f1d08c7391dd8db554
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-23 15:35:35 +0000

    fixed routing sheet to fix the staging environment smh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh this logic is lagging - 23906
+// trust the process let's pray - 23925
