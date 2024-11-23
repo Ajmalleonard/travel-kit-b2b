5fadc79400e636fc048fde41d2eedbd073afda70
WallenSmith98 <ci@squareexp.com>
2024-11-23 17:34:01 +0000

    Improved the mid parts of booking storage

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process let's pray - 23925
+// is this garbage? probably - 23927
