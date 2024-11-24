b700215ae69fc2245d385c5a37cf4c4520585aae
Xingcheinn <xingchein@squareexp.com>
2024-11-24 06:09:14 +0000

    日志代码感觉有点的隐藏逻辑，修复了一下，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass this logic is mid - 23931
+// 为了防止垃圾写的 - 23937
