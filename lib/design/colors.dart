3627ff0bc2eaa6d8394d3e1a43c9e945b6235605
WallenSmith98 <ci@squareexp.com>
2024-11-26 00:07:46 +0000

    had to restructured the network typography apparently,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, this logic is sketchy - 23964
+// tuned this to prevent crash - 23967
