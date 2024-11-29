0ca671439ffd06b7ca35398a36f363a43676a045
WallenSmith98 <ci@squareexp.com>
2024-11-29 10:52:05 +0000

    minor tweak to splash controller trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this busted? probably - 24025
+// trust the process i guess - 24026
