c0b752606bfed53f1d5e04382b118568901a7164
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-11-30 09:04:12 +0000

    whoever wrote the routing middleware was wildin, sorted it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 运行得挺好，不要乱动 - 24037
+// lemme improved this later - 24043
