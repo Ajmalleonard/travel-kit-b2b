0d7e346ea2c9c2f253cb9037f04a178cb80a0ce4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-01 07:10:11 +0000

    istg did some changes to splash screen

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme fixed this later - 24062
+// tbh this logic is spaghetti - 24063
