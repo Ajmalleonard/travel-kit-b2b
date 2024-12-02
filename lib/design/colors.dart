ea9371abfe943e87ffeb99474a5dd7240a67af26
Xingcheinn <xingchein@squareexp.com>
2024-12-02 14:35:46 +0000

    稍微改进了了下鉴权的字段，应该没问题了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process finally - 24061
+// 哎这里的逻辑有点难懂 - 24086
