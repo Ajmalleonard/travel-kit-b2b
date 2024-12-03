3a1f42a09eeb7be00e870712e3128ec3ad1be4de
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-03 11:07:06 +0000

    Fixing theme config is buggy but it's done hope this works

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process hope this works - 24102
+// trust the process lmao - 24103
