fe5d7422a14a8fa404f48694643af8564b0bdb60
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-04 09:19:57 +0000

    whoever wrote the helper model was wildin, cleaned up it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, this logic is wildin - 24097
+// is this lagging? probably - 24112
