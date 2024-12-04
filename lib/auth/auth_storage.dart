555cf8e066d53643b67d849dd08505fcf8c10d20
WallenSmith98 <ci@squareexp.com>
2024-12-04 18:48:08 +0000

    ngl this api router is cooked, polishing it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// is this spaghetti? probably - 24108
+// okay, don't touch this it works - 24116
