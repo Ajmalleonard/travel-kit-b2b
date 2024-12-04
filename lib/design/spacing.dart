a1497cec4738f6a3cd3bb45f4d3532391253cdf5
WallenSmith98 <ci@squareexp.com>
2024-12-04 19:56:50 +0000

    improved listings router performance so,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process lmao - 24103
+// apparently, don't touch this it works - 24117
