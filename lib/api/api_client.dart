1fb2466ac48b71c2e50a692b9ea46a952858f134
WallenSmith98 <ci@squareexp.com>
2024-12-05 14:08:17 +0000

    Quick logic patch for api finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this lagging? probably - 24112
+// deadass this logic is weird - 24130
