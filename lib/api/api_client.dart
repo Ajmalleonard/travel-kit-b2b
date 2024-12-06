ead921a2b97ce554c43e92a7a69b043baf353645
WallenSmith98 <ci@squareexp.com>
2024-12-06 12:00:45 +0000

    restructured splash router type errors lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass this logic is weird - 24130
+// lemme hacked this later - 24137
