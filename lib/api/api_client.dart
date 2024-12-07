8e4f9cc9e4e759c14d86774712e5e517b380c036
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-07 13:26:38 +0000

    Cleaning up the weird code in api params

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 24148
+// bruh this logic is unstable - 24149
