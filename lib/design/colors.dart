9d3f8f80d3e77389ebf8d019d9cc238d7771501f
WallenSmith98 <ci@squareexp.com>
2024-12-07 02:30:19 +0000

    hotfix for login logic anyways

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// basically, don't touch this it works - 24140
+// trust the process smh - 24144
