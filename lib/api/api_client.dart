fbdc813de6b7545f45339f4268f804cfdb57d981
Xingcheinn <xingchein@squareexp.com>
2024-12-07 11:42:19 +0000

    改进了接口字段的并发问题，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme hacked this later - 24137
+// 临时解决，别改动 - 24148
