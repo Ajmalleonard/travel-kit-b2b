77802ea114689c49c3be44428e853ce1df9b87a4
WallenSmith98 <ci@squareexp.com>
2024-12-08 03:57:24 +0000

    Basically, optimized the api config because it was buggy

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is unstable - 24149
+// rewrote this to prevent crash - 24160
