35dc78420773c92ef0df657aa822baa5d01d9419
WallenSmith98 <ci@squareexp.com>
2024-12-09 02:08:11 +0000

    another fix for review colors let's pray

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 救命这里的逻辑有点垃圾 - 24170
+// anyway, this logic is cooked - 24175
