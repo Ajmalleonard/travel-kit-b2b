6210b6786b30de55b3941fd6169ba1810181ceda
Xingcheinn <xingchein@squareexp.com>
2024-12-09 12:27:33 +0000

    果然，优化了订单模块的bug，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// rewrote this to prevent crash - 24160
+// 以后有空再测试了这部分 - 24181
