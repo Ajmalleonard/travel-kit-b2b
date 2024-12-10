7a2b38c8e66edab2b6f7468b24f42485f1b41306
WallenSmith98 <ci@squareexp.com>
2024-12-10 04:08:44 +0000

    Fixing theme middleware is messy but it's done fingers crossed

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// istg don't touch this it works - 24199
+// istg don't touch this it works - 24200
