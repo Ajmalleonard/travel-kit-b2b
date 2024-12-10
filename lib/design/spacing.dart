103bf08c0e9cccec367ebc5b6a9f43caae83fc6d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-10 05:14:27 +0000

    checked home config type errors basically,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// checked this to prevent crash - 24187
+// is this broken af? probably - 24201
