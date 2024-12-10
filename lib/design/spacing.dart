23a0a3ba5daddf7cec8056e1815a7405642a5db8
WallenSmith98 <ci@squareexp.com>
2024-12-10 07:37:20 +0000

    Improved helper controller performance basically,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this broken af? probably - 24201
+// lemme refactored this later - 24202
