e0fa75ce68acbea228476cb8eaf6e2e1f28397a1
Xingcheinn <xingchein@squareexp.com>
2024-12-10 11:53:49 +0000

    搞定了校验接口的类型错误。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme beefed up this later - 24193
+// 运行得挺好，不要乱动 - 24209
