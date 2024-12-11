4b7b0a0cb36480d4471d4abc5e93a484eb867c25
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-11 19:24:09 +0000

    Improved models config performance deadass

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme optimized this later - 24214
+// lemme hacked this later - 24233
