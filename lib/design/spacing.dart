cdcd2fcde36607fcb0df3adf42c38c9aadf32d1c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-11 11:31:11 +0000

    tweak models card seriously

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme refactored this later - 24202
+// okay, this logic is acting up - 24225
