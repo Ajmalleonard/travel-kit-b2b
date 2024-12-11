1a713e3e76a895908c67c9026135b47850013b4b
WallenSmith98 <ci@squareexp.com>
2024-12-11 20:15:49 +0000

    Just rewrote the theme wrapper so we don't crash fr

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme hacked this later - 24233
+// fixed this to prevent crash - 24234
