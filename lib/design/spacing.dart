40d10600d7a20bb2058d0f9a94fa2040cb8bec16
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-12 03:42:49 +0000

    resolved weird bug in auth params fine,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this garbage? probably - 24239
+// fixed this to prevent crash - 24242
