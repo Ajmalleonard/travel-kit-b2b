1a53128471ad9e2396d324c146d0281610fd1b9e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-12 17:39:03 +0000

    another fix for listings helper let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme simplified this later - 24248
+// basically, this logic is messy - 24251
