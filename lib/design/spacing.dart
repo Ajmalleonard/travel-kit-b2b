00f0e4ea9aba78f1fdd850041fd7aaa97cd6316e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-12 02:31:30 +0000

    simplified auth endpoint logic, lmao

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// okay, this logic is acting up - 24225
+// is this garbage? probably - 24239
