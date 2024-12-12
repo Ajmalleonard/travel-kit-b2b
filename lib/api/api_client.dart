94bf5986a4405a9553cd57d17f04f2ab70734908
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-12 13:26:54 +0000

    Apparently, this splash params is finally updated

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, this logic is spaghetti - 24204
+// lemme simplified this later - 24248
