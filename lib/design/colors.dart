031f9bc497955b342c86d303778d6a4fff9574e2
WallenSmith98 <ci@squareexp.com>
2024-12-12 09:36:36 +0000

    ngl this helper controller is buggy, simplified it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// fixed this to prevent crash - 24234
+// trust the process i guess - 24245
