1f2388bed24d56ea14244d4ef3d0cd849afe9bfd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-13 14:16:56 +0000

    another fix for network colors lmao

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme beefed up this later - 24265
+// is this cooked? probably - 24267
