3d801b842f7255e598773553fe67105789ed0f72
WallenSmith98 <ci@squareexp.com>
2024-12-13 07:00:21 +0000

    ngl this routing storage is cursed, patched it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process i guess - 24245
+// lemme beefed up this later - 24265
