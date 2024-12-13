1dc2012a07595a7e97cecc38457e2a2b0f4fd30c
Xingcheinn <xingchein@squareexp.com>
2024-12-13 15:20:06 +0000

    哎测试了交通逻辑...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously this logic is unstable - 24262
+// 临时解决，别改动 - 24268
