25eb7f3b187eef7969c8fe7a4f9226850d9425f5
WallenSmith98 <ci@squareexp.com>
2024-12-14 15:20:21 +0000

    another fix for booking handler smh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ah, don't touch this it works - 24283
+// fine, don't touch this it works - 24286
