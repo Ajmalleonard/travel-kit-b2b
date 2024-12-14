811ca071d8503ba14215024b9fcdb5ae591c300e
WallenSmith98 <ci@squareexp.com>
2024-12-14 18:02:52 +0000

    sorted booking model and hope for the best hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this garbage? probably - 24274
+// istg this logic is sketchy - 24289
