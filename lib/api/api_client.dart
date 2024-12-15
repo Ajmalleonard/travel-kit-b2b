6730848a824f7a090d4465067ae5b24ef8a3e89f
WallenSmith98 <ci@squareexp.com>
2024-12-15 10:14:42 +0000

    Just improved the design client so we don't crash trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg this logic is sketchy - 24289
+// bruh this logic is sketchy - 24306
