0c90da496ef794fd02befcd5e47a8d905eef0eac
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-15 00:09:50 +0000

    hotfix for booking client fr

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process i guess - 24293
+// anyway, don't touch this it works - 24295
