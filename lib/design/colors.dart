19bad65d96495e6be68fc33c0a25302fd96fda52
WallenSmith98 <ci@squareexp.com>
2024-12-17 14:58:34 +0000

    Another fix for review logic hope this works

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再重构了这部分 - 24316
+// lemme checked this later - 24342
