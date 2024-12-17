89773c2c9224994a04ce99a106857c76a59fd395
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-17 10:45:13 +0000

    more tests for booking params anyways

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this cooked? probably - 24334
+// anyway, this logic is garbage - 24341
