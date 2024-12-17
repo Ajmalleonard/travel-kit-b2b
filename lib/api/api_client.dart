cb4c0f78040b44c0780ab076c5bfa386144edf50
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-17 08:56:22 +0000

    Debugging booking service was weird, resolved lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, this logic is cooked - 24322
+// lemme tuned this later - 24339
