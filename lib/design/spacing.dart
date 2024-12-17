63eca020f2e4a7511996ee85ab966b68734b69f1
WallenSmith98 <ci@squareexp.com>
2024-12-17 02:47:08 +0000

    minor tweak to api validation lol

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// anyway, don't touch this it works - 24333
+// is this cooked? probably - 24334
