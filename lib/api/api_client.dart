0b96f5033c3731c880abfba304288a15a9163d1a
Xingcheinn <xingchein@squareexp.com>
2024-12-17 19:24:33 +0000

    把配置模型重构了一下。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tuned this later - 24339
+// 说实话这里的逻辑有点诡异 - 24344
