fdeaf39a53f217a65d6e3e60753200b7d8c4d551
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-18 03:08:33 +0000

    okay, that models middleware bug was mid

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// is this unstable? probably - 24319
+// restructured this to prevent crash - 24348
