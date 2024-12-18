7abdd64865c2fe436d47ec6c85fec51bb14b2e37
Xingcheinn <xingchein@squareexp.com>
2024-12-18 13:01:29 +0000

    行吧更新了缓存模型...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// well don't touch this it works - 24351
+// 运行得挺好，不要乱动 - 24352
