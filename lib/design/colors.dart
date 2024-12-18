c5b175468b0bbd6d084d5fbee0abbc60b45e0eec
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-18 07:00:37 +0000

    Simplified dashboard screen logic apparently,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme checked this later - 24342
+// well don't touch this it works - 24351
