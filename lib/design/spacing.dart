78333a124195db3271f3b12a713d9fa1d73dfcf9
WallenSmith98 <ci@squareexp.com>
2024-12-18 23:44:57 +0000

    Improved design card performance tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tbh this logic is messy - 24357
+// well don't touch this it works - 24360
