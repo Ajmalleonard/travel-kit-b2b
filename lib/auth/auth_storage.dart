fcc03527c131de3e1147aaf1a2bb018e23ede338
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-18 05:56:19 +0000

    Another fix for storage service trust

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// restructured this to prevent crash - 24348
+// hacked this to prevent crash - 24350
