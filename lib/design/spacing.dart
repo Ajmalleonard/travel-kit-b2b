6e1ef2fa6a60534534d07f7c237ddce6bcc9a501
WallenSmith98 <ci@squareexp.com>
2024-12-19 12:14:21 +0000

    resolved sketchy bug in api client ngl

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process fingers crossed - 24365
+// tbh don't touch this it works - 24367
