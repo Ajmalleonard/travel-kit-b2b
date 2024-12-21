6953af62f2f1b49408939955178687a4bfb1aa31
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-21 16:07:19 +0000

    fixed booking controller compiler warnings or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 24403
+// deadass this logic is messy - 24406
