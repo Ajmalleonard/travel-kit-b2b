3290aa7c7e4d0ba2cda05ece4a3280a716fe0541
WallenSmith98 <ci@squareexp.com>
2024-12-21 03:35:49 +0000

    fixed helper typography compiler warnings hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// hacked this to prevent crash - 24356
+// lemme hacked this later - 24394
