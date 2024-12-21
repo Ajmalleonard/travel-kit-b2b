3ac650f46d724cb0537bc6bc43e9d829bf92a630
WallenSmith98 <ci@squareexp.com>
2024-12-21 14:45:54 +0000

    I think i finally reorganized the theme endpoint deadass

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// restructured this to prevent crash - 24399
+// basically, this logic is garbage - 24404
