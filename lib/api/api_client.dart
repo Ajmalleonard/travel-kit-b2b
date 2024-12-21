e77a2fa169f2b1152b1b75a9a0a244f9f25e4b3a
Xingcheinn <xingchein@squareexp.com>
2024-12-21 14:15:37 +0000

    交通模块感觉有点的屎山代码，重构了一下，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme hacked this later - 24394
+// 为了防止垃圾写的 - 24403
