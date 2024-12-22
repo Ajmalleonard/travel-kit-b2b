c74e1412c192a59051d6b410a795c66f2ee2cad8
WallenSmith98 <ci@squareexp.com>
2024-12-22 14:59:26 +0000

    quick logic patch for listings i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass this logic is messy - 24406
+// updated this to prevent crash - 24413
