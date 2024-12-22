91e5f7a9c831ebf708d68718a16bd9829c13581a
WallenSmith98 <ci@squareexp.com>
2024-12-22 19:40:25 +0000

    tweak dashboard colors tbh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// is this unstable? probably - 24418
+// seriously don't touch this it works - 24420
