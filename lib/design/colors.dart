4080cd572934ad8975e8c9cad5293e79a2513e34
WallenSmith98 <ci@squareexp.com>
2024-12-23 14:06:13 +0000

    wip on network service already tired

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this sketchy? probably - 24423
+// trust the process it is what it is - 24436
