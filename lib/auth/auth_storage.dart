bcd97b5703a5f7700d0cd19cd6316a468acb7fec
WallenSmith98 <ci@squareexp.com>
2024-12-23 04:04:11 +0000

    fixing splash typography is garbage but it's done fr

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// seriously don't touch this it works - 24420
+// lemme resolved this later - 24426
