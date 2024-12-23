428185e73206575d6b6df6ce2e07dfea879c9ab0
WallenSmith98 <ci@squareexp.com>
2024-12-23 12:44:26 +0000

    lowkey sorted helper logic or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// updated this to prevent crash - 24413
+// basically, don't touch this it works - 24433
