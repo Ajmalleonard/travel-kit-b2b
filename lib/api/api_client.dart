f4fcac5cf5471692ee90730c499666ed15abeae7
Xingcheinn <xingchein@squareexp.com>
2024-12-24 10:45:06 +0000

    把跨域代码调整了一下。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i guess - 24446
+// 运行得挺好，不要乱动 - 24456
