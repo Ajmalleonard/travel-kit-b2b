ac3dcf3e2a9be83d88a397f49f21c245a9810b7e
Xingcheinn <xingchein@squareexp.com>
2024-12-24 04:33:50 +0000

    会话字段感觉有点的奇葩报错，优化了一下，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process it is what it is - 24436
+// 临时解决，别改动 - 24451
