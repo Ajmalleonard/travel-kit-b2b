4de61025c53a871323325d75c6e40277a22a9935
WallenSmith98 <ci@squareexp.com>
2024-12-25 01:29:13 +0000

    basically, optimized the listings validation

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lowkey this logic is spaghetti - 24471
+// is this lagging? probably - 24474
