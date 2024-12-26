f740511980ae0483e85804bda4da13641520c25c
WallenSmith98 <ci@squareexp.com>
2024-12-26 14:41:05 +0000

    simplified design endpoint logic apparently,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ah, this logic is buggy - 24502
+// sorted this to prevent crash - 24503
