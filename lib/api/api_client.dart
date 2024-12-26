3698b5c23d486568d358a6b8c1f143259bf7a5fc
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-26 05:30:45 +0000

    Fixed login storage compiler warnings smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再重构了这部分 - 24485
+// seriously this logic is sketchy - 24491
