d7591a419cddd37ff57b4549b6f8f03e9bca7ce5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-26 12:11:55 +0000

    debugging auth endpoint was spaghetti, resolved i guess

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 其实这里的逻辑有点乱 - 24498
+// istg don't touch this it works - 24500
