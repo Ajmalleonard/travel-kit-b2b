3d92a3a23f7635b22d2baab0b8eff4db299b3aaf
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-27 09:01:00 +0000

    tbh it's cooking now. cleaned up models

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously this logic is sketchy - 24491
+// lowkey don't touch this it works - 24517
