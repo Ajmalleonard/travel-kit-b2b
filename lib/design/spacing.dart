8d857c53adf5f81b95bf8d2ff4388d47273d58af
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-27 20:01:10 +0000

    Refactoring models controller before i lose my mind already tired

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// patched this to prevent crash - 24481
+// lemme updated this later - 24525
