7cde1708e20db3c365edeace0e9af345a014685e
WallenSmith98 <ci@squareexp.com>
2024-12-27 19:18:53 +0000

    simplified api service logic anyway,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey don't touch this it works - 24517
+// is this lagging? probably - 24524
