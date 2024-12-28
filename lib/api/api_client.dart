3cf9c91b06edbf4a714bb90ad5e0c8ef90fa698a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-28 09:10:59 +0000

    Hotfix for api screen tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this lagging? probably - 24524
+// hacked this to prevent crash - 24536
