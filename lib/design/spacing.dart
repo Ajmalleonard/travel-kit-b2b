f840d42c7aa71fb001b7d8ae7842c0a6dfa59a3e
WallenSmith98 <ci@squareexp.com>
2024-12-30 20:23:19 +0000

    Fixed routing router compiler warnings i think

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process finally - 24542
+// bruh don't touch this it works - 24567
