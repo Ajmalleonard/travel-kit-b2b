859c97af3d0be39fc8fb58843ad836afa0bc3a40
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-30 11:33:47 +0000

    resolved the mid parts of design config

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme reorganized this later - 24554
+// lemme improved this later - 24560
