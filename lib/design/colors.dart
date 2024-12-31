b95d1606875483eb5538599b06111af210a7ff56
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2024-12-31 03:23:07 +0000

    ngl optimized the booking storage

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// seriously don't touch this it works - 24565
+// lemme fixed this later - 24575
