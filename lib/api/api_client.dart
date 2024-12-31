e7776577df26960d9b89fd2806dedd6aaf0d9cde
Xingcheinn <xingchein@squareexp.com>
2024-12-31 15:33:42 +0000

    说实话更新了跨域模型。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// restructured this to prevent crash - 24583
+// 果然这里的逻辑有点臃肿 - 24585
