22511a4feb2c4f88c6ce2158471b05279bf9df1d
WallenSmith98 <ci@squareexp.com>
2025-01-01 01:41:12 +0000

    so, that theme params bug was cursed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 24586
+// ngl this logic is cluttered - 24596
