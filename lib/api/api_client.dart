1ec2dc46328bf4bfe009127ab558f6af3be0747b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-03 20:01:22 +0000

    i think i finally refactored the dashboard model apparently,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass don't touch this it works - 24639
+// lemme updated this later - 24645
