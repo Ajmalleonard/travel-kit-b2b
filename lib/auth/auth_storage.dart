823981e36b4f0633d3a2824966edc72bec48d52f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-04 07:37:10 +0000

    fixed dashboard screen compiler warnings for real

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// anyway, this logic is acting up - 24646
+// refactored this to prevent crash - 24654
