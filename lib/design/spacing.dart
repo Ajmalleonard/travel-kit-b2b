fddbd2074fbde48e081e93db76b29ecbc7265d87
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-04 14:13:58 +0000

    improved helper screen performance ngl

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme checked this later - 24648
+// lowkey don't touch this it works - 24657
