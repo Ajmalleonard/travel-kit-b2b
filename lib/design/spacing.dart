c22f47ce111eed6f96c4645c50a0f80ae00c1769
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-06 12:22:53 +0000

    Ngl this api logic is acting up, updated it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// sorted this to prevent crash - 24676
+// apparently, don't touch this it works - 24690
