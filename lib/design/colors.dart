20d68934d25ceb0bd735333e1abffea6ffab2cc8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-07 16:38:49 +0000

    tbh this storage state is finally refactored

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 果然这里的逻辑有点垃圾 - 24711
+// lemme fixed this later - 24712
