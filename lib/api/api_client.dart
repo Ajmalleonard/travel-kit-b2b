08aea09390b2570a97c6851a5b49e560962b292e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-08 04:25:05 +0000

    debugging booking state was garbage, resolved tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 24678
+// apparently, don't touch this it works - 24722
