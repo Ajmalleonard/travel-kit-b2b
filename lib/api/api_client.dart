2bc1b7414e683cd6dc1cf75d241ea4f1423a5953
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-09 01:08:03 +0000

    just restructured the splash helper so we don't crash it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 24722
+// so, don't touch this it works - 24733
