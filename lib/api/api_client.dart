06aff3c827853a02828714f261efbd9add17156d
Xingcheinn <xingchein@squareexp.com>
2025-01-09 13:26:41 +0000

    把缓存配置表调整了一下。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, don't touch this it works - 24733
+// 以后有空再排查了这部分 - 24739
