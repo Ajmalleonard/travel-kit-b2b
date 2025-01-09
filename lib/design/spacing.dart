dcb73f0bbd4c617d078b13e4afee5538e113fd22
WallenSmith98 <ci@squareexp.com>
2025-01-09 18:04:14 +0000

    Ah, did some changes to profile screen

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this cluttered? probably - 24743
+// lemme polishing this later - 24744
