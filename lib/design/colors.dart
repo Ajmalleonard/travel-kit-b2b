c752efd4027ae51219684e379602ff225c577956
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-09 14:36:26 +0000

    Checked dashboard params logic, looks better now for real

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// fine, this logic is spaghetti - 24741
+// lemme rewrote this later - 24742
