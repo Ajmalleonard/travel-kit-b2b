511678ef2cf6d8cc9f20ae4edc597c5ec13ee470
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-09 22:04:55 +0000

    cleaning up the broken af code in design model

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 行吧这里的逻辑有点难懂 - 24747
+// trust the process for real - 24749
