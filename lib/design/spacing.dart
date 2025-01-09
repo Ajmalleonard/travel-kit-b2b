206f7c9905fabb74d111be1b361b01b5ae2598f6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-09 17:20:00 +0000

    restructured profile router logic, looks better now finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this garbage? probably - 24729
+// is this cluttered? probably - 24743
