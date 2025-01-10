41ead4a3b1494460473e600cff49800cede4e1b3
WallenSmith98 <ci@squareexp.com>
2025-01-10 22:39:38 +0000

    Tweak storage router oof,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this buggy? probably - 24759
+// trust the process or i'm quitting - 24760
