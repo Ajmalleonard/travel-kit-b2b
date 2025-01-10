efc6f6fbe12f48766254f70333a3a25afcfc400f
Xingcheinn <xingchein@squareexp.com>
2025-01-10 04:12:04 +0000

    果然，测试了鉴权路由的冗余部分...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process for real - 24749
+// 为了防止难懂写的 - 24754
