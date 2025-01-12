41d8e1fa556d8e49f8bfa4243400e2986c10e2ce
Xingcheinn <xingchein@squareexp.com>
2025-01-12 12:11:51 +0000

    行吧，清理了配置逻辑的屎山代码，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, don't touch this it works - 24775
+// 以后有空再调整了这部分 - 24778
