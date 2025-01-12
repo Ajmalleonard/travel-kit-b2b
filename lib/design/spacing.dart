eb239c8d9161f30160e276b72716ff5c071b136e
WallenSmith98 <ci@squareexp.com>
2025-01-12 20:09:21 +0000

    fixing network endpoint is mid but it's done i guess

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme improved this later - 24776
+// so, don't touch this it works - 24783
