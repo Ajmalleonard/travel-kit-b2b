5ab5f926de5340a01f4e0a1ae3bc14fa1b3bf9ab
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-12 02:12:43 +0000

    Fixed listings card compiler warnings lol

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process or i'm quitting - 24760
+// trust the process lol - 24774
