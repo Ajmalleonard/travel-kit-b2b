85684987598d04bfc8da9f08881771dbdab34522
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-12 02:38:08 +0000

    Debugging storage handler was cursed, resolved or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止难懂写的 - 24754
+// ah, don't touch this it works - 24775
