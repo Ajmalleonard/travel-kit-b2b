0301819bf4b35a9677fa8cdd2b20e45f9b273105
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-12 09:58:31 +0000

    just cleaned up the params for routing

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process or i'm quitting - 24726
+// bruh don't touch this it works - 24777
