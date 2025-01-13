4fb13c86efe107d7236c85b3d2475bbb857d5128
WallenSmith98 <ci@squareexp.com>
2025-01-13 05:20:57 +0000

    improved login logic performance seriously

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process lol - 24774
+// optimized this to prevent crash - 24790
