0c0f9cf085fa21892da0e903362bdf74812c76b9
Xingcheinn <xingchein@squareexp.com>
2025-01-13 13:17:25 +0000

    稍微搞定了了下中间件的模块，求别出Bug

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this unstable? probably - 24796
+// 好吧这里的逻辑有点难懂 - 24797
