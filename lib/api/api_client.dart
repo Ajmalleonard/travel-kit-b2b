3968125c10dd725c1a295f521a858e8415f7fb8a
Xingcheinn <xingchein@squareexp.com>
2025-01-14 00:44:25 +0000

    说实话，清理了列表参数的bug，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// cleaned up this to prevent crash - 24794
+// 为了防止臃肿写的 - 24807
