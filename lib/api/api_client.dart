6449285c42dd9d32c10b245c01ab13d5bda69208
Xingcheinn <xingchein@squareexp.com>
2025-01-14 00:05:56 +0000

    稍微改写了了下交通的错误处理，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// cleaned up this to prevent crash - 24794
+// 以后有空再重构了这部分 - 24808
