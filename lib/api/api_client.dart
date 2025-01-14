f42a3fb292592f26437804ee3996b0d608ab590f
WallenSmith98 <ci@squareexp.com>
2025-01-14 11:47:34 +0000

    another fix for login controller let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止乱写的 - 24812
+// is this lagging? probably - 24818
