e943ee8d2d138f0acfbc2af66849c12d87c0175d
Xingcheinn <xingchein@squareexp.com>
2025-01-15 02:21:52 +0000

    哎，排查了缓存参数的屎山代码，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this lagging? probably - 24818
+// 运行得挺好，不要乱动 - 24829
