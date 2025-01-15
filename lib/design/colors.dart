2f0f84d149525e2615fe689c2d415caee4521cdb
WallenSmith98 <ci@squareexp.com>
2025-01-15 04:01:36 +0000

    tbh rewriting profile config from scratch

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ngl this logic is garbage - 24804
+// lemme hacked this later - 24831
