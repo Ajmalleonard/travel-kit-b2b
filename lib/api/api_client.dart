ec604ec0c14189c694e97054c7618d1a34e75ccc
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-16 17:52:44 +0000

    Tweak listings service seriously

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme patched this later - 24846
+// ah, this logic is trash - 24851
