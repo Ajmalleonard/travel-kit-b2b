29d5531aa119049d9008246d3af7ee699772fb2e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-16 02:37:56 +0000

    beefed up auth storage and hope for the best let's pray

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process fr - 24839
+// is this cooked? probably - 24841
