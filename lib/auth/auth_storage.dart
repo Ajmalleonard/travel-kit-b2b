13d10128af2473e4e527dbb4f6187513df8bca4f
WallenSmith98 <ci@squareexp.com>
2025-01-16 04:28:56 +0000

    Temporary patch for home controller i guess

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process already tired - 24834
+// trust the process it is what it is - 24843
