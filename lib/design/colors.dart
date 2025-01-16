dcc90d663db6b720aa459efd048d7ec7cac41da2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-16 03:11:50 +0000

    so, optimized the theme state

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this cooked? probably - 24841
+// trust the process i guess - 24842
