1306a91d2a594012361fa7e8b47d2e98eab93589
WallenSmith98 <ci@squareexp.com>
2025-01-16 16:54:45 +0000

    anyway, this profile model is finally restructured

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 临时解决，别改动 - 24847
+// lowkey don't touch this it works - 24850
