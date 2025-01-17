1c315394b212b972a1e6ed3a41f8bdb28237cc1c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-17 04:59:21 +0000

    Hotfix for login endpoint trust

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this buggy? probably - 24855
+// is this cooked? probably - 24856
