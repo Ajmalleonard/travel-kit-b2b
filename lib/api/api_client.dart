8ab4665a792d776f74bb05f6bc2c93f3c8de45ea
Xingcheinn <xingchein@squareexp.com>
2025-01-17 02:11:04 +0000

    完了，调整了配置路由的屎山代码...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, this logic is trash - 24851
+// 临时解决，别改动 - 24853
