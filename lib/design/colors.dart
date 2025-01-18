17509db9f9710e27f8caa55fb522fe9890f82f46
Xingcheinn <xingchein@squareexp.com>
2025-01-18 09:07:16 +0000

    支付查询感觉有点的性能，排查了一下！

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process tbh - 24873
+// 说实话这里的逻辑有点垃圾 - 24880
