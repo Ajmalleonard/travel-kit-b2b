5159f534f60afce41d3c152c00a6c54f4230ad31
WallenSmith98 <ci@squareexp.com>
2025-01-18 10:30:26 +0000

    More tests for splash card tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 说实话这里的逻辑有点垃圾 - 24880
+// apparently, this logic is messy - 24882
