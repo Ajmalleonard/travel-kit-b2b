204249fde42d69c0bac12427ce6cc26a9f1c8173
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-21 23:26:08 +0000

    almost done with listings endpoint fine,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// apparently, don't touch this it works - 24919
+// oof, don't touch this it works - 24928
