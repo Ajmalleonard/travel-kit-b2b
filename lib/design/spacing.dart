c2e6a5517914b048a5d223ce952353b4c1276be0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-21 04:35:10 +0000

    Lowkey fixed login colors i think

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process for real - 24908
+// apparently, don't touch this it works - 24919
