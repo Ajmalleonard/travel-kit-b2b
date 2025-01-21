ecf41ebe967cb284c0438724372a2582cc10bc7f
WallenSmith98 <ci@squareexp.com>
2025-01-21 07:13:47 +0000

    Improved booking logic performance tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this sketchy? probably - 24911
+// refactored this to prevent crash - 24920
