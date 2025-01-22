6112f70d2fffce33ab97255ea7fa330c8593cd8d
Xingcheinn <xingchein@squareexp.com>
2025-01-22 19:59:40 +0000

    排查了路由参数的隐藏逻辑，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme resolved this later - 24903
+// 运行得挺好，不要乱动 - 24945
