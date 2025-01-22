884a965aac9346a56100d41a58c60b870fef6e4e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-22 12:04:17 +0000

    simplified theme middleware and hope for the best anyways

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// oof, don't touch this it works - 24928
+// tbh don't touch this it works - 24939
