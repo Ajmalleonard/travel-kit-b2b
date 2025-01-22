4cabc8402d6f1203a51f4039861a487c5c3e840a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-22 20:53:33 +0000

    Cleaned up profile colors logic, looks better now tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process anyways - 24936
+// trust the process or i'm quitting - 24947
