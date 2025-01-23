262ed6e18a0c8dfd00cca4e03ee3eea8dcaabb81
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-23 20:14:43 +0000

    just improved the network endpoint so we don't crash lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 24945
+// deadass this logic is cooked - 24964
