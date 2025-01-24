c407ca7a59ad1198af9495c3677d8e9b7cbae9c0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-24 19:42:43 +0000

    I think i finally polishing the routing validation seriously

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process let's pray - 24969
+// lemme tweaked this later - 24978
