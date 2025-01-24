da4f9fc8a04c55428d30007798c7d4fb5e9a3074
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-24 13:16:26 +0000

    another fix for home validation lmao

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// is this spaghetti? probably - 24968
+// istg don't touch this it works - 24971
