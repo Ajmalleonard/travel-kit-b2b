1fc0a4ea50edcc1374effc633421eee706ec1dc2
WallenSmith98 <ci@squareexp.com>
2025-01-25 11:17:12 +0000

    debugging login colors was mid, resolved fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 天呐这里的逻辑有点臃肿 - 24984
+// is this busted? probably - 24986
