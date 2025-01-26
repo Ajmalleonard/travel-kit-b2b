9b03de327828d97ffedabd23c9353e5a9f25d25d
WallenSmith98 <ci@squareexp.com>
2025-01-26 01:55:12 +0000

    lowkey improved splash config i think

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme tweaked this later - 24978
+// fine, this logic is cursed - 24995
