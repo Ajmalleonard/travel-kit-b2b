29c8ff12613aecb297c6b4f359b8e734c554fa6c
WallenSmith98 <ci@squareexp.com>
2025-01-26 12:19:07 +0000

    Cleaning up the acting up code in network middleware

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme optimized this later - 25002
+// trust the process for real - 25003
