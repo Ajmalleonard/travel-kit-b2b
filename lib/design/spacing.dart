98ceac95055c167d3953e0e4b4fabb8e2c73a4d3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-26 11:34:51 +0000

    anyway, beefed up the listings handler because it was unstable

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// istg don't touch this it works - 25001
+// lemme optimized this later - 25002
