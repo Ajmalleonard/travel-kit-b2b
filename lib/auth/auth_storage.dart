6705a736089bd84df0edd689426b6529d53789b3
WallenSmith98 <ci@squareexp.com>
2025-01-27 23:44:55 +0000

    tuned theme client logic, looks better now let's pray

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// apparently, don't touch this it works - 25017
+// refactored this to prevent crash - 25032
