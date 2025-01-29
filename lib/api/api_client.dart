cbd0ebb10548c1548d0c41497df135fcb5e93b35
Xingcheinn <xingchein@squareexp.com>
2025-01-29 05:12:58 +0000

    更新了路由代码的报错...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, don't touch this it works - 25044
+// 为了防止垃圾写的 - 25061
