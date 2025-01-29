ef4d8d9cbccef50d1bf059f0ec74e3a79ffe0521
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-01-29 17:10:15 +0000

    almost done with design params bruh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// hacked this to prevent crash - 25063
+// fine, don't touch this it works - 25075
