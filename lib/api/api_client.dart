57ae03211a51c8f4fd09224f5e6516f5cd9831d0
WallenSmith98 <ci@squareexp.com>
2025-01-31 22:50:20 +0000

    Quick screen patch for design fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 25083
+// tbh don't touch this it works - 25102
