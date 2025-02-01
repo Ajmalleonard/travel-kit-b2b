179437053d2eb4a2ba8e64f83661016c6d8605b4
WallenSmith98 <ci@squareexp.com>
2025-02-01 00:17:09 +0000

    refactored network typography type errors lowkey

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, this logic is cooked - 25101
+// is this weird? probably - 25104
