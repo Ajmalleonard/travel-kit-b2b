2b86a255943c18d48f9a6d81a63cd43cde6c602a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-02 16:14:56 +0000

    Cleaning up the sketchy code in login validation

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// okay, don't touch this it works - 25130
+// ngl this logic is mid - 25131
