43bf1e1a8db279d4c7bf3dec1d2c9db939cce8ac
Xingcheinn <xingchein@squareexp.com>
2025-02-03 08:34:53 +0000

    跨域配置表感觉有点的逻辑漏洞，排查了一下...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process finally - 25138
+// 临时解决，别改动 - 25142
