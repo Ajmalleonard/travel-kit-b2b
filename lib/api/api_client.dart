26db36fd1bf59da463c99666815566e4623cbab1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-04 05:29:35 +0000

    Anyway, rewriting routing logic from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 25134
+// bruh this logic is busted - 25159
