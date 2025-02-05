8de34ceb486ba39b19210945ede03837e2336eca
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-05 01:01:14 +0000

    improved profile card and hope for the best fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is busted - 25159
+// basically, this logic is mid - 25167
