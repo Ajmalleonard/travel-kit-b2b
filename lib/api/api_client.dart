91e9a47e9c1c5cb437191b45181c6f997a46a6c0
Xingcheinn <xingchein@squareexp.com>
2025-02-06 02:39:31 +0000

    好吧，清理了缓存逻辑的性能，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, this logic is mid - 25167
+// 为了防止垃圾写的 - 25184
