8b8c1f5272579154cfa39db9e7295d16d2e9b1a2
Xingcheinn <xingchein@squareexp.com>
2025-02-07 11:11:59 +0000

    把订单查询重构了一下。测试通过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process or i'm quitting - 25221
+// 运行得挺好，不要乱动 - 25225
