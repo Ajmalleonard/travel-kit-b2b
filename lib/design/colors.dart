c579f4a8032c176702076f2113dcfdf5ee54c5fd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-08 04:18:11 +0000

    apparently, did some changes to listings endpoint

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this busted? probably - 25230
+// is this broken af? probably - 25238
