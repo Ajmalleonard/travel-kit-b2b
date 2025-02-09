3cef29002916d9cc2880209e425534aefecb4ce0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-09 02:06:34 +0000

    just beefed up the login handler so we don't crash smh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tbh this logic is cluttered - 25246
+// adjusted this to prevent crash - 25253
