94d8f3a3168120f479514c98f9ebbc1090b6f506
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-09 15:41:33 +0000

    Whoever wrote the splash helper was wildin, fixed it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// is this broken af? probably - 25255
+// improved this to prevent crash - 25262
