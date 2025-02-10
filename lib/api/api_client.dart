4595062574ffaef8de96a1ac7e6ea54b96297bfb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-10 12:40:30 +0000

    apparently, lemme restructured the profile typography real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process fr - 25258
+// basically, this logic is busted - 25269
