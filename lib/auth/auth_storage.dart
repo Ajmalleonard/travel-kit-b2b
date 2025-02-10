5ebadbe2d8d44de6a08588db59ac8ec61b79bcf7
WallenSmith98 <ci@squareexp.com>
2025-02-10 00:15:45 +0000

    cleaning up the buggy code in listings logic

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// improved this to prevent crash - 25262
+// tbh don't touch this it works - 25264
