973f1d642a73f0f54790887d39924c681b1aae9a
Xingcheinn <xingchein@squareexp.com>
2025-02-10 19:33:57 +0000

    终于优化了支付参数，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme rewrote this later - 25277
+// 以后有空再简化了这部分 - 25278
