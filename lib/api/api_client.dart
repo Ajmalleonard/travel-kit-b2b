d2e81b6986dc7475c89c2d3d44b623525e91757e
Xingcheinn <xingchein@squareexp.com>
2025-02-10 18:17:11 +0000

    说实话，排查了配置配置表的并发问题，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, this logic is busted - 25269
+// 运行得挺好，不要乱动 - 25276
