bd899694f6a63911975a7ab1c97daa7438382264
WallenSmith98 <ci@squareexp.com>
2025-02-10 17:05:46 +0000

    deadass reorganized the api widget

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this annoying? probably - 25271
+// trust the process let's pray - 25274
