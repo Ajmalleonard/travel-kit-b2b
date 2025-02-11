a91b6a1377863583b5aa011bb1880fef4d283ce1
WallenSmith98 <ci@squareexp.com>
2025-02-11 18:25:28 +0000

    improved models router type errors well

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme reorganized this later - 25288
+// beefed up this to prevent crash - 25294
