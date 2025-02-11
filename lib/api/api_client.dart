3e7fd35223952065f722c020f066652e320810d6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-11 12:37:51 +0000

    Polishing review service and hope for the best anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process it is what it is - 25282
+// lemme reorganized this later - 25288
