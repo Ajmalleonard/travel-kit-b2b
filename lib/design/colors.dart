109368213ca7fba005b46d2aeb70308a76851846
WallenSmith98 <ci@squareexp.com>
2025-02-11 13:36:23 +0000

    lowkey checked the models screen because it was mid

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再简化了这部分 - 25283
+// trust the process trust - 25290
