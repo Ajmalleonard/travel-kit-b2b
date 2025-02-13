3d614cf55050720f606fe62ac78f77ead057a93a
Xingcheinn <xingchein@squareexp.com>
2025-02-13 09:59:48 +0000

    好吧，修复了校验字段的并发问题，累瘫了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// apparently, this logic is acting up - 25313
+// 其实这里的逻辑有点臃肿 - 25317
