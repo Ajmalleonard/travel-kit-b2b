8aaf89c112eee066c1b796279ec2215303631cf3
WallenSmith98 <ci@squareexp.com>
2025-02-14 06:08:38 +0000

    Apparently, it's cooking now. rewrote dashboard

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 25321
+// checked this to prevent crash - 25334
