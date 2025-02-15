83eea23971e39be9032fa163cbcfbf9744feafe7
Xingcheinn <xingchein@squareexp.com>
2025-02-15 03:24:08 +0000

    把路由模块改写了一下，应该没问题了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this cooked? probably - 25348
+// 运行得挺好，不要乱动 - 25351
