10bf6e178f84c44fe4913266bcb2000c37790891
WallenSmith98 <ci@squareexp.com>
2025-02-16 13:06:02 +0000

    hacked the acting up parts of booking sheet

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// fine, this logic is garbage - 25372
+// ngl don't touch this it works - 25380
