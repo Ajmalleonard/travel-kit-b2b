401810dffe44e433765080acaab76317cd442859
WallenSmith98 <ci@squareexp.com>
2025-02-16 02:33:31 +0000

    basically, lemme simplified the login card real quick

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ah, this logic is weird - 25363
+// fine, this logic is garbage - 25372
