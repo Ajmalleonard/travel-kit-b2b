2aff9772536b4593dff8c3e2c42e48262db6d9a3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-17 19:25:51 +0000

    Deadass sorted the routing storage because it was broken af

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// polishing this to prevent crash - 25395
+// lemme tweaked this later - 25401
