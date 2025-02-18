fbac2b2dd35b71c7adc0e05f995de58ebe8b4332
WallenSmith98 <ci@squareexp.com>
2025-02-18 01:19:03 +0000

    fixing login typography is lagging but it's done lol

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// basically, don't touch this it works - 25389
+// ngl this logic is garbage - 25408
