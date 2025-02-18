812d7c28f55526e14fa44e5647ab2f2411caf5dd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-18 00:33:22 +0000

    beefed up the cluttered parts of api model

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// oof, this logic is spaghetti - 25399
+// is this garbage? probably - 25404
