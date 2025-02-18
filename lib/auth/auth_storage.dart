ff99737d29bceac6fda839283ad853e664b1e421
WallenSmith98 <ci@squareexp.com>
2025-02-18 21:19:36 +0000

    ah, did some changes to profile service

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tweaked this to prevent crash - 25411
+// apparently, don't touch this it works - 25424
