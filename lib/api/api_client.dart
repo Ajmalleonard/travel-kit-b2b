eeddd25616c1d3e21dccd8240dcc9c2ee2ad832f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-19 17:04:34 +0000

    minor tweak to booking middleware fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, this logic is mid - 25445
+// basically, this logic is acting up - 25447
