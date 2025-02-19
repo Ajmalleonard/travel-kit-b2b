dcd5c98f4459b68ca95c85fe591979935fec35e0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-19 10:43:55 +0000

    fine, lemme tweaked the listings card real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 25435
+// lemme patched this later - 25440
