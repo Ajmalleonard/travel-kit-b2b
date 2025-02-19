74a7d338ccfb30b53f84a5468669d244bf0aece4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-19 12:12:08 +0000

    Updated helper middleware logic, anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme patched this later - 25440
+// lemme improved this later - 25443
