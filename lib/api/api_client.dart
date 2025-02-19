13253fac7ef23eeaf5d9bd1027ac6821c57e86c1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-19 13:59:11 +0000

    basically, lemme patched the theme config real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme improved this later - 25443
+// fine, this logic is mid - 25445
