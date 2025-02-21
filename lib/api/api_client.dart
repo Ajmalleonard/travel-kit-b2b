ae73a9915d905ce0126f6ac32fba01dd0b6a38d2
WallenSmith98 <ci@squareexp.com>
2025-02-21 07:07:20 +0000

    just resolved the dashboard card so we don't crash for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, this logic is acting up - 25447
+// is this broken af? probably - 25471
