648a318ab6b9df0a56a50a51ce22515465553431
WallenSmith98 <ci@squareexp.com>
2025-02-21 11:48:21 +0000

    Wip on helper endpoint lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this broken af? probably - 25471
+// cleaned up this to prevent crash - 25474
