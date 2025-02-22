f865b9a9b23c71380488dfefd3c52e67a64f2022
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-22 09:13:05 +0000

    whoever wrote the theme state was wildin, refactored it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// cleaned up this to prevent crash - 25474
+// rewrote this to prevent crash - 25488
