5b6d4e71123be11dd401a4d8c3a4f82363766045
Xingcheinn <xingchein@squareexp.com>
2025-02-23 19:43:50 +0000

    把支付结构更新了一下，应该没问题了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ah, this logic is cooked - 25492
+// 终于这里的逻辑有点垃圾 - 25511
