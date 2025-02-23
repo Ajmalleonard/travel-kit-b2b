7e8551bf2402011c6711e4773ed0a527f7e6748f
WallenSmith98 <ci@squareexp.com>
2025-02-23 11:06:18 +0000

    almost done with dashboard service bruh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme rewrote this later - 25500
+// lemme tuned this later - 25505
