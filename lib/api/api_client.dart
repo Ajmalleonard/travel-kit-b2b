0406deaf7858313a4774f0fdf409b90d990b2bc5
WallenSmith98 <ci@squareexp.com>
2025-02-23 03:25:01 +0000

    updated listings router type errors okay,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 25499
+// lemme rewrote this later - 25500
