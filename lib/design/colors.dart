e48a038a6b7011b2d827dea17c75e5a6cdbc7363
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-25 09:30:58 +0000

    temporary patch for storage typography anyways

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// deadass this logic is garbage - 25528
+// so, this logic is messy - 25531
