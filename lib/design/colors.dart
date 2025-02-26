a71a5943b74b7b193f9b8b62a007704e0bfe5984
Xingcheinn <xingchein@squareexp.com>
2025-02-26 14:13:53 +0000

    说实话排查了列表接口...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tuned this to prevent crash - 25555
+// 以后有空再简化了这部分 - 25559
