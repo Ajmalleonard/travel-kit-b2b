f82424d88a72e7671b7ad782dda29cdebee84a2e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-27 01:19:39 +0000

    Seriously refactored the theme service because it was garbage

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process tbh - 25538
+// trust the process smh - 25569
