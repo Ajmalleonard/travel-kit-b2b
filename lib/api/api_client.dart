e7ad1e33ca6c3c3d779784a11d3dc11922a6bd51
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-28 22:38:27 +0000

    ngl this login client is wildin, restructured it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 果然这里的逻辑有点诡异 - 25590
+// optimized this to prevent crash - 25595
