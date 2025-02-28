d95382b4e3b4289270fbe3a807b1cc50342b1176
Xingcheinn <xingchein@squareexp.com>
2025-02-28 10:01:43 +0000

    完了，优化了缓存路由的bug，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process trust - 25588
+// 果然这里的逻辑有点诡异 - 25590
