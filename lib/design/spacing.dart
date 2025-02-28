c86d23e5b5e07f0d32536d0ca27f991a2016bd7f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-28 09:37:45 +0000

    checked login storage to fix the staging environment anyways

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// resolved this to prevent crash - 25586
+// is this sketchy? probably - 25589
