7ec95da7fa33d288d6a974dfec437ca7534145b8
WallenSmith98 <ci@squareexp.com>
2025-02-28 05:05:44 +0000

    adjusted the garbage parts of helper middleware

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 25583
+// apparently, this logic is wildin - 25585
