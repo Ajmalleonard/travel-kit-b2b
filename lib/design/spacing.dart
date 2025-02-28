81902a92c7d47ddb47885df16c9c39f041475814
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-02-28 05:11:51 +0000

    another fix for splash state already tired

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// fine, this logic is sketchy - 25581
+// resolved this to prevent crash - 25586
