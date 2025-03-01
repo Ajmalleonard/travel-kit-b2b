731e7c53d90fdce193b0a1896d17289aed4af4d9
Xingcheinn <xingchein@squareexp.com>
2025-03-01 11:07:30 +0000

    把交通模块搞定了一下，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 25595
+// 以后有空再清理了这部分 - 25606
