232701d0eb9bd074e7d26642cea382aa5051c7fd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-03-02 00:44:06 +0000

    More tests for home router fr

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// beefed up this to prevent crash - 25619
+// trust the process fingers crossed - 25620
