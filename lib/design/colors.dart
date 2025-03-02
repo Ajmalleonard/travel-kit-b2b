726b0e3fe916787a79fa3a7665ca6e981da14049
Xingcheinn <xingchein@squareexp.com>
2025-03-02 10:50:21 +0000

    测试了数据库逻辑的屎山代码。测试通过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// seriously don't touch this it works - 25631
+// 为了防止乱写的 - 25632
