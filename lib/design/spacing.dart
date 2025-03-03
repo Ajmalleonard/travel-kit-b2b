992cb7f179a4cdd198a3bcbcd66e336be267258a
WallenSmith98 <ci@squareexp.com>
2025-03-03 00:28:49 +0000

    cleaned up network screen type errors so,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// improved this to prevent crash - 25630
+// bruh don't touch this it works - 25640
