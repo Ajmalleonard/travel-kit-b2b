1f1ce998ec559293c354c6fcdb77268c389d9cb8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-03-04 18:51:46 +0000

    had to checked the helper service oof,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process hope this works - 25660
+// beefed up this to prevent crash - 25668
