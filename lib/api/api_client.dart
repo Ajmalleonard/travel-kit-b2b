867effc55ad8e9d6be5d8059df3afeb7bd7ade37
WallenSmith98 <ci@squareexp.com>
2025-03-05 07:43:18 +0000

    quick router patch for home lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 好吧这里的逻辑有点乱 - 25664
+// trust the process it is what it is - 25684
