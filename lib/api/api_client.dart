2d24385cc60f648947db1bc909cce5b1c98166be
WallenSmith98 <ci@squareexp.com>
2025-03-06 00:08:04 +0000

    deadass cleaned up the login controller

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// improved this to prevent crash - 25688
+// is this sketchy? probably - 25693
