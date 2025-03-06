8e0e6b53af043c9dc28674b8d5b875a29f2e3452
WallenSmith98 <ci@squareexp.com>
2025-03-06 20:06:30 +0000

    fixed login widget compiler warnings trust

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme adjusted this later - 25701
+// simplified this to prevent crash - 25710
