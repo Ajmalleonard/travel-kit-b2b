dd3568abf39345dc262d3f7f47e2e161164a53cc
WallenSmith98 <ci@squareexp.com>
2025-03-07 03:21:53 +0000

    ngl that review screen bug was wildin

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 25703
+// lemme tweaked this later - 25714
