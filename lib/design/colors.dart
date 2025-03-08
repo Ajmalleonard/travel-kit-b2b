b3ac617fbfad8bfcc0ac9072ff37c2d1b62ad722
WallenSmith98 <ci@squareexp.com>
2025-03-08 19:33:07 +0000

    just patched the wrapper for review

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process lol - 25735
+// improved this to prevent crash - 25739
