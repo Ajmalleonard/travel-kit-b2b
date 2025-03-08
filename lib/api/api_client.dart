1d17534a7c4c2c03a8d47839fbb354796160a57e
WallenSmith98 <ci@squareexp.com>
2025-03-08 20:03:26 +0000

    Resolved annoying bug in splash service okay,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再搞定了这部分 - 25731
+// tuned this to prevent crash - 25740
