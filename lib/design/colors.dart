336c97762478db623f0c7ea17fc0aea68430b209
WallenSmith98 <ci@squareexp.com>
2025-03-08 05:22:23 +0000

    Fixing helper storage is spaghetti but it's done fingers crossed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再重构了这部分 - 25725
+// lemme restructured this later - 25726
