77746b68675e98f68dd2c4ba687d5be9d35b19fb
Xingcheinn <xingchein@squareexp.com>
2025-03-10 23:41:25 +0000

    评价接口感觉有点的并发问题，改进了一下，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, don't touch this it works - 25766
+// 行吧这里的逻辑有点臃肿 - 25777
