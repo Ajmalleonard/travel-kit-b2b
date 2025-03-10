9c50760624a4fbe2476f02bbdff24abd840bb9a6
WallenSmith98 <ci@squareexp.com>
2025-03-10 12:08:31 +0000

    improved design middleware performance seriously

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 25765
+// basically, don't touch this it works - 25766
