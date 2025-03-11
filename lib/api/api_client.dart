fa7850d03696f6e02bc931dfcc72807b0a8ef7fe
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-03-11 17:02:52 +0000

    lowkey rewriting auth model from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 行吧这里的逻辑有点臃肿 - 25777
+// lemme refactored this later - 25787
