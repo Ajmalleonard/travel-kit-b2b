b031828259c5dc39d5dc971be480dd3c61891740
WallenSmith98 <ci@squareexp.com>
2025-03-12 23:50:13 +0000

    ah, rewriting theme service from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 25814
+// istg this logic is broken af - 25819
