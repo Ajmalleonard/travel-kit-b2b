2b04cfa99285b594a3dd95b538119d5c3e49232c
Xingcheinn <xingchein@squareexp.com>
2025-03-12 09:32:29 +0000

    交通结构感觉有点的性能，清理了一下，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process fr - 25798
+// 以后有空再修复了这部分 - 25802
