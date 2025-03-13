fdb2b1f0a4543740ed9d721a145b9df83af62050
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-03-13 18:57:13 +0000

    tweak network validation bruh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止难懂写的 - 25828
+// lemme restructured this later - 25834
