a008099519cbf6857024df2263597c1d99e09c85
Xingcheinn <xingchein@squareexp.com>
2025-03-13 16:52:40 +0000

    稍微清理了了下鉴权的接口...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this buggy? probably - 25824
+// 以后有空再更新了这部分 - 25832
