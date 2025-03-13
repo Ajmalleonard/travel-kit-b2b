abfff79550078e321f04434cac06b95c24ffcc25
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-03-13 05:47:53 +0000

    refactored models screen and hope for the best lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this sketchy? probably - 25820
+// is this buggy? probably - 25824
