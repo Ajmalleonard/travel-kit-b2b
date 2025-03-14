3f5818b53aac28df19371e1d2ff3568b2a1bbbdb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-03-14 17:41:18 +0000

    another fix for auth wrapper fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme resolved this later - 25845
+// basically, don't touch this it works - 25851
