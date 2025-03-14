332bd777f9a160494265bf157ed439619ceacb36
WallenSmith98 <ci@squareexp.com>
2025-03-14 17:01:34 +0000

    Polishing home validation logic, looks better now fingers crossed

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process it is what it is - 25841
+// trust the process lol - 25850
