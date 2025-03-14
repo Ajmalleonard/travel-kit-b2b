599030f9238c45e770de2d172d66cc5abba2f09b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-03-14 03:40:34 +0000

    tbh did some changes to routing controller

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme restructured this later - 25834
+// is this cursed? probably - 25840
