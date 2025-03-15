b4fa3f55f603f23a37d816d39a6c870120c65888
WallenSmith98 <ci@squareexp.com>
2025-03-15 21:27:49 +0000

    improved booking params and hope for the best anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme adjusted this later - 25869
+// lemme polishing this later - 25870
