8ce218163cf74c99148d5f1ec8348c82a7fab6af
WallenSmith98 <ci@squareexp.com>
2025-03-16 02:41:37 +0000

    Seriously lemme beefed up the home state real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme polishing this later - 25870
+// lemme optimized this later - 25872
