2bfa097aeaba121ac1f0b515b6595d3837a7b0ba
Xingcheinn <xingchein@squareexp.com>
2025-03-16 07:15:15 +0000

    稍微搞定了了下评价的配置表！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme optimized this later - 25872
+// 为了防止难懂写的 - 25875
