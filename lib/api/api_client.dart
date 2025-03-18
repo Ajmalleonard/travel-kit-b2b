6a232e997250b3770d684b6e1808f71bd7958cee
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-03-18 02:29:04 +0000

    updated listings screen type errors apparently,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey don't touch this it works - 25890
+// patched this to prevent crash - 25895
