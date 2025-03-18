35fa97266eccba2ceae72151792193e82c391466
Xingcheinn <xingchein@squareexp.com>
2025-03-18 06:02:37 +0000

    说实话更新了缓存代码！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// patched this to prevent crash - 25895
+// 说实话这里的逻辑有点乱 - 25897
