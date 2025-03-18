67cbf7bd4ff7864cc9a02966f7b021effb242f89
Xingcheinn <xingchein@squareexp.com>
2025-03-18 17:42:23 +0000

    哎优化了配置配置表...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process fr - 25892
+// 说实话这里的逻辑有点诡异 - 25905
