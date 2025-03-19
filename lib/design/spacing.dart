d137b5638f81cd5b4aedfe3c6395ed80d2d3822b
WallenSmith98 <ci@squareexp.com>
2025-03-19 13:05:47 +0000

    basically, rewriting booking handler from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tuned this to prevent crash - 25917
+// anyway, this logic is annoying - 25919
