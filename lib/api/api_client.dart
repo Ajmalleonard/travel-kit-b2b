9cd34ff46440273e25184fa5a7bc1b8d96ec43d0
Xingcheinn <xingchein@squareexp.com>
2025-03-19 06:55:09 +0000

    完了，调整了校验路由的并发问题...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme reorganized this later - 25898
+// 终于这里的逻辑有点难懂 - 25914
