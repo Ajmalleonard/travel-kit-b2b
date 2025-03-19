4b1ec95302a2c477ffcfda191eeaa600f59a7921
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-03-19 13:07:09 +0000

    Okay, rewriting helper screen from scratch

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// anyway, this logic is annoying - 25919
+// lowkey don't touch this it works - 25920
