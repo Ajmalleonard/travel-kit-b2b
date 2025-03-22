5aa0f26b70e7313ac1cdaa9489fdcc8dfd425479
Xingcheinn <xingchein@squareexp.com>
2025-03-22 05:29:10 +0000

    哎搞定了交通配置表，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this weird? probably - 25965
+// 以后有空再优化了这部分 - 25968
