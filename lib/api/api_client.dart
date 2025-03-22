5885039f0a855b0150948a221e52df28ff3f62d1
WallenSmith98 <ci@squareexp.com>
2025-03-22 02:30:39 +0000

    well that review controller bug was messy

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, don't touch this it works - 25945
+// is this weird? probably - 25965
