c265636710508f08fa9231b9f58b8ec053c4bfd2
Xingcheinn <xingchein@squareexp.com>
2025-03-23 01:08:39 +0000

    把中间件结构优化了一下，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 25972
+// 为了防止臃肿写的 - 25983
