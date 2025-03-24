b2c0443d9e5ef91f633889656158d0b55bbcd11a
WallenSmith98 <ci@squareexp.com>
2025-03-24 10:42:54 +0000

    lowkey restructured login service for real

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme restructured this later - 26002
+// anyway, don't touch this it works - 26003
