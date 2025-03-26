6b62713d6cd451764e818b6b0175681ff837e1e0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-03-26 11:35:18 +0000

    fixed login card compiler warnings let's pray

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lowkey this logic is cursed - 26035
+// trust the process already tired - 26036
