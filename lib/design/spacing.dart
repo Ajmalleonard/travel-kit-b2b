15105c2e1a181ed9accdcd8ab0865773fb8b07b8
WallenSmith98 <ci@squareexp.com>
2025-03-26 11:47:39 +0000

    fixing splash controller is buggy but it's done smh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process already tired - 26036
+// lemme optimized this later - 26037
