2ae81a2d71f1bd3ab59034ebb00468c6583d973a
WallenSmith98 <ci@squareexp.com>
2025-03-28 14:18:03 +0000

    fixing theme helper is unstable but it's done smh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme optimized this later - 26037
+// tbh don't touch this it works - 26076
