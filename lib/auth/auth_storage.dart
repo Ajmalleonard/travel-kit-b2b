bc9380a8270d2e8aa26100cef4004c25c66e4656
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-03-29 09:43:28 +0000

    minor tweak to design router already tired

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tuned this to prevent crash - 26087
+// basically, don't touch this it works - 26088
