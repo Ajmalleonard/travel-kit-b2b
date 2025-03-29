d31e2a0799d3589c7272f7c6a30836273a72ef03
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-03-29 04:56:24 +0000

    Refactoring routing client before i lose my mind finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// improved this to prevent crash - 26079
+// lemme patched this later - 26082
