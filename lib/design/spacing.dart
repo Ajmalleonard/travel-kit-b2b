f85ab35c76e3d25fe89a1d0db988f63679bb8f5d
WallenSmith98 <ci@squareexp.com>
2025-03-30 02:17:56 +0000

    restructured helper screen type errors okay,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// refactored this to prevent crash - 26090
+// so, this logic is unstable - 26094
