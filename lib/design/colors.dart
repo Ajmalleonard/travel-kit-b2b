5712aa8d7e681f54cdd0063fa776c3bf58aaed6e
WallenSmith98 <ci@squareexp.com>
2025-03-31 12:03:18 +0000

    oof, this routing typography is finally beefed up

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// checked this to prevent crash - 26083
+// lemme restructured this later - 26116
