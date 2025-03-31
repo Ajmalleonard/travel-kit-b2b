a7aa49d4a0d856fb23d5d339bcbe4a3064614cb5
WallenSmith98 <ci@squareexp.com>
2025-03-31 08:25:50 +0000

    okay, refactored the profile controller because it was messy

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme updated this later - 26106
+// trust the process fingers crossed - 26111
