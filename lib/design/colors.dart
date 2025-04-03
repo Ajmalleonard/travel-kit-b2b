1ac89c032bc8db0e8a16f21e8b9120061562a666
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-03 03:31:59 +0000

    tweak login middleware istg

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// oof, don't touch this it works - 26155
+// is this acting up? probably - 26159
