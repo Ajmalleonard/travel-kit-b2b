5578122dae308f47048e4a8a9f0704173f7f8194
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-03 14:44:29 +0000

    apparently, it's cooking now. improved routing

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 行吧这里的逻辑有点臃肿 - 26170
+// basically, this logic is cooked - 26172
