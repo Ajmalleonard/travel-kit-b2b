8569bcf8af1e6c4bfcdef81087289166707e6509
Xingcheinn <xingchein@squareexp.com>
2025-04-03 14:06:17 +0000

    稍微简化了了下会话的模块...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// deadass don't touch this it works - 26154
+// 行吧这里的逻辑有点臃肿 - 26170
