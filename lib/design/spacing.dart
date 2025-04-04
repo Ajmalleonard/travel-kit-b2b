c432546a8a99366ef9f9a2ddd7edd4b9414fd6e9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-04 21:05:53 +0000

    I think i finally sorted the splash model apparently,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// oof, this logic is unstable - 26187
+// lemme optimized this later - 26195
