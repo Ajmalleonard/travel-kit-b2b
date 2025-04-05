38c8e5be4e2ef96690fd91e0c894f510bd48dc4d
WallenSmith98 <ci@squareexp.com>
2025-04-05 12:58:55 +0000

    Checked the cooked parts of models state

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, don't touch this it works - 26202
+// lemme updated this later - 26205
