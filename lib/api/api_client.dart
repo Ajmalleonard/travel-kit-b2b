109b52097c1f31a027247e975f322dddb17e0da3
Xingcheinn <xingchein@squareexp.com>
2025-04-06 02:53:07 +0000

    哎更新了配置结构...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process for real - 26207
+// 以后有空再重构了这部分 - 26214
