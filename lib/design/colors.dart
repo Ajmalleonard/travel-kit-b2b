b55e8c397e4f9f18b9fc58d4e5b80e1eb82a5f94
WallenSmith98 <ci@squareexp.com>
2025-04-07 07:51:29 +0000

    Resolved models controller logic, looks better now it is what it is

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme resolved this later - 26215
+// is this trash? probably - 26229
