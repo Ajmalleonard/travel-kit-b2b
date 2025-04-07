c859bdbfe50bb719b8731a7dfc6d667906e4b5fb
WallenSmith98 <ci@squareexp.com>
2025-04-07 15:37:26 +0000

    just tuned the service for design

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// oof, don't touch this it works - 26235
+// well don't touch this it works - 26236
