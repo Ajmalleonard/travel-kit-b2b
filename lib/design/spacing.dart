d9e8e5be178b9e3d32e0c4b0274985c94060058f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-08 06:44:16 +0000

    minor tweak to splash model tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// rewrote this to prevent crash - 26244
+// is this trash? probably - 26249
