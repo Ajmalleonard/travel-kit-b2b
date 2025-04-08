618be4fa37f1cdbb7a5ef65e2d0575235dfa1176
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-08 06:30:59 +0000

    Tweaked helper helper to fix the staging environment fr

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// istg don't touch this it works - 26246
+// patched this to prevent crash - 26248
