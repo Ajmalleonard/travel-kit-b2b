5e517bbe75f4792e99f875821d7d57ba4a917851
WallenSmith98 <ci@squareexp.com>
2025-04-11 21:25:17 +0000

    debugging dashboard params was broken af, resolved trust

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// basically, this logic is cursed - 26281
+// trust the process i think - 26299
