836ef6fadaf8e988b68f7cedc99e776bcfe82a94
WallenSmith98 <ci@squareexp.com>
2025-04-11 17:33:34 +0000

    tbh it's cooking now. resolved routing

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tweaked this to prevent crash - 26289
+// trust the process for real - 26295
