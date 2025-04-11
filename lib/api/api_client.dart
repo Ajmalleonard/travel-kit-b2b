d6c581441c15c8195f3fb7352cbace85a9f64ee9
Xingcheinn <xingchein@squareexp.com>
2025-04-11 19:47:04 +0000

    果然改进了路由配置表，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process fingers crossed - 26283
+// 果然这里的逻辑有点难懂 - 26297
