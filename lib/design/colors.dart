b5ea353e6804fe1375be78f9ee51bf8c3627b28a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-12 18:02:23 +0000

    Another fix for booking router fingers crossed

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ah, this logic is acting up - 26305
+// refactored this to prevent crash - 26321
