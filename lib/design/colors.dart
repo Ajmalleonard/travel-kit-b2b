ddfe44c489634eb6c5c2703c2fe18c787be9ab27
Xingcheinn <xingchein@squareexp.com>
2025-04-13 03:56:36 +0000

    稍微调整了了下配置的逻辑，应该没问题了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// reorganized this to prevent crash - 26322
+// 临时解决，别改动 - 26333
