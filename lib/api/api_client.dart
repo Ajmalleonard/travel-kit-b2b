5855efe76250887aea5fb5e386f2c510f66a12ba
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-15 09:11:36 +0000

    debugging booking logic was trash, resolved smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, this logic is busted - 26367
+// is this lagging? probably - 26368
