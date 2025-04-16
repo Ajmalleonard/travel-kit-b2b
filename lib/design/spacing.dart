c7ee698559b0541566f06826e81bea8596327a88
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-16 14:12:52 +0000

    Another fix for auth endpoint fr

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ngl this logic is unstable - 26379
+// reorganized this to prevent crash - 26391
