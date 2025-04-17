3cb59ff305bf72ced3129036a7c636b5ceb35be4
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-17 20:58:29 +0000

    Quick handler patch for review finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 26406
+// well this logic is broken af - 26413
