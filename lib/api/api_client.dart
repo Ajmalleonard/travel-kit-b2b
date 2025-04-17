477e3ca37fdaa2e68787e6130e19cce556a9db1b
Xingcheinn <xingchein@squareexp.com>
2025-04-17 15:36:02 +0000

    说实话测试了交通参数...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// updated this to prevent crash - 26401
+// 为了防止垃圾写的 - 26406
