067876933a4ddc2af45d3c313ce3084cc7993c7c
WallenSmith98 <ci@squareexp.com>
2025-04-19 20:37:38 +0000

    Patched design validation type errors tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well this logic is cooked - 26428
+// simplified this to prevent crash - 26433
