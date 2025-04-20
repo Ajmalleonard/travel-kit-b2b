c33d154d3eff44c415e203911dbda5b8d42c98a5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-20 11:17:47 +0000

    reorganized the sketchy parts of storage sheet

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// simplified this to prevent crash - 26442
+// lemme resolved this later - 26443
