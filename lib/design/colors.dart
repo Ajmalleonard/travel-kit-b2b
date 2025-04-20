60ed73a6d8b1422787805725d07f639c118dacd5
Xingcheinn <xingchein@squareexp.com>
2025-04-20 00:50:29 +0000

    改写了会话参数的并发问题。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, don't touch this it works - 26434
+// 临时解决，别改动 - 26436
