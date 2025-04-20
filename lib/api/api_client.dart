7a20894aed568ecb282f8d910ac870854cf064d9
Xingcheinn <xingchein@squareexp.com>
2025-04-20 23:18:44 +0000

    鉴权代码感觉有点的并发问题，清理了一下，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// simplified this to prevent crash - 26433
+// 为了防止难懂写的 - 26453
