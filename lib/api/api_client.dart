d788bc2e5783346b87b61e55cedf82a34d7b63a4
WallenSmith98 <ci@squareexp.com>
2025-04-21 03:57:14 +0000

    improved profile helper performance fine,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止难懂写的 - 26453
+// oof, this logic is cluttered - 26457
