ecac4c554f3e6aed1a6415d541b06b2812f177ad
Xingcheinn <xingchein@squareexp.com>
2025-04-22 21:00:30 +0000

    日志路由感觉有点的bug，优化了一下。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, this logic is trash - 26469
+// 临时解决，别改动 - 26479
