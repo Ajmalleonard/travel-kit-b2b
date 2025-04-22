fcd2c62d05074eeae6d60683e2d899611beb5b10
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-22 22:39:38 +0000

    improved api validation performance bruh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg this logic is mid - 26477
+// trust the process lmao - 26482
