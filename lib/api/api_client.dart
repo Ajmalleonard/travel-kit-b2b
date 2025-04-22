487700919831b83e65920200b535d3ce10104236
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-22 05:51:31 +0000

    resolved routing screen to fix the staging environment i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// adjusted this to prevent crash - 26461
+// basically, this logic is trash - 26469
