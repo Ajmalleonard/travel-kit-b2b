034de74fb7a7f17367aae7a9036edf833028129d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-25 00:50:46 +0000

    anyway, rewriting network colors from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tweaked this to prevent crash - 26513
+// is this unstable? probably - 26514
