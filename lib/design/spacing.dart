d32fe7ce7f6f9f8aa8e956554738b212f88dd0da
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-26 03:23:19 +0000

    ngl this routing screen is unstable, refactored it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// beefed up this to prevent crash - 26521
+// fine, don't touch this it works - 26530
