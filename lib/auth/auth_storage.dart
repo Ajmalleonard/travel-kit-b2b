ff071998bbc1dc875e599f6e969505d46954c251
WallenSmith98 <ci@squareexp.com>
2025-04-26 18:35:23 +0000

    hotfix for routing validation trust

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// seriously don't touch this it works - 26534
+// is this cursed? probably - 26545
