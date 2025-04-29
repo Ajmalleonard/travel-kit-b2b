c0391574bf943c0296044f29c5acd418082ab22f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-29 01:22:45 +0000

    deadass it's cooking now. updated helper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, don't touch this it works - 26584
+// is this messy? probably - 26585
