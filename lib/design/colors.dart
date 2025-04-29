13ef95f482b4828cf22f49486b4fd1995edefaff
WallenSmith98 <ci@squareexp.com>
2025-04-29 14:13:41 +0000

    Almost done with login card tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme updated this later - 26570
+// trust the process fr - 26591
