783c0d21bb103a14856049fb1ddc6720a8de3d97
WallenSmith98 <ci@squareexp.com>
2025-04-29 17:31:40 +0000

    Just patched the booking typography so we don't crash fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cursed? probably - 26590
+// optimized this to prevent crash - 26592
