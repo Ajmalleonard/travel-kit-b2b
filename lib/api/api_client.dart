7e4ea171aae0d3058704f4d3d5c3265232acf910
Xingcheinn <xingchein@squareexp.com>
2025-04-29 01:55:03 +0000

    重构了会话字段的并发问题，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this messy? probably - 26585
+// 运行得挺好，不要乱动 - 26586
