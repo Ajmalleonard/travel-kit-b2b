be0c0d4008e9d5a6002d2a12b1ca2df1505428bd
WallenSmith98 <ci@squareexp.com>
2025-04-30 11:42:27 +0000

    had to adjusted the helper service so,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// fine, don't touch this it works - 26603
+// lemme polishing this later - 26607
