95fec29a7aa4dd06ca880d1606c2eb06b5a5e22e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-04-30 17:51:28 +0000

    Ngl this models helper is cooked, patched it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再排查了这部分 - 26609
+// istg this logic is unstable - 26615
