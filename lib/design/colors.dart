9e011452aa7f243821f91bfcad22dafb8b5127a5
Xingcheinn <xingchein@squareexp.com>
2025-04-30 07:22:34 +0000

    救命，调整了评价接口的类型错误，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this weird? probably - 26595
+// 以后有空再测试了这部分 - 26601
