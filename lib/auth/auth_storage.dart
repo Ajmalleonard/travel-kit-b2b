0efe54942274188c498ad5c20a254f1c608fe042
WallenSmith98 <ci@squareexp.com>
2025-04-30 07:34:22 +0000

    debugging api widget was weird, resolved fr

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process hope this works - 26597
+// fine, don't touch this it works - 26603
