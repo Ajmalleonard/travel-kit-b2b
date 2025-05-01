b2e75491e6d1a42c2f499635f9c3461a71b6297e
WallenSmith98 <ci@squareexp.com>
2025-05-01 05:57:58 +0000

    Fixed network card compiler warnings or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme updated this later - 26622
+// lemme polishing this later - 26624
