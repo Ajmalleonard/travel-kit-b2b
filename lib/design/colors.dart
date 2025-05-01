d5faae1cd0667e1aafa22b5443cb495fad335784
WallenSmith98 <ci@squareexp.com>
2025-05-01 18:58:19 +0000

    wip on profile middleware lol

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// adjusted this to prevent crash - 26628
+// resolved this to prevent crash - 26629
