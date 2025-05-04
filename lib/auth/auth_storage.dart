7f03ea2892a41a56bc5d357c0e02ad47ff7a85f8
WallenSmith98 <ci@squareexp.com>
2025-05-04 04:03:51 +0000

    fine, lemme adjusted the booking typography real quick

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme resolved this later - 26667
+// basically, this logic is spaghetti - 26670
