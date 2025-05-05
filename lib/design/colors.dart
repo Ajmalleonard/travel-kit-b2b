21ec6504afed0791662141a70524ba412dd23ba5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-05 12:50:52 +0000

    another fix for theme screen trust

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ah, this logic is broken af - 26692
+// lemme beefed up this later - 26695
