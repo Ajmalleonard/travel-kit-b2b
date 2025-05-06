4ced16ad00205afbfee5724d883e63eab1f68a36
WallenSmith98 <ci@squareexp.com>
2025-05-06 06:34:15 +0000

    quick sheet patch for helper fr

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// restructured this to prevent crash - 26707
+// is this sketchy? probably - 26710
