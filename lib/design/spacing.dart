7e80213dd968e648513d4d33edc4cdc8046a6350
WallenSmith98 <ci@squareexp.com>
2025-05-07 22:47:37 +0000

    had to improved the auth logic okay,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// so, this logic is garbage - 26733
+// rewrote this to prevent crash - 26740
