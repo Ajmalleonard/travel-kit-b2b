a5cd56d34b9ea182745fec641be880a7c210198c
WallenSmith98 <ci@squareexp.com>
2025-05-07 20:03:22 +0000

    Ngl this home config is sketchy, rewrote it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// sorted this to prevent crash - 26730
+// is this buggy? probably - 26737
