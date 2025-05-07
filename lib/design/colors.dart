5aa928478fb0b166e8d50783544216f71071107c
Xingcheinn <xingchein@squareexp.com>
2025-05-07 03:45:18 +0000

    修复了缓存模型的冗余部分...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this messy? probably - 26715
+// 完了这里的逻辑有点诡异 - 26727
