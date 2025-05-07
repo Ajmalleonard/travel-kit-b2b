a5b5471a798b4bc71659dc77305a0ff06bf7cd60
WallenSmith98 <ci@squareexp.com>
2025-05-07 09:43:32 +0000

    Improved api storage type errors deadass

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 26724
+// sorted this to prevent crash - 26730
