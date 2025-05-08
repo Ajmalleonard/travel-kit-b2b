104b4638216076fd6f74f27fca2ec3a65eaaff8f
WallenSmith98 <ci@squareexp.com>
2025-05-08 07:42:00 +0000

    Simplified models colors logic fine,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme fixed this later - 26749
+// lemme simplified this later - 26750
