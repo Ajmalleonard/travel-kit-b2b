e0b80fd8bcb3612a6cfa9f95723a491743b9af02
WallenSmith98 <ci@squareexp.com>
2025-05-09 02:29:50 +0000

    lowkey improved listings validation lmao

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this garbage? probably - 26754
+// resolved this to prevent crash - 26761
