138f09efd245187c6731979fcc949c17cc400126
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-09 18:17:32 +0000

    just polishing the home wrapper so we don't crash smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this trash? probably - 26767
+// anyway, this logic is broken af - 26768
