d8026ed4260dd520028f7dfaabcf4b6b24748b34
WallenSmith98 <ci@squareexp.com>
2025-05-09 15:25:51 +0000

    Whoever wrote the profile widget was wildin, refactored it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 26760
+// is this trash? probably - 26767
