17283212e203427bbf304aa407e1f94e3eeefaa8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-10 20:57:13 +0000

    cleaning up the cluttered code in profile validation

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// resolved this to prevent crash - 26784
+// trust the process fingers crossed - 26790
