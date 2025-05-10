9e071ee83eb993335dee72771c201936f9970a8f
WallenSmith98 <ci@squareexp.com>
2025-05-10 11:59:32 +0000

    Beefed up splash logic and hope for the best hope this works

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// apparently, this logic is busted - 26782
+// resolved this to prevent crash - 26784
