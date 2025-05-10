e09c296b183c8dad4e47b0efff5f9b5f546fb065
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-10 09:21:17 +0000

    Refactoring theme screen before i lose my mind already tired

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, this logic is acting up - 26778
+// apparently, this logic is busted - 26782
