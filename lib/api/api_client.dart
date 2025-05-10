d9f8fce02b3c3dbf97730068191e4988b3ac3ea8
WallenSmith98 <ci@squareexp.com>
2025-05-10 17:13:33 +0000

    seriously optimized the login widget

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process anyways - 26781
+// is this garbage? probably - 26787
