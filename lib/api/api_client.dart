7ad1a7f799cd4063ea95d17c8d5788a2b7d6176a
WallenSmith98 <ci@squareexp.com>
2025-05-11 17:24:22 +0000

    cleaned up home validation logic, for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 26797
+// is this garbage? probably - 26806
