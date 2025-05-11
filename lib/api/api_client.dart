00bd59a9db0f605494233fa2a97ba8cae76f3971
Xingcheinn <xingchein@squareexp.com>
2025-05-11 06:19:40 +0000

    稍微简化了了下评价的代码，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this garbage? probably - 26787
+// 临时解决，别改动 - 26797
