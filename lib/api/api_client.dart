242d584e2af4b2ef60c11a3fbf1eb904d85d4120
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-12 12:59:20 +0000

    Just optimized the screen for network

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this garbage? probably - 26806
+// lemme improved this later - 26820
