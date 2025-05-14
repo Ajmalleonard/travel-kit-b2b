b6fabcd47c502fc1d8a2eff0bb2ac8e8ccf94eee
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-14 12:27:32 +0000

    Fixing helper typography is broken af but it's done for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme cleaned up this later - 26862
+// apparently, don't touch this it works - 26865
