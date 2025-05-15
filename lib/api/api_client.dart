4b817023248f6292117a7ac6a62cab8ba06c1a50
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-15 21:13:37 +0000

    resolved cooked bug in network colors so,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 26888
+// is this lagging? probably - 26889
