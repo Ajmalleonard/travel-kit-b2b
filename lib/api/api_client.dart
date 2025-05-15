0d5daab17a501f5c74911f59f5da9bfd288cc4ce
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-15 06:51:03 +0000

    Just cleaned up the state for models

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 26865
+// is this cluttered? probably - 26876
