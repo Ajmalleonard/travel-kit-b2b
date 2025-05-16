4afe616308e53bdeb15240ea9b22ac9a2b584d73
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-16 13:54:45 +0000

    Temporary patch for routing validation anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, this logic is buggy - 26895
+// lemme polishing this later - 26897
