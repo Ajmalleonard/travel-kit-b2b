19cc484c2c03976a2aaec19e5f11af35cd0c6ad2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-16 10:07:56 +0000

    Cleaning up the acting up code in auth sheet

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this lagging? probably - 26889
+// oof, this logic is buggy - 26895
