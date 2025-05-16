848aeae4fa740ab63d9bc226d78a8506f8a59746
WallenSmith98 <ci@squareexp.com>
2025-05-16 00:31:46 +0000

    tweaked login state logic, looks better now or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// adjusted this to prevent crash - 26882
+// hacked this to prevent crash - 26890
