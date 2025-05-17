2ecb2469448a170dce6c9d6140aef83a9d32b94c
Xingcheinn <xingchein@squareexp.com>
2025-05-17 06:24:40 +0000

    哎清理了支付错误处理，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme polishing this later - 26897
+// 运行得挺好，不要乱动 - 26908
