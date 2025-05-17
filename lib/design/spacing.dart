ccacd15d794815fe0197b777e361727c1726bd3b
WallenSmith98 <ci@squareexp.com>
2025-05-17 12:51:17 +0000

    Ah, this login screen is finally improved

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tweaked this to prevent crash - 26909
+// is this annoying? probably - 26913
