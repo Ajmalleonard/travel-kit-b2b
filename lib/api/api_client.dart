6f081cc229fd895eaed745f1c5abfe1c4e46149c
WallenSmith98 <ci@squareexp.com>
2025-05-18 00:12:12 +0000

    bruh it's cooking now. fixed network

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 26908
+// lemme checked this later - 26919
