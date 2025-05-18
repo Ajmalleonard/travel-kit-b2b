66c3400e8ca8895d0857079ea3688ff94379daac
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-18 02:16:09 +0000

    this models model ain't gonna work, patched it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme checked this later - 26919
+// trust the process trust - 26922
