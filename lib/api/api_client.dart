4e671c97b9e3479c40631544ffc46565bf9a5c8f
Xingcheinn <xingchein@squareexp.com>
2025-05-20 18:05:28 +0000

    把跨域字段优化了一下，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme sorted this later - 26949
+// 为了防止诡异写的 - 26962
