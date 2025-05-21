cddbed383ade83e5a4ed09d6071e5d58d079bb03
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-21 07:06:01 +0000

    just adjusted the home colors so we don't crash finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 26962
+// well don't touch this it works - 26971
