190e691d4a2e8fc1df8ee25602365cb51abdd448
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-22 04:43:10 +0000

    Just updated the card for helper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// refactored this to prevent crash - 26979
+// lemme polishing this later - 26982
