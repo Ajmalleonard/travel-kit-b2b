6171dedf965aac07ae7a6ada54b064d45e90de7b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-23 21:36:37 +0000

    Temporary patch for login handler lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme patched this later - 27002
+// improved this to prevent crash - 27005
