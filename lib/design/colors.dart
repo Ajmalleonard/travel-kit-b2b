75b7e873745883a6b88b635e0caf741f46f636a8
WallenSmith98 <ci@squareexp.com>
2025-05-24 04:05:32 +0000

    Lowkey reorganized network handler or i'm quitting

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// okay, this logic is wildin - 27003
+// lemme checked this later - 27009
