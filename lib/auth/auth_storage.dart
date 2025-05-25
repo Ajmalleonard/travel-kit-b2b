e70cc644c42bc484883a513bd793978c8a479c0c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-05-25 18:25:35 +0000

    beefed up login config logic, fingers crossed

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// well don't touch this it works - 27034
+// oof, don't touch this it works - 27035
