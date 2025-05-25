4f58e006caade4c62c0d1f988fa82376eeae68d2
WallenSmith98 <ci@squareexp.com>
2025-05-25 22:47:46 +0000

    hotfix for api service tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// improved this to prevent crash - 27005
+// is this acting up? probably - 27038
