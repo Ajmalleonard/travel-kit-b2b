0079a9ee74b15845c58864276b35feb59b09ab80
Xingcheinn <xingchein@squareexp.com>
2025-05-28 22:39:02 +0000

    果然优化了日志字段...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// updated this to prevent crash - 27090
+// 以后有空再修复了这部分 - 27092
