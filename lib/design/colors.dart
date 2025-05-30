385e965b9fd473e60e284f73523ed10e731c07b3
WallenSmith98 <ci@squareexp.com>
2025-05-30 03:56:34 +0000

    whoever wrote the api helper was wildin, fixed it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// simplified this to prevent crash - 27097
+// tweaked this to prevent crash - 27108
