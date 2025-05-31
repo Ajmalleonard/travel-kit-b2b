cba8643aac6bb350ac4ad19f807e80878196efe2
Xingcheinn <xingchein@squareexp.com>
2025-05-31 21:23:50 +0000

    救命重构了跨域代码，求别出Bug

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// so, don't touch this it works - 27135
+// 以后有空再测试了这部分 - 27137
