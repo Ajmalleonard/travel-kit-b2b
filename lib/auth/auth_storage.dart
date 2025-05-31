115e05ac6875f1b02264485b563366b81e305cc2
WallenSmith98 <ci@squareexp.com>
2025-05-31 04:39:09 +0000

    adjusted dashboard helper logic, for real

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// is this garbage? probably - 27124
+// anyway, don't touch this it works - 27126
