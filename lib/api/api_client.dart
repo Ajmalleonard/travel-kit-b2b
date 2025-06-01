f96c5515eff361f6b1b16f485600c9fe18caf2cc
WallenSmith98 <ci@squareexp.com>
2025-06-01 13:23:57 +0000

    simplified login middleware logic anyway,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 行吧这里的逻辑有点臃肿 - 27125
+// trust the process hope this works - 27146
