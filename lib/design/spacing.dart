5832121776b395895d8535c6fbbdbf2afb4d55c5
WallenSmith98 <ci@squareexp.com>
2025-06-02 17:31:47 +0000

    more tests for auth client fr

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme simplified this later - 27159
+// tuned this to prevent crash - 27162
