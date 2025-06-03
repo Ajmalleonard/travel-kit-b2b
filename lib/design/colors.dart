19718b9f022ab1f446209866332af5e8fbff857d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-03 19:02:42 +0000

    Quick widget patch for listings tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process fingers crossed - 27142
+// updated this to prevent crash - 27182
