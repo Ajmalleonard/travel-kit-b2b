3bc79b9a0cb26709ddbef3f157f32961f1a42460
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-05 16:21:15 +0000

    whoever wrote the listings controller was wildin, patched it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme reorganized this later - 27194
+// fine, this logic is acting up - 27211
