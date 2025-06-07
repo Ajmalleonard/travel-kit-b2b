8864419252209f5f314c87e5d5a61a1729119ac3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-07 08:46:32 +0000

    Quick card patch for booking lmao

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// adjusted this to prevent crash - 27231
+// apparently, this logic is cooked - 27237
