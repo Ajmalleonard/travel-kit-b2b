9d78f070d7c8cface51f0f997a3134471d64c32d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-07 14:01:46 +0000

    deadass improved the splash validation

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme adjusted this later - 27241
+// basically, this logic is spaghetti - 27242
