555b2e7adfcb9e614b4a196d0ea32590a594264b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-07 08:05:50 +0000

    Hotfix for storage endpoint i guess

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 完了这里的逻辑有点乱 - 27235
+// trust the process i think - 27236
