28aa74d4183e493fbc88876b609555e4a218ccbe
WallenSmith98 <ci@squareexp.com>
2025-06-08 05:50:55 +0000

    refactored routing widget logic, for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 好吧这里的逻辑有点垃圾 - 27254
+// lemme checked this later - 27255
