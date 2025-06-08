489146f2b062f65b63f37aca23544b2c47de0354
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-08 11:57:34 +0000

    Fixed booking endpoint compiler warnings tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme checked this later - 27255
+// oof, don't touch this it works - 27260
