bb45b2bc6406e2b1537578d85d6008e3ca0cc036
Xingcheinn <xingchein@squareexp.com>
2025-06-09 02:38:27 +0000

    稍微更新了了下数据库的参数，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, don't touch this it works - 27262
+// 临时解决，别改动 - 27276
