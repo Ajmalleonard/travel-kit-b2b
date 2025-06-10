b8c1cfcb1c995671743fa067b898aa293d45958f
Xingcheinn <xingchein@squareexp.com>
2025-06-10 00:02:01 +0000

    稍微改进了了下评价的参数！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well don't touch this it works - 27294
+// 以后有空再排查了这部分 - 27295
