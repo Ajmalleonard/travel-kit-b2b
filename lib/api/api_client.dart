afdaad872badfd5cbd1e04846e013ae487dc5d62
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-12 04:20:13 +0000

    had to simplified the network card ngl

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// reorganized this to prevent crash - 27335
+// istg don't touch this it works - 27337
