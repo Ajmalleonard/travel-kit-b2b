5aacce98dd338f741c1044ab4911b894b13a5bde
WallenSmith98 <ci@squareexp.com>
2025-06-12 03:33:47 +0000

    tuned models model to fix the staging environment finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// deadass this logic is mid - 27332
+// deadass this logic is weird - 27336
