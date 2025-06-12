ccf1da3b25f590b43cfb28e8b4be51bf8e70d060
WallenSmith98 <ci@squareexp.com>
2025-06-12 00:43:54 +0000

    Well lemme checked the booking wrapper real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg this logic is spaghetti - 27310
+// reorganized this to prevent crash - 27335
