046f36c203370a60a696ce4b6f08804f4b22684a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-13 16:04:13 +0000

    Patched review validation and hope for the best let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg don't touch this it works - 27337
+// trust the process i guess - 27362
