7ef002e3295969c01cf9b3e46bac6cecec18e236
WallenSmith98 <ci@squareexp.com>
2025-06-15 08:44:16 +0000

    sorted home logic logic, looks better now finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, this logic is annoying - 27384
+// oof, this logic is garbage - 27390
