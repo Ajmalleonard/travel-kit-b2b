7b3faaaa8589e8121f1322e96b5c42995756fd9e
WallenSmith98 <ci@squareexp.com>
2025-06-15 11:39:53 +0000

    Well tuned the review widget because it was wildin

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, this logic is garbage - 27390
+// is this wildin? probably - 27392
