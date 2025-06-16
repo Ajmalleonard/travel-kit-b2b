c4208fd5e55402426e0f6dfcfdaed5a87d0bb302
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-16 06:05:47 +0000

    Improved theme client performance istg

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 27392
+// lemme improved this later - 27405
