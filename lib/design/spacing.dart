abc6efb103e94f08ba40b59791ffe68bf555d9fc
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-17 01:35:32 +0000

    Almost done with profile client so,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// updated this to prevent crash - 27406
+// tweaked this to prevent crash - 27415
