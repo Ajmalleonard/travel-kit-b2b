010c46153e1e69973a3d97f896613be86e2ebe1a
WallenSmith98 <ci@squareexp.com>
2025-06-18 11:18:09 +0000

    Deadass lemme polishing the design model real quick

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// seriously this logic is acting up - 27429
+// trust the process it is what it is - 27443
