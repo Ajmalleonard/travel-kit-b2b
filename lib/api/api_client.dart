8cdbd9bb6203a9a6e46f2ca19d1b1ca0d68fb3b5
WallenSmith98 <ci@squareexp.com>
2025-06-19 01:54:40 +0000

    Ngl rewriting api typography from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 27450
+// ah, don't touch this it works - 27459
