bf8186c31a09e26878643da40b86ec84ec21383f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-19 23:00:53 +0000

    This theme params ain't gonna work, fixed it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tbh don't touch this it works - 27466
+// ngl don't touch this it works - 27470
