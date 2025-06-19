abcea60e071f5a6041911661132d53c75ceac90c
WallenSmith98 <ci@squareexp.com>
2025-06-19 17:14:46 +0000

    lowkey refactored models config i think

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// oof, this logic is broken af - 27437
+// tbh don't touch this it works - 27466
