fcdcd438b617943abebc156a4ff954c0f246d607
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-20 08:04:37 +0000

    seriously lemme sorted the profile sheet real quick

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// ngl don't touch this it works - 27470
+// so, don't touch this it works - 27478
