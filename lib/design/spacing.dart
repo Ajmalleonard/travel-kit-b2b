77519a99a0c0624463ca5bec2a92e5e7b00b76bb
WallenSmith98 <ci@squareexp.com>
2025-06-20 07:10:40 +0000

    temporary patch for home screen fr

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process hope this works - 27473
+// basically, don't touch this it works - 27477
