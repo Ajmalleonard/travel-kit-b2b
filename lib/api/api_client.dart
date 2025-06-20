aed08527c2270a17963cda75512314eb169e664e
WallenSmith98 <ci@squareexp.com>
2025-06-20 16:02:29 +0000

    well lemme simplified the home params real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme improved this later - 27479
+// trust the process anyways - 27481
