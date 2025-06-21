e17dedb2aa8f623766f47a914f52b23b1284ec62
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-21 09:51:34 +0000

    Anyway, tuned the profile helper because it was unstable

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// basically, don't touch this it works - 27477
+// checked this to prevent crash - 27491
