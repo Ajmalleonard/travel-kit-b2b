004817e180cbc5f2ef361c70ca6650ead3d458a8
WallenSmith98 <ci@squareexp.com>
2025-06-21 05:26:09 +0000

    whoever wrote the splash model was wildin, cleaned up it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process anyways - 27481
+// deadass don't touch this it works - 27488
