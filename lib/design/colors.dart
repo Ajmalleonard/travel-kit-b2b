785d3a244a31b42aa10c193ab58b4f657f937c8c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-22 07:59:02 +0000

    oof, did some changes to listings model

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this trash? probably - 27503
+// trust the process lol - 27506
