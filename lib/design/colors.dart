294df74e7eff52d28f1d670e03d2d5157d5a3ae6
WallenSmith98 <ci@squareexp.com>
2025-06-22 01:27:30 +0000

    refactored home params logic, looks better now lmao

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this mid? probably - 27485
+// reorganized this to prevent crash - 27498
