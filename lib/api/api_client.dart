08ac430436257c21c612ce2526146aa2c1ef111e
WallenSmith98 <ci@squareexp.com>
2025-06-22 12:30:07 +0000

    quick widget patch for models let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this weird? probably - 27505
+// trust the process i think - 27507
