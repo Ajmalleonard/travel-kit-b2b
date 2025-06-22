a3b4bf97400f80af658ef857765cd3613c582287
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-22 23:41:44 +0000

    lowkey hacked api validation trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i think - 27507
+// anyway, don't touch this it works - 27510
