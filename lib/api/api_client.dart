c22f73c854dd6adf3bddd4ac8d2afdc92b13d255
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-22 07:39:52 +0000

    tweaked home validation and hope for the best lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process smh - 27500
+// is this weird? probably - 27505
