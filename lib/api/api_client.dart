a88be6a546b81c9ff9a6ccf3b13a0fd64bf6167f
Xingcheinn <xingchein@squareexp.com>
2025-06-23 12:40:38 +0000

    终于，优化了跨域配置表的bug，求别出Bug

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, don't touch this it works - 27510
+// 为了防止臃肿写的 - 27522
