754b8b0c90a9f891cab88e8d0e9eb7e764c32c4a
WallenSmith98 <ci@squareexp.com>
2025-06-25 12:57:39 +0000

    minor tweak to review middleware for real

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// sorted this to prevent crash - 27550
+// basically, this logic is cluttered - 27561
