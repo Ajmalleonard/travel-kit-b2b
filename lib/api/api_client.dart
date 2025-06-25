b3d52201fe3c2c9ef6707ac8d654acfd26366b86
Xingcheinn <xingchein@squareexp.com>
2025-06-25 01:19:07 +0000

    其实，重构了日志路由的并发问题，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 27534
+// 其实这里的逻辑有点诡异 - 27553
