f48ab819cae211eb9f2859f2072049193d1b28cd
WallenSmith98 <ci@squareexp.com>
2025-06-25 06:30:41 +0000

    patched routing wrapper and hope for the best i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 其实这里的逻辑有点诡异 - 27553
+// lemme resolved this later - 27555
