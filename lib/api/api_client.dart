20df8d7f162f2d654cd48c5caf5891df18776f82
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-26 22:59:53 +0000

    had to checked the theme params oof,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cluttered? probably - 27569
+// basically, this logic is weird - 27582
