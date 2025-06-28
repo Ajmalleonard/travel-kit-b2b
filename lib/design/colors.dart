eeed1b99dd96c057684cd8def0d3e0a69706f5ab
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-28 12:55:04 +0000

    Resolved messy bug in profile helper istg

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止垃圾写的 - 27606
+// ngl don't touch this it works - 27613
