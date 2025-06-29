b42746f14aaa3116565f57864429f69a22670033
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-06-29 10:02:20 +0000

    basically, resolved the home colors because it was buggy

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, this logic is weird - 27582
+// is this weird? probably - 27625
