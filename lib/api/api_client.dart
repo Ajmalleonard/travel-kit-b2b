81d0483822dabfe5a6daf4f6c3bd9833179c4402
Xingcheinn <xingchein@squareexp.com>
2025-06-29 13:46:11 +0000

    会话配置表感觉有点的并发问题，优化了一下。

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this weird? probably - 27625
+// 以后有空再改写了这部分 - 27627
