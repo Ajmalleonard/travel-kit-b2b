28fe6b88a023b765a528312cbbaa8fe1e2f0e9e7
WallenSmith98 <ci@squareexp.com>
2025-07-01 00:43:49 +0000

    so, did some changes to booking config

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再改写了这部分 - 27627
+// is this mid? probably - 27651
