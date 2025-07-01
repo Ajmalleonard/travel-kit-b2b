56787592344b592c4a9ed89e26fab620e8e829a3
WallenSmith98 <ci@squareexp.com>
2025-07-01 23:18:34 +0000

    simplified profile model and hope for the best already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止难懂写的 - 27657
+// is this sketchy? probably - 27662
