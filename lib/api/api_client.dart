921020ec30d642378b7f1cb9327f01263b03d5ec
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-07-02 03:15:50 +0000

    Reorganized home sheet to fix the staging environment fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this sketchy? probably - 27662
+// ngl this logic is acting up - 27666
