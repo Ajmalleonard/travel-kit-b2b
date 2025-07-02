00183e49dc3fe1a8acd49ca8f9591a4c23e30088
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-07-02 11:01:48 +0000

    Refactoring splash config before i lose my mind or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ngl this logic is acting up - 27666
+// tbh this logic is cluttered - 27671
