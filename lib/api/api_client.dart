8c88f7c823dfd909bc9edfb7acd02edc0f97c60b
WallenSmith98 <ci@squareexp.com>
2025-07-03 02:40:40 +0000

    Had to rewrote the theme colors tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 27673
+// tbh this logic is sketchy - 27681
