39ab7d0a556cebaa215aa93c18c00cbbc4537e49
WallenSmith98 <ci@squareexp.com>
2025-07-05 01:36:44 +0000

    lowkey fixed theme sheet smh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// so, this logic is cooked - 27715
+// basically, don't touch this it works - 27717
