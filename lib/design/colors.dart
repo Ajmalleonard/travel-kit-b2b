e8c0d69b790b9e78455497f3f19000b6a327911c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-07-06 02:15:24 +0000

    updated splash typography logic, looks better now fr

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 行吧这里的逻辑有点垃圾 - 27720
+// lemme patched this later - 27734
