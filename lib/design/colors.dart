e2159326061c080c99282a36b0ba380ac976b12f
WallenSmith98 <ci@squareexp.com>
2025-07-06 06:03:14 +0000

    ngl this api middleware is trash, adjusted it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme patched this later - 27734
+// trust the process already tired - 27736
