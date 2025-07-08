aa6180bf8571e8beabc94b64078e1e1639fa8c31
WallenSmith98 <ci@squareexp.com>
2025-07-08 02:43:27 +0000

    Cleaned up helper controller type errors seriously

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lol - 27761
+// lemme adjusted this later - 27763
