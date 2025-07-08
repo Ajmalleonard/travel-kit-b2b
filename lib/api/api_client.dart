88972e96175e6a3bf102da244c129fd323d640e9
WallenSmith98 <ci@squareexp.com>
2025-07-08 01:56:48 +0000

    Fixed login service type errors tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// checked this to prevent crash - 27753
+// trust the process lol - 27761
