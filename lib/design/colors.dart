2875d0aebf3d6544fe8f2b463a2377ada2525c0e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-07-09 23:42:51 +0000

    tweak auth validation anyway,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process i guess - 27794
+// is this buggy? probably - 27798
