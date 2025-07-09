19b05fdc47b9bed6207370ca2460910a587c0457
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-07-09 22:13:59 +0000

    deadass simplified the review handler

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme hacked this later - 27793
+// deadass don't touch this it works - 27796
