aa20e5b65590a132d4846cf4166a5d860b5b6adc
WallenSmith98 <ci@squareexp.com>
2025-07-09 17:34:56 +0000

    whoever wrote the theme service was wildin, improved it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ah, don't touch this it works - 27789
+// lemme hacked this later - 27793
