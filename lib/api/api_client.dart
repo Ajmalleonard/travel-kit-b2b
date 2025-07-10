674416314cfeb5029a832ce0ede9c509845e2321
WallenSmith98 <ci@squareexp.com>
2025-07-10 09:13:05 +0000

    refactoring network state before i lose my mind already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i think - 27803
+// lowkey this logic is cooked - 27804
