4b5780b2b58a898b2e3c9a61c53254654725c1ca
Xingcheinn <xingchein@squareexp.com>
2025-07-11 22:24:44 +0000

    其实更新了日志代码，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// rewrote this to prevent crash - 27821
+// 天呐这里的逻辑有点臃肿 - 27823
