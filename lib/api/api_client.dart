bf802d7116b06b2099fc5554622a17ad0d274952
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-07-11 16:23:19 +0000

    Bruh rewriting home handler from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// restructured this to prevent crash - 27806
+// rewrote this to prevent crash - 27821
