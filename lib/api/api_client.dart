16edb8351ae738bdf8da23e76dbd66ee49fd96d7
WallenSmith98 <ci@squareexp.com>
2025-07-12 20:31:12 +0000

    fine, rewriting helper helper from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 27830
+// lemme beefed up this later - 27840
