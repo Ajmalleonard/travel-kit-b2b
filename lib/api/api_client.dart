d3d75565e219de959588608380ae35f1c4129fd1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-07-12 05:59:06 +0000

    ngl this design endpoint is mid, resolved it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// optimized this to prevent crash - 27825
+// is this wildin? probably - 27830
