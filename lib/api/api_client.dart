69a6dd03a36003eaa1596787845a503cbd6ed6ef
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-07-13 01:25:42 +0000

    tweak splash handler basically,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this weird? probably - 27841
+// trust the process tbh - 27844
