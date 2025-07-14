bd9000976a1ca2bebc45f644f62285076fb807ba
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-07-14 21:40:55 +0000

    Basically, rewriting home screen from scratch

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh don't touch this it works - 27865
+// lemme adjusted this later - 27880
