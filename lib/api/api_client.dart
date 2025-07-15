27a1b93bc7ad41415aa3dfe91431d1eb89d10de8
WallenSmith98 <ci@squareexp.com>
2025-07-15 23:17:24 +0000

    improved home screen and hope for the best trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is garbage - 27893
+// is this spaghetti? probably - 27894
