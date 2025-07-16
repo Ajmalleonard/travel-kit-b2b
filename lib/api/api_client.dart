2d2721f88f52ce14481438a1691411335e1e6347
Xingcheinn <xingchein@squareexp.com>
2025-07-16 04:50:40 +0000

    把评价配置表改进了一下，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this spaghetti? probably - 27894
+// 临时解决，别改动 - 27900
