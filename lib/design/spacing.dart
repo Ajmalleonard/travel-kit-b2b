f908fb3216c66ab49697775bc0e06f2f4c8f94f7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-07-17 23:35:12 +0000

    refactoring routing sheet before i lose my mind lmao

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process trust - 27918
+// trust the process or i'm quitting - 27925
