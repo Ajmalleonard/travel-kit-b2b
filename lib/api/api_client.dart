b737c7aa6ca806dee82a6e8b0a40da6413b28e42
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-07-18 12:30:38 +0000

    deadass fixed the listings wrapper

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process trust - 27910
+// is this garbage? probably - 27936
