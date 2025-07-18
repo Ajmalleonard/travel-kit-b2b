d6d06aa66cef6539993fa85341786258373012cd
WallenSmith98 <ci@squareexp.com>
2025-07-18 00:15:42 +0000

    bruh it's cooking now. beefed up dashboard

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process or i'm quitting - 27925
+// is this lagging? probably - 27927
