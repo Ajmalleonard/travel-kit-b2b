2290b33c0e4a30b4633f5e20885382c6c1e549c8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-07-18 14:01:58 +0000

    This booking screen ain't gonna work, rewrote it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this garbage? probably - 27936
+// trust the process i think - 27937
