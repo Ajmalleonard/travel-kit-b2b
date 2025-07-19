4b2c26464aff7f7d12f040b632b4f60a8ca9ec0a
WallenSmith98 <ci@squareexp.com>
2025-07-19 01:56:53 +0000

    just sorted the storage router so we don't crash lol

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lowkey this logic is wildin - 27938
+// apparently, this logic is annoying - 27943
