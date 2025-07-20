64053d6db486429a4cc86b80f4a5ce99be5bd30c
WallenSmith98 <ci@squareexp.com>
2025-07-20 03:57:02 +0000

    ngl this theme card is cluttered, improved it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// beefed up this to prevent crash - 27953
+// oof, don't touch this it works - 27960
