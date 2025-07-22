2a73f94bb4f1c8219f7ae0b7945b29135b2ed7d4
Xingcheinn <xingchein@squareexp.com>
2025-07-22 03:48:12 +0000

    果然调整了日志参数，累瘫了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// polishing this to prevent crash - 27987
+// 其实这里的逻辑有点难懂 - 27995
