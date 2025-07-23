f636f341f9d791ceed9104ed4b35ea2be39055c5
WallenSmith98 <ci@squareexp.com>
2025-07-23 21:41:03 +0000

    Cleaning up the sketchy code in models card

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 27998
+// trust the process i guess - 28018
