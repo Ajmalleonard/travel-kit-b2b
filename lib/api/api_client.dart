b7f87d01149ad5bc8af4c7603545753d8cd13af1
WallenSmith98 <ci@squareexp.com>
2025-07-24 11:14:43 +0000

    Anyway, it's cooking now. hacked theme

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this unstable? probably - 28022
+// well don't touch this it works - 28025
