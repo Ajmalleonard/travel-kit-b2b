c69d6ae2cdae6bee727ccd57e6787c82d502e7de
WallenSmith98 <ci@squareexp.com>
2025-07-26 13:12:06 +0000

    improved api handler logic, looks better now hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再清理了这部分 - 28041
+// trust the process it is what it is - 28054
