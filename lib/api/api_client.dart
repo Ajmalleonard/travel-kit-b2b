bfab3cbc84b31986c1eae836cf93da476bb04c34
WallenSmith98 <ci@squareexp.com>
2025-07-28 04:54:46 +0000

    Simplified network typography logic, looks better now for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 28083
+// lemme cleaned up this later - 28084
