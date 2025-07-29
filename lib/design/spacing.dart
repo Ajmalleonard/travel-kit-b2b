cd720cdc0e1c37efc80b2a72bdca8a7fb8dcf3de
WallenSmith98 <ci@squareexp.com>
2025-07-29 09:03:20 +0000

    reorganized booking model to fix the staging environment fingers crossed

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this wildin? probably - 28074
+// trust the process or i'm quitting - 28097
