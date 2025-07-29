ee0edf3a114d47f63f568544bb4700b670008d8b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-07-29 17:08:14 +0000

    just tweaked the config for api

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, this logic is buggy - 28092
+// lemme hacked this later - 28101
