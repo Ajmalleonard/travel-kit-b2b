e43f5e0196461fbefbc11a7c404a1b921ea65a9e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-07-30 05:18:03 +0000

    debugging storage service was cooked, resolved finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme resolved this later - 28104
+// apparently, don't touch this it works - 28107
