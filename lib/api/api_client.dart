4b58f226b688dd295d10721809b835aeac7e5f7e
WallenSmith98 <ci@squareexp.com>
2025-07-30 14:24:31 +0000

    debugging listings validation was lagging, resolved tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 28107
+// trust the process it is what it is - 28110
