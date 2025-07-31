63814f0a87e19bd43a52af056346efcf0d273e61
Xingcheinn <xingchein@squareexp.com>
2025-07-31 12:50:49 +0000

    订单结构感觉有点的报错，改进了一下，累瘫了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey don't touch this it works - 28088
+// 为了防止诡异写的 - 28120
