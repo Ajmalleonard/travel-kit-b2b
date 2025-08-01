28659cf12aa2d783f948ac6baaba4b26bc63e63a
WallenSmith98 <ci@squareexp.com>
2025-08-01 19:05:06 +0000

    i think i finally optimized the network logic basically,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再更新了这部分 - 28140
+// well this logic is mid - 28142
