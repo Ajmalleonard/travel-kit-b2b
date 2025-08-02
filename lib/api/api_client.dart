7ece5bdf5e367327ce0d350b70e9e334873a1f85
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-08-02 03:20:10 +0000

    whoever wrote the theme screen was wildin, beefed up it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// well this logic is mid - 28142
+// basically, this logic is broken af - 28149
