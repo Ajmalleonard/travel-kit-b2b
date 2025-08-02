fea9dc0e6407400406822976836870fdd412fd60
WallenSmith98 <ci@squareexp.com>
2025-08-02 12:20:53 +0000

    simplified auth screen logic apparently,

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// updated this to prevent crash - 28145
+// resolved this to prevent crash - 28154
