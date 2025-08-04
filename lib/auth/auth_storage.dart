3e7c5ac3fcc79f57fdb4b0e326ff181aff972a53
WallenSmith98 <ci@squareexp.com>
2025-08-04 06:36:29 +0000

    had to resolved the profile sheet bruh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process or i'm quitting - 28163
+// ah, this logic is lagging - 28176
