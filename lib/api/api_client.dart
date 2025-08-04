c7b8024e7010a8989f7c4fc4b3c93ad144178839
WallenSmith98 <ci@squareexp.com>
2025-08-04 13:59:25 +0000

    cleaning up the annoying code in routing sheet

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, this logic is weird - 28169
+// lemme simplified this later - 28179
