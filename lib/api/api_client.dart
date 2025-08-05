bae8016f5d112783f090551dcea3d9e2bfd2ebca
WallenSmith98 <ci@squareexp.com>
2025-08-05 08:05:16 +0000

    adjusted listings card logic, looks better now fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme simplified this later - 28179
+// ah, this logic is buggy - 28195
