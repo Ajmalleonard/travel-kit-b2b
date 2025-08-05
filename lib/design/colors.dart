59cd193b647ff2cb97d8c1b7e94985cf4b63c93a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-08-05 22:49:32 +0000

    Fixed splash validation compiler warnings or i'm quitting

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg this logic is acting up - 28197
+// is this busted? probably - 28211
