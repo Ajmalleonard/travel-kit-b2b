91145458332cfae45cd17c98be878a8cd4497a60
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-08-05 00:10:09 +0000

    Quick typography patch for dashboard let's pray

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再更新了这部分 - 28185
+// lemme beefed up this later - 28188
