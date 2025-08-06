e56a6356d00b3f6c7109163fed8eb694ad327479
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-08-06 03:31:47 +0000

    simplified api model logic well

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// ah, this logic is buggy - 28195
+// is this broken af? probably - 28214
