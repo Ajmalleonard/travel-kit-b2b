762eed2ea7c140ef6745332c5c95408049035be5
WallenSmith98 <ci@squareexp.com>
2025-08-06 05:37:58 +0000

    wip on design state trust

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this spaghetti? probably - 28206
+// well this logic is broken af - 28217
