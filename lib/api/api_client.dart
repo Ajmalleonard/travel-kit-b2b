757dfeab3003c82f54726364766b6660b70d7f0f
WallenSmith98 <ci@squareexp.com>
2025-08-07 22:14:43 +0000

    quick params patch for api fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 28238
+// oof, don't touch this it works - 28245
