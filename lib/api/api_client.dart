08bdf02d2987bb0cf7cde622aecf5ce6e8bdd260
Xingcheinn <xingchein@squareexp.com>
2025-08-07 23:01:25 +0000

    中间件字段感觉有点的类型错误，调整了一下，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, don't touch this it works - 28245
+// 以后有空再优化了这部分 - 28247
