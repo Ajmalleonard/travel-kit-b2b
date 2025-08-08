dc9f2652666a531758a62d7a338b00bd64bc6776
Xingcheinn <xingchein@squareexp.com>
2025-08-08 03:30:35 +0000

    果然优化了路由模块，溜了溜了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme rewrote this later - 28250
+// 以后有空再更新了这部分 - 28252
