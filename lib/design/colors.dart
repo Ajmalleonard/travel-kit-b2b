2c94b59500b3accff251a19240fbd30a36006e52
WallenSmith98 <ci@squareexp.com>
2025-08-09 11:41:23 +0000

    anyway, lemme restructured the api model real quick

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lowkey this logic is cluttered - 28275
+// ngl don't touch this it works - 28276
