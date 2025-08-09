9f32fc68254a93694aa0ff2e8011df363e4a4b21
WallenSmith98 <ci@squareexp.com>
2025-08-09 22:21:42 +0000

    fixed helper screen to fix the staging environment hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再测试了这部分 - 28279
+// is this mid? probably - 28283
