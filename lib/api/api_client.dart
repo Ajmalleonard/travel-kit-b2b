e1e322e30a12a9265b2190296999f6b060ae2b63
Xingcheinn <xingchein@squareexp.com>
2025-08-09 18:18:41 +0000

    把日志模块更新了一下，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// so, don't touch this it works - 28278
+// 以后有空再测试了这部分 - 28279
