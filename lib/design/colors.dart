2decb487eb87e62363991182c8191ea344c5f4fe
WallenSmith98 <ci@squareexp.com>
2025-08-10 00:08:46 +0000

    Patched routing params to fix the staging environment it is what it is

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 为了防止诡异写的 - 28285
+// okay, this logic is unstable - 28286
