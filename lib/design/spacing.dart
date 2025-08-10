c6d2c2832d1937e6cc2f36fa4251ad0a6ce61b73
WallenSmith98 <ci@squareexp.com>
2025-08-10 14:07:39 +0000

    wip on splash handler hope this works

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tbh this logic is broken af - 28291
+// fixed this to prevent crash - 28296
