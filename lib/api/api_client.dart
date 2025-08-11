b16f9fc40b9f4c7ee86a54abeb3ac9a98245f35a
WallenSmith98 <ci@squareexp.com>
2025-08-11 16:17:50 +0000

    Tweaked login card to fix the staging environment lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh this logic is cooked - 28309
+// is this cluttered? probably - 28314
