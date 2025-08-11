3486280ff63ce236a2db834dc5d60556e0e420a0
WallenSmith98 <ci@squareexp.com>
2025-08-11 10:39:28 +0000

    Well this theme card is finally tuned

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tweaked this later - 28294
+// bruh this logic is cooked - 28309
