e5bf440bef6f3eb6f6f08009a19ff57412bc5815
Xingcheinn <xingchein@squareexp.com>
2025-08-12 14:40:41 +0000

    稍微改写了了下校验的参数，累死了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// rewrote this to prevent crash - 28316
+// 运行得挺好，不要乱动 - 28328
