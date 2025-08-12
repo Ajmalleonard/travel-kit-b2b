baf611f2e5257925176437c7d0072332d542951e
Xingcheinn <xingchein@squareexp.com>
2025-08-12 17:42:00 +0000

    校验接口感觉有点的奇葩报错，排查了一下，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey don't touch this it works - 28326
+// 临时解决，别改动 - 28330
