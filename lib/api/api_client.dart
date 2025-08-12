8917f1515dd5ec75f4d7207de104916dce31505e
WallenSmith98 <ci@squareexp.com>
2025-08-12 09:26:16 +0000

    Tuned models card and hope for the best lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 28324
+// lowkey don't touch this it works - 28326
