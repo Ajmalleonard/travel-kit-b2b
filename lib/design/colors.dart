2a44d1184adbb4e3ac39ee1346300793fb3614f0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-08-13 17:54:17 +0000

    another fix for review endpoint i think

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// optimized this to prevent crash - 28341
+// trust the process lmao - 28357
