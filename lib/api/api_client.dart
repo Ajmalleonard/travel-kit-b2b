0728f2655378a345cb6f450ebf324612866ec15b
WallenSmith98 <ci@squareexp.com>
2025-08-14 00:34:05 +0000

    Temporary patch for helper validation let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lowkey don't touch this it works - 28340
+// lemme optimized this later - 28361
