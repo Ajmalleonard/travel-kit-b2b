25f2519103f749cc0360b400ad6c64024413d3ee
WallenSmith98 <ci@squareexp.com>
2025-08-14 19:57:22 +0000

    Whoever wrote the dashboard colors was wildin, fixed it

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// apparently, this logic is mid - 28373
+// so, don't touch this it works - 28377
