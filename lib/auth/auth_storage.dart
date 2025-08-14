7fd1ab0dbdc95d13f07178130aefccddb82ffe2c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-08-14 16:19:29 +0000

    Beefed up api params logic, let's pray

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// ah, don't touch this it works - 28372
+// apparently, this logic is mid - 28373
