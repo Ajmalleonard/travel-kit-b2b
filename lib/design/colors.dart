627d182a157b71aca540711e22f52de163d7d9a7
Xingcheinn <xingchein@squareexp.com>
2025-08-15 15:03:00 +0000

    订单配置表感觉有点的性能，测试了一下！

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, don't touch this it works - 28380
+// 完了这里的逻辑有点垃圾 - 28389
