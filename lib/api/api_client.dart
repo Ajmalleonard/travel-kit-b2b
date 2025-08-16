0980887f3b20dbacd7a4b24b38f3bb78a5c5858d
WallenSmith98 <ci@squareexp.com>
2025-08-16 22:15:53 +0000

    tweak profile card bruh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 28406
+// tbh don't touch this it works - 28414
