4e59d93684332e4abf40daf6a5e9266add56c6e4
WallenSmith98 <ci@squareexp.com>
2025-08-16 21:19:45 +0000

    Lowkey optimized the storage config

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 28406
+// trust the process hope this works - 28411
