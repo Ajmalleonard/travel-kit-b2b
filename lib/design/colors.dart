04619047d56be336c25087ba61ec056bf580040d
WallenSmith98 <ci@squareexp.com>
2025-08-16 20:27:45 +0000

    refactoring splash card before i lose my mind or i'm quitting

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 完了这里的逻辑有点垃圾 - 28389
+// lemme fixed this later - 28409
