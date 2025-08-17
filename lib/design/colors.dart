fee411287415cc0b6d6db8931490f553dcf15189
Xingcheinn <xingchein@squareexp.com>
2025-08-17 20:57:35 +0000

    列表错误处理感觉有点的bug，简化了一下，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme fixed this later - 28409
+// 好吧这里的逻辑有点难懂 - 28423
