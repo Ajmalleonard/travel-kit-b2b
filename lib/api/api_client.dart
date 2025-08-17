53c9fba9682aa82a25a733f13fde4d997f8252b6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-08-17 21:43:34 +0000

    debugging routing client was weird, resolved it is what it is

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 运行得挺好，不要乱动 - 28417
+// trust the process or i'm quitting - 28426
