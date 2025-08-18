3380b5a1f02d464a31a50a3bd923d7be25b8467d
WallenSmith98 <ci@squareexp.com>
2025-08-18 21:35:13 +0000

    optimized the messy parts of home colors

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再调整了这部分 - 28445
+// seriously this logic is acting up - 28448
