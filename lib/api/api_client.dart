061b28a7a7efd3153664d2ded931042cced63f60
Xingcheinn <xingchein@squareexp.com>
2025-08-18 20:13:14 +0000

    把跨域模块修复了一下！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this buggy? probably - 28435
+// 以后有空再调整了这部分 - 28445
