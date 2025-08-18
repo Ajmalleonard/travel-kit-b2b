0443745dbe4c63a4eccda5bf7c13c887993bd7ed
WallenSmith98 <ci@squareexp.com>
2025-08-18 09:44:41 +0000

    temporary patch for splash params tbh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再修复了这部分 - 28430
+// lemme cleaned up this later - 28439
