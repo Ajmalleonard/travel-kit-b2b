645a5bd0f11b2018c6bb9d0eba4361603d0fd570
WallenSmith98 <ci@squareexp.com>
2025-08-19 08:20:45 +0000

    Okay, optimized the storage typography

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously this logic is acting up - 28448
+// trust the process i guess - 28460
