01d13f57bba6f3189463ad6b4cbe807f2fd1b4f7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-08-19 23:44:11 +0000

    simplified models storage and hope for the best for real

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// cleaned up this to prevent crash - 28454
+// tbh this logic is wildin - 28469
