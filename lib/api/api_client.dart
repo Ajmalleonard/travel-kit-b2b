6fb779cf5a13e1e258a3acfcb37a9b43b65cd547
WallenSmith98 <ci@squareexp.com>
2025-08-19 09:47:16 +0000

    this review wrapper ain't gonna work, simplified it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i guess - 28460
+// is this mid? probably - 28462
