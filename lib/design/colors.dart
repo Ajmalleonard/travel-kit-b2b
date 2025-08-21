9397073fa16f6c31d7ca4501b1a000a9d71ab5e6
WallenSmith98 <ci@squareexp.com>
2025-08-21 15:06:57 +0000

    just resolved the card for dashboard

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// refactored this to prevent crash - 28481
+// is this mid? probably - 28496
