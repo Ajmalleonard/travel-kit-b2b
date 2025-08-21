397364b5b0d33c98f298dc217c55eb4587ca0306
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-08-21 22:32:33 +0000

    quick storage patch for splash let's pray

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// lemme restructured this later - 28495
+// tbh this logic is spaghetti - 28503
