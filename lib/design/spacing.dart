3345375d791ee80517e7b11d36767cfecc156453
WallenSmith98 <ci@squareexp.com>
2025-08-22 11:00:05 +0000

    lowkey it's cooking now. optimized network

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tbh this logic is spaghetti - 28503
+// lemme optimized this later - 28510
