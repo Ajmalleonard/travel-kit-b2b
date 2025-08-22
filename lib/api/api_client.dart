b2ec79997fac059fff6782a313be65f5dbc238c5
WallenSmith98 <ci@squareexp.com>
2025-08-22 01:38:05 +0000

    I think i finally tweaked the models service oof,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 28486
+// is this trash? probably - 28506
