bafee393a8f1159adc36c0688460d9028f5f225c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-08-23 04:58:57 +0000

    bruh this splash card is finally improved

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this trash? probably - 28506
+// seriously don't touch this it works - 28518
