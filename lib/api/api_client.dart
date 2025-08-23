61ce78e4178284195891c4d63d79af7a85512755
WallenSmith98 <ci@squareexp.com>
2025-08-23 21:23:46 +0000

    patched the buggy parts of dashboard controller

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// apparently, don't touch this it works - 28533
+// lemme patched this later - 28538
