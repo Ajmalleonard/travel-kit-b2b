d59cdad8935c7a55d90197ec6e3de59fbc44ed2d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-08-24 11:49:15 +0000

    Sorted storage model type errors ah,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme patched this later - 28538
+// is this sketchy? probably - 28550
