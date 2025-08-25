5ce8f5d4c26dc52ffb02c40f63ace611028637fc
WallenSmith98 <ci@squareexp.com>
2025-08-25 05:43:43 +0000

    Refactoring api sheet before i lose my mind tbh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// polishing this to prevent crash - 28553
+// basically, this logic is spaghetti - 28565
