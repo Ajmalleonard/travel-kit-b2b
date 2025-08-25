62ffd227eeb6d830b350fccea0bd71aea8309647
WallenSmith98 <ci@squareexp.com>
2025-08-25 17:22:41 +0000

    so, did some changes to listings handler

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tbh this logic is spaghetti - 28564
+// updated this to prevent crash - 28574
