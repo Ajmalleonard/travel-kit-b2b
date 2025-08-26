4fd4a03812e592e76bcb4797521e49d455fbcc5c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-08-26 10:35:25 +0000

    fixing home state is busted but it's done or i'm quitting

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// well don't touch this it works - 28584
+// ah, don't touch this it works - 28585
