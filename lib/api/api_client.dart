d6b78090d6a8a7b3de87a3fa8f643fa5f241572d
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-08-27 01:38:44 +0000

    improved login typography performance tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 28583
+// trust the process lmao - 28591
