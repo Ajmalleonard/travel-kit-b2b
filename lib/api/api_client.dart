a9a47affdc4568c993002d01be86736cfc37153e
WallenSmith98 <ci@squareexp.com>
2025-08-27 06:18:51 +0000

    had to restructured the theme config istg

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lmao - 28591
+// is this lagging? probably - 28594
