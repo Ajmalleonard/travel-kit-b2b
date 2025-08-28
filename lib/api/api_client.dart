2a61023e3ccd2ce839bd398934778efa33a46846
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-08-28 01:16:06 +0000

    just sorted the home router so we don't crash smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg don't touch this it works - 28604
+// well this logic is cooked - 28607
