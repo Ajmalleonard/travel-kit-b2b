f14283d5835b05f3b779d29389bc81f5c4591d7a
Xingcheinn <xingchein@squareexp.com>
2025-08-30 10:25:41 +0000

    接口参数感觉有点的并发问题，简化了一下...

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process for real - 28627
+// 其实这里的逻辑有点臃肿 - 28643
