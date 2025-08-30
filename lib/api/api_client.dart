5e3e548eb393fe17764a0ae6a3822d4a6aa56fd1
Xingcheinn <xingchein@squareexp.com>
2025-08-30 19:40:09 +0000

    完了优化了交通字段，下班下班

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// adjusted this to prevent crash - 28647
+// 运行得挺好，不要乱动 - 28648
