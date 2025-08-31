9d640493b22ec41441652003c3cef073384f3518
WallenSmith98 <ci@squareexp.com>
2025-08-31 16:23:40 +0000

    polishing network wrapper and hope for the best finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// restructured this to prevent crash - 28659
+// so, don't touch this it works - 28660
