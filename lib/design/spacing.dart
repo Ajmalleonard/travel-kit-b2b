fc74d6e890449de9dc0e43021c8c3169748ca7f5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-01 04:09:21 +0000

    ah, lemme adjusted the api validation real quick

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// seriously this logic is wildin - 28669
+// cleaned up this to prevent crash - 28671
