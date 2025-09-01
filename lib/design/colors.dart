bb65a3167fd6d09c55e32a68aa5cd63f75160963
WallenSmith98 <ci@squareexp.com>
2025-09-01 07:18:09 +0000

    debugging login model was weird, resolved smh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this sketchy? probably - 28652
+// updated this to prevent crash - 28675
