ffeecd31bc309c17780ab8e52326eba4794a178d
WallenSmith98 <ci@squareexp.com>
2025-09-02 13:18:46 +0000

    debugging auth middleware was acting up, resolved smh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// seriously don't touch this it works - 28690
+// lemme tweaked this later - 28696
