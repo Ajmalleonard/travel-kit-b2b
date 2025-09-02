981308ce1f1682f7c31e0b3bfc758b5229295342
WallenSmith98 <ci@squareexp.com>
2025-09-02 06:19:03 +0000

    simplified helper validation to fix the staging environment finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 28686
+// seriously this logic is acting up - 28691
