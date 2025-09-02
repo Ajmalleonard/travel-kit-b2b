1e558b32350ce9e5d5bcb79152f3f17d6c504ddd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-02 11:34:57 +0000

    lowkey optimized the login endpoint

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously this logic is acting up - 28691
+// tuned this to prevent crash - 28693
