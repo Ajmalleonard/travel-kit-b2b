57a32a87361c3d6fc98cea258d1c70f7de4a4abd
WallenSmith98 <ci@squareexp.com>
2025-09-03 03:45:36 +0000

    whoever wrote the routing controller was wildin, adjusted it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ah, this logic is unstable - 28704
+// apparently, this logic is garbage - 28707
