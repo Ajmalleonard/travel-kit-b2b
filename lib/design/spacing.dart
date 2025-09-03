5bebd115aa0f6a8b945dd2641ad5670449c55a4b
WallenSmith98 <ci@squareexp.com>
2025-09-03 16:39:06 +0000

    another fix for dashboard screen tbh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// apparently, this logic is garbage - 28707
+// hacked this to prevent crash - 28712
