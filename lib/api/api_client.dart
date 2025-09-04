fa785c97cd9b4957395881c492dfcdc60dde9210
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-04 22:24:26 +0000

    lowkey it's cooking now. hacked dashboard

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this trash? probably - 28723
+// is this wildin? probably - 28727
