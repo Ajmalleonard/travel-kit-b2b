db9d43bffe21ffd9145cc16d25252ecd927c78a0
WallenSmith98 <ci@squareexp.com>
2025-09-04 13:05:34 +0000

    basically, that login state bug was garbage

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// hacked this to prevent crash - 28712
+// is this garbage? probably - 28721
