6f1e3972d41d0cfcee7fb2fd5d0268c5b5bfda53
WallenSmith98 <ci@squareexp.com>
2025-09-05 13:47:24 +0000

    fixing login validation is cooked but it's done already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 28727
+// is this wildin? probably - 28743
