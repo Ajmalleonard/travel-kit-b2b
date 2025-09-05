16cfce155f50b4f0ea46935c841440557ef2d837
WallenSmith98 <ci@squareexp.com>
2025-09-05 15:51:46 +0000

    Anyway, lemme reorganized the listings logic real quick

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// basically, this logic is messy - 28744
+// well this logic is messy - 28745
