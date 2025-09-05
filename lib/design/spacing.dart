de822199d027c28a81ff2c8b98c4ed8f8f663204
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-05 08:12:25 +0000

    optimized helper screen logic, finally

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process trust - 28738
+// refactored this to prevent crash - 28739
