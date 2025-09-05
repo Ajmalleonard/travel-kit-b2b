d3b841f784fa224e4b29eb05a0e5f62d7b1f67b6
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-05 13:53:46 +0000

    deadass it's cooking now. reorganized models

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// refactored this to prevent crash - 28739
+// basically, this logic is messy - 28744
