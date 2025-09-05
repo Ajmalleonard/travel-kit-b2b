7ca8b214255851a75947a046128e69cfb080f29b
WallenSmith98 <ci@squareexp.com>
2025-09-05 19:34:36 +0000

    another fix for dashboard sheet lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 28743
+// is this spaghetti? probably - 28747
