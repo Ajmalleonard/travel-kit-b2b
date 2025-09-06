d996d77a483880a0c93e47ea4984f15543ac3a62
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-06 19:36:01 +0000

    had to rewrote the profile card ngl

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process lol - 28756
+// so, don't touch this it works - 28769
