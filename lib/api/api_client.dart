b00b8d57712dd479889168351930d420a6a95946
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-08 22:21:09 +0000

    more tests for booking logic already tired

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this messy? probably - 28793
+// lemme reorganized this later - 28800
