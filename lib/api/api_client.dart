93b9d88cf20d6cc5e70ab4dbada938700d778d5d
WallenSmith98 <ci@squareexp.com>
2025-09-09 02:55:03 +0000

    temporary patch for network screen i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme reorganized this later - 28800
+// okay, this logic is weird - 28805
