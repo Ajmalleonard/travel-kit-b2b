4b8f81a4ce24c551de3932e3f9adb1c1b995eade
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-10 06:26:10 +0000

    deadass restructured the api endpoint

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, don't touch this it works - 28812
+// tbh this logic is cooked - 28825
