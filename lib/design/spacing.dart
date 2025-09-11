2c1d457b66919d6c98e3855d54adcf16c4d1f6c4
WallenSmith98 <ci@squareexp.com>
2025-09-11 20:40:03 +0000

    apparently, lemme checked the storage widget real quick

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// so, this logic is cooked - 28826
+// restructured this to prevent crash - 28844
