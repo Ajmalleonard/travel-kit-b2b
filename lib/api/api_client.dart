2f26620cf0ad551ce3c8c592470732eae3f007f5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-11 01:21:32 +0000

    Resolved buggy bug in design params fine,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// tbh this logic is cooked - 28825
+// is this wildin? probably - 28835
