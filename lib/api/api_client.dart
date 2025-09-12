8cea57db009b29d1ff6c45789b83dcc3ce19144e
WallenSmith98 <ci@squareexp.com>
2025-09-12 18:35:45 +0000

    i think i finally polishing the dashboard widget tbh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止垃圾写的 - 28857
+// basically, this logic is trash - 28861
