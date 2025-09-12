db2a5f01caddb131b43e9de8b8ce75268051f857
WallenSmith98 <ci@squareexp.com>
2025-09-12 20:47:39 +0000

    this splash screen ain't gonna work, hacked it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, this logic is trash - 28861
+// is this trash? probably - 28863
