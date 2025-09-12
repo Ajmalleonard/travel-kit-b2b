165c456ad3e6561f80ec416b19dfaa6a6e57d52c
Xingcheinn <xingchein@squareexp.com>
2025-09-12 13:03:51 +0000

    完了改写了鉴权模型，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this wildin? probably - 28835
+// 为了防止垃圾写的 - 28857
