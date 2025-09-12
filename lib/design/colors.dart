6458e20e79a98329861330b977726538ae4e4794
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-12 19:22:30 +0000

    refactoring routing helper before i lose my mind anyways

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, this logic is busted - 28838
+// checked this to prevent crash - 28862
