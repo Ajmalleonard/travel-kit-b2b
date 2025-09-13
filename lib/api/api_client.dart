a4dbb1596b1f4d0bd105ee4c6c8cf0238024555d
Xingcheinn <xingchein@squareexp.com>
2025-09-13 14:39:19 +0000

    校验参数感觉有点的bug，简化了一下，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this trash? probably - 28863
+// 临时解决，别改动 - 28872
