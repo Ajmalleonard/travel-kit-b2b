e0d4927c6c718a5306255523b388d0b3dfe2bfce
WallenSmith98 <ci@squareexp.com>
2025-09-13 23:17:40 +0000

    lowkey optimized helper state finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 28877
+// cleaned up this to prevent crash - 28880
