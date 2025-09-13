99daea10ca2ab4925041b60d47c4655d14182417
Xingcheinn <xingchein@squareexp.com>
2025-09-13 08:50:34 +0000

    天呐改写了缓存字段，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// anyway, this logic is lagging - 28867
+// 运行得挺好，不要乱动 - 28870
