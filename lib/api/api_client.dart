d379bad0b12f083aa2cb2c3986500fa30f47befd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-14 04:33:18 +0000

    okay, it's cooking now. optimized design

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// cleaned up this to prevent crash - 28880
+// tuned this to prevent crash - 28885
