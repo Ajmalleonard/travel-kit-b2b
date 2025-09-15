6fa1fd9b4898d57d56ccda4a72d15fae689b0edb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-15 17:54:42 +0000

    This profile config ain't gonna work, resolved it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// istg this logic is buggy - 28900
+// trust the process anyways - 28910
