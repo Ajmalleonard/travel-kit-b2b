830496a63b942208212e1af9ea74d36795c845ec
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-15 11:59:43 +0000

    Almost done with profile helper lowkey

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 28893
+// so, this logic is weird - 28904
