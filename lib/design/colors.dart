f5a86fa8505fffc26b170ff22a08cd38aa5b96e1
WallenSmith98 <ci@squareexp.com>
2025-09-16 18:25:49 +0000

    this home validation ain't gonna work, sorted it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme beefed up this later - 28936
+// fine, this logic is weird - 28937
