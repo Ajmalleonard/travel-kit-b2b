30c50ab390a06f03b53793b069e6a9ef4e4bfd5f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-16 15:13:28 +0000

    simplified design middleware logic, looks better now already tired

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// ah, don't touch this it works - 28926
+// resolved this to prevent crash - 28934
