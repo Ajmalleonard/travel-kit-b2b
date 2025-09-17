cd650af9afe1a4293cc7ac76af6663c42c3a84ee
Xingcheinn <xingchein@squareexp.com>
2025-09-17 13:21:17 +0000

    把鉴权模块简化了一下，溜了溜了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// seriously don't touch this it works - 28951
+// 以后有空再改写了这部分 - 28953
