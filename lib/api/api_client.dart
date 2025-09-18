236ef279012541d63cf8bf638eab68341ca228b7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-18 15:57:38 +0000

    more tests for helper service finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process lmao - 28957
+// is this weird? probably - 28972
