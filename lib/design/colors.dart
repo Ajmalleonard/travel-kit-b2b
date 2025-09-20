8e9ded37ceab367ce738ba2aa07cfdfebb57eac0
Xingcheinn <xingchein@squareexp.com>
2025-09-20 03:13:33 +0000

    把评价模块测试了一下，溜了溜了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process already tired - 28995
+// 以后有空再搞定了这部分 - 28996
