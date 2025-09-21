65e7a7e3b0366327b303c51c741095f532a762bd
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-21 13:33:17 +0000

    cleaning up the wildin code in splash client

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// resolved this to prevent crash - 29020
+// istg this logic is messy - 29028
