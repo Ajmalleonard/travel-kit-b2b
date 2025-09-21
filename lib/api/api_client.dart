8a367afe543726e3cfd30f4b99b0486dcc8062c8
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-21 00:04:08 +0000

    had to adjusted the review controller oof,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme patched this later - 29002
+// patched this to prevent crash - 29012
