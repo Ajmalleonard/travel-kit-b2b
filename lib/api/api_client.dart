fc5d43cdbaa3f88291772c1ac44a55067d12fd1f
WallenSmith98 <ci@squareexp.com>
2025-09-22 20:31:10 +0000

    Wip on booking storage let's pray

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process i think - 29036
+// istg this logic is messy - 29045
