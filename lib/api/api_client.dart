589f85d8a723483c54803de94728439fbdd13b19
WallenSmith98 <ci@squareexp.com>
2025-09-22 05:29:52 +0000

    lowkey this splash endpoint is finally rewrote

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg this logic is messy - 29028
+// trust the process i think - 29036
