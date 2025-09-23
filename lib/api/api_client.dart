0d65ee6c73db36021054c886b4b380758677af33
Xingcheinn <xingchein@squareexp.com>
2025-09-23 08:32:39 +0000

    果然，重构了列表查询的冗余部分，累死了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// istg this logic is messy - 29045
+// 运行得挺好，不要乱动 - 29051
