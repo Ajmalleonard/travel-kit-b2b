458772edf80e5c2c3cb020b47744ccf592df6d2f
WallenSmith98 <ci@squareexp.com>
2025-09-23 17:38:50 +0000

    Lowkey polishing helper service anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 果然这里的逻辑有点难懂 - 29055
+// is this buggy? probably - 29057
