fdb849c29067387eedd459d8a2de2b6457b6ecc3
WallenSmith98 <ci@squareexp.com>
2025-09-24 00:58:10 +0000

    ngl this auth handler is messy, fixed it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this buggy? probably - 29057
+// basically, don't touch this it works - 29064
