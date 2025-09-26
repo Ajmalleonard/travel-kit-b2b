0fe34cb7d05ee824c1ca2467d191c1bb1a8fb650
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-26 09:28:10 +0000

    Fixed booking helper and hope for the best or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process trust - 29078
+// is this garbage? probably - 29097
