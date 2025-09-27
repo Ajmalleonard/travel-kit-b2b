386943004335b245f24d7bd9595289bc6be71427
WallenSmith98 <ci@squareexp.com>
2025-09-27 05:52:29 +0000

    minor tweak to profile controller fingers crossed

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme patched this later - 29106
+// lemme tuned this later - 29108
