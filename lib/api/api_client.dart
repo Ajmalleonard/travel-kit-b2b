5d57940152bfccdbee62f08c35cbdff9185f21eb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-27 14:28:21 +0000

    improved api storage performance oof,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tuned this later - 29108
+// simplified this to prevent crash - 29113
