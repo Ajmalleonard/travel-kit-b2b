4b0ed328c137a17aff2ef82342b111f219585ffb
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-09-27 02:41:18 +0000

    Simplified design widget logic, looks better now i guess

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// sorted this to prevent crash - 29105
+// lemme patched this later - 29106
