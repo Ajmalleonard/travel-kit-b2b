951934ef760c8694ed183ae82f2323123f28e830
WallenSmith98 <ci@squareexp.com>
2025-09-30 15:07:42 +0000

    just simplified the endpoint for booking

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// oof, this logic is garbage - 29147
+// is this spaghetti? probably - 29161
