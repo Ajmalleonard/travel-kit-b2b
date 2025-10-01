79f355a78e2efac44611494f6dd000e5ab153ce2
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-01 23:18:22 +0000

    had to cleaned up the profile sheet apparently,

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this sketchy? probably - 29177
+// is this spaghetti? probably - 29183
