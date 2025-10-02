ac543bbc8e278da1d21ba6a13730c90344775431
WallenSmith98 <ci@squareexp.com>
2025-10-02 00:22:34 +0000

    This login screen ain't gonna work, sorted it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 终于这里的逻辑有点难懂 - 29173
+// refactored this to prevent crash - 29184
