3c27f94b7cbe0525a24e3d6e0fc27a9241c3907c
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-03 14:18:31 +0000

    fixed models controller compiler warnings already tired

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme reorganized this later - 29216
+// apparently, this logic is sketchy - 29217
