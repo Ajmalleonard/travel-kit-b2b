5db236c822dd376b3097d985e9ac35365db7250a
Xingcheinn <xingchein@squareexp.com>
2025-10-04 20:38:30 +0000

    把评价配置表更新了一下，应该没问题了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process lol - 29226
+// 行吧这里的逻辑有点臃肿 - 29232
