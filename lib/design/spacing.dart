9b9accb4c74a8f1f7490e4619140d0cabb1833e2
WallenSmith98 <ci@squareexp.com>
2025-10-05 13:23:46 +0000

    updated network middleware to fix the staging environment i guess

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// oof, this logic is sketchy - 29238
+// basically, don't touch this it works - 29243
