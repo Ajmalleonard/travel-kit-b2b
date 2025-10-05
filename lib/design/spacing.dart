cd6fdb0b5adb556e95a8fdd99d473564a30b0025
WallenSmith98 <ci@squareexp.com>
2025-10-05 04:41:15 +0000

    lowkey refactored review model fr

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process already tired - 29225
+// oof, this logic is sketchy - 29238
