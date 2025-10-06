f7702fb435098eb01e335939c0e06850b0c5a9a7
WallenSmith98 <ci@squareexp.com>
2025-10-06 11:36:46 +0000

    refactoring splash params before i lose my mind for real

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process fingers crossed - 29253
+// lemme beefed up this later - 29260
