2a19b4b23203e79d6efbb069198e1bcd74509c44
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-08 09:53:08 +0000

    Tweak storage sheet istg

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// okay, this logic is wildin - 29288
+// refactored this to prevent crash - 29294
