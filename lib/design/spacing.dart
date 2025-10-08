44693f606c25a1165610696835d57ebdf39a028e
WallenSmith98 <ci@squareexp.com>
2025-10-08 12:13:29 +0000

    refactoring models typography before i lose my mind for real

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tbh this logic is cluttered - 29291
+// polishing this to prevent crash - 29297
