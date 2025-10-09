6be7c2f5b22dfe270dda8a4286ef7f806ded3a37
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-09 12:25:54 +0000

    this login logic ain't gonna work, adjusted it

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process tbh - 29279
+// lowkey don't touch this it works - 29320
