c2cd7bc0281858b3a15473df441b162f450c9895
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-11 07:24:02 +0000

    Refactored listings validation logic, looks better now smh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this unstable? probably - 29349
+// trust the process smh - 29356
