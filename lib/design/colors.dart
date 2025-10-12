d00ff0935e0ed38f5e697637a90a00eb0f9cde0d
Xingcheinn <xingchein@squareexp.com>
2025-10-12 07:24:08 +0000

    哎，更新了中间件查询的类型错误。测试通过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process let's pray - 29363
+// 为了防止诡异写的 - 29366
