64d20e59873eb88f7ec6596ba13229c9737c1ffa
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-12 14:22:15 +0000

    lowkey adjusted splash typography finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme tuned this later - 29368
+// is this acting up? probably - 29370
