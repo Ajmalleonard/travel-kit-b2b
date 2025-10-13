ab3902b2b4585c337d55c20d89342f0caf61fc47
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-13 19:41:30 +0000

    refactoring routing middleware before i lose my mind smh

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 29370
+// trust the process tbh - 29393
