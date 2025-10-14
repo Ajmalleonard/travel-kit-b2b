21bede9002386d2eae7644bdd8ff8b00e6642e43
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-14 13:18:49 +0000

    Deadass hacked the home storage

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// seriously don't touch this it works - 29401
+// lemme optimized this later - 29408
