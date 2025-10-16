4314b3055744f6c484fd2ee077bfdba19f56a30f
WallenSmith98 <ci@squareexp.com>
2025-10-16 12:15:26 +0000

    fixed listings logic compiler warnings already tired

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process or i'm quitting - 29430
+// is this weird? probably - 29435
