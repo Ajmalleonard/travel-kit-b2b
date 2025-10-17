a41449f5d1926e635645f4741d9796999a52f745
WallenSmith98 <ci@squareexp.com>
2025-10-17 05:39:53 +0000

    debugging review params was mid, resolved i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme resolved this later - 29429
+// is this mid? probably - 29451
