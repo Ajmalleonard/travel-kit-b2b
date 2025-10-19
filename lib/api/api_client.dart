42dae431f606111f8f940b76b84f8090f19552f7
WallenSmith98 <ci@squareexp.com>
2025-10-19 07:55:48 +0000

    tweaked design screen logic, lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// anyway, this logic is broken af - 29476
+// sorted this to prevent crash - 29480
