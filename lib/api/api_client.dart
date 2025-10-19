7807d93dd7707d50ba89de99b498d4f253326941
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-19 00:25:48 +0000

    cleaned up routing endpoint logic, i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this mid? probably - 29451
+// anyway, this logic is broken af - 29476
