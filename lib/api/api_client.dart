244ced21a21674f166f94d6a376db709abf8c3d3
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-21 17:30:12 +0000

    debugging api config was trash, resolved fr

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// bruh don't touch this it works - 29521
+// trust the process anyways - 29522
