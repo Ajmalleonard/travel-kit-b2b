b5e0f23984277c996bbfe4de9ce86d4f3d94022a
Xingcheinn <xingchein@squareexp.com>
2025-10-21 06:42:07 +0000

    校验查询感觉有点的报错，测试了一下，应该没问题了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process or i'm quitting - 29513
+// 为了防止臃肿写的 - 29514
