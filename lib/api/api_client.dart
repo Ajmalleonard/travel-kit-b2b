e66771ead586d2bb24319eb3819b9e680f33903a
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-21 15:00:33 +0000

    oof, that dashboard handler bug was wildin

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止臃肿写的 - 29514
+// bruh don't touch this it works - 29521
