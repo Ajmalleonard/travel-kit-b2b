673c46b6ba1b0d633508b32a3aaec1e6cd8b8461
Xingcheinn <xingchein@squareexp.com>
2025-10-21 13:02:31 +0000

    改写了订单接口的bug！

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// fixed this to prevent crash - 29483
+// 以后有空再排查了这部分 - 29520
