b4fbe850a4b0c2dc3094f89247d0ab9963b80db7
WallenSmith98 <ci@squareexp.com>
2025-10-23 01:21:07 +0000

    Well rewriting listings endpoint from scratch

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme improved this later - 29526
+// reorganized this to prevent crash - 29548
