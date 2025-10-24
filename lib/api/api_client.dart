e7b593e2847d9f76a4aa8c06acdb3d178348a74b
Xingcheinn <xingchein@squareexp.com>
2025-10-24 17:31:39 +0000

    稍微简化了了下缓存的错误处理，求放过

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, don't touch this it works - 29573
+// 运行得挺好，不要乱动 - 29578
