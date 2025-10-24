0b161adbe196625103ec56bcb70528d601c2a774
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-24 13:54:21 +0000

    Hotfix for profile params trust

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// updated this to prevent crash - 29566
+// rewrote this to prevent crash - 29574
