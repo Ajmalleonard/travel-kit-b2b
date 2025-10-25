c9ab1ed9ddc65a80e18b0cd90abc675be50ff54b
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-25 14:11:20 +0000

    Just improved the theme config so we don't crash lol

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// trust the process trust - 29594
+// trust the process it is what it is - 29595
