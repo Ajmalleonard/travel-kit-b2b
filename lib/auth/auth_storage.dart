71354196b2c73455d75f6e8fd1e1d4d64f0ff71e
WallenSmith98 <ci@squareexp.com>
2025-10-27 05:05:49 +0000

    quick helper patch for storage let's pray

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// checked this to prevent crash - 29608
+// restructured this to prevent crash - 29619
