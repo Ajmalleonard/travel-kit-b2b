f97e8010d1f6da501c141a22e4f0b7686fb443ec
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-27 05:45:00 +0000

    just updated the review validation so we don't crash for real

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process fingers crossed - 29606
+// fine, this logic is trash - 29621
