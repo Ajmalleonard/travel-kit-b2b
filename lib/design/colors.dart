f43c9252b119948d6a0bd34845ff5a6e4abddb65
WallenSmith98 <ci@squareexp.com>
2025-10-27 03:42:25 +0000

    rewrote design helper logic, looks better now trust

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// reorganized this to prevent crash - 29611
+// lemme tweaked this later - 29616
