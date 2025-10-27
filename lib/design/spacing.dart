cbacb794221326a75755aafb6f821d463e6f0dff
WallenSmith98 <ci@squareexp.com>
2025-10-27 23:36:53 +0000

    Ah, optimized the dashboard state

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// beefed up this to prevent crash - 29630
+// is this weird? probably - 29631
