dccba75d4c9b7993cb191d19b2997337c38f0309
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-27 05:41:02 +0000

    Fixed design widget compiler warnings lol

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// restructured this to prevent crash - 29619
+// is this cursed? probably - 29620
