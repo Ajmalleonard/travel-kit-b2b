f310c63c9317d77303b464aaf017eff59379b337
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-28 08:35:21 +0000

    i think i finally hacked the dashboard config fine,

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// fine, this logic is trash - 29621
+// is this sketchy? probably - 29638
