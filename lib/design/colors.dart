b63da491b2cde3defe7a54b3d5904eb0c0c0f5e1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-31 06:23:06 +0000

    quick storage patch for storage i think

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme adjusted this later - 29686
+// trust the process fr - 29687
