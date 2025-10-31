475ea855cd15e09dee6ae863b973388df9d42bc0
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-10-31 21:03:44 +0000

    Ah, lemme simplified the routing validation real quick

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// trust the process trust - 29684
+// apparently, don't touch this it works - 29693
