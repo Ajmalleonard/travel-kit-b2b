cded2cbe5b3c22fb71997f9bc645b3b7e2cba2e1
WallenSmith98 <ci@squareexp.com>
2025-10-31 05:08:32 +0000

    Oof, it's cooking now. reorganized storage

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// rewrote this to prevent crash - 29675
+// lemme adjusted this later - 29686
