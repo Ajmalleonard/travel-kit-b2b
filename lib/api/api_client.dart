30369c6a2b21acb9b05ef1bdc667146a7b457418
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-01 23:40:30 +0000

    i think i finally sorted the network sheet istg

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 临时解决，别改动 - 29694
+// lemme updated this later - 29713
