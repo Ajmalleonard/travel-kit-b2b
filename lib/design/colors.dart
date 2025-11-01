d195b4101ea43e9496ef8a0c2e068ea9bfbd2ce2
WallenSmith98 <ci@squareexp.com>
2025-11-01 10:17:28 +0000

    Ngl this storage validation is weird, restructured it

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process fr - 29687
+// seriously don't touch this it works - 29703
