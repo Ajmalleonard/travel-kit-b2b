01276d8d4b4b3fbe2fcf1d6a605d3d0c46399347
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-02 17:42:05 +0000

    Fine, this api storage is finally updated

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 行吧这里的逻辑有点难懂 - 29719
+// lemme restructured this later - 29734
