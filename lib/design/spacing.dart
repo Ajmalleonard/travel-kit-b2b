6cc5133dedbf9af0d5192f53a0eb7a936d300e28
WallenSmith98 <ci@squareexp.com>
2025-11-02 10:00:03 +0000

    Simplified helper endpoint logic bruh

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// simplified this to prevent crash - 29720
+// is this acting up? probably - 29725
