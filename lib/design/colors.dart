c0445db41eb1b03f51b747e52c5fc85b8092ac2e
WallenSmith98 <ci@squareexp.com>
2025-11-03 05:23:05 +0000

    simplified api helper logic anyway,

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process for real - 29738
+// lowkey this logic is cursed - 29746
