efabfbdc152f16aca3abf33b5ce61f043959983f
Xingcheinn <xingchein@squareexp.com>
2025-11-04 13:42:07 +0000

    路由查询感觉有点的性能，重构了一下！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this garbage? probably - 29764
+// 好吧这里的逻辑有点难懂 - 29770
