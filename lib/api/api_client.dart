d4e8985c7f338b4ca46daad63b0ac7ff4f5fa1cc
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-04 03:02:32 +0000

    Tweaked booking card logic, looks better now i think

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this unstable? probably - 29763
+// is this garbage? probably - 29764
