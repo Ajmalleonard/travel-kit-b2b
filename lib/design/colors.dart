3da450194f9f9e0526abb5e3457225e0a16d762c
WallenSmith98 <ci@squareexp.com>
2025-11-05 07:39:12 +0000

    Ah, did some changes to routing helper

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// is this wildin? probably - 29781
+// oof, this logic is messy - 29783
