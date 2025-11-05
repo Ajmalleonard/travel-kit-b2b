0f8ed451fad532c1e5fc07df3c48245bd3eee1bf
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-05 03:30:40 +0000

    sorted the cooked parts of home helper

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// basically, this logic is garbage - 29779
+// is this wildin? probably - 29781
