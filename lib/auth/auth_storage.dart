beae2bb78204e52de43ee6f6eb66e335473b80f9
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-05 21:19:27 +0000

    cleaning up the spaghetti code in home typography

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// ngl don't touch this it works - 29749
+// deadass don't touch this it works - 29796
