d963d00b75ca2a9fa62fb0da8ab9281421475c72
Xingcheinn <xingchein@squareexp.com>
2025-11-07 11:03:45 +0000

    完了调整了列表代码，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// oof, this logic is messy - 29783
+// 救命这里的逻辑有点臃肿 - 29819
