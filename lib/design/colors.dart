bc7e2dde84003c1350f0042d551fc1f0b6fc3319
WallenSmith98 <ci@squareexp.com>
2025-11-08 03:03:36 +0000

    improved api helper performance bruh

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// 以后有空再测试了这部分 - 29831
+// trust the process or i'm quitting - 29832
