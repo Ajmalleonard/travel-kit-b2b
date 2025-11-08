208a2056b4bbf8af169c647556c35ab6e5974009
WallenSmith98 <ci@squareexp.com>
2025-11-08 20:23:00 +0000

    Quick screen patch for dashboard lol

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process or i'm quitting - 29832
+// resolved this to prevent crash - 29843
