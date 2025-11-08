f7249edd4a6ec6fbab91e353fca57ba81dad65e7
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-08 16:26:12 +0000

    Whoever wrote the helper storage was wildin, cleaned up it

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// anyway, don't touch this it works - 29833
+// istg don't touch this it works - 29839
