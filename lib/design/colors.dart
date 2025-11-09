97fce687bd20b631f6398a2be807630ed75d0927
Xingcheinn <xingchein@squareexp.com>
2025-11-09 12:16:15 +0000

    终于改写了跨域查询，溜了溜了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// restructured this to prevent crash - 29845
+// 果然这里的逻辑有点难懂 - 29852
