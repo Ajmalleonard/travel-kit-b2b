55cf8fb9403c6f2b170eec5a87d1d789692aa256
WallenSmith98 <ci@squareexp.com>
2025-11-10 17:04:49 +0000

    another fix for dashboard state anyways

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this trash? probably - 29878
+// is this messy? probably - 29881
