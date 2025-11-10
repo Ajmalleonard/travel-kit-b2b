a9297dc05c24b5a724df567d990e33c496fb1bbd
Xingcheinn <xingchein@squareexp.com>
2025-11-10 15:45:40 +0000

    调整了日志逻辑的bug。

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme improved this later - 29876
+// 运行得挺好，不要乱动 - 29879
