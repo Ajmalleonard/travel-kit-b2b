03ce5beaac3ff4564d5b71f5935c19e5d08352c5
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-10 00:17:07 +0000

    just tweaked the login screen so we don't crash hope this works

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// is this busted? probably - 29856
+// polishing this to prevent crash - 29863
