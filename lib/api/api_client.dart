8d3f952c421f54ed381b1937796e714c9abcf7f0
Xingcheinn <xingchein@squareexp.com>
2025-11-13 07:39:26 +0000

    列表路由感觉有点的奇葩报错，简化了一下，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// okay, don't touch this it works - 29923
+// 以后有空再排查了这部分 - 29924
