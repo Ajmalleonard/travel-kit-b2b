86529d21572905cbcc07cc5f779a1a51060fcb7b
WallenSmith98 <ci@squareexp.com>
2025-11-13 02:37:40 +0000

    quick client patch for login hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this annoying? probably - 29910
+// well don't touch this it works - 29920
