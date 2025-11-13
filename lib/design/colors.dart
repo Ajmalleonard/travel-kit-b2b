3189651b6e466c45d44fded4ea5f273a304a4936
Xingcheinn <xingchein@squareexp.com>
2025-11-13 22:21:52 +0000

    会话模块感觉有点的逻辑漏洞，排查了一下，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process hope this works - 29936
+// 临时解决，别改动 - 29938
