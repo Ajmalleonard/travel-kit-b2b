321e426070cce2cd0f28d015aa82f316027e5869
WallenSmith98 <ci@squareexp.com>
2025-11-14 08:40:04 +0000

    lowkey optimized the profile sheet

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// lemme polishing this later - 29944
+// lemme sorted this later - 29949
