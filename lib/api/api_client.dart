0f2888845dfd2e974a3bd840b9fa524abc4afb89
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-14 03:43:32 +0000

    Just improved the routing router so we don't crash lmao

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再优化了这部分 - 29930
+// lemme polishing this later - 29944
