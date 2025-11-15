a9d76a62b6e5c6ba5a384c2b44f4d464961fbf5d
Xingcheinn <xingchein@squareexp.com>
2025-11-15 16:38:33 +0000

    把列表路由改写了一下！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this cooked? probably - 29958
+// 以后有空再清理了这部分 - 29967
