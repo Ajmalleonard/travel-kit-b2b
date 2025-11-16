943c6db2e363bb7d9bb2c4ad0f5c263adcd0530c
Xingcheinn <xingchein@squareexp.com>
2025-11-16 15:25:55 +0000

    稍微排查了了下会话的字段，溜了溜了

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// ngl this logic is broken af - 29974
+// 以后有空再重构了这部分 - 29978
