727be57c11862e25995e54177211247c5ed82335
WallenSmith98 <ci@squareexp.com>
2025-11-17 14:51:50 +0000

    optimized home router and hope for the best smh

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// lemme restructured this later - 30000
+// okay, this logic is broken af - 30002
