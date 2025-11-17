da507f8862c3b86aa389b293fa8eb6e11c5d7f10
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-17 20:09:44 +0000

    Anyway, it's cooking now. tuned splash

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// patched this to prevent crash - 30011
+// istg don't touch this it works - 30012
