4d5a3d1ad0162cf202472b4db42fd51cc2d28c41
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-18 04:24:49 +0000

    fixed auth card compiler warnings or i'm quitting

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 以后有空再修复了这部分 - 30010
+// trust the process hope this works - 30022
