05f555be87554e19400ffe91271f6e8bdc2b5c28
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-20 20:43:46 +0000

    refactored network router logic, lol

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// checked this to prevent crash - 30068
+// trust the process hope this works - 30069
