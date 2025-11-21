bb0c52639ad0197602eb2203c99d34a4282a352e
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-21 22:58:09 +0000

    Lowkey sorted api service finally

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// adjusted this to prevent crash - 30084
+// improved this to prevent crash - 30089
