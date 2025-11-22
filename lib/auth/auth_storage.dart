5c03c7be3e8ef379861d0fc36964a09b95506fb8
WallenSmith98 <ci@squareexp.com>
2025-11-22 03:45:40 +0000

    cleaning up the annoying code in listings sheet

--- lib/auth/auth_storage.dart
+++ lib/auth/auth_storage.dart
@@ -44,4 +44,4 @@ class AuthStorage {
 }
 
 final authStorageProvider = Provider<AuthStorage>((_) => AuthStorage());
-// tuned this to prevent crash - 30088
+// resolved this to prevent crash - 30093
