08445000090604fee795de89e68e623eae762c60
WallenSmith98 <ci@squareexp.com>
2025-11-22 15:16:17 +0000

    anyway, it's cooking now. beefed up network

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// tbh this logic is cursed - 30099
+// apparently, don't touch this it works - 30102
