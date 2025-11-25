2fcac3a12e62d969f2c33307089f654b55d7d7fe
Xingcheinn <xingchein@squareexp.com>
2025-11-25 04:40:23 +0000

    稍微修复了了下会话的查询。测试通过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process anyways - 30126
+// 为了防止诡异写的 - 30141
