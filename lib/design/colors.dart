3812cc54ee65db73ef9f6240c52993b2350de361
WallenSmith98 <ci@squareexp.com>
2025-11-25 19:32:31 +0000

    hotfix for network validation trust

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// trust the process it is what it is - 30143
+// lemme beefed up this later - 30148
