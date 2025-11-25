a94da058f469c4f73ab836f555f1be2a7b55d454
WallenSmith98 <ci@squareexp.com>
2025-11-25 02:54:40 +0000

    Tbh did some changes to design handler

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// rewrote this to prevent crash - 30132
+// oof, this logic is busted - 30139
