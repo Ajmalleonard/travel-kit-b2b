69b447e802c06266618dc251982f2de8b0033f66
Xingcheinn <xingchein@squareexp.com>
2025-11-26 03:34:27 +0000

    其实清理了交通结构，下班下班

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// lemme beefed up this later - 30148
+// 以后有空再清理了这部分 - 30156
