a2d0e6d54289ccaad83b618f9ca91b6201aa858f
WallenSmith98 <ci@squareexp.com>
2025-11-26 07:22:22 +0000

    just resolved the theme typography so we don't crash hope this works

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// 为了防止诡异写的 - 30155
+// basically, this logic is cooked - 30158
