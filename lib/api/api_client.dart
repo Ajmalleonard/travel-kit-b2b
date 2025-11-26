8305eb39d6c342566629faac99d4505969c842ce
Xingcheinn <xingchein@squareexp.com>
2025-11-26 03:19:35 +0000

    哎，排查了中间件字段的屎山代码！

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 30113
+// 为了防止诡异写的 - 30155
