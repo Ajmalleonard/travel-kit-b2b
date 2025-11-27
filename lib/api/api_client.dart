42bab5bd62b4ee9f32068c26e10a1c9f74b2d690
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-27 16:16:23 +0000

    optimized booking router logic, looks better now trust

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// basically, this logic is cooked - 30158
+// is this acting up? probably - 30171
