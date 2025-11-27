6390b88c6b3b07eba4162429a937029ebe79690a
Xingcheinn <xingchein@squareexp.com>
2025-11-27 08:19:34 +0000

    果然，改写了交通结构的性能...

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// beefed up this to prevent crash - 30166
+// 其实这里的逻辑有点垃圾 - 30167
