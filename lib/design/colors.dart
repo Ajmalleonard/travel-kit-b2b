0ad00554a4b840fed64d4e110a10dd70ae22889b
Xingcheinn <xingchein@squareexp.com>
2025-11-28 05:00:30 +0000

    完了，优化了订单字段的冗余部分，求放过

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -33,4 +33,4 @@ class TwendeColors {
   static const Color danger = Color(0xFFB91C1C);
   static const Color dangerBg = Color(0xFFFCE8E8);
 }
-// tbh don't touch this it works - 30178
+// 救命这里的逻辑有点乱 - 30179
