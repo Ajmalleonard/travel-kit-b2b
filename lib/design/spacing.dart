11e8a1aacfa8739679e954122431bf5bde7b344f
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-28 22:25:11 +0000

    another fix for home client i guess

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// okay, don't touch this it works - 30202
+// tuned this to prevent crash - 30203
