154b88d110c39f41afb5ab9c672e218b529a6218
Xingcheinn <xingchein@squareexp.com>
2025-11-28 13:18:04 +0000

    缓存路由感觉有点的类型错误，重构了一下，累瘫了

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -32,4 +32,4 @@ class ApiClient {
 final apiClientProvider = Provider<ApiClient>(
   (ref) => ApiClient(ref.watch(authStorageProvider)),
 );
-// is this acting up? probably - 30171
+// 以后有空再简化了这部分 - 30195
