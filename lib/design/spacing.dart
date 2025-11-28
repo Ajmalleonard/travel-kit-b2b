d4354a944ba5610e0b2fbe3ab0c9ee6912668bf1
Ajmalleonard <109855853+Ajmalleonard@users.noreply.github.com>
2025-11-28 15:53:49 +0000

    i think i finally reorganized the profile validation ngl

--- lib/design/spacing.dart
+++ lib/design/spacing.dart
@@ -15,4 +15,4 @@ class TwendeSpacing {
   static const double radiusXl = 28;
   static const double radiusPill = 999;
 }
-// trust the process already tired - 30194
+// trust the process tbh - 30197
